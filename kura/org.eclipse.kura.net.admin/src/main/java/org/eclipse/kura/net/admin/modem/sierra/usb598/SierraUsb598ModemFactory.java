5a18b2a0941f1e1fe226422ca8201cc9ec2e7536
ibinshtok <ilya.binshtok@eurotech.com>
2014-08-21 11:37:22 -0500

    Made RSSI available on the 'Hardware' tab.

--- kura/org.eclipse.kura.net.admin/src/main/java/org/eclipse/kura/net/admin/modem/sierra/usb598/SierraUsb598ModemFactory.java
+++ kura/org.eclipse.kura.net.admin/src/main/java/org/eclipse/kura/net/admin/modem/sierra/usb598/SierraUsb598ModemFactory.java
@@ -14,11 +14,10 @@ package org.eclipse.kura.net.admin.modem.sierra.usb598;
 import java.util.Hashtable;
 
 import org.eclipse.kura.net.admin.NetworkConfigurationService;
-import org.eclipse.kura.net.admin.modem.CellularModem;
 import org.eclipse.kura.net.admin.modem.CellularModemFactory;
+import org.eclipse.kura.net.modem.CellularModem;
 import org.eclipse.kura.net.modem.ModemDevice;
 import org.eclipse.kura.net.modem.ModemTechnologyType;
-import org.eclipse.kura.usb.UsbModemDevice;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.FrameworkUtil;
 import org.osgi.service.io.ConnectionFactory;

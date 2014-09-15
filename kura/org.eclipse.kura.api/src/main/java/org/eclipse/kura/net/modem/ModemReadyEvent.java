8e81d1be9db51536b729df15a86a40e9b33f0bd4
ibinshtok <ilya.binshtok@eurotech.com>
2014-09-15 11:39:29 -0500

    include IMEI, IMSI, and ICCID to birth certificate

---
+++ kura/org.eclipse.kura.api/src/main/java/org/eclipse/kura/net/modem/ModemReadyEvent.java
@@ -0,0 +1,19 @@
+package org.eclipse.kura.net.modem;
+
+import java.util.Map;
+
+import org.osgi.service.event.Event;
+
+public class ModemReadyEvent extends Event {
+
+	/** Topic of the ModemRemovedEvent */
+    public static final String MODEM_EVENT_READY_TOPIC = "org/eclipse/kura/net/modem/READY";
+    
+    public static final String IMEI = "IMEI";
+    public static final String IMSI = "IMSI";
+    public static final String ICCID = "ICCID";
+    
+    public ModemReadyEvent(Map<String, String> properties) {
+        super(MODEM_EVENT_READY_TOPIC, properties);
+    }
+}

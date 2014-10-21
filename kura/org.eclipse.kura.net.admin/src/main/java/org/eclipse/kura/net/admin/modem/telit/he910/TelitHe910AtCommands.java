f9caf988772c89aca77d2c2d0a46cd8b1ab52b66
ibinshtok <ilya.binshtok@eurotech.com>
2014-10-21 18:59:43 -0500

    ported cellular optimizations from ESF 2.0.10

--- kura/org.eclipse.kura.net.admin/src/main/java/org/eclipse/kura/net/admin/modem/telit/he910/TelitHe910AtCommands.java
+++ kura/org.eclipse.kura.net.admin/src/main/java/org/eclipse/kura/net/admin/modem/telit/he910/TelitHe910AtCommands.java
@@ -24,30 +24,16 @@ package org.eclipse.kura.net.admin.modem.telit.he910;
  */
 public enum TelitHe910AtCommands {
 	
-	at("at\r\n"),
 	getSimStatus("at#qss?\r\n"),
 	getSimPinStatus("at+cpin?\r\n"),
 	setAutoSimDetection("at#simdet=2\r\n"),
 	simulateSimNotInserted("at#simdet=0\r\n"),
 	simulateSimInserted("at#simdet=1\r\n"),
 	getSmsc("at+csca?\r\n"),
-	getModelNumber("at+gmm\r\n"),
-    getManufacturer("at+gmi\r\n"),
-    getSerialNumber("at#cgsn\r\n"),
-    getIMSI("at#cimi\r\n"),
-    getICCID("at#ccid\r\n"),
-    getRevision("at+gmr\r\n"),
-    getSignalStrength("at+csq\r\n"),
     getMobileStationClass("at+cgclass?\r\n"),
     getRegistrationStatus("at+cgreg?\r\n"),
     getGprsSessionDataVolume("at#gdatavol=1\r\n"),
-	pdpContext("AT+CGDCONT"),
-	softReset("atz\r\n"),
-	isGpsPowered("at$GPSP?\r\n"),
-	gpsPowerUp("at$GPSP=1\r\n"),
-	gpsPowerDown("at$GPSP=0\r\n"),
-	gpsEnableNMEA("AT$GPSNMUN=3,0,0,1,1,1,1\r\n"),
-	gpsDisableNMEA("+++");
+	pdpContext("AT+CGDCONT");
 	
 	private String m_command;
 	

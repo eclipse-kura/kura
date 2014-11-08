94b8665f26ec332f230b0a35c0e979bbae00db42
Marco Carrer <mcarrer@gmail.com>
2014-11-08 10:12:59 +0100

    lwm2m

---
+++ kura/distrib/src/main/java/distrib/Main.java
@@ -0,0 +1,11 @@
+package distrib;
+
+/**
+ * Placeholder class to keep maven happy
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+}
---
+++ kura/org.eclipse.kura.qa/src/main/java/org/eclipse/kura/qa/Main.java
@@ -0,0 +1,11 @@
+package org.eclipse.kura.qa;
+
+/**
+ * Placeholder to keep maven happy.
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+}

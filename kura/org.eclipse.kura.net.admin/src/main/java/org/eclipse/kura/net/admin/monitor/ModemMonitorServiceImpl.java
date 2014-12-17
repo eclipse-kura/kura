222a1bd8bac56e24ee40db9a7ba3decc61393298
ibinshtok <ilya.binshtok@eurotech.com>
2014-12-17 17:13:21 -0600

    fixes to exception handling

--- kura/org.eclipse.kura.net.admin/src/main/java/org/eclipse/kura/net/admin/monitor/ModemMonitorServiceImpl.java
+++ kura/org.eclipse.kura.net.admin/src/main/java/org/eclipse/kura/net/admin/monitor/ModemMonitorServiceImpl.java
@@ -147,6 +147,7 @@ public class ModemMonitorServiceImpl implements ModemMonitorService, ModemManage
 	}
 	
     protected void activate(ComponentContext componentContext)  {
+    	
     	// save the bundle context
     	m_ctx = componentContext;
     	
@@ -388,7 +389,7 @@ public class ModemMonitorServiceImpl implements ModemMonitorService, ModemManage
 	    				}
 	    				
 	    				s_logger.debug("handleEvent() :: gpsSupported={}", m_gpsSupported);
-	    				if (m_gpsSupported) {
+	    				if ((m_gpsSupported != null) && m_gpsSupported) {
 	    					if (isGpsEnabledInConfig(newNetConfigs)) {
 	    						s_logger.debug("handleEvent() :: enabling GPS");
 	    						modem.enableGps();
@@ -699,12 +700,16 @@ public class ModemMonitorServiceImpl implements ModemMonitorService, ModemManage
 				}
 				
 				if (m_gpsSupported == null) {
-					boolean gpsSupported = modem.isGpsSupported();
-					m_gpsSupported = gpsSupported;
+					try {
+						boolean gpsSupported = modem.isGpsSupported();
+						m_gpsSupported = gpsSupported;
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
 				}
 				
 				s_logger.debug("trackModem() :: gpsSupported={}", m_gpsSupported);
-				if (m_gpsSupported) {
+				if ((m_gpsSupported != null) && m_gpsSupported) {
 					if (isGpsEnabledInConfig(netConfigs)) {
 						s_logger.debug("trackModem() :: enabling GPS");
 						modem.enableGps();

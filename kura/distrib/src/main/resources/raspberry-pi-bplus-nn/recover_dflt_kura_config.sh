bb8113f34f279f4e190d8c9499881bf97ec930c5
ibinshtok <ilya.binshtok@eurotech.com>
2015-02-10 17:10:52 -0600

    fixed path in the md5.info file

--- kura/distrib/src/main/resources/raspberry-pi-bplus-nn/recover_dflt_kura_config.sh
+++ kura/distrib/src/main/resources/raspberry-pi-bplus-nn/recover_dflt_kura_config.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 #
 # Copyright (c) 2011, 2014 Eurotech and/or its affiliates
 #
@@ -16,9 +16,9 @@ INSTALL_DIR=/opt/eclipse
 cd /tmp
 tar xzvf ${INSTALL_DIR}/kura/.data/recover_dflt_kura_config.tgz
 sum=`md5sum /tmp/${INSTALL_DIR}/kura/data/kuranet.conf`
-echo ${sum/\/tmp\//\/} > /tmp/${INSTALL_DIR}/kura/data/md5.info
+echo ${sum/\/tmp\/\//\/} > /tmp/${INSTALL_DIR}/kura/data/md5.info
 sum=`md5sum /tmp/${INSTALL_DIR}/kura/data/snapshots/snapshot_0.xml`
-echo ${sum/\/tmp\//\/} >> /tmp/${INSTALL_DIR}/kura/data/md5.info
+echo ${sum/\/tmp\/\//\/} >> /tmp/${INSTALL_DIR}/kura/data/md5.info
 
 MD5_1=`md5sum /tmp/${INSTALL_DIR}/kura/data/md5.info | cut -d ' ' -f 1`
 MD5_2=`md5sum ${INSTALL_DIR}/kura/.data/md5.info | cut -d ' ' -f 1`

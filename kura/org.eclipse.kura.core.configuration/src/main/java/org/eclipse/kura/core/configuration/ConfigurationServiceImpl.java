f45e7bedf634ce886863cf72f06e5d87cc137332
MMaiero <matteo.maiero@eurotech.com>
2015-07-23 16:12:23 +0200

    Changed the configuration service in order to send placeholders instead of passwords to the local web interface and to the cloud.
    Signed-off-by: MMaiero <matteo.maiero@eurotech.com>

--- kura/org.eclipse.kura.core.configuration/src/main/java/org/eclipse/kura/core/configuration/ConfigurationServiceImpl.java
+++ kura/org.eclipse.kura.core.configuration/src/main/java/org/eclipse/kura/core/configuration/ConfigurationServiceImpl.java
@@ -18,6 +18,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
@@ -94,6 +95,8 @@ public class ConfigurationServiceImpl implements ConfigurationService, Configura
 	// contains all pids which have been configured and for which we have not
 	// received the corresponding ConfigurationEvent yet
 	private Set<String> m_pendingConfigurationPids;
+
+	private final char[] GENERIC_PLACEHOLDER = "PlaceHolder".toCharArray();
 
 	// ----------------------------------------------------------------
 	//
@@ -271,8 +274,26 @@ public class ConfigurationServiceImpl implements ConfigurationService, Configura
 			cc = getSelfConfiguringComponentConfiguration(pid);
 		}
 
-		decryptPasswords(cc);
+		//decryptPasswords(cc);
+		replacePasswordsWithPlaceholder(cc);
 		return cc;
+	}
+
+	private Map<String, Object> replacePasswordsWithPlaceholder(ComponentConfiguration config) {
+		Map<String, Object> configProperties = config.getConfigurationProperties();
+		Iterator<String> keys = configProperties.keySet().iterator();
+		while (keys.hasNext()) {
+			String key = keys.next();
+			Object value = configProperties.get(key);
+			if (value instanceof Password) {
+				try {
+					Password decryptedPassword = new Password(GENERIC_PLACEHOLDER );
+					configProperties.put(key, decryptedPassword);
+				} catch (Exception e) {
+				}
+			}
+		}
+		return configProperties;
 	}
 
 	@SuppressWarnings("unchecked")
@@ -418,6 +439,7 @@ public class ConfigurationServiceImpl implements ConfigurationService, Configura
 		for (ComponentConfiguration config : configsToUpdate) {
 			if (config != null) {
 				encryptPasswords(config);
+				replacePlaceholdersWithPasswords(config);
 			}
 		}
 
@@ -446,7 +468,6 @@ public class ConfigurationServiceImpl implements ConfigurationService, Configura
 	// Package APIs
 	//
 	// ----------------------------------------------------------------
-
 	synchronized void registerComponentConfiguration(Bundle bundle, String pid) throws KuraException {
 		if (!m_allPids.contains(pid)) {
 
@@ -549,12 +570,39 @@ public class ConfigurationServiceImpl implements ConfigurationService, Configura
 			String key = keys.next();
 			Object value = propertiesToUpdate.get(key);
 			if (value != null) {
-				if (value instanceof Password) {
+				if (value instanceof Password && !Arrays.equals(value.toString().toCharArray(), GENERIC_PLACEHOLDER)) {
 					try {
 						propertiesToUpdate.put(key, new Password(m_cryptoService.encryptAes(value.toString().toCharArray())));
 					} catch (Exception e) {
 						s_logger.warn("Failed to encrypt Password property: {}", key);
 						propertiesToUpdate.remove(key);
+					}
+				}
+			}
+		}
+	}
+	
+	private void replacePlaceholdersWithPasswords(ComponentConfiguration config) {
+		Map<String, Object> propertiesToUpdate = config.getConfigurationProperties();
+		Iterator<String> keys = propertiesToUpdate.keySet().iterator();
+		while (keys.hasNext()) {
+			String key = keys.next();
+			Object value = propertiesToUpdate.get(key);
+			if (value != null) {
+				if (value instanceof Password && Arrays.equals(value.toString().toCharArray(), GENERIC_PLACEHOLDER)) {
+					try {
+
+						String pid= config.getPid();
+						Tocd ocd = m_ocds.get(pid);
+
+						Configuration cfg = m_configurationAdmin.getConfiguration(pid);
+						Map<String, Object> props = CollectionsUtil.dictionaryToMap(cfg.getProperties(), ocd);
+						
+						Password encPassword= (Password) props.get(key);
+						
+						propertiesToUpdate.put(key, encPassword);
+					} catch (Exception e) {
+						s_logger.error("Error while trying to remove placeholders for: {}", key);
 					}
 				}
 			}

0278c9ffa8add46174b941ecadb7dfa7204dc1ea
malikm <Martin.Malik@eurotech.com>
2015-10-27 15:21:03 +0100

    Updates after merge with 1.3.0
    + Added org.eclipse.kura.windows.status
    + Disabled dio (uses org.eclipse.kura.emulator.gpio instead)

---
+++ kura/org.eclipse.kura.windows.status/src/main/java/org/eclipse/kura/windows/status/CloudConnectionStatusURL.java
@@ -0,0 +1,46 @@
+package org.eclipse.kura.windows.status;
+
+import java.util.Locale;
+import java.util.Properties;
+
+public class CloudConnectionStatusURL {
+
+	public static final String S_CCS		= "ccs:";
+	public static final String S_LED		= "led:";
+	public static final String S_LOG		= "log";
+	public static final String S_NONE		= "none";
+	
+	public static final int TYPE_LED = 0;
+	public static final int TYPE_LOG = 1;
+	public static final int TYPE_NONE = 2;
+	
+	
+	public static Properties parseURL(String ccsUrl){
+		
+		String urlImage = ccsUrl.toLowerCase(Locale.ENGLISH);
+		
+		Properties props = new Properties();
+		
+		if(urlImage.startsWith(S_CCS)){
+			urlImage = urlImage.replace(S_CCS, "");
+			props.put("url", ccsUrl);
+			if(urlImage.startsWith(S_LED)){
+				//Cloud Connection Status on LED
+				urlImage = urlImage.replace(S_LED, "");
+				try{
+					int LEDPin = Integer.parseInt(urlImage.trim());
+					props.put("notification_type", TYPE_LED);
+					props.put("led", LEDPin);
+				}catch(Exception ex){
+					//Do nothing
+				}
+			}else if (urlImage.startsWith(S_LOG)){
+				props.put("notification_type", TYPE_LOG);
+			}else if(urlImage.startsWith(S_NONE)){
+				props.put("notification_type", TYPE_NONE);				
+			}
+		}
+		
+		return props;
+	}
+}

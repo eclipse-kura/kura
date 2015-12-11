d9e9c52224dc117e8347d13a52ef6d5ff7ad2098
Mark Cullen <mark.cullen@eurotech.com>
2015-12-11 15:33:25 +0000

    Added project to build a wrapper service which starts KURA under control of the Windows Service manager

---
+++ target-platform/org.eclipse.kura.windows.service/src/KURAService/main.c
@@ -0,0 +1,48 @@
+#include <windows.h>
+#include <tchar.h>
+
+#include "KURAService.h"
+
+BOOL g_bTerminate;							// Set to true to indicate the service should terminate
+//SERVICE_STATUS g_ServiceStatus;
+//SERVICE_STATUS_HANDLE g_StatusHandle;
+
+extern void WINAPI ServiceCtrlHandler(DWORD CtrlCode);
+
+void WINAPI ServiceMain(DWORD argc, LPTSTR *argv)
+{
+	g_bTerminate = 0;
+
+	// Register our service control handler with the SCM
+	g_StatusHandle = RegisterServiceCtrlHandler(SERVICE_NAME, ServiceCtrlHandler);
+ 
+	if(g_StatusHandle != NULL) 
+	{
+		ZeroMemory (&g_ServiceStatus, sizeof (g_ServiceStatus));
+		g_ServiceStatus.dwServiceType = SERVICE_WIN32_OWN_PROCESS;
+
+		// Tell the service controller we are starting
+		UpdateStatus(SERVICE_START_PENDING, 0, 0, NO_ERROR);
+
+		RunService();
+
+		// Tell the service controller we are stopped
+		UpdateStatus(SERVICE_STOPPED, 0, 3, NO_ERROR);
+	}
+	return;
+}
+
+int main(int argc, char* argv[])
+{
+	SERVICE_TABLE_ENTRY ServiceTable[] = 
+	{
+		{ SERVICE_NAME, (LPSERVICE_MAIN_FUNCTION)ServiceMain },
+		{ NULL, NULL }
+	};
+ 
+	if(StartServiceCtrlDispatcher(ServiceTable) == FALSE)
+	{
+		return GetLastError();
+	}
+	return 0;
+}

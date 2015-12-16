d9dd7628cf2454e613fc3aaf925da52314ed040e
malikm <Martin.Malik@eurotech.com>
2015-12-16 16:18:17 +0100

    Updated KuraNativeWin build process

--- target-platform/org.eclipse.kura.windows/src/main/c/build-mingw-win.bat
+++ target-platform/org.eclipse.kura.windows/src/main/c/build-mingw-win.bat
@@ -1,4 +1,11 @@
-@REM MUST SET PATH TO mingw32 install folder \bin
-del *.o
+@REM MUST HAVE mingw32 and mingw-w64 in PATH
+del Objs\x86\*.o
+del Objs\x64\*.o
+mkdir Objs\x86
+mkdir Objs\x64
+del Release\win32\x86\*.dll
+del Release\win32\x64\*.dll
 mkdir Release\win32\x86
-mingw32-make.exe -f makefile.mingw_win32
+mkdir Release\win32\x64
+mingw32-make.exe -f makefile.mingw KuraNativeWin32
+mingw32-make.exe -f makefile.mingw KuraNativeWin64

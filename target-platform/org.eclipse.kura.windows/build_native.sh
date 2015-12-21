588f961ac198b6e11ee1ba9e949f3ea9c69a2623
Mark Cullen <mark.cullen@eurotech.com>
2015-12-21 09:51:28 +0000

    Added missing -P options on some mkdir

--- target-platform/org.eclipse.kura.windows/build_native.sh
+++ target-platform/org.eclipse.kura.windows/build_native.sh
@@ -27,13 +27,13 @@ fi
 # If the argument is 'clean' just work through all the files in the Objs directory and delete them.
 #
 if [ "$1" == "clean" ]; then
-	for f in src/main/c/Objs/x86*; do
+	for f in src/main/c/Objs/x86/*; do
 		if [ -f "$f" ]; then
 			echo Deleting $f
 			rm $f
 		fi
 	done
-	for f in src/main/c/Objs/x64*; do
+	for f in src/main/c/Objs/x64/*; do
 		if [ -f "$f" ]; then
 			echo Deleting $f
 			rm $f
@@ -52,29 +52,29 @@ if [ "$1" == "build" ]; then
 		
 		cd src/main/c
 		if [ ! -d Objs/x86 ]; then
-			mkdir Objs/x86
+			mkdir -p Objs/x86
 		fi
 
 		if [ ! -d Objs/x64 ]; then
-			mkdir Objs/x64
+			mkdir -p Objs/x64
 		fi
 
-		if [ ! -d Release/win32/x86 ]; then
-			mkdir -p Release/win32/x86
+		if [ ! -d Release/Win32/x86 ]; then
+			mkdir -p Release/Win32/x86
 		fi
 
-		if [ ! -d Release/win32/x64 ]; then
-			mkdir -p Release/win32/x64
+		if [ ! -d Release/Win32/x64 ]; then
+			mkdir -p Release/Win32/x64
 		fi
 
 		if [ -x "$(command -v i686-w64-mingw32-gcc)" ]; then
-			$MAKECMD KuraNativeWin32
+			$MAKECMD -f makefile.mingw KuraNativeWin32
 		else
 			echo "  WARNING: No viable 32 bit 'mingw' compiler installed. 32 bit KURA Windows Native will not be rebuilt"
 		fi
 
 		if [ -x "$(command -v x86_64-w64-mingw32-gcc)" ]; then
-			$MAKECMD KuraNativeWin64
+			$MAKECMD -f makefile.mingw KuraNativeWin64
 		else
 			echo "  WARNING: No viable 64 bit 'mingw' compiler installed. 64 bit KURA Windows Native will not be rebuilt"
 		fi

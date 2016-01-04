dadaa1a565c67b458bcf8b2a73dbc9e437f530bd
Mark Cullen <mark.cullen@eurotech.com>
2016-01-04 14:34:05 +0000

    Changes to Windows native library build directory

---
+++ target-platform/org.eclipse.kura.windows.native/src/main/java/org/eclipse/kura/windows/KuraNativeWin.java
@@ -0,0 +1,19 @@
+package org.eclipse.kura.windows;
+
+import java.io.IOException;
+
+public class KuraNativeWin {
+
+	public native void setSystemTime( short year, short month, short day, short hour, short minutes, short second, short msec );
+	public native long getTickCount( );
+
+	static {
+		try {
+			System.loadLibrary("KuraNativeWin");
+		} 
+	
+		catch (final UnsatisfiedLinkError e) {
+			e.printStackTrace();
+		}
+    }
+}

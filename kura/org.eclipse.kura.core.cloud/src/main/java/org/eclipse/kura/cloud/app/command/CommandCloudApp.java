a0cdd675ee63d135afc5ba0175ae0ba53d1c7fb7
Mark Cullen <mark.cullen@eurotech.com>
2016-01-13 15:08:31 +0000

    Modified so that the default working directory is empty, and that if the working directory is not specified or empty it uses the temp directory specified by java.io.tmpdir

--- kura/org.eclipse.kura.core.cloud/src/main/java/org/eclipse/kura/cloud/app/command/CommandCloudApp.java
+++ kura/org.eclipse.kura.core.cloud/src/main/java/org/eclipse/kura/cloud/app/command/CommandCloudApp.java
@@ -191,8 +191,9 @@ PasswordCommandService {
 
 			String[] cmdarray = prepareCommandArray(commandReq, command);
 
+//			String dir = getDir(commandReq);
+			String dir = getDefaultWorkDir();
 			String[] envp = getEnvironment(commandReq);
-			String dir = getDir(commandReq);
 
 			byte[] zipBytes = commandReq.getZipBytes();
 			if (zipBytes != null) {
@@ -297,7 +298,16 @@ PasswordCommandService {
 
 	// command service defaults getters
 	private String getDefaultWorkDir() {
-		return (String) properties.get(COMMAND_WORKDIR_ID);
+
+		String workDir = (String)properties.get(COMMAND_WORKDIR_ID);
+		if(workDir != null)
+		{
+			if(!workDir.isEmpty())
+			{
+				return workDir;
+			}
+		}
+		return System.getProperty("java.io.tmpdir");
 	}
 
 	private int getDefaultTimeout() {

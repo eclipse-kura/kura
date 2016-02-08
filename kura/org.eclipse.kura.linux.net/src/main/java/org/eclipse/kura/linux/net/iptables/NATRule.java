2120bf37a545faa8e8d904fbddd511acc4d497f4
MMaiero <matteo.maiero@eurotech.com>
2016-02-08 12:09:52 +0100

    Updated copyright for linux.* bundles
    Signed-off-by: MMaiero <matteo.maiero@eurotech.com>

--- kura/org.eclipse.kura.linux.net/src/main/java/org/eclipse/kura/linux/net/iptables/NATRule.java
+++ kura/org.eclipse.kura.linux.net/src/main/java/org/eclipse/kura/linux/net/iptables/NATRule.java
@@ -1,25 +1,18 @@
-/**
- * Copyright (c) 2011, 2014 Eurotech and/or its affiliates
- *
- *  All rights reserved. This program and the accompanying materials
- *  are made available under the terms of the Eclipse Public License v1.0
- *  which accompanies this distribution, and is available at
- *  http://www.eclipse.org/legal/epl-v10.html
+/*******************************************************************************
+ * Copyright (c) 2011, 2016 Eurotech and/or its affiliates
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *   Eurotech
- */
+ *     Eurotech
+ *******************************************************************************/
 package org.eclipse.kura.linux.net.iptables;
 
 import org.eclipse.kura.KuraException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-
-
-/* 
- * Copyright (c) 2013 Eurotech Inc. All rights reserved.
- */
 
 /**
  * Creates an iptables command for a NAT Rule.

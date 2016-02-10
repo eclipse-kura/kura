842bbf4ecafe2d32a0e0defcc4ac8f88f9453a02
MMaiero <matteo.maiero@eurotech.com>
2016-02-10 11:59:11 +0100

    Copyright style update
    Signed-off-by: MMaiero <matteo.maiero@eurotech.com>

--- kura/org.eclipse.kura.api/src/main/java/org/eclipse/kura/system/SystemAdminService.java
+++ kura/org.eclipse.kura.api/src/main/java/org/eclipse/kura/system/SystemAdminService.java
@@ -1,20 +1,19 @@
-/**
- * Copyright (c) 2011, 2014 Eurotech and/or its affiliates
+/*******************************************************************************
+ * Copyright (c) 2011, 2016 Eurotech and/or its affiliates
  *
- *  All rights reserved. This program and the accompanying materials
- *  are made available under the terms of the Eclipse Public License v1.0
- *  which accompanies this distribution, and is available at
- *  http://www.eclipse.org/legal/epl-v10.html
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *   Eurotech
- */
+ *     Eurotech
+ *******************************************************************************/
 package org.eclipse.kura.system;
 
 /**
  * Service to perform basic system tasks.
  * 
- * Copyright (c) 2012 Eurotech Inc. All rights reserved.
  */
 public interface SystemAdminService 
 {

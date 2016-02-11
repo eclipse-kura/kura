920ef3c97fb7172fc3fa816be74c4849a57a0d44
malikm <Martin.Malik@eurotech.com>
2016-02-11 15:19:29 +0100

    Updated service to directly start java engine (same as launcher)

--- target-platform/org.eclipse.kura.windows.service/src/main/c/main.c
+++ target-platform/org.eclipse.kura.windows.service/src/main/c/main.c
@@ -1,49 +1,199 @@
-
 #include "KURAService.h"
 
 BOOL g_bTerminate;							// Set to true to indicate the service should terminate
 
+#define KURA_CMD_LINE_SIZE			32768
+char *g_pszKuraCommandLine = NULL;
+
+char KuraJavaArgs[] = "java \
+-Xms256m -Xmx256m -Dkura.os.version=win32 \
+-Dkura.arch=x86_64 \
+-Dtarget.device=windows \
+-Declipse.ignoreApp=true \
+-Dkura.home=%KURA_HOME% \
+-Dkura.data.dir=%KURA_DATA% \
+-Dkura.configuration=file:%KURA_HOME_URI%/kura.properties \
+-Dkura.custom.configuration=file:%KURA_HOME_URI%/kura_custom.properties \
+-Ddpa.configuration=%KURA_HOME%\\dpa.properties \
+-Dlog4j.configuration=file:%KURA_HOME_URI%/log4j.properties \
+-Djdk.tls.trustNameService=true \
+-jar \"%~dp0\\plugins\\org.eclipse.osgi_3.8.1.v20120830-144521.jar\" \
+-configuration %KURA_TEMP%\\Configuration \
+-console \
+-consoleLog \
+";
+
+// Replace substr with replacement in string and return number of replacements made
+// Caller should provide a buffer large enough for the replaced string to fit it
+int str_replace ( char *string, size_t max_buf, const char *substr, const char *replacement )
+{
+  char *tok = NULL;
+  char *newstr = NULL;
+  char *oldstr = NULL;
+  char *head = NULL;
+  int cnt = 0;
+ 
+  if ( substr == NULL || replacement == NULL )
+	  return 0;
+
+  newstr = _strdup (string);
+  head = newstr;
+
+  while ( (tok = strstr ( head, substr )) )
+  {
+    oldstr = newstr;
+    newstr = malloc ( strlen ( oldstr ) - strlen ( substr ) + strlen ( replacement ) + 1 );
+    /*failed to alloc mem, free old string and return NULL */
+    if ( newstr == NULL )
+	{
+      free (oldstr);
+      return cnt;
+    }
+    memcpy ( newstr, oldstr, tok - oldstr );
+    memcpy ( newstr + (tok - oldstr), replacement, strlen ( replacement ) );
+    memcpy ( newstr + (tok - oldstr) + strlen( replacement ), tok + strlen ( substr ), strlen ( oldstr ) - strlen ( substr ) - ( tok - oldstr ) );
+    memset ( newstr + strlen ( oldstr ) - strlen ( substr ) + strlen ( replacement ) , 0, 1 );
+    /* move back head right after the last replacement */
+    head = newstr + (tok - oldstr) + strlen( replacement );
+    free (oldstr);
+	cnt++;
+  }
+
+  // now copy back the new string into the original one based on max buffer size
+  memset( string, 0, max_buf );
+  memcpy( string, newstr, min(max_buf, strlen(newstr)) );
+  free( newstr );
+  return cnt;
+}
+
+// This appends a backslash at the end of string if not already present
+void append_backslash( char *str, size_t max_buf )
+{
+	if( str[ strlen(str)-1 ] != '\\' )
+		strcat_s( str, max_buf, "\\" );
+}
+
+
+char *PrepareCommandLine( LPTSTR lpInstallDir )
+{
+	char szInstallPath[MAX_PATH*2];
+	char szProgramDataPath[MAX_PATH*2];
+	char szKuraHomePath[MAX_PATH*2];
+	char szKuraHomeURI[MAX_PATH*2];
+	char szKuraDataPath[MAX_PATH*2];
+	char szKuraTempPath[MAX_PATH*2];
+	char szKuraHomePathCfg[MAX_PATH*2];
+	char szKuraTempPathCfg[MAX_PATH*2];
+	char *buf;
+
+	buf = malloc( KURA_CMD_LINE_SIZE );
+	if( !buf )
+	{
+		MessageBox( NULL, "Kura Launcher v1.0\n\nNot enough memory", "Error", MB_ICONERROR );
+		return NULL;
+	}
+
+	// Get the first argument, which should contain the installation path
+	strcpy_s( szInstallPath, sizeof(szInstallPath), lpInstallDir );
+	// Remove quotes in case they were present
+	str_replace( szInstallPath, sizeof(szInstallPath), "\"", "" );
+	append_backslash( szInstallPath, sizeof(szInstallPath) );
+
+	// Get the folder where Kura Data files are stored (this is by default C:\ProgramData)
+	GetEnvironmentVariable( "ALLUSERSPROFILE", szProgramDataPath, sizeof(szProgramDataPath) );
+
+	// Now create the KURA_HOME and KURA_DATA paths
+	strcpy_s( szKuraHomePath, sizeof(szKuraHomePath), szProgramDataPath );
+	strcpy_s( szKuraDataPath, sizeof(szKuraDataPath), szProgramDataPath );
+	append_backslash( szKuraHomePath, sizeof(szKuraHomePath) );
+	append_backslash( szKuraDataPath, sizeof(szKuraDataPath) );
+	strcat_s( szKuraHomePath, sizeof(szKuraHomePath), "kura\\kura" );
+	strcat_s( szKuraDataPath, sizeof(szKuraDataPath), "kura\\data" );
+
+	// Now need to create the URI version of the KURA_HOME path that's using in some settings
+	strcpy_s( szKuraHomeURI, sizeof(szKuraHomeURI), "/" );
+	strcat_s( szKuraHomeURI, sizeof(szKuraHomeURI), szKuraHomePath );
+	str_replace( szKuraHomeURI, sizeof(szKuraHomeURI), "\\", "/" );  // need to replace \ with /
+
+	// Get the %TEMP% path
+	GetEnvironmentVariable( "TEMP", szKuraTempPath, sizeof(szKuraTempPath) );
+	append_backslash( szKuraTempPath, sizeof(szKuraTempPath) );
+	strcat_s( szKuraTempPath, sizeof(szKuraTempPath), "kura" );
+
+	// Now we need to create a temporary directory and copy the CONFIG.INI file there
+	strcpy_s( szKuraTempPathCfg, sizeof(szKuraTempPathCfg), szKuraTempPath );
+	append_backslash( szKuraTempPathCfg, sizeof(szKuraTempPathCfg) );
+	strcat_s( szKuraTempPathCfg, sizeof(szKuraTempPathCfg), "Configuration" );
+
+	strcpy_s( szKuraHomePathCfg, sizeof(szKuraHomePathCfg), szKuraHomePath );
+	append_backslash( szKuraHomePathCfg, sizeof(szKuraHomePathCfg) );
+	strcat_s( szKuraHomePathCfg, sizeof(szKuraHomePathCfg), "config.ini" );
+
+	CreateDirectory( szKuraTempPath, NULL );
+	CreateDirectory( szKuraTempPathCfg, NULL );
+	strcat_s( szKuraTempPathCfg, sizeof(szKuraTempPathCfg), "\\config.ini" );
+	CopyFile( szKuraHomePathCfg, szKuraTempPathCfg, FALSE );
+
+	// Now we need to replace the variables in KuraJavaArgs with actual paths
+	strcpy_s( buf, KURA_CMD_LINE_SIZE, KuraJavaArgs );
+	str_replace( buf, KURA_CMD_LINE_SIZE, "%KURA_HOME%", szKuraHomePath );
+	str_replace( buf, KURA_CMD_LINE_SIZE, "%KURA_DATA%", szKuraDataPath );
+	str_replace( buf, KURA_CMD_LINE_SIZE, "%KURA_HOME_URI%", szKuraHomeURI );
+	str_replace( buf, KURA_CMD_LINE_SIZE, "%KURA_TEMP%", szKuraTempPath );
+	str_replace( buf, KURA_CMD_LINE_SIZE, "%~dp0\\", szInstallPath );
+
+	return buf;
+}
+
+
 void WINAPI ServiceMain(DWORD argc, LPTSTR *argv)
 {
 	HKEY hKey;
-	DWORD dwSize;
-	g_bTerminate = 0;
-	TCHAR szString[256];
-	TCHAR *pSubKey = _T("System\\CurrentControlSet\\Services\\");
+	DWORD dwType, dwSize;
+	BYTE buf[MAX_PATH];
+	LPTSTR lpInstallPath = NULL;
 
+	ZeroMemory (&g_ServiceStatus, sizeof (g_ServiceStatus));
+	g_ServiceStatus.dwServiceType = SERVICE_WIN32_OWN_PROCESS;
+	g_bTerminate = 0;
+
+	// First check the installation dir path in registry (created during setup), which should tell us where Kura is installed
+	if( RegOpenKeyEx( HKEY_LOCAL_MACHINE, "SOFTWARE\\Kura", 0, KEY_ALL_ACCESS, &hKey ) == ERROR_SUCCESS ||
+		RegOpenKeyEx( HKEY_LOCAL_MACHINE, "SOFTWARE\\Wow6432Node\\Kura", 0, KEY_ALL_ACCESS, &hKey ) == ERROR_SUCCESS )
+	{
+	    dwSize = sizeof(buf);
+	    if( RegQueryValueEx( hKey, "Install_Dir", 0, &dwType, buf, &dwSize) == ERROR_SUCCESS )
+		{
+			lpInstallPath = (LPTSTR)buf;
+		}
+		RegCloseKey( hKey );
+	}
+
+	g_pszKuraCommandLine = PrepareCommandLine( lpInstallPath );
+	if(g_pszKuraCommandLine == NULL)
+	{
+		UpdateStatus(SERVICE_STOPPED, 0, 3, ERROR_PATH_NOT_FOUND);
+		return;
+	}
 
 	// Register our service control handler with the SCM
 	g_StatusHandle = RegisterServiceCtrlHandler(SERVICE_NAME, ServiceCtrlHandler);
- 
-	g_bTerminate = 0;
-	if(g_StatusHandle != NULL) 
+ 	if(g_StatusHandle == NULL)
 	{
-		ZeroMemory (&g_ServiceStatus, sizeof (g_ServiceStatus));
-		g_ServiceStatus.dwServiceType = SERVICE_WIN32_OWN_PROCESS;
-
-		// Tell the service controller we are starting
-		UpdateStatus(SERVICE_START_PENDING, 0, 0, NO_ERROR);
-
-		// Check that the sevice name will fit onto the end of the services key name
-		if(((_tcslen(pSubKey) + _tcslen(argv[0]) + 1) * sizeof(TCHAR)) < 256)
-		{
-			_tcscpy(szString, pSubKey);
-			_tcscat(szString, argv[0]);
-
-			if(RegOpenKey(HKEY_LOCAL_MACHINE, szString, &hKey) == ERROR_SUCCESS)
-			{
-				dwSize = 256;
-				if(RegQueryValueEx(hKey, _T("ServiceCommand"), 0, NULL, (BYTE *)szString, &dwSize) == ERROR_SUCCESS)
-				{
-					RunService(szString);
-				}
-				RegCloseKey(hKey);
-			}
-		}
-		// Tell the service controller we are stopped
-		UpdateStatus(SERVICE_STOPPED, 0, 3, NO_ERROR);
+		free(g_pszKuraCommandLine);
+		UpdateStatus(SERVICE_STOPPED, 0, 3,  GetLastError());
+		return;
 	}
-	return;
+
+	// Tell the service controller we are starting
+	UpdateStatus(SERVICE_START_PENDING, 0, 0, NO_ERROR);
+
+	RunService(g_pszKuraCommandLine);
+
+	// Tell the service controller we are stopped
+	UpdateStatus(SERVICE_STOPPED, 0, 3, NO_ERROR);
+
+	free(g_pszKuraCommandLine);
 }
 
 int main(int argc, char* argv[])
@@ -53,7 +203,7 @@ int main(int argc, char* argv[])
 		{ SERVICE_NAME, (LPSERVICE_MAIN_FUNCTION)ServiceMain },
 		{ NULL, NULL }
 	};
- 
+
 	if(StartServiceCtrlDispatcher(ServiceTable) == FALSE)
 	{
 		return GetLastError();

5e46ba90d30bf69faf77ec739305e43d3734e4c2
Mark Cullen <mark.cullen@eurotech.com>
2016-03-14 11:45:03 +0000

    Added windows.launcher. When kura is installed on Windows it gets launched as a task, this utility is used by the task manager to start the Java process and collects output from stdout to prevent windows from popping up.
    Signed-off-by: Mark Cullen <mark.cullen@eurotech.com>

---
+++ target-platform/org.eclipse.kura.windows.launcher/build_launcher.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+#
+# This script is used to build the KURA Launcher executables used to start Kura on Windows based systems. It relies on
+# mingw64 to do the build. If mingw64 is not installed it skips the build without error so that the pre built binary will be used
+#
+# It's called with one of two arguments 'clean' in which case the object files are deleted, or 'build' in which case the files are
+# rebuilt. Take care here to make sure that the required build tools are installed, i686-w64-mingw32-gcc for the 32 bit version
+# and x86_64-w64-mingw32-gcc for the 64 bit version. These should all be installed with the mingw64 package.
+
+#==============================================================================================================================
+# First check to see what make we have, if it's Windows it will probably be mingw32-make, if it's Linux it's probably just make
+# either way set the MAKECMD variable accordingly. If we can't find either issue an warning message and exit the script with
+# code 0. This will stop maven from showing an error but will leave the pre built binary in place.
+#
+if [ -x "$(command -v mingw32-make)" ] || [ -x "$(command -v make)" ]; then
+	if [ -x "$(command -v make)" ]; then
+		MAKECMD="make"
+	else
+		MAKECMD="mingw32-make"
+	fi
+else
+	echo "  WARNING: No viable 'make' command installed. KURA Windows Launcher will not be rebuilt"
+
+# Just copy the release files into the intermediate Release dir
+if [ "$1" == "build" ]; then
+	if [ ! -d src/main/c/Release/Win32/x86 ]; then
+		mkdir -p src/main/c/Release/Win32/x86
+	fi
+	if [ ! -d src/main/c/Release/Win32/x64 ]; then
+		mkdir -p src/main/c/Release/Win32/x64
+	fi
+
+	cp src/main/bin/Win32/x86/* src/main/c/Release/Win32/x86
+	cp src/main/bin/Win32/x64/* src/main/c/Release/Win32/x64
+fi
+
+	exit 0
+fi
+
+#===============================================================================================================================
+# If the argument is 'clean' just work through all the files in the Objs directory and delete them.
+#
+if [ "$1" == "clean" ]; then
+	for f in src/main/c/Objs/x86/*; do
+		if [ -f "$f" ]; then
+			echo Deleting $f
+			rm $f
+		fi
+	done
+	for f in src/main/c/Objs/x64/*; do
+		if [ -f "$f" ]; then
+			echo Deleting $f
+			rm $f
+		fi
+	done
+fi
+
+#===============================================================================================================================
+# If the argument is 'build' change to the source directory, create the Objs and Release directories if required, then check to
+# see if we have viable 32 bit compiler if so call make with the 32 bit target. Next check for 64 bit compiler and run make with
+# the 64 bit target. If the compilers are not found just issue a warning and continue without doing anything. This will leave the
+# pre built binaries in the Release directory to be used in the final installer. This should mean that you get a working installer
+# even if you don't have mingw installed
+#
+if [ "$1" == "build" ]; then
+		
+		cd src/main/c
+		if [ ! -d Objs/x86 ]; then
+			mkdir -p Objs/x86
+		fi
+
+		if [ ! -d Objs/x64 ]; then
+			mkdir -p Objs/x64
+		fi
+
+		if [ ! -d Release/Win32/x86 ]; then
+			mkdir -p Release/Win32/x86
+		fi
+
+		if [ ! -d Release/Win32/x64 ]; then
+			mkdir -p Release/Win32/x64
+		fi
+
+		if [ -x "$(command -v i686-w64-mingw32-gcc)" ]; then
+			$MAKECMD -f makefile.mingw KURALauncher32
+		else
+			echo "  WARNING: No viable 32 bit 'mingw' compiler installed. 32 bit KURA Windows Launcher will not be rebuilt"
+		fi
+
+		if [ -x "$(command -v x86_64-w64-mingw32-gcc)" ]; then
+			$MAKECMD -f makefile.mingw KURALauncher64
+		else
+			echo "  WARNING: No viable 64 bit 'mingw' compiler installed. 64 bit KURA Windows Launcher will not be rebuilt"
+		fi
+fi
+

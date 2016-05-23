dc84561054e946ae6e2f5265d9de0be0dde06966
Henryk Konsek <hekonsek@gmail.com>
2016-05-23 11:54:48 +0200

    Added missing licenses. Minor fixes.

--- kura/examples/org.eclipse.kura.example.camel.aggregation/src/main/java/io/rhiot/quickstarts/kura/camel/GatewayRouter.java
+++ kura/examples/org.eclipse.kura.example.camel.aggregation/src/main/java/io/rhiot/quickstarts/kura/camel/GatewayRouter.java
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) 2011, 2016 Red Hat and/or its affiliates
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Red Hat
+ *******************************************************************************/
 package io.rhiot.quickstarts.kura.camel;
 
 import org.apache.camel.Exchange;

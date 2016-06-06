97f64eb4e620d43e4f8ad04ad7a60c10d591755f
Amit Kumar Mondal <admin@amitinside.com>
2016-06-06 12:00:31 +0200

    First draft of Device and Driver Management Test Bundle
    All Device and Driver management tests including driver specific tests
    will reside in this bundle
    
    Signed-off-by: Amit Kumar Mondal <admin@amitinside.com>

---
+++ kura/org.eclipse.kura.device.test/src/test/java/org/eclipse/kura/device/internal/test/DeviceStub.java
@@ -0,0 +1,22 @@
+/**
+ * Copyright (c) 2011, 2016 Eurotech and/or its affiliates
+ *
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  which accompanies this distribution, and is available at
+ *  http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Eurotech
+ *   Amit Kumar Mondal (admin@amitinside.com)
+ */
+package org.eclipse.kura.device.internal.test;
+
+import org.eclipse.kura.device.internal.AbstractDevice;
+
+/**
+ * The Class DeviceStub is a basic device implementation stub for testing
+ * purposes
+ */
+public final class DeviceStub extends AbstractDevice {
+}
---
+++ kura/org.eclipse.kura.device.test/src/test/java/org/eclipse/kura/device/internal/test/DriverStub.java
@@ -0,0 +1,102 @@
+/**
+ * Copyright (c) 2011, 2016 Eurotech and/or its affiliates
+ *
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  which accompanies this distribution, and is available at
+ *  http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Eurotech
+ *   Amit Kumar Mondal (admin@amitinside.com)
+ */
+package org.eclipse.kura.device.internal.test;
+
+import java.util.List;
+import java.util.Map;
+
+import org.eclipse.kura.KuraException;
+import org.eclipse.kura.KuraRuntimeException;
+import org.eclipse.kura.device.ChannelDescriptor;
+import org.eclipse.kura.device.Driver;
+import org.eclipse.kura.device.DriverEvent;
+import org.eclipse.kura.device.DriverFlag;
+import org.eclipse.kura.device.DriverListener;
+import org.eclipse.kura.device.DriverRecord;
+import org.eclipse.kura.device.util.Devices;
+import org.eclipse.kura.type.util.TypedValues;
+
+import com.google.common.collect.Lists;
+
+/**
+ * Example Driver Implementation
+ */
+public final class DriverStub implements Driver {
+
+	/**
+	 * List of driver listeners
+	 */
+	protected List<DriverListener> m_listeners;
+
+	/** Constructor */
+	public DriverStub() {
+		this.m_listeners = Lists.newCopyOnWriteArrayList();
+	}
+
+	/** {@inheritDoc} */
+	@Override
+	public synchronized void connect() throws KuraException {
+		// Not required to be tested as Device doesn't use this directly
+	}
+
+	/** {@inheritDoc} */
+	@Override
+	public synchronized void disconnect() throws KuraException {
+		// Not required to be tested as Device doesn't use this directly
+	}
+
+	/** {@inheritDoc} */
+	@Override
+	public synchronized ChannelDescriptor getChannelDescriptor() {
+		// Not required to be tested as Device doesn't use this directly
+		throw new UnsupportedOperationException();
+	}
+
+	/** {@inheritDoc} */
+	@Override
+	public synchronized void read(final List<DriverRecord> records) throws KuraRuntimeException, KuraException {
+		for (final DriverRecord record : records) {
+			record.setValue(TypedValues.newBooleanValue(true));
+			record.setDriverFlag(DriverFlag.READ_SUCCESSFUL);
+		}
+	}
+
+	/** {@inheritDoc} */
+	@Override
+	public synchronized void registerDriverListener(final Map<String, Object> channelConfig,
+			final DriverListener listener) throws KuraRuntimeException, KuraException {
+		final DriverRecord record = Devices.newDriverRecord("sample.channel3");
+		record.setValue(TypedValues.newIntegerValue(20));
+		record.setDriverFlag(DriverFlag.READ_SUCCESSFUL);
+		record.setTimestamp(System.nanoTime());
+		final DriverEvent event = new DriverEvent(record);
+		this.m_listeners.add(listener);
+		listener.onDriverEvent(event);
+	}
+
+	/** {@inheritDoc} */
+	@Override
+	public synchronized void unregisterDriverListener(final DriverListener listener)
+			throws KuraRuntimeException, KuraException {
+		this.m_listeners.remove(listener);
+	}
+
+	/** {@inheritDoc} */
+	@Override
+	public synchronized void write(final List<DriverRecord> records) throws KuraRuntimeException, KuraException {
+		for (final DriverRecord record : records) {
+			record.setDriverFlag(DriverFlag.WRITE_SUCCESSFUL);
+		}
+	}
+
+}

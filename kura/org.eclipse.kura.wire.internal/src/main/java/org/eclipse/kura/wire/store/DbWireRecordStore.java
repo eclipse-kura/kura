de1465911f3760eabb33ab468a47f4d14c0a39f0
Amit Kumar Mondal <admin@amitinside.com>
2016-07-17 05:28:36 +0200

    Removed Unnecessary Imports and Added Bundle License and Category
    Signed-off-by: Amit Kumar Mondal <admin@amitinside.com>

--- kura/org.eclipse.kura.wire.internal/src/main/java/org/eclipse/kura/wire/store/DbWireRecordStore.java
+++ kura/org.eclipse.kura.wire.internal/src/main/java/org/eclipse/kura/wire/store/DbWireRecordStore.java
@@ -22,7 +22,6 @@ import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.text.MessageFormat;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.Executors;

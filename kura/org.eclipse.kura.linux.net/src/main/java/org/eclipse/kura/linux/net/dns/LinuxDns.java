c641b966487a4c9b516af2310730fbba89efa3ff
Jens Reimann <ctron@dentrassi.de>
2016-08-19 17:44:58 +0200

    Clean up locking (#392)
    This change removes the explicit lock object and uses synchronized
    methods instead.
    
    Signed-off-by: Jens Reimann <jreimann@redhat.com>

--- kura/org.eclipse.kura.linux.net/src/main/java/org/eclipse/kura/linux/net/dns/LinuxDns.java
+++ kura/org.eclipse.kura.linux.net/src/main/java/org/eclipse/kura/linux/net/dns/LinuxDns.java
@@ -8,6 +8,7 @@
  *
  * Contributors:
  *     Eurotech
+ *     Jens Reimann <jreimann@redhat.com> - clean up locking
  *******************************************************************************/
 package org.eclipse.kura.linux.net.dns;
 
@@ -34,10 +35,9 @@ import org.eclipse.kura.net.IPAddress;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
 public class LinuxDns {
 
-	private static Logger s_logger = LoggerFactory.getLogger(LinuxDns.class);
+	private static final Logger s_logger = LoggerFactory.getLogger(LinuxDns.class);
 	
 	private static final String DNS_FILE_NAME = "/etc/resolv.conf";
 	//private static final String PPP_DNS_FILE_NAME = "/etc/ppp/resolv.conf";
@@ -47,12 +47,7 @@ public class LinuxDns {
 	private static final String GLOBAL_DHCP_LEASES_DIR = "/var/lib/dhcp";
 	private static final String IFACE_DHCP_LEASES_DIR = "/var/lib/dhclient";
 	
-	private static Object s_lock;
 	private static LinuxDns s_linuxDns = null;
-	
-	private LinuxDns() {
-		s_lock = new Object();
-	}
 	
 	public static synchronized LinuxDns getInstance() {
 		if(s_linuxDns == null) {
@@ -63,46 +58,44 @@ public class LinuxDns {
 	}
 
 	
-	public Set<IPAddress> getDnServers() {
-		synchronized(s_lock) {
-			BufferedReader br = null;
-			Set<IPAddress> servers = new HashSet<IPAddress>();
+	public synchronized Set<IPAddress> getDnServers() {
+		BufferedReader br = null;
+		Set<IPAddress> servers = new HashSet<IPAddress>();
+		
+		try {
+			File f = new File(DNS_FILE_NAME);
+			if(!f.exists()) {
+				f.createNewFile();
+			}
 			
-			try {
-				File f = new File(DNS_FILE_NAME);
-				if(!f.exists()) {
-					f.createNewFile();
+			br = new BufferedReader(new FileReader(new File(DNS_FILE_NAME)));
+			
+			String line = null;
+			while((line = br.readLine()) != null) {
+				line = line.trim();
+				if(line.indexOf("nameserver") == 0) {
+					StringTokenizer st = new StringTokenizer(line);
+					st.nextToken();
+					servers.add(IPAddress.parseHostAddress(st.nextToken()));
 				}
-				
-				br = new BufferedReader(new FileReader(new File(DNS_FILE_NAME)));
-				
-				String line = null;
-				while((line = br.readLine()) != null) {
-					line = line.trim();
-					if(line.indexOf("nameserver") == 0) {
-						StringTokenizer st = new StringTokenizer(line);
-						st.nextToken();
-						servers.add(IPAddress.parseHostAddress(st.nextToken()));
-					}
-				}
-				
-				if(servers.size() > 0) {
-					return servers;
-				} else {
-					s_logger.debug("No DNS servers found");
-					return null;
-				}
-			} catch (Exception e) {
-				e.printStackTrace();
+			}
+			
+			if(servers.size() > 0) {
+				return servers;
+			} else {
+				s_logger.debug("No DNS servers found");
 				return null;
-			} finally {
-				if (br != null) {
-					try {
-						br.close();
-						br = null;
-					} catch (IOException e) {
-						e.printStackTrace();
-					}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		} finally {
+			if (br != null) {
+				try {
+					br.close();
+					br = null;
+				} catch (IOException e) {
+					e.printStackTrace();
 				}
 			}
 		}
@@ -118,39 +111,37 @@ public class LinuxDns {
 		}
 	}
 	
-	public List<IPAddress> getPppDnServers() throws KuraException {
-		synchronized(s_lock) {
-			BufferedReader br = null;
-			ArrayList<IPAddress> serversList = new ArrayList<IPAddress>();
-			try {
-				String pppDnsFileName = getPppDnsFileName();
-				 if (pppDnsFileName != null) {
-					File pppDnsFile = new File(pppDnsFileName);
-					br = new BufferedReader(new FileReader(pppDnsFile));
-					String line;
-					while((line = br.readLine()) != null) {
-						line = line.trim();
-						if(line.indexOf("nameserver") == 0) {
-							StringTokenizer st = new StringTokenizer(line);
-							st.nextToken();
-							serversList.add(IP4Address.parseHostAddress(st.nextToken()));
-						}
-					}					
-			    }
-			} catch (Exception e) {
-				throw new KuraException(KuraErrorCode.INTERNAL_ERROR, e);
-			} finally {
-				if(br != null) {
-					try {
-						br.close();
-						br = null;
-					} catch (IOException e) {
-						throw new KuraException(KuraErrorCode.INTERNAL_ERROR, e);
+	public synchronized List<IPAddress> getPppDnServers() throws KuraException {
+		BufferedReader br = null;
+		ArrayList<IPAddress> serversList = new ArrayList<IPAddress>();
+		try {
+			String pppDnsFileName = getPppDnsFileName();
+			 if (pppDnsFileName != null) {
+				File pppDnsFile = new File(pppDnsFileName);
+				br = new BufferedReader(new FileReader(pppDnsFile));
+				String line;
+				while((line = br.readLine()) != null) {
+					line = line.trim();
+					if(line.indexOf("nameserver") == 0) {
+						StringTokenizer st = new StringTokenizer(line);
+						st.nextToken();
+						serversList.add(IP4Address.parseHostAddress(st.nextToken()));
 					}
+				}					
+		    }
+		} catch (Exception e) {
+			throw new KuraException(KuraErrorCode.INTERNAL_ERROR, e);
+		} finally {
+			if(br != null) {
+				try {
+					br.close();
+					br = null;
+				} catch (IOException e) {
+					throw new KuraException(KuraErrorCode.INTERNAL_ERROR, e);
 				}
 			}
-			return serversList;
 		}
+		return serversList;
 	}
 	
 	public List<IPAddress> getDhcpDnsServers(String interfaceName, String address) throws KuraException {
@@ -331,107 +322,63 @@ public class LinuxDns {
 		return servers;
 	}
 
-	public void removeDnsServer(IPAddress serverIpAddress) {
-		synchronized(s_lock) {
-			try {
-				if(isPppDnsSet()) {
-					return;
-				}
-			} catch(Exception e) {
-				e.printStackTrace();
-			}
-			
-			Set<IPAddress> servers = getDnServers();
-			Set<IPAddress> newServers = new HashSet<IPAddress>();
-			
-			for(IPAddress server : servers) {
-				if(server.equals(serverIpAddress)) {
-					s_logger.info("removed the DNS server: " + serverIpAddress);
-				} else {
-					//keep it for the new Dns file
-					newServers.add(server);
-				}
-			}
-			
-			writeDnsFile(newServers);
-		}
-	}
-
-	public void addDnsServer(IPAddress serverIpAddress) {
-		synchronized(s_lock) {
-			try {
-				if(isPppDnsSet()) {
-					return;
-				}
-			} catch(Exception e) {
-				e.printStackTrace();
-			}
-			
-			Set<IPAddress> servers = getDnServers();
-			
-			if (servers == null) {
-				servers = new HashSet<IPAddress>();
-			}
-			servers.add(serverIpAddress);
-			
-			writeDnsFile(servers);
-		}
-	}
-
-	public void setPppDns() throws Exception {
-		synchronized(s_lock) {
-			String sPppDnsFileName = getPppDnsFileName();
-			if (sPppDnsFileName != null) {
-				if(!isPppDnsSet()) {
-					File file = new File(DNS_FILE_NAME);
-					if(file.exists()) {
-						SafeProcess proc = null;
-						try {
-							proc = ProcessUtil.exec("mv " + DNS_FILE_NAME + " " + BACKUP_DNS_FILE_NAME);
-							if(proc.waitFor() != 0) {
-								s_logger.error("failed to backup " + DNS_FILE_NAME);
-								throw new Exception("Failed to backup " + DNS_FILE_NAME);
-							} else {
-								s_logger.info("successfully backed up " + BACKUP_DNS_FILE_NAME);
-							}
-						}
-						finally {
-							if (proc != null) ProcessUtil.destroy(proc);
-						}
-					}
-					
-					SafeProcess proc = null;
-					try {
-						proc = ProcessUtil.exec("ln -sf " + sPppDnsFileName + " " + DNS_FILE_NAME);
-						if(proc.waitFor() != 0) {
-							s_logger.error("failed to link " + DNS_FILE_NAME + " to " + sPppDnsFileName);
-							throw new Exception("Failed to backup " + DNS_FILE_NAME);
-						} else {
-							s_logger.info("set DNS to use ppp resolv.conf");
-						}
-					}
-					finally {
-						if (proc != null) ProcessUtil.destroy(proc);
-					}
-				}
-			}
-		}
-	}
-
-	public void unsetPppDns() throws Exception {
-		synchronized(s_lock) {
+	public synchronized void removeDnsServer(IPAddress serverIpAddress) {
+		try {
 			if(isPppDnsSet()) {
-				String pppDnsFilename = getPppDnsFileName();
+				return;
+			}
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		Set<IPAddress> servers = getDnServers();
+		Set<IPAddress> newServers = new HashSet<IPAddress>();
+		
+		for(IPAddress server : servers) {
+			if(server.equals(serverIpAddress)) {
+				s_logger.info("removed the DNS server: " + serverIpAddress);
+			} else {
+				//keep it for the new Dns file
+				newServers.add(server);
+			}
+		}
+		
+		writeDnsFile(newServers);
+	}
+
+	public synchronized void addDnsServer(IPAddress serverIpAddress) {
+		try {
+			if(isPppDnsSet()) {
+				return;
+			}
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		Set<IPAddress> servers = getDnServers();
+		
+		if (servers == null) {
+			servers = new HashSet<IPAddress>();
+		}
+		servers.add(serverIpAddress);
+		
+		writeDnsFile(servers);
+	}
+
+	public synchronized void setPppDns() throws Exception {
+		String sPppDnsFileName = getPppDnsFileName();
+		if (sPppDnsFileName != null) {
+			if(!isPppDnsSet()) {
 				File file = new File(DNS_FILE_NAME);
 				if(file.exists()) {
 					SafeProcess proc = null;
 					try {
-						proc = ProcessUtil.exec("rm " + DNS_FILE_NAME);
+						proc = ProcessUtil.exec("mv " + DNS_FILE_NAME + " " + BACKUP_DNS_FILE_NAME);
 						if(proc.waitFor() != 0) {
-							s_logger.error("failed to delete " + DNS_FILE_NAME + " symlink that points to " + pppDnsFilename);
-							throw new Exception("failed to delete " + DNS_FILE_NAME + " symlink that points to " + pppDnsFilename);
+							s_logger.error("failed to backup " + DNS_FILE_NAME);
+							throw new Exception("Failed to backup " + DNS_FILE_NAME);
 						} else {
-							s_logger.info("successfully removed symlink that points to " + pppDnsFilename);
+							s_logger.info("successfully backed up " + BACKUP_DNS_FILE_NAME);
 						}
 					}
 					finally {
@@ -439,167 +386,193 @@ public class LinuxDns {
 					}
 				}
 				
-				file = new File(BACKUP_DNS_FILE_NAME);
-				if(file.exists()) {
-					SafeProcess proc = null;
-					try {
-						proc = ProcessUtil.exec("mv " + BACKUP_DNS_FILE_NAME + " " + DNS_FILE_NAME);
-						if(proc.waitFor() != 0) {
-							s_logger.error("failed to restore " + BACKUP_DNS_FILE_NAME + " to " + DNS_FILE_NAME);
-							throw new Exception("failed to restore " + BACKUP_DNS_FILE_NAME + " to " + DNS_FILE_NAME);
-						} else {
-							s_logger.info("successfully restored " + DNS_FILE_NAME + " from " + BACKUP_DNS_FILE_NAME);
-						}
-					}
-					finally {
-						if (proc != null) ProcessUtil.destroy(proc);
-					}						
-				} else {
-					SafeProcess proc = null;
-					try {
-						proc = ProcessUtil.exec("touch " + DNS_FILE_NAME);
-						if(proc.waitFor() != 0) {
-							s_logger.error("failed to create empty " + DNS_FILE_NAME);
-							throw new Exception("failed to create empty " + DNS_FILE_NAME);
-						} else {
-							s_logger.info("successfully created empty " + DNS_FILE_NAME);
-						}
-					}
-					finally {
-						if (proc != null) ProcessUtil.destroy(proc);
+				SafeProcess proc = null;
+				try {
+					proc = ProcessUtil.exec("ln -sf " + sPppDnsFileName + " " + DNS_FILE_NAME);
+					if(proc.waitFor() != 0) {
+						s_logger.error("failed to link " + DNS_FILE_NAME + " to " + sPppDnsFileName);
+						throw new Exception("Failed to backup " + DNS_FILE_NAME);
+					} else {
+						s_logger.info("set DNS to use ppp resolv.conf");
 					}
 				}
-				
-				// remove actual PPP DNS file
-				File pppDnsFile = new File (pppDnsFilename);
-				if(pppDnsFile.exists()) {
-					pppDnsFile.delete();
+				finally {
+					if (proc != null) ProcessUtil.destroy(proc);
 				}
 			}
 		}
 	}
-	
-	public boolean isPppDnsSet() throws Exception {
-		synchronized(s_lock) {
+
+	public synchronized void unsetPppDns() throws Exception {
+		if(isPppDnsSet()) {
+			String pppDnsFilename = getPppDnsFileName();
 			File file = new File(DNS_FILE_NAME);
-			if(isSymlink(file)) {
-				if(getRealPath(file).compareTo(getPppDnsFileName()) == 0) {
-					return true;
-				} else {
-					return false;
+			if(file.exists()) {
+				SafeProcess proc = null;
+				try {
+					proc = ProcessUtil.exec("rm " + DNS_FILE_NAME);
+					if(proc.waitFor() != 0) {
+						s_logger.error("failed to delete " + DNS_FILE_NAME + " symlink that points to " + pppDnsFilename);
+						throw new Exception("failed to delete " + DNS_FILE_NAME + " symlink that points to " + pppDnsFilename);
+					} else {
+						s_logger.info("successfully removed symlink that points to " + pppDnsFilename);
+					}
 				}
+				finally {
+					if (proc != null) ProcessUtil.destroy(proc);
+				}
+			}
+			
+			file = new File(BACKUP_DNS_FILE_NAME);
+			if(file.exists()) {
+				SafeProcess proc = null;
+				try {
+					proc = ProcessUtil.exec("mv " + BACKUP_DNS_FILE_NAME + " " + DNS_FILE_NAME);
+					if(proc.waitFor() != 0) {
+						s_logger.error("failed to restore " + BACKUP_DNS_FILE_NAME + " to " + DNS_FILE_NAME);
+						throw new Exception("failed to restore " + BACKUP_DNS_FILE_NAME + " to " + DNS_FILE_NAME);
+					} else {
+						s_logger.info("successfully restored " + DNS_FILE_NAME + " from " + BACKUP_DNS_FILE_NAME);
+					}
+				}
+				finally {
+					if (proc != null) ProcessUtil.destroy(proc);
+				}						
+			} else {
+				SafeProcess proc = null;
+				try {
+					proc = ProcessUtil.exec("touch " + DNS_FILE_NAME);
+					if(proc.waitFor() != 0) {
+						s_logger.error("failed to create empty " + DNS_FILE_NAME);
+						throw new Exception("failed to create empty " + DNS_FILE_NAME);
+					} else {
+						s_logger.info("successfully created empty " + DNS_FILE_NAME);
+					}
+				}
+				finally {
+					if (proc != null) ProcessUtil.destroy(proc);
+				}
+			}
+			
+			// remove actual PPP DNS file
+			File pppDnsFile = new File (pppDnsFilename);
+			if(pppDnsFile.exists()) {
+				pppDnsFile.delete();
+			}
+		}
+	}
+	
+	public synchronized boolean isPppDnsSet() throws Exception {
+		File file = new File(DNS_FILE_NAME);
+		if(isSymlink(file)) {
+			if(getRealPath(file).compareTo(getPppDnsFileName()) == 0) {
+				return true;
 			} else {
 				return false;
 			}
+		} else {
+			return false;
 		}
 	}
 	
-    private boolean isSymlink(File file) throws IOException {
-    	synchronized(s_lock) {
-	        if (file == null) {
-	        	throw new NullPointerException("File must not be null");
-	        }
-	
-	        File canon;
-	        if (file.getParent() == null) {
-	                canon = file;
-	        } else {
-	                File canonDir = file.getParentFile().getCanonicalFile();
-	                canon = new File(canonDir, file.getName());
-	        }
-	
+    private synchronized boolean isSymlink(File file) throws IOException {
+        if (file == null) {
+        	throw new NullPointerException("File must not be null");
+        }
+
+        File canon;
+        if (file.getParent() == null) {
+                canon = file;
+        } else {
+                File canonDir = file.getParentFile().getCanonicalFile();
+                canon = new File(canonDir, file.getName());
+        }
+
 	        return !canon.getCanonicalFile().equals(canon.getAbsoluteFile());
-    	}
     }
 
-    private String getRealPath(File file) throws IOException {
-    	synchronized(s_lock) {
-	        if (file == null) {
-	        	throw new NullPointerException("File must not be null");
-	        }
-	
-	        File canon;
-	        if (file.getParent() == null) {
-	                canon = file;
-	        } else {
-	                File canonDir = file.getParentFile().getCanonicalFile();
-	                canon = new File(canonDir, file.getName());
-	        }
-	        
-	        return canon.getCanonicalFile().toString();
-    	}
+    private synchronized String getRealPath(File file) throws IOException {
+        if (file == null) {
+        	throw new NullPointerException("File must not be null");
+        }
+
+        File canon;
+        if (file.getParent() == null) {
+                canon = file;
+        } else {
+                File canonDir = file.getParentFile().getCanonicalFile();
+                canon = new File(canonDir, file.getName());
+        }
+        
+        return canon.getCanonicalFile().toString();
     }
     
-    private void writeDnsFile(Set<IPAddress> servers) {
+    private synchronized void writeDnsFile(Set<IPAddress> servers) {
 		s_logger.debug("Writing DNS servers to file");
-		synchronized(s_lock) {
-			try {
-				FileOutputStream fos = new FileOutputStream(DNS_FILE_NAME);
-				PrintWriter pw = new PrintWriter(fos);
-				
-				String[] existingFile = getModifiedFile();
-				for(int i=0; i<existingFile.length; i++) {
-					pw.write(existingFile[i] + "\n");
-				}
-				pw.write("\n");
-				
-				for(IPAddress server : servers) {
-					pw.write("nameserver " + server.getHostAddress() + "\n");
-				}
-				
-				if(pw != null) {
-					pw.flush();
-					fos.getFD().sync();
-					pw.close();
-					fos.close();
-				}
-			} catch (Exception e) {
-				e.printStackTrace();
+		try {
+			FileOutputStream fos = new FileOutputStream(DNS_FILE_NAME);
+			PrintWriter pw = new PrintWriter(fos);
+			
+			String[] existingFile = getModifiedFile();
+			for(int i=0; i<existingFile.length; i++) {
+				pw.write(existingFile[i] + "\n");
 			}
+			pw.write("\n");
+			
+			for(IPAddress server : servers) {
+				pw.write("nameserver " + server.getHostAddress() + "\n");
+			}
+			
+			if(pw != null) {
+				pw.flush();
+				fos.getFD().sync();
+				pw.close();
+				fos.close();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 	}
 
-    private String[] getModifiedFile() {
-		synchronized(s_lock) {
-			BufferedReader br = null;
+    private synchronized String[] getModifiedFile() {
+		BufferedReader br = null;
+		try {
+		    File dnsFile = new File(DNS_FILE_NAME);
+		    if(!dnsFile.exists()) {
+		        dnsFile.createNewFile();
+		    }
+		    
+			ArrayList<String> linesWithoutServers = new ArrayList<String>();
+			br = new BufferedReader(new FileReader(dnsFile));
+			
+			String line;
+			while((line = br.readLine()) != null) {
+				line = line.trim();
+				if(line.indexOf("nameserver") != 0) {
+					linesWithoutServers.add(line);
+				}
+			}
+			br.close();
+			
+			String[] lines = new String[linesWithoutServers.size()];
+			for(int i=0; i<linesWithoutServers.size(); i++) {
+				lines[i] = (String) linesWithoutServers.get(i);
+			}
+			return lines;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		if(br != null) {
 			try {
-			    File dnsFile = new File(DNS_FILE_NAME);
-			    if(!dnsFile.exists()) {
-			        dnsFile.createNewFile();
-			    }
-			    
-				ArrayList<String> linesWithoutServers = new ArrayList<String>();
-				br = new BufferedReader(new FileReader(dnsFile));
-				
-				String line;
-				while((line = br.readLine()) != null) {
-					line = line.trim();
-					if(line.indexOf("nameserver") != 0) {
-						linesWithoutServers.add(line);
-					}
-				}
 				br.close();
-				
-				String[] lines = new String[linesWithoutServers.size()];
-				for(int i=0; i<linesWithoutServers.size(); i++) {
-					lines[i] = (String) linesWithoutServers.get(i);
-				}
-				return lines;
-			} catch (Exception e) {
+			} catch (IOException e) {
 				e.printStackTrace();
 			}
-			
-			if(br != null) {
-				try {
-					br.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-			br = null;
-			
-			return null;
 		}
+		br = null;
+		
+		return null;
 	}
     
     private String getPppDnsFileName() {

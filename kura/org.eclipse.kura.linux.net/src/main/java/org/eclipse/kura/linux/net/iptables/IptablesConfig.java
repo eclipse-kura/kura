90ad7bda0daf8a98533440ffa55ad90872c81735
ibinshtok <ilya.binshtok@eurotech.com>
2016-08-25 01:10:06 -0500

    Fixed source and destination strings builers for the NAT rule in the restore() method.

--- kura/org.eclipse.kura.linux.net/src/main/java/org/eclipse/kura/linux/net/iptables/IptablesConfig.java
+++ kura/org.eclipse.kura.linux.net/src/main/java/org/eclipse/kura/linux/net/iptables/IptablesConfig.java
@@ -425,14 +425,14 @@ public class IptablesConfig {
 					String source = natPostroutingChainRule.getSrcNetwork();
 					String destination = natPostroutingChainRule.getDstNetwork();
 					if (destination != null) {
-						StringBuilder sbDestination = new StringBuilder().append(destination).append(':').append(natPostroutingChainRule.getDstMask());
+						StringBuilder sbDestination = new StringBuilder().append(destination).append('/').append(natPostroutingChainRule.getDstMask());
 						destination = sbDestination.toString();
 					} else {
 						isNATrule = true;
 					}
 					if (source != null) {
 						isNATrule = true;
-						StringBuilder sbSource = new StringBuilder().append(source).append(':').append(natPostroutingChainRule.getSrcMask());
+						StringBuilder sbSource = new StringBuilder().append(source).append('/').append(natPostroutingChainRule.getSrcMask());
 						source = sbSource.toString();
 					} else {
 						if (!isNATrule) {

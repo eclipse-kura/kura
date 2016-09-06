edc2a01dd70e3d66fc3b3ec4814aa7881f3c7c7c
Jens Reimann <ctron@dentrassi.de>
2016-09-06 17:46:31 +0200

    #119: Add unit test for Camel component (#507)
    This change adds a basic unit test for the Camel component. It also does
    add a simple log4j configuration fragment for testing.
    
    Signed-off-by: Jens Reimann <jreimann@redhat.com>

---
+++ kura/org.eclipse.kura.camel.test/src/org/eclipse/kura/camel/router/AbstractRouterTest.java
@@ -0,0 +1,64 @@
+/*******************************************************************************
+ * Copyright (c) 2016 Red Hat Inc and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Red Hat Inc - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.kura.camel.router;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.Collections;
+import java.util.Map;
+
+import org.apache.camel.Route;
+import org.apache.commons.io.IOUtils;
+import org.junit.After;
+import org.junit.Before;
+import org.osgi.framework.FrameworkUtil;
+
+/**
+ * Unit test base for testing an activated router
+ */
+public abstract class AbstractRouterTest {
+
+    protected CamelRouter router;
+
+    @Before
+    public void before() throws Exception {
+        this.router = createRouter();
+        this.router.start(FrameworkUtil.getBundle(RouterTest.class).getBundleContext());
+    }
+
+    @After
+    public void after() throws Exception {
+        this.router.stop(FrameworkUtil.getBundle(RouterTest.class).getBundleContext());
+    }
+
+    protected Route firstRoute() {
+        return this.router.getContext().getRoutes().iterator().next();
+    }
+
+    protected static Map<String, Object> xmlProperties(String resourceName) {
+        return Collections.<String, Object>singletonMap("camel.route.xml", readStringResource(resourceName));
+    }
+
+    protected static CamelRouter createRouter() {
+        return new CamelRouter() {
+        };
+    }
+
+    protected static String readStringResource(String resourceName) {
+        try (InputStreamReader reader = new InputStreamReader(RouterTest.class.getResourceAsStream(resourceName), StandardCharsets.UTF_8)) {
+            return IOUtils.toString(reader);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}

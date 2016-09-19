cbdd7530489e28637b0b22f45a2f907e9499213c
Jens Reimann <jreimann@redhat.com>
2016-09-19 16:12:02 +0200

    Don't derive from KuraRouter anymore
    This change refactors the Camel part so that it does no longer derive
    from the Apache Camel Kura module, but directly setups up the router.
    
    Signed-off-by: Jens Reimann <jreimann@redhat.com>

--- kura/org.eclipse.kura.camel/src/main/java/org/eclipse/kura/camel/router/AbstractCamelRouter.java
+++ kura/org.eclipse.kura.camel/src/main/java/org/eclipse/kura/camel/router/AbstractCamelRouter.java
@@ -14,34 +14,130 @@ package org.eclipse.kura.camel.router;
 import java.util.Objects;
 
 import org.apache.camel.CamelContext;
-import org.apache.camel.component.kura.KuraRouter;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.core.osgi.OsgiDefaultCamelContext;
 import org.eclipse.kura.camel.cloud.KuraCloudComponent;
 import org.eclipse.kura.cloud.CloudService;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.FrameworkUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * An abstract base camel router for the use inside of Kura
  */
-public abstract class AbstractCamelRouter extends KuraRouter {
+public abstract class AbstractCamelRouter extends RouteBuilder {
 
     public static final String COMPONENT_NAME_KURA_CLOUD = "kura-cloud";
 
-    @Override
-    protected void beforeStart(final CamelContext camelContext) {
-        camelContext.getShutdownStrategy().setTimeout(5);
-        camelContext.disableJMX();
+    private static final Logger logger = LoggerFactory.getLogger(AbstractCamelRouter.class);
 
-        super.beforeStart(camelContext);
+    private CamelContext camelContext;
+
+    protected void start() throws Exception {
+        logger.info("Starting camel router");
+
+        // create and configure
+
+        final CamelContext camelContext = createCamelContext();
+
+        registerFeatures(camelContext);
+        configure(camelContext);
+        beforeStart(camelContext);
+
+        // assign an start
+
+        this.camelContext = camelContext;
+        this.camelContext.start();
+    }
+
+    protected void stop() throws Exception {
+        logger.info("Stopping camel router");
+
+        // stopping
+
+        if (this.camelContext != null) {
+            this.camelContext.stop();
+            this.camelContext = null;
+        }
+    }
+
+    protected BundleContext getBundleContext() {
+        return FrameworkUtil.getBundle(AbstractCamelRouter.class).getBundleContext();
+    }
+
+    protected CamelContext getCamelContext() {
+        return this.camelContext;
     }
 
     /**
-     * Register a custom cloud service
+     * Create a new camel context
      * <p>
-     * <strong>Note: </strong> This method should be called from {@link #beforeStart(CamelContext)} method
+     * This defaults to creating an {@link OsgiDefaultCamelContext} with the
+     * {@link BundleContext} provided by a call to {@link #getBundleContext()}.
      * </p>
-     * @param camelContext the camel context
-     * @param cloudService the cloud service
+     * 
+     * @return a new {@link CamelContext} instead which is neither configured nor started
+     */
+    protected CamelContext createCamelContext() {
+        return new OsgiDefaultCamelContext(getBundleContext());
+    }
+
+    /**
+     * A method which can register additional features (like components)
+     * 
+     * @param camelContext
+     *            the camel context to use
+     */
+    protected void registerFeatures(final CamelContext camelContext) {
+        logger.debug("Registering features - context: {}", camelContext);
+    }
+
+    /**
+     * Called to configure the camel context
+     * <p>
+     * This method is called after the context is created.
+     * It defaults to a call to {@link CamelContext#addRoutes(org.apache.camel.RoutesBuilder)} with {@code this} as
+     * argument.
+     * </p>
+     * 
+     * @param camelContext
+     * @throws Exception
+     */
+    protected void configure(final CamelContext camelContext) throws Exception {
+        logger.debug("Configuring - context: {}", camelContext);
+        camelContext.addRoutes(this);
+    }
+
+    /**
+     * Called before the context is started
+     * <p>
+     * The default implementation sets a timeout for the shutdown strategy and disables JMX.
+     * </p>
+     * 
+     * @param camelContext
+     *            the context to configure
+     */
+    protected void beforeStart(final CamelContext camelContext) {
+        logger.debug("Before start - context: {}", camelContext);
+        camelContext.getShutdownStrategy().setTimeout(5);
+        camelContext.disableJMX();
+    }
+
+    /**
+     * Register a custom cloud service for the component name "kura-cloud"
+     * <p>
+     * <strong>Note: </strong> This method should be called from {@link #registerFeatures(CamelContext)} method
+     * </p>
+     * 
+     * @param camelContext
+     *            the camel context
+     * @param cloudService
+     *            the cloud service
      */
     protected void registerCloudService(final CamelContext camelContext, final CloudService cloudService) {
+        logger.debug("Registering cloud service - context: {}, service: {}", camelContext, cloudService);
+
         Objects.requireNonNull(camelContext);
         Objects.requireNonNull(cloudService);
 

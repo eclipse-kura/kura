7bf7b3601631bed10c54a190b7d9d6324c3ccb09
Amit Kumar Mondal <admin@amitinside.com>
2016-09-29 12:35:21 +0200

    Added Utility Bundle and its localization resource
    Signed-off-by: Amit Kumar Mondal <admin@amitinside.com>

---
+++ kura/org.eclipse.kura.util/src/main/java/org/eclipse/kura/util/base/StringUtil.java
@@ -0,0 +1,73 @@
+/**
+ * Copyright (c) 2016 Eurotech and/or its affiliates
+ *
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  which accompanies this distribution, and is available at
+ *  http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Amit Kumar Mondal (admin@amitinside.com)
+ */
+package org.eclipse.kura.util.base;
+
+import static org.eclipse.kura.Preconditions.checkNull;
+
+import java.util.Iterator;
+
+import org.eclipse.kura.KuraRuntimeException;
+import org.eclipse.kura.annotation.Nullable;
+import org.eclipse.kura.localization.LocalizationAdapter;
+import org.eclipse.kura.localization.resources.UtilMessages;
+
+/**
+ * The Class StringUtil contains all necessary static factory methods for
+ * manipulating String instances
+ */
+public final class StringUtil {
+
+    /** Localization Resource. */
+    private static final UtilMessages s_message = LocalizationAdapter.adapt(UtilMessages.class);
+
+    /** Constructor */
+    private StringUtil() {
+        // Static Factory Methods container. No need to instantiate.
+    }
+
+    /**
+     * Returns {@code true} if the given string is null or is the empty string.
+     *
+     * @param string
+     *            a string reference to check
+     * @return {@code true} if the string is null or is the empty string
+     */
+    public static boolean isNullOrEmpty(@Nullable final String string) {
+        return (string == null) || string.isEmpty();
+    }
+
+    /**
+     * Returns a string containing the tokens joined by delimiters.
+     *
+     * @param tokens
+     *            an array objects to be joined. Strings will be formed from the
+     *            objects by calling object.toString().
+     * @throws KuraRuntimeException
+     *             if any of the arguments is null
+     */
+    public static String join(final CharSequence delimiter, final Iterable<?> tokens) {
+        checkNull(delimiter, s_message.delimiterNonNull());
+        checkNull(tokens, s_message.iterableNonNull());
+
+        final StringBuilder sb = new StringBuilder();
+        final Iterator<?> it = tokens.iterator();
+        if (it.hasNext()) {
+            sb.append(it.next());
+            while (it.hasNext()) {
+                sb.append(delimiter);
+                sb.append(it.next());
+            }
+        }
+        return sb.toString();
+    }
+
+}
---
+++ kura/org.eclipse.kura.util/src/main/java/org/eclipse/kura/util/base/ThrowableUtil.java
@@ -0,0 +1,56 @@
+/**
+ * Copyright (c) 2016 Eurotech and/or its affiliates
+ *
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  which accompanies this distribution, and is available at
+ *  http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Amit Kumar Mondal (admin@amitinside.com)
+ */
+package org.eclipse.kura.util.base;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+import org.eclipse.kura.KuraErrorCode;
+import org.eclipse.kura.KuraRuntimeException;
+
+/**
+ * The Class ThrowableUtil contains all necessary static factory methods for
+ * manipulating Throwable instances
+ */
+public final class ThrowableUtil {
+
+    /** Constructor */
+    private ThrowableUtil() {
+        // Static Factory Methods container. No need to instantiate.
+    }
+
+    /**
+     * Propagates a {@code throwable}.
+     *
+     * @param throwable
+     *            the throwable to propagate
+     */
+    public static void propagate(final Throwable throwable) {
+        throw new KuraRuntimeException(KuraErrorCode.INTERNAL_ERROR, throwable);
+    }
+
+    /**
+     * Returns a string containing the result of {@link Throwable#toString()},
+     * comprising recursive stack trace of {@code throwable}.
+     *
+     * @param throwable
+     *            The {@code throwable} instance
+     * @return the string
+     */
+    public static String stackTraceAsString(final Throwable throwable) {
+        final StringWriter sw = new StringWriter();
+        final PrintWriter pw = new PrintWriter(sw);
+        throwable.printStackTrace(pw);
+        return sw.toString();
+    }
+
+}

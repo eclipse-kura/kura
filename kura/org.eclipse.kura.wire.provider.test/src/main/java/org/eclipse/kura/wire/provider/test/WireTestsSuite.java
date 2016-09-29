4ffd725df42dbe2115edffacae07e7acbda3d18e
Amit Kumar Mondal <admin@amitinside.com>
2016-09-29 13:46:44 +0200

    Added Wire Provider, its test and localization resource
    Signed-off-by: Amit Kumar Mondal <admin@amitinside.com>

---
+++ kura/org.eclipse.kura.wire.provider.test/src/main/java/org/eclipse/kura/wire/provider/test/WireTestsSuite.java
@@ -0,0 +1,26 @@
+/*******************************************************************************
+ * Copyright (c) 2016 Eurotech and/or its affiliates
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Amit Kumar Mondal (admin@amitinside.com)
+ *******************************************************************************/
+package org.eclipse.kura.wire.provider.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+/**
+ * WireTestsSuite is a JUnit Test Suite containing a list of test cases to
+ * execute
+ */
+@RunWith(Suite.class)
+@SuiteClasses({ WireServiceTest.class })
+public final class WireTestsSuite {
+
+}

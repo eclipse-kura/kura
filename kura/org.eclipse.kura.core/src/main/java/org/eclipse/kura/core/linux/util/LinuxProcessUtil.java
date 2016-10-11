8a2f8500fda421f693c3ad16e4578cbeefe5b033
MMaiero <matteo.maiero@eurotech.com>
2016-10-11 10:40:44 +0200

    Removed empty line from LinuxProcessUtil generated by rebase.
    Signed-off-by: MMaiero <matteo.maiero@eurotech.com>

--- kura/org.eclipse.kura.core/src/main/java/org/eclipse/kura/core/linux/util/LinuxProcessUtil.java
+++ kura/org.eclipse.kura.core/src/main/java/org/eclipse/kura/core/linux/util/LinuxProcessUtil.java
@@ -18,7 +18,6 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-
 import java.util.StringTokenizer;
 
 import org.eclipse.kura.core.util.ProcessUtil;

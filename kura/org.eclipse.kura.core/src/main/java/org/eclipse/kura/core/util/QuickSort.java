b17034333da22030b1796106f68ae80db91ee6ec
Jens Reimann <jreimann@redhat.com>
2016-11-02 15:43:09 +0100

    Clean up and deprecate QuickSort
    * Forward all calls to plain Java methods
    * Deprecate class and methods, comment
    * Prevent subclassing
    
    Signed-off-by: Jens Reimann <jreimann@redhat.com>

--- kura/org.eclipse.kura.core/src/main/java/org/eclipse/kura/core/util/QuickSort.java
+++ kura/org.eclipse.kura.core/src/main/java/org/eclipse/kura/core/util/QuickSort.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011, 2016 Eurotech and/or its affiliates
+ * Copyright (c) 2011, 2016 Eurotech and/or its affiliates and others
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -8,160 +8,35 @@
  *
  * Contributors:
  *     Eurotech
+ *     Red Hat Inc
  *******************************************************************************/
 package org.eclipse.kura.core.util;
 
+import java.util.Arrays;
+
 /**
  * Utilities to quickly sort an array of values
+ * @deprecated Use sort methods from {@link Arrays} instead
  */
-public class QuickSort {
+@Deprecated
+public final class QuickSort {
 
-    public static void sort(int[] array) {
-        if (array.length > 0) {
-            sort(array, 0, array.length - 1);
-        }
+    private QuickSort() {
     }
 
-    public static void sort(long[] array) {
-        if (array.length > 0) {
-            sort(array, 0, array.length - 1);
-        }
-    }
-
-    private static void sort(int[] array, int lo, int hi) {
-        int i = lo, j = hi, h;
-        int x = array[(lo + hi) / 2];
-
-        // partition
-        do {
-            while (array[i] < x) {
-                i++;
-            }
-            while (array[j] > x) {
-                j--;
-            }
-            if (i <= j) {
-                h = array[i];
-                array[i] = array[j];
-                array[j] = h;
-                i++;
-                j--;
-            }
-        } while (i <= j);
-
-        // recursion
-        if (lo < j) {
-            sort(array, lo, j);
-        }
-        if (i < hi) {
-            sort(array, i, hi);
-        }
-    }
-
-    private static void sort(long[] array, int lo, int hi) {
-        int i = lo, j = hi;
-        long h;
-        long x = array[(lo + hi) / 2];
-
-        // partition
-        do {
-            while (array[i] < x) {
-                i++;
-            }
-            while (array[j] > x) {
-                j--;
-            }
-            if (i <= j) {
-                h = array[i];
-                array[i] = array[j];
-                array[j] = h;
-                i++;
-                j--;
-            }
-        } while (i <= j);
-
-        // recursion
-        if (lo < j) {
-            sort(array, lo, j);
-        }
-        if (i < hi) {
-            sort(array, i, hi);
-        }
-    }
-
-    /*
-     * public static void sort(int[] array) {
-     * quicksort(array, 0, array.length - 1);
-     * }
-     * 
-     * private static void quicksort(int[] array, int left, int right) {
-     * if (right <= left) return;
-     * int i = partition(array, left, right);
-     * quicksort(array, left, i-1);
-     * quicksort(array, i+1, right);
-     * }
-     * 
-     * private static int partition(int[] array, int left, int right) {
-     * int i = left, j = right;
-     * int tmp;
-     * int pivot = array[(left + right) / 2];
-     * 
-     * while (i <= j) {
-     * while (array[i] < pivot) {
-     * i++;
-     * }
-     * 
-     * while (array[j] > pivot) {
-     * j--;
-     * }
-     * 
-     * if (i <= j) {
-     * tmp = array[i];
-     * array[i] = array[j];
-     * array[j] = tmp;
-     * i++;
-     * j--;
-     * }
-     * }
-     * 
-     * return i;
-     * }
-     * 
-     * public static void sort(long[] array) {
-     * quicksort(array, 0, array.length - 1);
-     * }
-     * 
-     * private static void quicksort(long[] array, int left, int right) {
-     * if (right <= left) return;
-     * int i = partition(array, left, right);
-     * quicksort(array, left, i-1);
-     * quicksort(array, i+1, right);
-     * }
-     * 
-     * private static int partition(long[] array, int left, int right) {
-     * int i = left, j = right;
-     * long tmp;
-     * long pivot = array[(left + right) / 2];
-     * 
-     * while (i <= j) {
-     * while (array[i] < pivot) {
-     * i++;
-     * }
-     * 
-     * while (array[j] > pivot) {
-     * j--;
-     * }
-     * 
-     * if (i <= j) {
-     * tmp = array[i];
-     * array[i] = array[j];
-     * array[j] = tmp;
-     * i++;
-     * j--;
-     * }
-     * }
-     * 
-     * return i;
-     * }
+    /**
+     * @deprecated Use {@link Arrays#sort(int[])} instead
      */
+    @Deprecated
+    public static void sort(int[] array) {
+        Arrays.sort(array);
+    }
+
+    /**
+     * @deprecated Use {@link Arrays#sort(long[])} instead
+     */
+    @Deprecated
+    public static void sort(long[] array) {
+        Arrays.sort(array);
+    }
 }

3f3a0eaf795ceddce6ed20a5f7b213675e1bd8de
nicolatimeus <nicola.timeus@eurotech.com>
2016-11-21 17:31:57 +0100

    Implement starting/stopping bundles from the Web Ui
    Signed-off-by: nicolatimeus <nicola.timeus@eurotech.com>

--- kura/org.eclipse.kura.web2/src/main/java/org/eclipse/kura/web/client/ui/Device/BundlesTabUi.java
+++ kura/org.eclipse.kura.web2/src/main/java/org/eclipse/kura/web/client/ui/Device/BundlesTabUi.java
@@ -23,9 +23,12 @@ import org.eclipse.kura.web.shared.service.GwtDeviceService;
 import org.eclipse.kura.web.shared.service.GwtDeviceServiceAsync;
 import org.eclipse.kura.web.shared.service.GwtSecurityTokenService;
 import org.eclipse.kura.web.shared.service.GwtSecurityTokenServiceAsync;
+import org.gwtbootstrap3.client.ui.Button;
 import org.gwtbootstrap3.client.ui.gwt.CellTable;
 
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.cellview.client.TextColumn;
@@ -33,6 +36,8 @@ import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Widget;
 import com.google.gwt.view.client.ListDataProvider;
+import com.google.gwt.view.client.SelectionChangeEvent;
+import com.google.gwt.view.client.SingleSelectionModel;
 
 public class BundlesTabUi extends Composite {
 
@@ -48,18 +53,148 @@ public class BundlesTabUi extends Composite {
     private final GwtDeviceServiceAsync gwtDeviceService = GWT.create(GwtDeviceService.class);
 
     @UiField
+    Button bundlesRefresh;
+    @UiField
+    Button bundleStart;
+    @UiField
+    Button bundleStop;
+
+    @UiField
     CellTable<GwtGroupedNVPair> bundlesGrid = new CellTable<GwtGroupedNVPair>();
     private final ListDataProvider<GwtGroupedNVPair> bundlesDataProvider = new ListDataProvider<GwtGroupedNVPair>();
+    private final SingleSelectionModel<GwtGroupedNVPair> selectionModel = new SingleSelectionModel<GwtGroupedNVPair>();
+
+    private GwtDeviceServiceAsync deviceService = GWT.create(GwtDeviceService.class);
+    private GwtSecurityTokenServiceAsync securityTokenService = GWT.create(GwtSecurityTokenService.class);
 
     public BundlesTabUi() {
         initWidget(uiBinder.createAndBindUi(this));
         loadBundlesTable(this.bundlesGrid, this.bundlesDataProvider);
+
+        bundlesRefresh.setText("Refresh");
+        bundleStart.setText("Start Bundle");
+        bundleStop.setText("Stop Bundle");
+
+        updateButtons();
+
+        bundlesGrid.setSelectionModel(selectionModel);
+        selectionModel.addSelectionChangeHandler(new SelectionChangeEvent.Handler() {
+
+            @Override
+            public void onSelectionChange(SelectionChangeEvent event) {
+                updateButtons();
+            }
+        });
+        bundlesRefresh.addClickHandler(new ClickHandler() {
+
+            @Override
+            public void onClick(ClickEvent event) {
+                loadBundlesData();
+
+            }
+        });
+        bundleStart.addClickHandler(new ClickHandler() {
+
+            @Override
+            public void onClick(ClickEvent event) {
+                startSelectedBundle();
+
+            }
+        });
+        bundleStop.addClickHandler(new ClickHandler() {
+
+            @Override
+            public void onClick(ClickEvent event) {
+                stopSelectedBundle();
+
+            }
+        });
         // loadBundlesData();
+    }
+
+    private void updateButtons() {
+        GwtGroupedNVPair selected = selectionModel.getSelectedObject();
+
+        bundleStart.setEnabled(false);
+        bundleStop.setEnabled(false);
+
+        String status;
+
+        if (selected == null || (status = selected.getStatus()) == null) {
+            return;
+        }
+
+        boolean isActive = "bndActive".equals(status);
+
+        bundleStart.setEnabled(!isActive);
+        bundleStop.setEnabled(isActive);
+    }
+
+    private void startSelectedBundle() {
+        EntryClassUi.showWaitModal();
+
+        securityTokenService.generateSecurityToken(new AsyncCallback<GwtXSRFToken>() {
+
+            @Override
+            public void onSuccess(GwtXSRFToken token) {
+                deviceService.startBundle(token, selectionModel.getSelectedObject().getId(), new AsyncCallback<Void>() {
+
+                    @Override
+                    public void onFailure(Throwable caught) {
+                        EntryClassUi.hideWaitModal();
+                        FailureHandler.handle(caught);
+
+                    }
+
+                    @Override
+                    public void onSuccess(Void result) {
+                        EntryClassUi.hideWaitModal();
+                    }
+                });
+
+            }
+
+            @Override
+            public void onFailure(Throwable caught) {
+                EntryClassUi.hideWaitModal();
+                FailureHandler.handle(caught);
+            }
+        });
+    }
+
+    private void stopSelectedBundle() {
+        EntryClassUi.showWaitModal();
+        securityTokenService.generateSecurityToken(new AsyncCallback<GwtXSRFToken>() {
+
+            @Override
+            public void onFailure(Throwable caught) {
+                EntryClassUi.hideWaitModal();
+                FailureHandler.handle(caught);
+            }
+
+            @Override
+            public void onSuccess(GwtXSRFToken token) {
+                deviceService.stopBundle(token, selectionModel.getSelectedObject().getId(), new AsyncCallback<Void>() {
+
+                    @Override
+                    public void onFailure(Throwable caught) {
+                        EntryClassUi.hideWaitModal();
+                        FailureHandler.handle(caught);
+
+                    }
+
+                    @Override
+                    public void onSuccess(Void result) {
+                        EntryClassUi.hideWaitModal();
+                    }
+                });
+            }
+        });
     }
 
     private void loadBundlesTable(CellTable<GwtGroupedNVPair> bundlesGrid2,
 
-    ListDataProvider<GwtGroupedNVPair> dataProvider) {
+            ListDataProvider<GwtGroupedNVPair> dataProvider) {
 
         TextColumn<GwtGroupedNVPair> col1 = new TextColumn<GwtGroupedNVPair>() {
 

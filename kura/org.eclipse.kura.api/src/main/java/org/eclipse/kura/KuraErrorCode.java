9d28c9f9cf5e07dc9d1e281d01af6b41efee6762
Amit Kumar Mondal <admin@amitinside.com>
2016-11-27 03:30:06 +0100

    Added Proper Javadoc to INTERNAL_ERROR for discouraged access
    Signed-off-by: Amit Kumar Mondal <admin@amitinside.com>

--- kura/org.eclipse.kura.api/src/main/java/org/eclipse/kura/KuraErrorCode.java
+++ kura/org.eclipse.kura.api/src/main/java/org/eclipse/kura/KuraErrorCode.java
@@ -12,47 +12,53 @@
 package org.eclipse.kura;
 
 /**
- * KuraErrorCode holds the enumeration of valid error codes for the exception message. For each defined enum value, a corresponding message should be defined in the properties bundle named:
+ * KuraErrorCode holds the enumeration of valid error codes for the exception message. For each defined enum value, a
+ * corresponding message should be defined in the properties bundle named:
  * KuraExceptionMessagesBundle.properties.
  * 
  */
 public enum KuraErrorCode {
-	CONFIGURATION_ERROR,
-	CONFIGURATION_UPDATE,
-	CONFIGURATION_ROLLBACK,
-	CONFIGURATION_ATTRIBUTE_UNDEFINED,
-	CONFIGURATION_ATTRIBUTE_INVALID,
-	CONFIGURATION_REQUIRED_ATTRIBUTE_MISSING,
-	CONFIGURATION_SNAPSHOT_NOT_FOUND,
-	CONFIGURATION_SNAPSHOT_TAKING,
-	CONFIGURATION_SNAPSHOT_LISTING,
-	CONFIGURATION_SNAPSHOT_LOADING,
-	INTERNAL_ERROR,
-	SERIAL_PORT_INVALID_CONFIGURATION,
-	SERIAL_PORT_NOT_EXISTING,
-	PORT_IN_USE,
-	PARTIAL_SUCCESS,
-	SECURITY_EXCEPTION,
-	NOT_CONNECTED,
-	TIMED_OUT,
-	CONNECTION_FAILED,
-	TOO_MANY_INFLIGHT_MESSAGES,
-	STORE_ERROR,
-	ENCODE_ERROR,
-	DECODER_ERROR,
-	INVALID_METRIC_EXCEPTION,
-	INVALID_MESSAGE_EXCEPTION,
-	OPERATION_NOT_SUPPORTED,
-	UNAVAILABLE_DEVICE,
-	CLOSED_DEVICE,
-	GPIO_EXCEPTION,
-	/**
-	 * Error executing an OS command.
-	 * @since {@link org.eclipse.kura} 1.1.0
-	 */
-	OS_COMMAND_ERROR,
-	/**
-	 * @since {@link org.eclipse.kura} 1.2.0
-	 */
-	INVALID_PARAMETER
+    CONFIGURATION_ERROR,
+    CONFIGURATION_UPDATE,
+    CONFIGURATION_ROLLBACK,
+    CONFIGURATION_ATTRIBUTE_UNDEFINED,
+    CONFIGURATION_ATTRIBUTE_INVALID,
+    CONFIGURATION_REQUIRED_ATTRIBUTE_MISSING,
+    CONFIGURATION_SNAPSHOT_NOT_FOUND,
+    CONFIGURATION_SNAPSHOT_TAKING,
+    CONFIGURATION_SNAPSHOT_LISTING,
+    CONFIGURATION_SNAPSHOT_LOADING,
+    /**
+     * @deprecated
+     * @noreference Indicates that clients must not reference this type
+     */
+    INTERNAL_ERROR,
+    SERIAL_PORT_INVALID_CONFIGURATION,
+    SERIAL_PORT_NOT_EXISTING,
+    PORT_IN_USE,
+    PARTIAL_SUCCESS,
+    SECURITY_EXCEPTION,
+    NOT_CONNECTED,
+    TIMED_OUT,
+    CONNECTION_FAILED,
+    TOO_MANY_INFLIGHT_MESSAGES,
+    STORE_ERROR,
+    ENCODE_ERROR,
+    DECODER_ERROR,
+    INVALID_METRIC_EXCEPTION,
+    INVALID_MESSAGE_EXCEPTION,
+    OPERATION_NOT_SUPPORTED,
+    UNAVAILABLE_DEVICE,
+    CLOSED_DEVICE,
+    GPIO_EXCEPTION,
+    /**
+     * Error executing an OS command.
+     * 
+     * @since {@link org.eclipse.kura} 1.1.0
+     */
+    OS_COMMAND_ERROR,
+    /**
+     * @since {@link org.eclipse.kura} 1.2.0
+     */
+    INVALID_PARAMETER
 }

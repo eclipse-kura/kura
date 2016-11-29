0c3a8ee475486a6d6e14d90d4ac0fdab61506a2e
Jens Reimann <jreimann@redhat.com>
2016-11-29 15:44:35 +0100

    execute javadoc generation in the "site" phase
    This change is necessary due to the fact that the current javadoc
    configuration is run in the aggregator project and is configured to
    fork the generate-source phase, which then triggers the build, which may
    run into the issue that depencies are missing, since they haven't been
    compiled yet.
    
    The simple solution is to follow the standard maven javadoc generation
    and keep the javadoc generation with the "site" phase, so that the
    generation will take place **after** the main build is done.
    
    This requires the maven command to the called with "site" in addition
    if the javadoc should be generated.
    
    Signed-off-by: Jens Reimann <jreimann@redhat.com>

--- build-all.sh
+++ build-all.sh
@@ -15,7 +15,7 @@
 [ -z "$RUN_TESTS" ] && MAVEN_PROPS="-Dmaven.test.skip=true"
 
 mvn "$@" -f target-platform/pom.xml clean install $MAVEN_PROPS &&
-mvn "$@" -f kura/manifest_pom.xml clean install  $MAVEN_PROPS &&
+mvn "$@" -f kura/manifest_pom.xml clean install $MAVEN_PROPS &&
 mvn "$@" -f kura/pom_pom.xml clean install $MAVEN_PROPS -Pweb &&
 mvn "$@" -f karaf/pom.xml clean install $MAVEN_PROPS
 

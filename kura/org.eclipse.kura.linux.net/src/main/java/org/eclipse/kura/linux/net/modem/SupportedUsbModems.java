eclipse-kura/kura

commit 795cd8cb225d4446caecb24d96b9c380d7538cd1

Replace a second call the "linux utils" with the local exec command
This change does replace another call to the "linux utils" call in
"isAttached" with the new exec method.

Signed-off-by: Jens Reimann <jreimann@redhat.com>

kura/org.eclipse.kura.linux.net/src/main/java/org/eclipse/kura/linux/net/modem/SupportedUsbModems.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011, 2016 Eurotech and/or its affiliates
+ * Copyright (c) 2011, 2016 Eurotech and others
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -8,14 +8,13 @@
  *
  * Contributors:
  *     Eurotech
+ *     Red Hat Inc - fix issue #640
  *******************************************************************************/
 package org.eclipse.kura.linux.net.modem;
 
-import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -25,9 +24,6 @@ import org.apache.commons.exec.ExecuteException;
 import org.apache.commons.exec.PumpStreamHandler;
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.io.output.NullOutputStream;
-import org.eclipse.kura.core.linux.util.LinuxProcessUtil;
-import org.eclipse.kura.core.linux.util.ProcessStats;
-import org.eclipse.kura.core.util.ProcessUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -71,8 +67,8 @@ public class SupportedUsbModems {
         List<LsusbEntry> lsusbEntries = null;
         try {
             lsusbEntries = getLsusbInfo();
-        } catch (Exception e1) {
-            s_logger.error("failed to obtain lsusb information - {}", e1);
+        } catch (Exception e) {
+            s_logger.error("failed to obtain lsusb information", e);
         }
         for (SupportedUsbModemInfo modem : SupportedUsbModemInfo.values()) {
             try {
@@ -110,38 +106,18 @@ public class SupportedUsbModems {
     }
 
     public static boolean isAttached(String vendor, String product) throws Exception {
-        boolean attached = false;
-        String lsusbCmd = formLsusbCommand(vendor, product); // e.g. lsusb -d 1bc7:1010
-        BufferedReader br = null;
-        InputStreamReader isr = null;
-        ProcessStats processStats = null;
-        try {
-            processStats = LinuxProcessUtil.startWithStats(lsusbCmd);
-            isr = new InputStreamReader(processStats.getInputStream());
-            br = new BufferedReader(isr);
-            String line;
-            while ((line = br.readLine()) != null) {
-                LsusbEntry lsusbEntry = getLsusbEntry(line);
-                if (lsusbEntry != null && vendor != null && product != null && vendor.equals(lsusbEntry.m_vendor)
-                        && product.equals(lsusbEntry.m_product)) {
-                    s_logger.info("The '{}' command detected {}", lsusbCmd, lsusbEntry);
-                    attached = true;
-                    break;
-                }
-            }
-        } finally {
-            if (br != null) {
-                br.close();
-            }
-            if (isr != null) {
-                isr.close();
-            }
-            if (processStats != null) {
-                ProcessUtil.destroy(processStats.getProcess());
+        final String lsusbCmd = formLsusbCommand(vendor, product); // e.g. lsusb -d 1bc7:1010
+        final List<String> lines = execute(lsusbCmd);
+
+        for (final String line : lines) {
+            final LsusbEntry lsusbEntry = getLsusbEntry(line);
+            if (lsusbEntry != null && vendor != null && product != null && vendor.equals(lsusbEntry.m_vendor)
+                    && product.equals(lsusbEntry.m_product)) {
+                s_logger.info("The '{}' command detected {}", lsusbCmd, lsusbEntry);
+                return true;
             }
         }
-
-        return attached;
+        return false;
     }
 
     private static boolean isAttached(String vendor, String product, List<LsusbEntry> lsusbEntries) throws Exception {

commit f187f65fe23a30ce1c6ab5db18503a8063ec9d14

Solved modem reset failure.
The regression was caused by commit 795cd8cb225d4446caecb24d96b9c380d7538cd1
that determined the impossibility to have a proper successful modem reset.

Signed-off-by: MMaiero <matteo.maiero@eurotech.com>

kura/org.eclipse.kura.linux.net/src/main/java/org/eclipse/kura/linux/net/modem/SupportedUsbModems.java
@@ -107,15 +107,19 @@ public class SupportedUsbModems {
 
     public static boolean isAttached(String vendor, String product) throws Exception {
         final String lsusbCmd = formLsusbCommand(vendor, product); // e.g. lsusb -d 1bc7:1010
-        final List<String> lines = execute(lsusbCmd);
+        try {
+            final List<String> lines = execute(lsusbCmd);
 
-        for (final String line : lines) {
-            final LsusbEntry lsusbEntry = getLsusbEntry(line);
-            if (lsusbEntry != null && vendor != null && product != null && vendor.equals(lsusbEntry.m_vendor)
-                    && product.equals(lsusbEntry.m_product)) {
-                s_logger.info("The '{}' command detected {}", lsusbCmd, lsusbEntry);
-                return true;
+            for (final String line : lines) {
+                final LsusbEntry lsusbEntry = getLsusbEntry(line);
+                if (lsusbEntry != null && vendor != null && product != null && vendor.equals(lsusbEntry.m_vendor)
+                        && product.equals(lsusbEntry.m_product)) {
+                    s_logger.info("The '{}' command detected {}", lsusbCmd, lsusbEntry);
+                    return true;
+                }
             }
+        } catch (ExecuteException e) {
+            s_logger.debug("Lsusb command returned with ExecuteException: ", e);
         }
         return false;
     }
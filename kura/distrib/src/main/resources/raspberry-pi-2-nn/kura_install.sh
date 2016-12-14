13703bb3a44440f7904735587f051ea8ccfa95ba
MMaiero <matteo.maiero@eurotech.com>
2016-12-14 11:36:32 +0100

    Included missing .data folder creation instructions.
    The fix is applied to kura_install.sh scripts for all nn profiles.
    
    Signed-off-by: MMaiero <matteo.maiero@eurotech.com>

--- kura/distrib/src/main/resources/raspberry-pi-2-nn/kura_install.sh
+++ kura/distrib/src/main/resources/raspberry-pi-2-nn/kura_install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# Copyright (c) 2011, 2014 Eurotech and/or its affiliates
+# Copyright (c) 2011, 2016 Eurotech and/or its affiliates
 #
 #  All rights reserved. This program and the accompanying materials
 #  are made available under the terms of the Eclipse Public License v1.0
@@ -23,6 +23,11 @@ chmod +x ${INSTALL_DIR}/kura/bin/*.sh
 
 #set up runlevels to start/stop Kura by default
 update-rc.d kura defaults
+
+# setup snapshot_0 recovery folder
+if [ ! -d ${INSTALL_DIR}/kura/.data ]; then
+    mkdir ${INSTALL_DIR}/kura/.data
+fi
 
 #set up logrotate - no need to restart as it is a cronjob
 cp ${INSTALL_DIR}/kura/install/logrotate.conf /etc/logrotate.conf

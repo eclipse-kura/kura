cdedf00dd44b581af7bfcaf4b9bff34cba95cc36
Aurelien Pupier <apupier@redhat.com>
2017-02-28 17:52:39 +0100

    Provide better Separation of Concerns
    - extract classes to have a more readable Camel route
    
    Signed-off-by: Aurelien Pupier <apupier@redhat.com>

---
+++ kura/examples/org.eclipse.kura.example.camel.aggregation/src/main/java/org/eclipse/kura/example/camel/aggregation/AverageAggregationStrategy.java
@@ -0,0 +1,29 @@
+/*******************************************************************************
+ * Copyright (c) 2017 Red Hat Inc and others
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Red Hat Inc - Initial API and implementation
+ *******************************************************************************/
+package org.eclipse.kura.example.camel.aggregation;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.processor.aggregate.AggregationStrategy;
+
+final class AverageAggregationStrategy implements AggregationStrategy {
+    @Override
+    public Exchange aggregate(Exchange oldExchange, Exchange newExchange) {
+        if (oldExchange == null) {
+            return newExchange;
+        } else {
+            double incomingValue = newExchange.getIn().getBody(double.class);
+            double existingValue = oldExchange.getIn().getBody(double.class);
+            newExchange.getIn().setBody((incomingValue + existingValue) / 2d);
+            return newExchange;
+        }
+    }
+}

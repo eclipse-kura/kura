eclipse-kura/kura

commit 681c285fe58de8bb41365fb3244cc4d43d910113

Removed duplicates from Hotspot list
Signed-off-by: pierantoniomerlino <pierantonio.merlino@eurotech.com>

kura/org.eclipse.kura.api/src/main/java/org/eclipse/kura/net/wifi/WifiHotspotInfo.java
@@ -11,84 +11,92 @@
  *******************************************************************************/
 package org.eclipse.kura.net.wifi;
 
-import java.util.EnumSet;
+import java.util.Set;
 
 public class WifiHotspotInfo {
 
-    private final String m_ssid;
-    private final String m_macAddress;
-    private final int m_signalLevel;
-    private final int m_channel;
-    private final int m_frequency;
-    private final WifiSecurity m_security;
-    private EnumSet<WifiSecurity> m_pairCiphers;
-    private EnumSet<WifiSecurity> m_groupCiphers;
+    private final String ssid;
+    private final String macAddress;
+    private final int signalLevel;
+    private final int channel;
+    private final int frequency;
+    private final WifiSecurity security;
+    private Set<WifiSecurity> pairCiphers;
+    private Set<WifiSecurity> groupCiphers;
 
     public WifiHotspotInfo(String ssid, String macAddress, int signalLevel, int channel, int frequency,
             WifiSecurity security) {
         super();
-        this.m_ssid = ssid;
-        this.m_macAddress = macAddress;
-        this.m_signalLevel = signalLevel;
-        this.m_channel = channel;
-        this.m_frequency = frequency;
-        this.m_security = security;
+        this.ssid = ssid;
+        this.macAddress = macAddress;
+        this.signalLevel = signalLevel;
+        this.channel = channel;
+        this.frequency = frequency;
+        this.security = security;
     }
 
     public WifiHotspotInfo(String ssid, String macAddress, int signalLevel, int channel, int frequency,
-            WifiSecurity security, EnumSet<WifiSecurity> pairCiphers, EnumSet<WifiSecurity> groupCiphers) {
+            WifiSecurity security, Set<WifiSecurity> pairCiphers, Set<WifiSecurity> groupCiphers) {
         this(ssid, macAddress, signalLevel, channel, frequency, security);
-        this.m_pairCiphers = pairCiphers;
-        this.m_groupCiphers = groupCiphers;
+        this.pairCiphers = pairCiphers;
+        this.groupCiphers = groupCiphers;
     }
 
     public String getSsid() {
-        return this.m_ssid;
+        return this.ssid;
     }
 
     public String getMacAddress() {
-        return this.m_macAddress;
+        return this.macAddress;
     }
 
     public int getSignalLevel() {
-        return this.m_signalLevel;
+        return this.signalLevel;
     }
 
     public int getChannel() {
-        return this.m_channel;
+        return this.channel;
     }
 
     public int getFrequency() {
-        return this.m_frequency;
+        return this.frequency;
     }
 
     public WifiSecurity getSecurity() {
-        return this.m_security;
+        return this.security;
     }
 
-    public EnumSet<WifiSecurity> getPairCiphers() {
-        return this.m_pairCiphers;
+    public Set<WifiSecurity> getPairCiphers() {
+        return this.pairCiphers;
     }
 
-    public EnumSet<WifiSecurity> getGroupCiphers() {
-        return this.m_groupCiphers;
+    public Set<WifiSecurity> getGroupCiphers() {
+        return this.groupCiphers;
+    }
+
+    public void setPairCiphers(Set<WifiSecurity> pairCiphers) {
+        this.pairCiphers = pairCiphers;
+    }
+
+    public void setGroupCiphers(Set<WifiSecurity> groupCiphers) {
+        this.groupCiphers = groupCiphers;
     }
 
     @Override
     public String toString() {
 
-        StringBuffer sb = new StringBuffer();
-        sb.append(this.m_macAddress);
+        StringBuilder sb = new StringBuilder();
+        sb.append(this.macAddress);
         sb.append(" :: ");
-        sb.append(this.m_ssid);
+        sb.append(this.ssid);
         sb.append(" :: ");
-        sb.append(this.m_signalLevel);
+        sb.append(this.signalLevel);
         sb.append(" :: ");
-        sb.append(this.m_channel);
+        sb.append(this.channel);
         sb.append(" :: ");
-        sb.append(this.m_frequency);
+        sb.append(this.frequency);
         sb.append(" :: ");
-        sb.append(this.m_security);
+        sb.append(this.security);
 
         return sb.toString();
     }

kura/org.eclipse.kura.api/META-INF/MANIFEST.MF
@@ -29,13 +29,13 @@ Export-Package: org.eclipse.kura;version="1.3.0",
  org.eclipse.kura.gpio;version="1.0.1",
  org.eclipse.kura.linux.udev;version="1.0.0",
  org.eclipse.kura.message;version="1.1.0",
- org.eclipse.kura.net;version="1.2.1",
+ org.eclipse.kura.net;version="1.3.0",
  org.eclipse.kura.net.dhcp;version="1.0.1",
  org.eclipse.kura.net.dns;version="1.0.0",
  org.eclipse.kura.net.firewall;version="1.0.0",
  org.eclipse.kura.net.modem;version="1.1.1",
  org.eclipse.kura.net.route;version="1.0.1",
- org.eclipse.kura.net.wifi;version="1.2.0",
+ org.eclipse.kura.net.wifi;version="1.3.0",
  org.eclipse.kura.position;version="1.1.1",
  org.eclipse.kura.security;version="1.0.1",
  org.eclipse.kura.ssl;version="1.2.1",

kura/org.eclipse.kura.api/src/main/java/org/eclipse/kura/net/NetworkAdminService.java
@@ -199,7 +199,18 @@ public interface NetworkAdminService {
      * @return map of SSIDs with respective hotspot information.
      * @throws KuraException
      */
+    @Deprecated
     public Map<String, WifiHotspotInfo> getWifiHotspots(String ifaceName) throws KuraException;
+
+    /**
+     * Obtains information for WiFi hotspots in range.
+     *
+     * @param ifaceName
+     *            - name of WiFi interface
+     * @return list of hotspot information.
+     * @throws KuraException
+     */
+    public List<WifiHotspotInfo> getWifiHotspotList(String ifaceName) throws KuraException;
 
     /**
      * Verifies WiFi credentials by trying to establish connection with access point.

kura/org.eclipse.kura.net.admin/META-INF/MANIFEST.MF
@@ -4,7 +4,7 @@ Bundle-Name: org.eclipse.kura.net.admin
 Bundle-SymbolicName: org.eclipse.kura.net.admin;singleton:=true
 Bundle-Version: 1.0.13.qualifier
 Bundle-Vendor: Eclipse Kura
-Bundle-RequiredExecutionEnvironment: JavaSE-1.6
+Bundle-RequiredExecutionEnvironment: JavaSE-1.8
 Import-Package: javax.microedition.io,
  org.apache.commons.io;version="2.4.0",
  org.eclipse.kura;version="[1.0,2.0)",

kura/org.eclipse.kura.web2/src/main/java/org/eclipse/kura/web/client/ui/Network/TabWirelessUi.java
@@ -1472,6 +1472,7 @@ public class TabWirelessUi extends Composite implements NetworkTab {
                 public void onSuccess(GwtXSRFToken token) {
                     TabWirelessUi.this.gwtNetworkService.findWifiHotspots(token,
                             TabWirelessUi.this.selectedNetIfConfig.getName(),
+                            TabWirelessUi.this.selectedNetIfConfig.getAccessPointWifiConfig().getWirelessSsid(),
                             new AsyncCallback<List<GwtWifiHotspotEntry>>() {
 
                                 @Override

kura/org.eclipse.kura.web2/src/main/java/org/eclipse/kura/web/server/GwtNetworkServiceImpl.java
@@ -19,7 +19,6 @@ import java.util.Collection;
 import java.util.EnumSet;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
 
 import org.eclipse.kura.KuraErrorCode;
 import org.eclipse.kura.KuraException;
@@ -131,7 +130,7 @@ public class GwtNetworkServiceImpl extends OsgiRemoteServiceServlet implements G
     private List<GwtNetInterfaceConfig> privateFindNetInterfaceConfigurations() throws GwtKuraException {
         s_logger.debug("Starting");
 
-        List<GwtNetInterfaceConfig> gwtNetConfigs = new ArrayList<GwtNetInterfaceConfig>();
+        List<GwtNetInterfaceConfig> gwtNetConfigs = new ArrayList<>();
         NetworkAdminService nas = null;
         try {
             nas = ServiceLocator.getInstance().getService(NetworkAdminService.class);
@@ -173,7 +172,7 @@ public class GwtNetworkServiceImpl extends OsgiRemoteServiceServlet implements G
                             .setManufacturer(((ModemInterface) netIfConfig).getManufacturer());
                     ((GwtModemInterfaceConfig) gwtNetConfig).setModel(((ModemInterface) netIfConfig).getModel());
 
-                    List<String> technologyList = new ArrayList<String>();
+                    List<String> technologyList = new ArrayList<>();
                     List<ModemTechnologyType> technologyTypes = ((ModemInterface) netIfConfig).getTechnologyTypes();
                     if (technologyTypes != null) {
                         for (ModemTechnologyType techType : technologyTypes) {
@@ -446,7 +445,7 @@ public class GwtNetworkServiceImpl extends OsgiRemoteServiceServlet implements G
                                     // channel
                                     int[] channels = wifiConfig.getChannels();
                                     if (channels != null) {
-                                        ArrayList<Integer> alChannels = new ArrayList<Integer>();
+                                        ArrayList<Integer> alChannels = new ArrayList<>();
                                         for (int channel : channels) {
                                             alChannels.add(new Integer(channel));
                                         }
@@ -747,7 +746,7 @@ public class GwtNetworkServiceImpl extends OsgiRemoteServiceServlet implements G
             }
 
             // Set up configs
-            List<NetConfig> netConfigs = new ArrayList<NetConfig>();
+            List<NetConfig> netConfigs = new ArrayList<>();
 
             // Initialize NetConfigIP4 object
             NetConfigIP4 netConfig4 = new NetConfigIP4(netInterfaceStatus, autoConnect);
@@ -783,7 +782,7 @@ public class GwtNetworkServiceImpl extends OsgiRemoteServiceServlet implements G
                     String[] winServersString = config.getSearchDomains().split(regexp);
                     if (winServersString != null && winServersString.length > 0) {
                         IP4Address winServer;
-                        List<IP4Address> dnsServers = new ArrayList<IP4Address>();
+                        List<IP4Address> dnsServers = new ArrayList<>();
                         for (String winsEntry : winServersString) {
                             if (!winsEntry.trim().isEmpty()) {
                                 s_logger.debug("setting WINs: {}", winsEntry);
@@ -798,7 +797,7 @@ public class GwtNetworkServiceImpl extends OsgiRemoteServiceServlet implements G
                 String[] dnsServersString = config.getDnsServers().split(regexp);
                 if (dnsServersString != null && dnsServersString.length > 0) {
                     IP4Address dnsServer;
-                    List<IP4Address> dnsServers = new ArrayList<IP4Address>();
+                    List<IP4Address> dnsServers = new ArrayList<>();
                     for (String dnsEntry : dnsServersString) {
                         if (!dnsEntry.trim().isEmpty()) {
                             s_logger.debug("setting DNS: {}", dnsEntry);
@@ -958,7 +957,7 @@ public class GwtNetworkServiceImpl extends OsgiRemoteServiceServlet implements G
             throws GwtKuraException {
         checkXSRFToken(xsrfToken);
         NetworkAdminService nas = ServiceLocator.getInstance().getService(NetworkAdminService.class);
-        List<GwtFirewallOpenPortEntry> gwtOpenPortEntries = new ArrayList<GwtFirewallOpenPortEntry>();
+        List<GwtFirewallOpenPortEntry> gwtOpenPortEntries = new ArrayList<>();
 
         try {
             List<NetConfig> firewallConfigs = nas.getFirewallConfiguration();
@@ -989,7 +988,7 @@ public class GwtNetworkServiceImpl extends OsgiRemoteServiceServlet implements G
                 }
             }
 
-            return new ArrayList<GwtFirewallOpenPortEntry>(gwtOpenPortEntries);
+            return new ArrayList<>(gwtOpenPortEntries);
 
         } catch (KuraException e) {
             throw new GwtKuraException(GwtKuraErrorCode.INTERNAL_ERROR, e);
@@ -997,26 +996,24 @@ public class GwtNetworkServiceImpl extends OsgiRemoteServiceServlet implements G
     }
 
     @Override
-    public ArrayList<GwtWifiHotspotEntry> findWifiHotspots(GwtXSRFToken xsrfToken, String interfaceName)
-            throws GwtKuraException {
+    public ArrayList<GwtWifiHotspotEntry> findWifiHotspots(GwtXSRFToken xsrfToken, String interfaceName,
+            String wirelessSsid) throws GwtKuraException {
 
         checkXSRFToken(xsrfToken);
         NetworkAdminService nas = ServiceLocator.getInstance().getService(NetworkAdminService.class);
         SystemService systemService = ServiceLocator.getInstance().getService(SystemService.class);
-        List<GwtWifiHotspotEntry> gwtWifiHotspotsEntries = new ArrayList<GwtWifiHotspotEntry>();
+        List<GwtWifiHotspotEntry> gwtWifiHotspotsEntries = new ArrayList<>();
 
         try {
-            Map<String, WifiHotspotInfo> wifiHotspotInfoMap = nas.getWifiHotspots(interfaceName);
-            if (wifiHotspotInfoMap != null && !wifiHotspotInfoMap.isEmpty()) {
-                Collection<WifiHotspotInfo> wifiHotspotInfoCollection = wifiHotspotInfoMap.values();
-                Iterator<WifiHotspotInfo> it = wifiHotspotInfoCollection.iterator();
-                while (it.hasNext()) {
-                    WifiHotspotInfo wifiHotspotInfo = it.next();
+            List<WifiHotspotInfo> wifiHotspotInfoList = nas.getWifiHotspotList(interfaceName);
+            if (wifiHotspotInfoList != null) {
+                for (WifiHotspotInfo wifiHotspotInfo : wifiHotspotInfoList) {
                     String ssid = GwtSafeHtmlUtils.htmlEscape(wifiHotspotInfo.getSsid());
                     // if(!ssid.matches("[0-9A-Za-z/.@*#:\\ \\_\\-]+")){
                     // ssid= null;
                     // }
-                    if (wifiHotspotInfo.getChannel() <= systemService.getKuraWifiTopChannel() && ssid != null) {
+                    if (wifiHotspotInfo.getChannel() <= systemService.getKuraWifiTopChannel() && ssid != null
+                            && !ssid.equals(wirelessSsid)) {
                         GwtWifiHotspotEntry gwtWifiHotspotEntry = new GwtWifiHotspotEntry();
                         gwtWifiHotspotEntry.setMacAddress(wifiHotspotInfo.getMacAddress());
                         gwtWifiHotspotEntry.setSSID(ssid);
@@ -1039,7 +1036,7 @@ public class GwtNetworkServiceImpl extends OsgiRemoteServiceServlet implements G
 
                         GwtWifiCiphers gwtPairCiphers = null;
                         GwtWifiCiphers gwtGroupCiphers = null;
-                        EnumSet<WifiSecurity> pairCiphers = wifiHotspotInfo.getPairCiphers();
+                        EnumSet<WifiSecurity> pairCiphers = (EnumSet<WifiSecurity>) wifiHotspotInfo.getPairCiphers();
                         Iterator<WifiSecurity> itPairCiphers = pairCiphers.iterator();
                         while (itPairCiphers.hasNext()) {
                             WifiSecurity cipher = itPairCiphers.next();
@@ -1060,7 +1057,7 @@ public class GwtNetworkServiceImpl extends OsgiRemoteServiceServlet implements G
                             }
                         }
 
-                        EnumSet<WifiSecurity> groupCiphers = wifiHotspotInfo.getGroupCiphers();
+                        EnumSet<WifiSecurity> groupCiphers = (EnumSet<WifiSecurity>) wifiHotspotInfo.getGroupCiphers();
                         Iterator<WifiSecurity> itGroupCiphers = groupCiphers.iterator();
                         while (itGroupCiphers.hasNext()) {
                             WifiSecurity cipher = itGroupCiphers.next();
@@ -1097,7 +1094,7 @@ public class GwtNetworkServiceImpl extends OsgiRemoteServiceServlet implements G
             KuraExceptionHandler.handle(t);
         }
 
-        return new ArrayList<GwtWifiHotspotEntry>(gwtWifiHotspotsEntries);
+        return new ArrayList<>(gwtWifiHotspotsEntries);
     }
 
     @Override
@@ -1115,7 +1112,7 @@ public class GwtNetworkServiceImpl extends OsgiRemoteServiceServlet implements G
             throws GwtKuraException {
         checkXSRFToken(xsrfToken);
         NetworkAdminService nas = ServiceLocator.getInstance().getService(NetworkAdminService.class);
-        List<GwtFirewallPortForwardEntry> gwtPortForwardEntries = new ArrayList<GwtFirewallPortForwardEntry>();
+        List<GwtFirewallPortForwardEntry> gwtPortForwardEntries = new ArrayList<>();
 
         try {
             List<NetConfig> firewallConfigs = nas.getFirewallConfiguration();
@@ -1142,7 +1139,7 @@ public class GwtNetworkServiceImpl extends OsgiRemoteServiceServlet implements G
                 }
             }
 
-            return new ArrayList<GwtFirewallPortForwardEntry>(gwtPortForwardEntries);
+            return new ArrayList<>(gwtPortForwardEntries);
 
         } catch (KuraException e) {
             s_logger.warn("Failed", e);
@@ -1155,7 +1152,7 @@ public class GwtNetworkServiceImpl extends OsgiRemoteServiceServlet implements G
 
         checkXSRFToken(xsrfToken);
         NetworkAdminService nas = ServiceLocator.getInstance().getService(NetworkAdminService.class);
-        List<GwtFirewallNatEntry> gwtNatEntries = new ArrayList<GwtFirewallNatEntry>();
+        List<GwtFirewallNatEntry> gwtNatEntries = new ArrayList<>();
 
         try {
             List<NetConfig> firewallConfigs = nas.getFirewallConfiguration();
@@ -1176,7 +1173,7 @@ public class GwtNetworkServiceImpl extends OsgiRemoteServiceServlet implements G
                 }
             }
 
-            return new ArrayList<GwtFirewallNatEntry>(gwtNatEntries);
+            return new ArrayList<>(gwtNatEntries);
 
         } catch (KuraException e) {
             s_logger.warn("Failed", e);
@@ -1200,7 +1197,7 @@ public class GwtNetworkServiceImpl extends OsgiRemoteServiceServlet implements G
                 || routerMode.equals(GwtNetRouterMode.netRouterDchpNat.name())
                 || routerMode.equals(GwtNetRouterMode.netRouterNat.name())) {
             try {
-                List<NetConfig> netConfigs = new ArrayList<NetConfig>();
+                List<NetConfig> netConfigs = new ArrayList<>();
 
                 if (routerMode.equals(GwtNetRouterMode.netRouterDchp.name())
                         || routerMode.equals(GwtNetRouterMode.netRouterDchpNat.name())) {
@@ -1217,7 +1214,7 @@ public class GwtNetworkServiceImpl extends OsgiRemoteServiceServlet implements G
                     short prefix = NetworkUtil.getNetmaskShortForm(subnetMask.getHostAddress());
 
                     // Use our IP as the DNS server and we'll use named to proxy DNS queries
-                    List<IP4Address> dnsServers = new ArrayList<IP4Address>();
+                    List<IP4Address> dnsServers = new ArrayList<>();
                     dnsServers.add((IP4Address) IPAddress.parseHostAddress(config.getIpAddress()));
 
                     s_logger.debug("DhcpServerConfigIP4 - start: {}, end: {}, prefix: {}, subnet: {}, subnetMask: {}",
@@ -1265,7 +1262,7 @@ public class GwtNetworkServiceImpl extends OsgiRemoteServiceServlet implements G
             throws GwtKuraException {
         checkXSRFToken(xsrfToken);
         NetworkAdminService nas = ServiceLocator.getInstance().getService(NetworkAdminService.class);
-        List<FirewallOpenPortConfigIP<? extends IPAddress>> firewallOpenPortConfigIPs = new ArrayList<FirewallOpenPortConfigIP<? extends IPAddress>>();
+        List<FirewallOpenPortConfigIP<? extends IPAddress>> firewallOpenPortConfigIPs = new ArrayList<>();
         s_logger.debug("updating open ports");
 
         try {
@@ -1290,7 +1287,7 @@ public class GwtNetworkServiceImpl extends OsgiRemoteServiceServlet implements G
                 firewallOpenPortConfigIP
                         .setProtocol(NetProtocol.valueOf(GwtSafeHtmlUtils.htmlEscape(entry.getProtocol())));
                 if (network != null && prefix != null) {
-                    firewallOpenPortConfigIP.setPermittedNetwork(new NetworkPair<IP4Address>(
+                    firewallOpenPortConfigIP.setPermittedNetwork(new NetworkPair<>(
                             (IP4Address) IPAddress.parseHostAddress(network), Short.parseShort(prefix)));
                 }
                 firewallOpenPortConfigIP
@@ -1324,7 +1321,7 @@ public class GwtNetworkServiceImpl extends OsgiRemoteServiceServlet implements G
         s_logger.debug("updateDeviceFirewallPortForwards() :: updating port forward entries");
         checkXSRFToken(xsrfToken);
         NetworkAdminService nas = ServiceLocator.getInstance().getService(NetworkAdminService.class);
-        List<FirewallPortForwardConfigIP<? extends IPAddress>> firewallPortForwardConfigIPs = new ArrayList<FirewallPortForwardConfigIP<? extends IPAddress>>();
+        List<FirewallPortForwardConfigIP<? extends IPAddress>> firewallPortForwardConfigIPs = new ArrayList<>();
 
         try {
             for (GwtFirewallPortForwardEntry entry : entries) {
@@ -1351,7 +1348,7 @@ public class GwtNetworkServiceImpl extends OsgiRemoteServiceServlet implements G
                 boolean masquerade = entry.getMasquerade().equals("yes") ? true : false;
                 firewallPortForwardConfigIP.setMasquerade(masquerade);
                 if (network != null && prefix != null) {
-                    firewallPortForwardConfigIP.setPermittedNetwork(new NetworkPair<IP4Address>(
+                    firewallPortForwardConfigIP.setPermittedNetwork(new NetworkPair<>(
                             (IP4Address) IPAddress.parseHostAddress(network), Short.parseShort(prefix)));
                 }
                 firewallPortForwardConfigIP.setPermittedMac(GwtSafeHtmlUtils.htmlEscape(entry.getPermittedMAC()));
@@ -1382,7 +1379,7 @@ public class GwtNetworkServiceImpl extends OsgiRemoteServiceServlet implements G
         s_logger.debug("updateDeviceFirewallNATs() :: updating NAT entries");
         checkXSRFToken(xsrfToken);
         NetworkAdminService nas = ServiceLocator.getInstance().getService(NetworkAdminService.class);
-        List<FirewallNatConfig> firewallNatConfigs = new ArrayList<FirewallNatConfig>();
+        List<FirewallNatConfig> firewallNatConfigs = new ArrayList<>();
 
         for (GwtFirewallNatEntry entry : entries) {
 

kura/org.eclipse.kura.web2/src/main/java/org/eclipse/kura/web/shared/service/GwtNetworkService.java
@@ -51,7 +51,7 @@ public interface GwtNetworkService extends RemoteService {
 
     public void renewDhcpLease(GwtXSRFToken xsrfToken, String interfaceName) throws GwtKuraException;
 
-    public List<GwtWifiHotspotEntry> findWifiHotspots(GwtXSRFToken xsrfToken, String interfaceName)
+    public List<GwtWifiHotspotEntry> findWifiHotspots(GwtXSRFToken xsrfToken, String interfaceName, String wirelessSsid)
             throws GwtKuraException;
 
     public boolean verifyWifiCredentials(GwtXSRFToken xsrfToken, String interfaceName, GwtWifiConfig gwtWifiConfig)

commit c980493da94e7cd89f4af8bf1881c594b77f1c98

Partially revert net.wifi breaking change
This commit partially revert an API breaking change introduced by 681c285
and specifically part of the refactor of o.e.k.net.wifi.WifiHotspotInfo.
Replacing EnumSet with Set in return types is not backward compatible.

Signed-off-by: Cristiano De Alti <cristiano.dealti@eurotech.com>

kura/org.eclipse.kura.api/src/main/java/org/eclipse/kura/net/wifi/WifiHotspotInfo.java
@@ -11,7 +11,7 @@
  *******************************************************************************/
 package org.eclipse.kura.net.wifi;
 
-import java.util.Set;
+import java.util.EnumSet;
 
 public class WifiHotspotInfo {
 
@@ -21,8 +21,8 @@ public class WifiHotspotInfo {
     private final int channel;
     private final int frequency;
     private final WifiSecurity security;
-    private Set<WifiSecurity> pairCiphers;
-    private Set<WifiSecurity> groupCiphers;
+    private EnumSet<WifiSecurity> pairCiphers;
+    private EnumSet<WifiSecurity> groupCiphers;
 
     public WifiHotspotInfo(String ssid, String macAddress, int signalLevel, int channel, int frequency,
             WifiSecurity security) {
@@ -36,7 +36,7 @@ public class WifiHotspotInfo {
     }
 
     public WifiHotspotInfo(String ssid, String macAddress, int signalLevel, int channel, int frequency,
-            WifiSecurity security, Set<WifiSecurity> pairCiphers, Set<WifiSecurity> groupCiphers) {
+            WifiSecurity security, EnumSet<WifiSecurity> pairCiphers, EnumSet<WifiSecurity> groupCiphers) {
         this(ssid, macAddress, signalLevel, channel, frequency, security);
         this.pairCiphers = pairCiphers;
         this.groupCiphers = groupCiphers;
@@ -66,19 +66,19 @@ public class WifiHotspotInfo {
         return this.security;
     }
 
-    public Set<WifiSecurity> getPairCiphers() {
+    public EnumSet<WifiSecurity> getPairCiphers() {
         return this.pairCiphers;
     }
 
-    public Set<WifiSecurity> getGroupCiphers() {
+    public EnumSet<WifiSecurity> getGroupCiphers() {
         return this.groupCiphers;
     }
 
-    public void setPairCiphers(Set<WifiSecurity> pairCiphers) {
+    public void setPairCiphers(EnumSet<WifiSecurity> pairCiphers) {
         this.pairCiphers = pairCiphers;
     }
 
-    public void setGroupCiphers(Set<WifiSecurity> groupCiphers) {
+    public void setGroupCiphers(EnumSet<WifiSecurity> groupCiphers) {
         this.groupCiphers = groupCiphers;
     }
 
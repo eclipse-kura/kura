5c42326c1bacef978d0d59e749150bc36e532da5
MMaiero <matteo.maiero@eurotech.com>
2017-06-23 12:35:23 +0200

    Removed references to EDC in javadocs.
    Signed-off-by: MMaiero <matteo.maiero@eurotech.com>

--- kura/org.eclipse.kura.api/src/main/java/org/eclipse/kura/message/KuraPosition.java
+++ kura/org.eclipse.kura.api/src/main/java/org/eclipse/kura/message/KuraPosition.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011, 2016 Eurotech and/or its affiliates
+ * Copyright (c) 2011, 2017 Eurotech and/or its affiliates
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -16,10 +16,10 @@ import java.util.Date;
 import org.osgi.annotation.versioning.ProviderType;
 
 /**
- * EdcPosition is a data structure to capture a geo location. It can be
- * associated to an EdcPayload to geotag an EdcMessage before sending to the
- * Everyware Cloud. Refer to the description of each of the fields for more
- * information on the model of EdcPosition.
+ * KuraPosition is a data structure to capture a geo location. It can be
+ * associated to a KuraPayload to geotag a KuraMessage before sending to a
+ * remote cloud platform. Refer to the description of each of the fields for more
+ * information on the model of KuraPosition.
  * 
  * @noextend This class is not intended to be subclassed by clients.
  */

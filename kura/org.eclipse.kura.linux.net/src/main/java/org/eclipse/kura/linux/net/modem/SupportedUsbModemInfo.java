d94db89bdf3836af2706eb1bb9df7b5bc5821567
nicolatimeus <nicola.timeus@eurotech.com>
2017-09-08 17:57:13 +0200

    Fixed Telit LE 910 v2 reset
    Signed-off-by: nicolatimeus <nicola.timeus@eurotech.com>

--- kura/org.eclipse.kura.linux.net/src/main/java/org/eclipse/kura/linux/net/modem/SupportedUsbModemInfo.java
+++ kura/org.eclipse.kura.linux.net/src/main/java/org/eclipse/kura/linux/net/modem/SupportedUsbModemInfo.java
@@ -19,29 +19,30 @@ import org.eclipse.kura.net.modem.ModemTechnologyType;
 
 public enum SupportedUsbModemInfo {
 
-    // device name, vendor, product, ttyDevs, blockDevs, AT Port, Data Port, GPS Port, technology types, device driver
-    Telit_HE910_DG("HE910-DG", "1bc7", "0021", 6, 0, 3, 0, 3, Arrays.asList(ModemTechnologyType.HSPA,
+    // device name, vendor, product, ttyDevs, blockDevs, AT Port, Data Port, GPS Port, Turn off delay, technology types,
+    // device driver
+    Telit_HE910_DG("HE910-DG", "1bc7", "0021", 6, 0, 3, 0, 3, 5000, Arrays.asList(ModemTechnologyType.HSPA,
             ModemTechnologyType.UMTS), Arrays.asList(new UsbModemDriver("cdc_acm", "1bc7", "0021")), "6 CDC-ACM"),
-    Telit_HE910_D("HE910-D", "1bc7", "0021", 7, 0, 3, 0, 3, Arrays.asList(ModemTechnologyType.HSPA,
+    Telit_HE910_D("HE910-D", "1bc7", "0021", 7, 0, 3, 0, 3, 5000, Arrays.asList(ModemTechnologyType.HSPA,
             ModemTechnologyType.UMTS), Arrays.asList(new UsbModemDriver("cdc_acm", "1bc7", "0021")), ""),
-    Telit_GE910("GE910", "1bc7", "0022", 2, 0, 0, 1, 0, Arrays.asList(ModemTechnologyType.GSM_GPRS), Arrays
+    Telit_GE910("GE910", "1bc7", "0022", 2, 0, 0, 1, 0, 5000, Arrays.asList(ModemTechnologyType.GSM_GPRS), Arrays
             .asList(new UsbModemDriver("cdc_acm", "1bc7", "0022")), ""),
-    Telit_DE910_DUAL("DE910-DUAL", "1bc7", "1010", 4, 0, 2, 3, 1, Arrays.asList(ModemTechnologyType.EVDO,
+    Telit_DE910_DUAL("DE910-DUAL", "1bc7", "1010", 4, 0, 2, 3, 1, 5000, Arrays.asList(ModemTechnologyType.EVDO,
             ModemTechnologyType.CDMA), Arrays.asList(new De910ModemDriver()), ""),
-    Telit_LE910("LE910", "1bc7", "1201", 5, 0, 2, 3, 1, Arrays.asList(ModemTechnologyType.LTE, ModemTechnologyType.HSPA,
-            ModemTechnologyType.UMTS), Arrays.asList(new Le910ModemDriver()), ""),
-    Telit_LE910_V2("LE910-V2", "1bc7", "0036", 6, 0, 3, 0, -1, Arrays.asList(ModemTechnologyType.LTE,
+    Telit_LE910("LE910", "1bc7", "1201", 5, 0, 2, 3, 1, 5000, Arrays.asList(ModemTechnologyType.LTE,
+            ModemTechnologyType.HSPA, ModemTechnologyType.UMTS), Arrays.asList(new Le910ModemDriver()), ""),
+    Telit_LE910_V2("LE910-V2", "1bc7", "0036", 6, 0, 3, 0, -1, 15000, Arrays.asList(ModemTechnologyType.LTE,
             ModemTechnologyType.HSPA,
             ModemTechnologyType.UMTS), Arrays.asList(new UsbModemDriver("cdc_acm", "1bc7", "0036")), ""),
-    Telit_CE910_DUAL("CE910-DUAL", "1bc7", "1011", 2, 0, 1, 1, -1, Arrays.asList(ModemTechnologyType.CDMA), Arrays
+    Telit_CE910_DUAL("CE910-DUAL", "1bc7", "1011", 2, 0, 1, 1, -1, 5000, Arrays.asList(ModemTechnologyType.CDMA), Arrays
             .asList(new Ce910ModemDriver()), ""),
-    Sierra_MC8775("MC8775", "1199", "6812", 3, 0, 2, 0, -1, Arrays.asList(ModemTechnologyType.HSDPA), Arrays
+    Sierra_MC8775("MC8775", "1199", "6812", 3, 0, 2, 0, -1, 5000, Arrays.asList(ModemTechnologyType.HSDPA), Arrays
             .asList(new UsbModemDriver("sierra", "1199", "6812")), ""),
-    Sierra_MC8790("MC8790", "1199", "683c", 7, 0, 3, 4, -1, Arrays.asList(ModemTechnologyType.HSDPA), Arrays
+    Sierra_MC8790("MC8790", "1199", "683c", 7, 0, 3, 4, -1, 5000, Arrays.asList(ModemTechnologyType.HSDPA), Arrays
             .asList(new UsbModemDriver("sierra", "1199", "683c")), ""),
-    Sierra_USB598("USB598", "1199", "0025", 4, 1, 0, 0, -1, Arrays.asList(ModemTechnologyType.EVDO), Arrays
+    Sierra_USB598("USB598", "1199", "0025", 4, 1, 0, 0, -1, 5000, Arrays.asList(ModemTechnologyType.EVDO), Arrays
             .asList(new UsbModemDriver("sierra", "1199", "0025")), ""),
-    Ublox_SARA_U2("SARA-U2", "1546", "1102", 7, 0, 1, 0, -1, Arrays.asList(ModemTechnologyType.HSPA), Arrays
+    Ublox_SARA_U2("SARA-U2", "1546", "1102", 7, 0, 1, 0, -1, 5000, Arrays.asList(ModemTechnologyType.HSPA), Arrays
             .asList(new UsbModemDriver("cdc_acm", "1546", "1102")), "");
 
     private static final String TARGET_NAME = System.getProperty("target.device");
@@ -51,6 +52,7 @@ public enum SupportedUsbModemInfo {
     private String productId;
     private int numTtyDevs;
     private int numBlockDevs;
+    private long turnOffDelay;
 
     private int atPort;
     private int dataPort;
@@ -62,8 +64,8 @@ public enum SupportedUsbModemInfo {
     private String productName;
 
     private SupportedUsbModemInfo(String deviceName, String vendorId, String productId, int numTtyDevs,
-            int numBlockDevs, int atPort, int dataPort, int gpsPort, List<ModemTechnologyType> modemTechnology,
-            List<? extends UsbModemDriver> drivers, String prodName) {
+            int numBlockDevs, int atPort, int dataPort, int gpsPort, long turnOffDelay,
+            List<ModemTechnologyType> modemTechnology, List<? extends UsbModemDriver> drivers, String prodName) {
         this.deviceName = deviceName;
         this.vendorId = vendorId;
         this.productId = productId;
@@ -72,6 +74,7 @@ public enum SupportedUsbModemInfo {
         this.atPort = atPort;
         this.dataPort = dataPort;
         this.gpsPort = gpsPort;
+        this.turnOffDelay = turnOffDelay;
         this.technologyTypes = modemTechnology;
         this.deviceDrivers = drivers;
         this.productName = prodName;
@@ -147,5 +150,9 @@ public enum SupportedUsbModemInfo {
 
     public String getProductName() {
         return this.productName;
+    }
+
+    public long getTurnOffDelay() {
+        return turnOffDelay;
     }
 }

771c723cbfcd649ca47001d3be76fa9515e8373d
Federico Claramonte <fede.claramonte@caviumnetworks.com>
2017-09-11 08:24:22 +0200

    Adding cavium as contributor and updating copyright years
    Signed-off-by: Federico Claramonte <fede.claramonte@caviumnetworks.com>

--- kura/distrib/src/main/resources/aarch64-nn/kura_upgrade.sh
+++ kura/distrib/src/main/resources/aarch64-nn/kura_upgrade.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# Copyright (c) 2011, 2014, 2017 Eurotech and/or its affiliates
+# Copyright (c) 2011, 2017 Eurotech and/or its affiliates
 #
 #  All rights reserved. This program and the accompanying materials
 #  are made available under the terms of the Eclipse Public License v1.0
@@ -9,6 +9,7 @@
 #
 # Contributors:
 #   Eurotech
+#   Cavium
 #
 
 INSTALL_DIR=/opt/eclipse

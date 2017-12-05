0acb261e4d4ff4e44325d883c2ab93dab856344e
ibinshtok <ilya.binshtok@eurotech.com>
2017-12-05 16:13:37 -0600

    Added 'turnOnDelay' to SupportedUsbModemInfo for each modem to fix LE910 V1 reset issue.
    Signed-off-by: ibinshtok <ilya.binshtok@eurotech.com>

--- kura/org.eclipse.kura.linux.net/src/main/java/org/eclipse/kura/linux/net/modem/ModemDriver.java
+++ kura/org.eclipse.kura.linux.net/src/main/java/org/eclipse/kura/linux/net/modem/ModemDriver.java
@@ -230,6 +230,7 @@ public class ModemDriver {
         }
         boolean retVal = true;
         int remainingAttempts = 5;
+        final long turnOnDelay = getTurnOnDelay();
         while (!isOn()) {
             if (remainingAttempts <= 0) {
                 retVal = false;
@@ -294,7 +295,7 @@ public class ModemDriver {
                 break;
             }
             remainingAttempts--;
-            sleep(10000);
+            sleep(turnOnDelay);
         }
 
         logger.info("turnModemOn() :: Modem is ON? - {}", retVal);
@@ -497,6 +498,14 @@ public class ModemDriver {
             return usbModemInfo.getTurnOffDelay();
         }
         return 5000;
+    }
+    
+    private long getTurnOnDelay() {
+        final SupportedUsbModemInfo usbModemInfo = getSupportedUsbModemInfo();
+        if (usbModemInfo != null) {
+            return usbModemInfo.getTurnOnDelay();
+        }
+        return 10000;
     }
 
     private boolean isOn() throws KuraException {

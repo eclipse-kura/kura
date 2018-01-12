1c10a0fe744a922f0e6561a506685f32029fa1e7
Marko Erker <marko.erker@comtrade.com>
2018-01-12 17:26:35 +0100

    added eddystone BLE tests, updated comment in the code
    Signed-off-by: Marko Erker <marko.erker@comtrade.com>

---
+++ kura/test/org.eclipse.kura.internal.ble.eddystone.test/src/test/java/org/eclipse/kura/internal/ble/eddystone/BluetoothLeEddystoneServiceImplTest.java
@@ -0,0 +1,80 @@
+/*******************************************************************************
+ * Copyright (c) 2018 Eurotech and/or its affiliates and others
+ *
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License v1.0
+ *   which accompanies this distribution, and is available at
+ *   http://www.eclipse.org/legal/epl-v10.html
+ ******************************************************************************/
+package org.eclipse.kura.internal.ble.eddystone;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+import static org.mockito.Matchers.anyObject;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.eclipse.kura.KuraBluetoothBeaconAdvertiserNotAvailable;
+import org.eclipse.kura.ble.eddystone.BluetoothLeEddystone;
+import org.eclipse.kura.bluetooth.le.BluetoothLeAdapter;
+import org.eclipse.kura.bluetooth.le.beacon.BluetoothLeBeaconAdvertiser;
+import org.eclipse.kura.bluetooth.le.beacon.BluetoothLeBeaconManager;
+import org.eclipse.kura.bluetooth.le.beacon.BluetoothLeBeaconScanner;
+import org.eclipse.kura.internal.ble.eddystone.BluetoothLeEddystoneServiceImpl;
+import org.junit.Test;
+
+
+public class BluetoothLeEddystoneServiceImplTest {
+
+    @Test
+    public void testSequence() throws KuraBluetoothBeaconAdvertiserNotAvailable {
+        BluetoothLeBeaconManager mgrMock = mock(BluetoothLeBeaconManager.class);
+
+        BluetoothLeAdapter adapter = mock(BluetoothLeAdapter.class);
+
+        BluetoothLeBeaconAdvertiser advMock = mock(BluetoothLeBeaconAdvertiser.class);
+        when(mgrMock.newBeaconAdvertiser(eq(adapter), anyObject())).thenReturn(advMock);
+
+        BluetoothLeBeaconScanner scMock = mock(BluetoothLeBeaconScanner.class);
+        when(mgrMock.newBeaconScanner(eq(adapter), anyObject())).thenReturn(scMock);
+
+        BluetoothLeEddystoneServiceImpl svc = new BluetoothLeEddystoneServiceImpl();
+
+        svc.activate(null);
+
+        try {
+            svc.newBeaconAdvertiser(adapter);
+            fail("Exception was expected.");
+        } catch (NullPointerException e) {
+            // expected
+        }
+
+        svc.setBluetoothLeBeaconManager(mgrMock);
+
+        BluetoothLeBeaconAdvertiser<BluetoothLeEddystone> advertiser = svc.newBeaconAdvertiser(adapter);
+
+        assertEquals(advMock, advertiser);
+        verify(mgrMock, times(1)).newBeaconAdvertiser(eq(adapter), anyObject());
+
+        svc.deleteBeaconAdvertiser(advertiser);
+
+        verify(mgrMock, times(1)).deleteBeaconAdvertiser(advertiser);
+
+        BluetoothLeBeaconScanner<BluetoothLeEddystone> scanner = svc.newBeaconScanner(adapter);
+
+        assertEquals(scMock, scanner);
+        verify(mgrMock, times(1)).newBeaconScanner(eq(adapter), anyObject());
+
+        svc.deleteBeaconScanner(scanner);
+
+        verify(mgrMock, times(1)).deleteBeaconScanner(scanner);
+
+        svc.unsetBluetoothLeBeaconManager(mgrMock);
+
+        svc.deactivate(null);
+    }
+
+}

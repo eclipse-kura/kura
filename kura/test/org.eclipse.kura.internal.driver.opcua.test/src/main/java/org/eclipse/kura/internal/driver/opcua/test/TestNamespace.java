43fb6d65647b9d8f80cc657e0593f699ea8ca6a0
nicolatimeus <nicola.timeus@eurotech.com>
2018-04-20 10:58:42 +0200

    Updated kura.build.properties and copyright headers
    Signed-off-by: nicolatimeus <nicola.timeus@eurotech.com>

--- kura/test/org.eclipse.kura.internal.driver.opcua.test/src/main/java/org/eclipse/kura/internal/driver/opcua/test/TestNamespace.java
+++ kura/test/org.eclipse.kura.internal.driver.opcua.test/src/main/java/org/eclipse/kura/internal/driver/opcua/test/TestNamespace.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016, 2017 Kevin Herron and others
+ * Copyright (c) 2016, 2018 Kevin Herron and others
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0

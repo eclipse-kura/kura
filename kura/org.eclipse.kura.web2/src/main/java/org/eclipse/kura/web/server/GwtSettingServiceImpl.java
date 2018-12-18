72867d2f6764aef87ce2cee25796b91ffe34cc04
Maiero <matteo.maiero@eurotech.com>
2018-12-18 10:51:47 +0100

    Refactored console code in web2 bundle.
    Few cleanups and optimizations.
    
    Signed-off-by: Maiero <matteo.maiero@eurotech.com>

--- kura/org.eclipse.kura.web2/src/main/java/org/eclipse/kura/web/server/GwtSettingServiceImpl.java
+++ kura/org.eclipse.kura.web2/src/main/java/org/eclipse/kura/web/server/GwtSettingServiceImpl.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011, 2016 Eurotech and/or its affiliates
+ * Copyright (c) 2011, 2018 Eurotech and/or its affiliates
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -15,43 +15,13 @@ import java.util.Enumeration;
 
 import javax.servlet.http.HttpSession;
 
-import org.eclipse.kura.web.AuthenticationManager;
-import org.eclipse.kura.web.shared.GwtKuraErrorCode;
 import org.eclipse.kura.web.shared.GwtKuraException;
-import org.eclipse.kura.web.shared.model.GwtSettings;
 import org.eclipse.kura.web.shared.model.GwtXSRFToken;
 import org.eclipse.kura.web.shared.service.GwtSettingService;
 
 public class GwtSettingServiceImpl extends OsgiRemoteServiceServlet implements GwtSettingService {
 
     private static final long serialVersionUID = -3422518194598042896L;
-
-    @Override
-    public void updateSettings(GwtXSRFToken xsrfToken, GwtSettings settings) throws GwtKuraException {
-        checkXSRFToken(xsrfToken);
-        AuthenticationManager authMgr = AuthenticationManager.getInstance();
-
-        //
-        // verify the current password
-        boolean validCurrPwd = false;
-
-        validCurrPwd = authMgr.authenticate("admin", settings.getPasswordCurrent());
-
-        if (!validCurrPwd) {
-            throw new GwtKuraException(GwtKuraErrorCode.CURRENT_ADMIN_PASSWORD_DOES_NOT_MATCH);
-        }
-
-        //
-        // set the new password
-        /*
-         * try {
-         * authMgr.changeAdminPassword(settings.getPasswordNew());
-         * }
-         * catch (SQLException e) {
-         * throw new GwtKuraException(GwtKuraErrorCode.INTERNAL_ERROR, e);
-         * }
-         */
-    }
 
     @Override
     @SuppressWarnings("rawtypes")

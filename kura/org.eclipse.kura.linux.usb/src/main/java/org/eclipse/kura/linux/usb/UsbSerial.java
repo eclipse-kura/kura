f4cc5293350f1325796ff29cd228179ca6d0d7ab
MMaiero <matteo.maiero@eurotech.com>
2018-12-21 17:45:08 +0100

    Solved some blocking lint issues. (#2347)
    Signed-off-by: Maiero <matteo.maiero@eurotech.com>

--- kura/org.eclipse.kura.linux.usb/src/main/java/org/eclipse/kura/linux/usb/UsbSerial.java
+++ kura/org.eclipse.kura.linux.usb/src/main/java/org/eclipse/kura/linux/usb/UsbSerial.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011, 2016 Eurotech and/or its affiliates
+ * Copyright (c) 2011, 2018 Eurotech and/or its affiliates
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -13,6 +13,7 @@ package org.eclipse.kura.linux.usb;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.StringTokenizer;
 
@@ -20,52 +21,57 @@ public class UsbSerial {
 
     private static final String FILENAME = "/proc/tty/driver/usbserial";
 
-    private static ArrayList<UsbSerialEntry> s_entries = null;
+    private static ArrayList<UsbSerialEntry> entries = null;
+
+    private UsbSerial() {
+
+    }
 
     /*
      * (non-Javadoc)
      *
      * @see org.eclipse.kura.device.usb.usbmanager.usbserial.service.IUsbSerialService#getInfo()
      */
-    private static void getInfo() throws Exception {
+    private static void getInfo() throws IOException {
 
-        BufferedReader in = new BufferedReader(new FileReader(FILENAME));
-        s_entries = new ArrayList<UsbSerialEntry>();
+        try (FileReader fr = new FileReader(FILENAME);
+                BufferedReader in = new BufferedReader(fr);) {
+            entries = new ArrayList<>();
 
-        int ttyUsbNo = 0;
-        String vendor = null;
-        String product = null;
-        int numPorts = 0;
-        int portEnum = 0;
-        String path = null;
+            int ttyUsbNo = 0;
+            String vendor = null;
+            String product = null;
+            int numPorts = 0;
+            int portEnum = 0;
+            String path = null;
 
-        String line = in.readLine(); // read first line (do not need to parse it)
-        while ((line = in.readLine()) != null) {
+            String line = in.readLine(); // read first line (do not need to parse it)
+            while ((line = in.readLine()) != null) {
 
-            ttyUsbNo = Integer.parseInt(line.substring(0, line.indexOf(':')));
-            line = line.substring(line.indexOf("vendor:"));
-            StringTokenizer st = new StringTokenizer(line, " ");
-            while (st.hasMoreTokens()) {
-                String token = st.nextToken();
-                if (token.startsWith("vendor:")) {
-                    vendor = token.substring(token.indexOf(":") + 1).trim();
+                ttyUsbNo = Integer.parseInt(line.substring(0, line.indexOf(':')));
+                line = line.substring(line.indexOf("vendor:"));
+                StringTokenizer st = new StringTokenizer(line, " ");
+                while (st.hasMoreTokens()) {
+                    String token = st.nextToken();
+                    if (token.startsWith("vendor:")) {
+                        vendor = token.substring(token.indexOf(':') + 1).trim();
+                    }
+                    if (token.startsWith("product:")) {
+                        product = token.substring(token.indexOf(':') + 1).trim();
+                    }
+                    if (token.startsWith("num_ports:")) {
+                        numPorts = Integer.parseInt(token.substring(token.indexOf(':') + 1).trim());
+                    }
+                    if (token.startsWith("port:")) {
+                        portEnum = Integer.parseInt(token.substring(token.indexOf(':') + 1).trim());
+                    }
+                    if (token.startsWith("path:")) {
+                        path = token.substring(token.indexOf(':') + 1).trim();
+                    }
                 }
-                if (token.startsWith("product:")) {
-                    product = token.substring(token.indexOf(":") + 1).trim();
-                }
-                if (token.startsWith("num_ports:")) {
-                    numPorts = Integer.parseInt(token.substring(token.indexOf(":") + 1).trim());
-                }
-                if (token.startsWith("port:")) {
-                    portEnum = Integer.parseInt(token.substring(token.indexOf(":") + 1).trim());
-                }
-                if (token.startsWith("path:")) {
-                    path = token.substring(token.indexOf(":") + 1).trim();
-                }
+                entries.add(new UsbSerialEntry(ttyUsbNo, vendor, product, numPorts, portEnum, path));
             }
-            s_entries.add(new UsbSerialEntry(ttyUsbNo, vendor, product, numPorts, portEnum, path));
         }
-        in.close();
     }
 
     /*
@@ -80,10 +86,10 @@ public class UsbSerial {
         getInfo();
 
         UsbSerialEntry entry = null;
-        ArrayList<UsbSerialEntry> matches = new ArrayList<UsbSerialEntry>();
+        ArrayList<UsbSerialEntry> matches = new ArrayList<>();
 
-        for (int i = 0; i < s_entries.size(); i++) {
-            entry = s_entries.get(i);
+        for (int i = 0; i < entries.size(); i++) {
+            entry = entries.get(i);
             if (entry.getVendorID().compareTo(vendor) == 0 && entry.getProductID().compareTo(product) == 0) {
                 matches.add(entry);
             }

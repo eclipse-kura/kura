2a646072a25845137b5161e1230f11bb8c74e50d
MMaiero <matteo.maiero@eurotech.com>
2019-01-09 17:21:50 +0100

    Changes to solve lint issues in CloudConnectionStatus. (#2346)
    Signed-off-by: Maiero <matteo.maiero@eurotech.com>

--- kura/org.eclipse.kura.core.status/src/main/java/org/eclipse/kura/core/status/runnables/LogStatusRunnable.java
+++ kura/org.eclipse.kura.core.status/src/main/java/org/eclipse/kura/core/status/runnables/LogStatusRunnable.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011, 2017 Eurotech and/or its affiliates
+ * Copyright (c) 2011, 2018 Eurotech and/or its affiliates
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -15,34 +15,39 @@ import org.eclipse.kura.status.CloudConnectionStatusEnum;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class LogStatusRunnable implements Runnable {
+public class LogStatusRunnable implements StatusRunnable {
 
-    private static final Logger s_logger = LoggerFactory.getLogger(LogStatusRunnable.class);
+    private static final Logger logger = LoggerFactory.getLogger(LogStatusRunnable.class);
 
-    private final CloudConnectionStatusEnum m_status;
+    private final CloudConnectionStatusEnum status;
 
     public LogStatusRunnable(CloudConnectionStatusEnum status) {
-        this.m_status = status;
+        this.status = status;
     }
 
     @Override
     public void run() {
-        switch (this.m_status) {
+        switch (this.status) {
         case ON:
-            s_logger.info("Notification LED on");
+            logger.info("Notification LED on");
             break;
         case SLOW_BLINKING:
-            s_logger.info("Notification LED slow blinking");
+            logger.info("Notification LED slow blinking");
             break;
         case FAST_BLINKING:
-            s_logger.info("Notification LED fast blinking");
+            logger.info("Notification LED fast blinking");
             break;
         case HEARTBEAT:
-            s_logger.info("Notification LED heartbeating");
+            logger.info("Notification LED heartbeating");
             break;
         default:
-            s_logger.info("Notification LED off");
+            logger.info("Notification LED off");
         }
+    }
+
+    @Override
+    public void stopRunnable() {
+        return;
     }
 
 }

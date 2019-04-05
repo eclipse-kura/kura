52c9d2e3f04f9c54c8252d17c9c5dc8deacb5ed2
nicolatimeus <nicola.timeus@eurotech.com>
2019-04-05 16:25:42 +0200

    Implemented wifi capability probing using iw
    Signed-off-by: nicolatimeus <nicola.timeus@eurotech.com>

--- kura/org.eclipse.kura.linux.net/src/main/java/org/eclipse/kura/linux/net/util/LinuxNetworkUtil.java
+++ kura/org.eclipse.kura.linux.net/src/main/java/org/eclipse/kura/linux/net/util/LinuxNetworkUtil.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011, 2018 Eurotech and/or its affiliates
+ * Copyright (c) 2011, 2019 Eurotech and/or its affiliates
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -18,7 +18,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.EnumSet;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -634,65 +633,12 @@ public class LinuxNetworkUtil {
      * Returns an empty capabilities set if the interface is not found or on error
      */
     public static Set<Capability> getWifiCapabilities(String ifaceName) throws KuraException {
-        Set<Capability> capabilities = EnumSet.noneOf(Capability.class);
-
-        // ignore logical interfaces like "1-1.2"
-        if (Character.isDigit(ifaceName.charAt(0))) {
-            return capabilities;
-        }
-
-        SafeProcess proc = null;
-        String cmd = "iwlist " + ifaceName + " auth";
-        try {
-            // start the process
-            proc = ProcessUtil.exec(cmd);
-            if (proc.waitFor() != 0) {
-                logger.warn("error executing command --- iwlist --- exit value = {}", proc.exitValue());
-                throw new KuraException(KuraErrorCode.OS_COMMAND_ERROR, cmd, proc.exitValue());
-            }
-
-            // get the output
-            getWifiCapabilitiesParse(cmd, capabilities, proc);
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-            throw new KuraException(KuraErrorCode.PROCESS_EXECUTION_ERROR, e, formInterruptedCommandMessage(cmd));
-        } catch (Exception e) {
-            throw new KuraException(KuraErrorCode.PROCESS_EXECUTION_ERROR, e, formFailedCommandMessage(cmd));
-        } finally {
-            if (proc != null) {
-                ProcessUtil.destroy(proc);
-            }
-        }
-        return capabilities;
-    }
-
-    private static void getWifiCapabilitiesParse(String cmd, Set<Capability> capabilities, SafeProcess proc)
-            throws KuraException {
-
-        try (InputStreamReader isr = new InputStreamReader(proc.getInputStream());
-                BufferedReader br = new BufferedReader(isr)) {
-            String line = null;
-            while ((line = br.readLine()) != null) {
-                // Remove all whitespace
-                String cleanLine = line.replaceAll("\\s", "");
-
-                if ("WPA".equals(cleanLine)) {
-                    capabilities.add(Capability.WPA);
-                } else if ("WPA2".equals(cleanLine)) {
-                    capabilities.add(Capability.RSN);
-                } else if ("CIPHER-TKIP".equals(cleanLine)) {
-                    capabilities.add(Capability.CIPHER_TKIP);
-                } else if ("CIPHER-CCMP".equals(cleanLine)) {
-                    capabilities.add(Capability.CIPHER_CCMP);
-                    // TODO: WEP options don't always seem to be displayed?
-                } else if ("WEP-104".equals(cleanLine)) {
-                    capabilities.add(Capability.CIPHER_WEP104);
-                } else if ("WEP-40".equals(cleanLine)) {
-                    capabilities.add(Capability.CIPHER_WEP40);
-                }
-            }
-        } catch (IOException e) {
-            throw new KuraException(KuraErrorCode.PROCESS_EXECUTION_ERROR, e, formFailedCommandMessage(cmd));
+        if (toolExists("iwlist")) {
+            return IwlistCapabilityTool.probeCapabilities(ifaceName);
+        } else if (toolExists("iw")) {
+            return IwCapabilityTool.probeCapabilities(ifaceName);
+        } else {
+            throw new KuraException(KuraErrorCode.OPERATION_NOT_SUPPORTED);
         }
     }
 
@@ -1130,37 +1076,37 @@ public class LinuxNetworkUtil {
         return config != null ? config.isUp() : false;
     }
 
-    private static String formIfconfigIfaceCommand(String ifaceName) {
+    static String formIfconfigIfaceCommand(String ifaceName) {
         StringBuilder sb = new StringBuilder("ifconfig ");
         sb.append(ifaceName);
         return sb.toString();
     }
 
-    private static String formIwDevIfaceInfoCommand(String ifaceName) {
+    static String formIwDevIfaceInfoCommand(String ifaceName) {
         StringBuilder sb = new StringBuilder("iw dev ");
         sb.append(ifaceName).append(" info");
         return sb.toString();
     }
 
-    private static String formIwDevIfaceLinkCommand(String ifaceName) {
+    static String formIwDevIfaceLinkCommand(String ifaceName) {
         StringBuilder sb = new StringBuilder("iw dev ");
         sb.append(ifaceName).append(" link");
         return sb.toString();
     }
 
-    private static String formIwconfigIfaceCommand(String ifaceName) {
+    static String formIwconfigIfaceCommand(String ifaceName) {
         StringBuilder sb = new StringBuilder("iwconfig ");
         sb.append(ifaceName);
         return sb.toString();
     }
 
-    private static String formFailedCommandMessage(String cmd) {
+    static String formFailedCommandMessage(String cmd) {
         StringBuilder sb = new StringBuilder();
         sb.append("'").append(cmd).append("' failed");
         return sb.toString();
     }
 
-    private static String formInterruptedCommandMessage(String cmd) {
+    static String formInterruptedCommandMessage(String cmd) {
         StringBuilder sb = new StringBuilder();
         sb.append("'").append(cmd).append("' interrupted");
         return sb.toString();

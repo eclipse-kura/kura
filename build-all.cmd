bc6629f8f45eef764c2af0fde401dd0dac2d3115
heyoulin <heyoulin@gmail.com>
2019-07-27 03:26:15 +0800

    bat

--- build-all.cmd
+++ build-all.cmd
@@ -1,4 +1,4 @@
-mvn  -f target-platform/pom.xml clean install -B -Dmaven.test.skip=true
-mvn  -f kura/pom.xml clean install -B -Dmaven.test.skip=true
-mvn  -f kura/examples/pom.xml clean install -B -Dmaven.test.skip=true
+call mvn  -f target-platform/pom.xml clean install -B -Dmaven.test.skip=true
+call mvn  -f kura/pom.xml clean install -B -Dmaven.test.skip=true
+call mvn  -f kura/examples/pom.xml clean install -B -Dmaven.test.skip=true
 

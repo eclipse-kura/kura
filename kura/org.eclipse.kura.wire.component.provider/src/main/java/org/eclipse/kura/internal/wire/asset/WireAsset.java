98118859acf053ca678f1526c6aea6bb67b71dd4
heyoulin <heyoulin@gmail.com>
2019-10-11 22:34:20 +0800

    remove wireAsset muitiThread
    Signed-off-by: heyoulin <heyoulin@gmail.com>

--- kura/org.eclipse.kura.wire.component.provider/src/main/java/org/eclipse/kura/internal/wire/asset/WireAsset.java
+++ kura/org.eclipse.kura.wire.component.provider/src/main/java/org/eclipse/kura/internal/wire/asset/WireAsset.java
@@ -14,23 +14,15 @@
 package org.eclipse.kura.internal.wire.asset;
 
 import static java.util.Objects.isNull;
-import static java.util.Objects.nonNull;
 import static java.util.Objects.requireNonNull;
 import static org.eclipse.kura.channel.ChannelType.READ_WRITE;
 import static org.eclipse.kura.channel.ChannelType.WRITE;
-import static org.eclipse.kura.configuration.ConfigurationService.KURA_SERVICE_PID;
 
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ThreadFactory;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicInteger;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -113,15 +105,12 @@ public final class WireAsset extends BaseAsset implements WireEmitter, WireRecei
     private WireAssetOptions options = new WireAssetOptions();
 
     private PreparedEmit preparedEmit;
-    private String kuraServicePid;
-    private ScheduledExecutorService emitExecutor;
-    private int threadTimeout = 10000;
 
     /**
      * Binds the Wire Helper Service.
      *
      * @param wireHelperService
-     *                              the new Wire Helper Service
+     *            the new Wire Helper Service
      */
     public void bindWireHelperService(final WireHelperService wireHelperService) {
         if (isNull(this.wireHelperService)) {
@@ -133,7 +122,7 @@ public final class WireAsset extends BaseAsset implements WireEmitter, WireRecei
      * Unbinds the Wire Helper Service.
      *
      * @param wireHelperService
-     *                              the new Wire Helper Service
+     *            the new Wire Helper Service
      */
     public void unbindWireHelperService(final WireHelperService wireHelperService) {
         if (this.wireHelperService == wireHelperService) {
@@ -145,56 +134,31 @@ public final class WireAsset extends BaseAsset implements WireEmitter, WireRecei
      * OSGi service component activation callback.
      *
      * @param componentContext
-     *                             the component context
+     *            the component context
      * @param properties
-     *                             the service properties
+     *            the service properties
      */
     @SuppressWarnings("unchecked")
     @Override
     protected void activate(final ComponentContext componentContext, final Map<String, Object> properties) {
         logger.debug("Activating Wire Asset...");
-        ServiceReference<WireComponent> wireComponentRef = (ServiceReference<WireComponent>) componentContext
-                .getServiceReference();
-        this.wireSupport = this.wireHelperService.newWireSupport(this, wireComponentRef);
-        this.kuraServicePid = (String) wireComponentRef.getProperty(KURA_SERVICE_PID);
+        this.wireSupport = this.wireHelperService.newWireSupport(this,
+                (ServiceReference<WireComponent>) componentContext.getServiceReference());
         super.activate(componentContext, properties);
-        setupEmitExecutor(properties);
         logger.debug("Activating Wire Asset...Done");
-    }
-
-    private void setupEmitExecutor(Map<String, Object> properties) {
-        if (emitExecutor != null) {
-            emitExecutor.shutdownNow();
-            emitExecutor = null;
-        }
-        final Object emitMutipleThreadObj = properties.get(WireAssetOptions.EMIT_MUTIPLE_THREAD_PROP_NAME);
-        Boolean emitMutipleThread = emitMutipleThreadObj instanceof Boolean && (Boolean) emitMutipleThreadObj;
-        if (emitMutipleThread) {
-            final Object threadCountObj = properties.get(WireAssetOptions.EMIT_THREAD_COUNT_PROP_NAME);
-            int threadCount = 2;
-            final Object threadTimeoutObj = properties.get(WireAssetOptions.EMIT_THREAD_TIMEOUT_PROP_NAME);
-            if (nonNull(threadCountObj) && threadCountObj instanceof Integer) {
-                threadCount = (Integer) threadCountObj;
-            }
-            if (nonNull(threadTimeoutObj) && threadTimeoutObj instanceof Integer) {
-                this.threadTimeout = (Integer) threadTimeoutObj;
-            }
-            emitExecutor = Executors.newScheduledThreadPool(threadCount, new WireDefaultThreadFactory(kuraServicePid));
-        }
     }
 
     /**
      * OSGi service component update callback.
      *
      * @param properties
-     *                       the service properties
+     *            the service properties
      */
     @Override
     public void updated(final Map<String, Object> properties) {
         logger.debug("Updating Wire Asset...");
         this.options = new WireAssetOptions(properties);
         super.updated(properties);
-        setupEmitExecutor(properties);
         logger.debug("Updating Wire Asset...Done");
     }
 
@@ -202,17 +166,13 @@ public final class WireAsset extends BaseAsset implements WireEmitter, WireRecei
      * OSGi service component deactivate callback.
      *
      * @param context
-     *                    the context
+     *            the context
      */
     @Override
     protected void deactivate(final ComponentContext context) {
         logger.debug("Deactivating Wire Asset...");
         super.deactivate(context);
         logger.debug("Deactivating Wire Asset...Done");
-        if (emitExecutor != null) {
-            emitExecutor.shutdownNow();
-            emitExecutor = null;
-        }
     }
 
     /** {@inheritDoc} */
@@ -242,23 +202,14 @@ public final class WireAsset extends BaseAsset implements WireEmitter, WireRecei
      * receives {@code Non Null} {@link WireEnvelop} from its upstream Wire Component(s).
      *
      * @param wireEnvelope
-     *                         the received {@link WireEnvelope}
+     *            the received {@link WireEnvelope}
      * @throws NullPointerException
-     *                                  if {@link WireEnvelope} is null
+     *             if {@link WireEnvelope} is null
      */
     @Override
     public void onWireReceive(final WireEnvelope wireEnvelope) {
         requireNonNull(wireEnvelope, "Wire Envelope cannot be null");
-        if (this.emitExecutor != null) {
-            final Future<?> handler = emitExecutor.submit(() -> runWireReceive(wireEnvelope));
-            if (this.threadTimeout > 0)
-                emitExecutor.schedule(() -> handler.cancel(true), this.threadTimeout, TimeUnit.MILLISECONDS);
-        } else
-            runWireReceive(wireEnvelope);
 
-    }
-
-    private void runWireReceive(final WireEnvelope wireEnvelope) {
         emitAllReadChannels();
 
         final List<WireRecord> records = wireEnvelope.getRecords();
@@ -292,10 +243,10 @@ public final class WireAsset extends BaseAsset implements WireEmitter, WireRecei
      * Determine the channels to write
      *
      * @param records
-     *                    the list of {@link WireRecord}s to parse
+     *            the list of {@link WireRecord}s to parse
      * @return list of Channel Records containing the values to be written
      * @throws NullPointerException
-     *                                  if argument is null
+     *             if argument is null
      */
     private List<ChannelRecord> determineWritingChannels(final WireRecord record) {
         requireNonNull(record, "Wire Record cannot be null");
@@ -328,12 +279,12 @@ public final class WireAsset extends BaseAsset implements WireEmitter, WireRecei
      * Emit the provided list of channel records to the associated wires.
      *
      * @param channelRecords
-     *                           the list of channel records conforming to the aforementioned
-     *                           specification
+     *            the list of channel records conforming to the aforementioned
+     *            specification
      * @throws NullPointerException
-     *                                      if provided records list is null
+     *             if provided records list is null
      * @throws IllegalArgumentException
-     *                                      if provided records list is empty
+     *             if provided records list is empty
      */
     private void emitChannelRecords(final List<ChannelRecord> channelRecords) {
         requireNonNull(channelRecords, "List of Channel Records cannot be null");
@@ -346,7 +297,7 @@ public final class WireAsset extends BaseAsset implements WireEmitter, WireRecei
         if (this.preparedEmit != null) {
             wireRecordProperties = this.preparedEmit.execute(channelRecords);
         } else {
-            wireRecordProperties = Utils.toWireRecordProperties(channelRecords, options);
+            wireRecordProperties = Utils.toWireRecordProperties(channelRecords, this.options);
         }
 
         try {
@@ -363,9 +314,9 @@ public final class WireAsset extends BaseAsset implements WireEmitter, WireRecei
      * Perform Channel Write operation
      *
      * @param channelRecordsToWrite
-     *                                  the list of {@link ChannelRecord}s
+     *            the list of {@link ChannelRecord}s
      * @throws NullPointerException
-     *                                  if the provided list is null
+     *             if the provided list is null
      */
     private void writeChannels(final List<ChannelRecord> channelRecordsToWrite) {
         requireNonNull(channelRecordsToWrite, "List of Channel Records cannot be null");
@@ -449,7 +400,7 @@ public final class WireAsset extends BaseAsset implements WireEmitter, WireRecei
 
         @Override
         public void onChannelEvent(ChannelEvent event) {
-            if (options.emitAllChannels()) {
+            if (WireAsset.this.options.emitAllChannels()) {
                 emitAllReadChannels();
             } else {
                 emitChannelRecords(Collections.singletonList(event.getChannelRecord()));
@@ -468,42 +419,19 @@ public final class WireAsset extends BaseAsset implements WireEmitter, WireRecei
 
         PreparedEmit(final List<ChannelRecord> records) {
             this.preparedRecords = records;
-            this.recordFillers = RecordFillers.create(preparedRecords, options);
+            this.recordFillers = RecordFillers.create(this.preparedRecords, WireAsset.this.options);
         }
 
         Map<String, TypedValue<?>> execute(final List<ChannelRecord> channelRecords) {
 
-            if (channelRecords != preparedRecords) {
+            if (channelRecords != this.preparedRecords) {
                 // driver changed the record list
                 // fallback to slow mode
-                return Utils.toWireRecordProperties(channelRecords, options);
+                return Utils.toWireRecordProperties(channelRecords, WireAsset.this.options);
             }
 
-            return Utils.toWireRecordProperties(channelRecords, options, recordFillers);
+            return Utils.toWireRecordProperties(channelRecords, WireAsset.this.options, this.recordFillers);
         }
 
-    }
-
-    static class WireDefaultThreadFactory implements ThreadFactory {
-
-        private static final AtomicInteger poolNumber = new AtomicInteger(1);
-        private final ThreadGroup group;
-        private final AtomicInteger threadNumber = new AtomicInteger(1);
-        private final String namePrefix;
-
-        WireDefaultThreadFactory(String name) {
-            SecurityManager s = System.getSecurityManager();
-            group = (s != null) ? s.getThreadGroup() : Thread.currentThread().getThreadGroup();
-            namePrefix = "WirePool-" + poolNumber.getAndIncrement() + "-" + name + "-";
-        }
-
-        public Thread newThread(Runnable r) {
-            Thread t = new Thread(group, r, namePrefix + threadNumber.getAndIncrement(), 0);
-            if (t.isDaemon())
-                t.setDaemon(false);
-            if (t.getPriority() != Thread.NORM_PRIORITY)
-                t.setPriority(Thread.NORM_PRIORITY);
-            return t;
-        }
     }
 }
--- kura/org.eclipse.kura.wire.component.provider/src/main/java/org/eclipse/kura/internal/wire/asset/WireAssetOptions.java
+++ kura/org.eclipse.kura.wire.component.provider/src/main/java/org/eclipse/kura/internal/wire/asset/WireAssetOptions.java
@@ -10,25 +10,17 @@
 
 package org.eclipse.kura.internal.wire.asset;
 
-import static java.util.Objects.nonNull;
-
 import java.util.Map;
 
 class WireAssetOptions {
 
     public static final String EMIT_ALL_CHANNELS_PROP_NAME = "emit.all.channels";
-    public static final String EMIT_THREAD_COUNT_PROP_NAME = "threadCount";
-    public static final String EMIT_THREAD_TIMEOUT_PROP_NAME = "threadTimeout";
-    public static final String EMIT_MUTIPLE_THREAD_PROP_NAME = "emit.mutiplethread";
     public static final String TIMESTAMP_MODE_PROP_NAME = "timestamp.mode";
     public static final String EMIT_ERRORS_PROP_NAME = "emit.errors";
 
     private boolean emitAllChannels;
-    private boolean emitMutipleThread;
     private TimestampMode timestampMode;
     private boolean emitErrors;
-    private int threadCount;
-    private int threadTimeout;
 
     public WireAssetOptions() {
     }
@@ -36,38 +28,15 @@ class WireAssetOptions {
     public WireAssetOptions(Map<String, Object> properties) {
         final Object emitAllChannels = properties.get(EMIT_ALL_CHANNELS_PROP_NAME);
         final Object emitErrors = properties.get(EMIT_ERRORS_PROP_NAME);
-        final Object emitMutipleThread = properties.get(EMIT_MUTIPLE_THREAD_PROP_NAME);
-        final Object threadCount = properties.get(EMIT_THREAD_COUNT_PROP_NAME);
-        final Object threadTimeout = properties.get(EMIT_THREAD_TIMEOUT_PROP_NAME);
 
         this.emitAllChannels = emitAllChannels instanceof Boolean && (Boolean) emitAllChannels;
         this.emitErrors = emitErrors instanceof Boolean && (Boolean) emitErrors;
-        this.emitMutipleThread = emitMutipleThread instanceof Boolean && (Boolean) emitMutipleThread;
-        this.threadCount = 2;
-        this.threadTimeout = 10000;
-        if (nonNull(threadCount) && threadCount instanceof Integer) {
-            this.threadCount = (Integer) threadCount;
-        }
-        if (nonNull(threadTimeout) && threadTimeout instanceof Integer) {
-            this.threadTimeout = (Integer) threadTimeout;
-        }
+
         this.timestampMode = extractTimestampMode(properties);
     }
 
     public boolean emitAllChannels() {
         return emitAllChannels;
-    }
-
-    public boolean emitMutipleThread() {
-        return emitMutipleThread;
-    }
-
-    public int getThreadCount() {
-        return threadCount;
-    }
-
-    public int getThreadTimeout() {
-        return threadTimeout;
     }
 
     public TimestampMode getTimestampMode() {

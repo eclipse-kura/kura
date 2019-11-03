ec912e25d68d7f27244daa2416cab60d6b58f8ee
heyoulin <heyoulin@gmail.com>
2019-11-04 01:45:03 +0800

    remove test code
    Signed-off-by: heyoulin <heyoulin@gmail.com>

--- kura/org.eclipse.kura.core.configuration/src/main/java/org/eclipse/kura/core/configuration/ConfigurationServiceImpl.java
+++ kura/org.eclipse.kura.core.configuration/src/main/java/org/eclipse/kura/core/configuration/ConfigurationServiceImpl.java
@@ -929,8 +929,6 @@ public class ConfigurationServiceImpl implements ConfigurationService, OCDServic
         allPids.addAll(this.waittingForActivatedPids);
         for (String pid : allPids) {
             try {
-                if (pid.equals("唯一"))
-                    logger.debug("");
                 ComponentConfiguration cc = getComponentConfigurationInternal(pid);
                 if (cc != null) {
                     configs.add(cc);

23c897a0efafa02607d1a8a91c6ab49d3a4719cf
pierantoniomerlino <pierantonio.merlino@eurotech.com>
2019-11-06 09:21:49 +0100

    Added missing copyright header
    Signed-off-by: pierantoniomerlino <pierantonio.merlino@eurotech.com>

--- kura/test/org.eclipse.kura.core.testutil/src/main/java/org/eclipse/kura/core/testutil/AssumingIsNotJenkins.java
+++ kura/test/org.eclipse.kura.core.testutil/src/main/java/org/eclipse/kura/core/testutil/AssumingIsNotJenkins.java
@@ -1,3 +1,11 @@
+/*******************************************************************************
+ * Copyright (c) 2019 Eurotech and/or its affiliates and others
+ *
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License v1.0
+ *   which accompanies this distribution, and is available at
+ *   http://www.eclipse.org/legal/epl-v10.html
+ ******************************************************************************/
 package org.eclipse.kura.core.testutil;
 
 import java.io.File;
@@ -5,7 +13,6 @@ import java.io.File;
 import org.junit.AssumptionViolatedException;
 import org.junit.rules.TestRule;
 import org.junit.runner.Description;
-import org.junit.runners.model.Statement;
 
 public class AssumingIsNotJenkins implements TestRule {
 

2d2f412af3657000da7486ef2b395e4b8c753a8a
Maiero <matteo.maiero@eurotech.com>
2020-01-21 17:18:07 +0100

    First set of copyright date changes
    Signed-off-by: Maiero <matteo.maiero@eurotech.com>

--- kura/examples/org.eclipse.kura.example.camel.publisher/src/org/eclipse/kura/example/camel/publisher/ExamplePublisher.java
+++ kura/examples/org.eclipse.kura.example.camel.publisher/src/org/eclipse/kura/example/camel/publisher/ExamplePublisher.java
@@ -1,12 +1,14 @@
 /*******************************************************************************
- * Copyright (c) 2016, 2017 Red Hat Inc and others.
+ * Copyright (c) 2016, 2020 Red Hat Inc and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *     Red Hat Inc - initial API and implementation
+ *     Red Hat Inc
+ *     Eurotech
+ *     
  *******************************************************************************/
 
 package org.eclipse.kura.example.camel.publisher;

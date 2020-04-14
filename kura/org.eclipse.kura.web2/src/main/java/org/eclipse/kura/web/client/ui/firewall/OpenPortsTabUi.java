1b960eb806a1e626d65237430e3e95c5e008c35d
pierantoniomerlino <pierantonio.merlino@eurotech.com>
2020-04-14 12:31:16 +0200

    Added length check on interface name
    Signed-off-by: pierantoniomerlino <pierantonio.merlino@eurotech.com>

--- kura/org.eclipse.kura.web2/src/main/java/org/eclipse/kura/web/client/ui/firewall/OpenPortsTabUi.java
+++ kura/org.eclipse.kura.web2/src/main/java/org/eclipse/kura/web/client/ui/firewall/OpenPortsTabUi.java
@@ -597,16 +597,18 @@ public class OpenPortsTabUi extends Composite implements Tab, ButtonBar.Listener
             }
         });
         this.permittedI.addBlurHandler(event -> {
-            if (!OpenPortsTabUi.this.permittedI.getText().trim().matches(FieldType.ALPHANUMERIC.getRegex())
-                    && OpenPortsTabUi.this.permittedI.getText().trim().length() > 0) {
+            if ((!OpenPortsTabUi.this.permittedI.getText().trim().matches(FieldType.ALPHANUMERIC.getRegex())
+                    && OpenPortsTabUi.this.permittedI.getText().trim().length() > 0)
+                    || OpenPortsTabUi.this.permittedI.getText().trim().length() > 15) {
                 OpenPortsTabUi.this.groupPermittedI.setValidationState(ValidationState.ERROR);
             } else {
                 OpenPortsTabUi.this.groupPermittedI.setValidationState(ValidationState.NONE);
             }
         });
         this.unpermittedI.addBlurHandler(event -> {
-            if (!OpenPortsTabUi.this.unpermittedI.getText().trim().matches(FieldType.ALPHANUMERIC.getRegex())
-                    && OpenPortsTabUi.this.unpermittedI.getText().trim().length() > 0) {
+            if ((!OpenPortsTabUi.this.unpermittedI.getText().trim().matches(FieldType.ALPHANUMERIC.getRegex())
+                    && OpenPortsTabUi.this.unpermittedI.getText().trim().length() > 0)
+                    || OpenPortsTabUi.this.unpermittedI.getText().trim().length() > 15) {
                 OpenPortsTabUi.this.groupUnpermittedI.setValidationState(ValidationState.ERROR);
             } else {
                 OpenPortsTabUi.this.groupPermittedI.setValidationState(ValidationState.NONE);

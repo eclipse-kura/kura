ed2d66c680d8f29af422ae8ee66198ad8de1b9b1
Maiero <matteo.maiero@eurotech.com>
2020-04-22 15:02:34 +0200

    Fixed issue in can service with two can interfaces.
    Now the different can interfaces can concurrently read and write.
    
    Signed-off-by: Maiero <matteo.maiero@eurotech.com>

--- kura/org.eclipse.kura.protocol.can/src/main/java/org/eclipse/kura/protocol/can/CanConnectionServiceImpl.java
+++ kura/org.eclipse.kura.protocol.can/src/main/java/org/eclipse/kura/protocol/can/CanConnectionServiceImpl.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011, 2017 Eurotech and/or its affiliates
+ * Copyright (c) 2011, 2020 Eurotech and/or its affiliates
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -26,12 +26,12 @@ import de.entropia.can.CanSocket.Mode;
 
 public class CanConnectionServiceImpl implements CanConnectionService {
 
-    private static final Logger s_logger = LoggerFactory.getLogger(CanConnectionServiceImpl.class);
-    
+    private static final Logger logger = LoggerFactory.getLogger(CanConnectionServiceImpl.class);
+
     private CanSocket socket = null;
 
     protected void activate() {
-        s_logger.info("activating CanConnectionService");
+        logger.info("activating CanConnectionService");
     }
 
     protected void deactivate() {
@@ -39,43 +39,51 @@ public class CanConnectionServiceImpl implements CanConnectionService {
             try {
                 this.socket.close();
             } catch (IOException e) {
-                s_logger.error("Error closing CAN socket");
+                logger.error("Error closing CAN socket");
             }
         }
     }
 
     @Override
     public void connectCanSocket() throws IOException {
-        this.socket = new CanSocket(Mode.RAW);
-        this.socket.setLoopbackMode(false);
-        this.socket.bind(CanSocket.CAN_ALL_INTERFACES);
+        if (this.socket == null) {
+            this.socket = new CanSocket(Mode.RAW);
+            this.socket.setLoopbackMode(false);
+            this.socket.bind(CanSocket.CAN_ALL_INTERFACES);
+        }
     }
-    
+
     @Override
     public void disconnectCanSocket() throws IOException {
         if (this.socket != null) {
             this.socket.close();
+            this.socket = null;
         }
     }
-    
+
     @Override
     public void sendCanMessage(String ifName, int canId, byte[] message) throws KuraException, IOException {
         if (message.length > 8) {
             throw new KuraException(KuraErrorCode.OPERATION_NOT_SUPPORTED, "CAN send : Incorrect frame length");
         }
+        if (this.socket == null) {
+            throw new KuraException(KuraErrorCode.BAD_REQUEST, "CAN Socket must be open before sending");
+        }
 
         try {
             CanInterface canif = new CanInterface(this.socket, ifName);
-            socket.bind(canif);
-            socket.send(new CanFrame(canif, new CanId(canId), message));
+            this.socket.send(new CanFrame(canif, new CanId(canId), message));
         } catch (IOException e) {
-            s_logger.error("Error on CanSocket in sendCanMessage: {}", e.getMessage());
+            logger.error("Error on CanSocket in sendCanMessage");
             throw e;
         }
     }
 
     @Override
     public CanMessage receiveCanMessage(int canId, int canMask) throws IOException {
+        if (this.socket == null) {
+            throw new IllegalStateException("CAN Socket must be open before receiving");
+        }
         try {
             if (canId >= 0) {
                 this.socket.setCanFilter(canId, canMask);
@@ -88,7 +96,7 @@ public class CanConnectionServiceImpl implements CanConnectionService {
             cm.setData(cf.getData());
             return cm;
         } catch (IOException e) {
-            s_logger.error("Error on CanSocket in receiveCanMessage: {}", e.getMessage());
+            logger.error("Error on CanSocket in receiveCanMessage");
             throw e;
         }
     }

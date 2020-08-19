30db04bff9d6c474568e715da90ca195a043f195
henry-lp <33935517+henry-lp@users.noreply.github.com>
2020-08-19 09:07:42 +0200

    Proposal for patching SonarQube rule 3032 (#2987)
    * Proposal for patching SonarQube rule 3032
    
    Signed-off-by: henry-lp <tailp@kth.se>
    
    * reset change for one line
    
    Signed-off-by: henry-lp <tailp@kth.se>

--- target-platform/com.codeminders.hidapi-parent/com.codeminders.hidapi/src/main/java/com/codeminders/hidapplet/HidApplet.java
+++ target-platform/com.codeminders.hidapi-parent/com.codeminders.hidapi/src/main/java/com/codeminders/hidapplet/HidApplet.java
@@ -8,7 +8,7 @@ import java.awt.*;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.InputStream;
-import java.io.OutputStream;;
+import java.io.OutputStream;
 
 public class HidApplet extends JApplet
 {
@@ -37,7 +37,7 @@ public class HidApplet extends JApplet
         System.out.println("Using library: " + library);
         try
         {
-            InputStream libSrc = getClass().getClassLoader().getResourceAsStream("native/" + library);
+            InputStream libSrc = Thread.currentThread().getContextClassLoader().getResourceAsStream("native/" + library);
             if (libSrc == null) {
                 System.err.println("No library found");
                 return;

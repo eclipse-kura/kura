daa740457d99ad5fcb277a50e04e94b915a74e88
Matteo Maiero <matteo.maiero@eurotech.com>
2020-08-25 10:39:51 +0200

    Restoring the previous regex validation. (#3038)
    * Restoring the previous regex validation.
    
    The GWT implementation seems to be incompatible with the regexes defined for wifi and
    have some known limitations.
    
    Signed-off-by: Maiero <matteo.maiero@eurotech.com>
    
    * Updated error message to have it more comprehensive
    
    Signed-off-by: Maiero <matteo.maiero@eurotech.com>

--- kura/org.eclipse.kura.web2/src/main/java/org/eclipse/kura/web/client/ui/RegexValidator.java
+++ kura/org.eclipse.kura.web2/src/main/java/org/eclipse/kura/web/client/ui/RegexValidator.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2019 Eurotech and/or its affiliates and others
+ * Copyright (c) 2019, 2020 Eurotech and/or its affiliates and others
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -9,16 +9,10 @@
  *******************************************************************************/
 package org.eclipse.kura.web.client.ui;
 
-import com.google.gwt.regexp.shared.RegExp;
-
 public class RegexValidator extends PredicateValidator {
 
     public RegexValidator(final String pattern, final String message) {
-        this(pattern, "g", message);
-    }
-
-    public RegexValidator(final String pattern, final String flags, final String message) {
-        super(v -> RegExp.compile(pattern, flags).exec(v) != null, message);
+        super(v -> v.matches(pattern), message);
     }
 
 }

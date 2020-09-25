ff28d67c48683772ffd970a6b5dfa9e885d06373
Andrej Krota <andrej.krota@gmail.com>
2020-09-25 19:43:32 +0200

    Fixed DNS service using bind9 on Armbian 20.04
    The service checks if the file is called /etc/init.d/named or
    /etc/init.d/bind9. The bind9 allow-query parameter is now generated
    using the host's network ip instead of the host's ip.
    
    Signed-off-by: Andrej Krota <andrej.krota@gmail.com>

--- kura/org.eclipse.kura.linux.sysv.provider/src/main/java/org/eclipse/kura/internal/linux/sysv/net/dns/LinuxDnsServerSysV.java
+++ kura/org.eclipse.kura.linux.sysv.provider/src/main/java/org/eclipse/kura/internal/linux/sysv/net/dns/LinuxDnsServerSysV.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2018, 2019 Eurotech and others
+ * Copyright (c) 2018, 2020 Eurotech and others
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -8,8 +8,11 @@
  *
  * Contributors:
  *     Eurotech
+ *     3 Port d.o.o.
  *******************************************************************************/
 package org.eclipse.kura.internal.linux.sysv.net.dns;
+
+import java.io.File;
 
 import org.eclipse.kura.internal.linux.net.dns.DnsServerService;
 import org.eclipse.kura.linux.net.dns.LinuxDnsServer;
@@ -17,6 +20,7 @@ import org.eclipse.kura.linux.net.dns.LinuxDnsServer;
 public class LinuxDnsServerSysV extends LinuxDnsServer implements DnsServerService {
 
     private static final String BIND9_COMMAND = "/etc/init.d/bind9";
+    private static final String NAMED_COMMAND = "/etc/init.d/named";
 
     @Override
     public String getDnsConfigFileName() {
@@ -35,16 +39,20 @@ public class LinuxDnsServerSysV extends LinuxDnsServer implements DnsServerServi
 
     @Override
     public String[] getDnsStartCommand() {
-        return new String[] { BIND9_COMMAND, "start" };
+        return new String[] { getCommandName(), "start" };
     }
 
     @Override
     public String[] getDnsRestartCommand() {
-        return new String[] { BIND9_COMMAND, "restart" };
+        return new String[] { getCommandName(), "restart" };
     }
 
     @Override
     public String[] getDnsStopCommand() {
-        return new String[] { BIND9_COMMAND, "stop" };
+        return new String[] { getCommandName(), "stop" };
+    }
+
+    private String getCommandName() {
+        return new File(NAMED_COMMAND).exists() ? NAMED_COMMAND : BIND9_COMMAND;
     }
 }

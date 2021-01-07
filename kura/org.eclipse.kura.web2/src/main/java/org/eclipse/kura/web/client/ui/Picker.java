765fd6f3a6d85a49e2a85a8f807c1ac6e7622dea
Nicola Timeus <nicola.timeus@eurotech.com>
2021-01-07 15:42:11 +0100

    Updated picker modals to not report error on show
    Signed-off-by: Nicola Timeus <nicola.timeus@eurotech.com>

--- kura/org.eclipse.kura.web2/src/main/java/org/eclipse/kura/web/client/ui/Picker.java
+++ kura/org.eclipse.kura.web2/src/main/java/org/eclipse/kura/web/client/ui/Picker.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2020 Eurotech and/or its affiliates and others
+ * Copyright (c) 2020, 2021 Eurotech and/or its affiliates and others
  *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
@@ -181,6 +181,7 @@ public class Picker extends Composite implements HasId {
             Picker.this.inputPanel.add(input);
 
             input.validate();
+            input.reset();
             Picker.this.modal.show();
         }
     }

1311ea27e60c887a90a0ff836c3353b598fe6e89
Maiero <matteo.maiero@eurotech.com>
2021-03-23 15:55:15 +0100

    Partial rollback on a previous PR to restore empty string support
    Signed-off-by: Maiero <matteo.maiero@eurotech.com>

--- kura/org.eclipse.kura.web2/src/main/java/org/eclipse/kura/web/client/ui/AbstractServicesUi.java
+++ kura/org.eclipse.kura.web2/src/main/java/org/eclipse/kura/web/client/ui/AbstractServicesUi.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
  * Copyright (c) 2016, 2020 Eurotech and/or its affiliates and others
- * 
+ *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
  * which is available at https://www.eclipse.org/legal/epl-2.0/
- * 
+ *
  * SPDX-License-Identifier: EPL-2.0
- * 
+ *
  * Contributors:
  *  Eurotech
  *  Red Hat
@@ -643,8 +643,8 @@ public abstract class AbstractServicesUi extends Composite {
             case STRING:
                 TextBoxBase tb = (TextBoxBase) wg;
                 String value = tb.getText();
-                if (value != null && !value.trim().isEmpty()) {
-                    return value.trim();
+                if (value != null) {
+                    return value;
                 } else {
                     return null;
                 }

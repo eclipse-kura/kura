d2aea4d34a1341228caa1f0180ac0738c93e5c10
Salvatore Coppola <salvatore.coppola@eurotech.com>
2021-06-04 14:22:44 +0200

    Removed unrelated file

--- build-all.bat
+++
@@ -1,4 +0,0 @@
-mvn -f target-platform/pom.xml clean install "-Dmaven.test.skip=true" &&^
-mvn -Pcan -f kura/pom.xml clean install "-Dmaven.test.skip=true" &&^
-mvn -Pcan -f kura/examples/pom.xml clean install "-Dmaven.test.skip=true" 
-rem && mvn -f kura/distrib/pom.xml clean install "-Dmaven.test.skip=true"

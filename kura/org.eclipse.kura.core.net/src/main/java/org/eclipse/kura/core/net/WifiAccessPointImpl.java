8e295ed6b3d2b89280043b16758e0034c9e65d90
Salvatore Coppola <salvatore.coppola@eurotech.com>
2021-09-09 16:36:24 +0200

    Now channel is parsed from iw scan.

--- kura/org.eclipse.kura.core.net/src/main/java/org/eclipse/kura/core/net/WifiAccessPointImpl.java
+++ kura/org.eclipse.kura.core.net/src/main/java/org/eclipse/kura/core/net/WifiAccessPointImpl.java
@@ -167,12 +167,10 @@ public class WifiAccessPointImpl implements WifiAccessPoint {
 
     @Override
     public boolean equals(Object obj) {
-        if (this == obj) {
+        if (this == obj)
             return true;
-        }
-        if (obj == null) {
+        if (obj == null)
             return false;
-        }
         if (getClass() != obj.getClass())
             return false;
         WifiAccessPointImpl other = (WifiAccessPointImpl) obj;

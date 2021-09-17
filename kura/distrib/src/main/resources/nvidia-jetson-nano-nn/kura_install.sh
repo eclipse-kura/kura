3420fb2cf4e4ee3fe0cf3fde6bda7f8f7aa9b185
pierantoniomerlino <pierantonio.merlino@eurotech.com>
2021-09-17 15:44:39 +0200

    Added support for Nvidia Jetson Nano NN
    Signed-off-by: pierantoniomerlino <pierantonio.merlino@eurotech.com>

---
+++ kura/distrib/src/main/resources/nvidia-jetson-nano-nn/kura_install.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+#
+#  Copyright (c) 2011, 2021 Eurotech and/or its affiliates and others
+#
+#  This program and the accompanying materials are made
+#  available under the terms of the Eclipse Public License 2.0
+#  which is available at https://www.eclipse.org/legal/epl-2.0/
+#
+#  SPDX-License-Identifier: EPL-2.0
+#
+#  Contributors:
+#   Eurotech
+#   Cavium
+#
+
+INSTALL_DIR=/opt/eclipse
+
+#create known kura install location
+ln -sf ${INSTALL_DIR}/kura_* ${INSTALL_DIR}/kura
+
+#set up Kura init
+sed "s|INSTALL_DIR|${INSTALL_DIR}|" ${INSTALL_DIR}/kura/install/kura.service > /lib/systemd/system/kura.service
+systemctl daemon-reload
+systemctl enable kura
+chmod +x ${INSTALL_DIR}/kura/bin/*.sh
+
+mkdir -p ${INSTALL_DIR}/kura/data
+
+#set up logrotate - no need to restart as it is a cronjob
+cp ${INSTALL_DIR}/kura/install/logrotate.conf /etc/logrotate.conf
+cp ${INSTALL_DIR}/kura/install/kura.logrotate /etc/logrotate.d/kura
+
+# setup snapshot_0 recovery folder
+if [ ! -d ${INSTALL_DIR}/kura/.data ]; then
+    mkdir ${INSTALL_DIR}/kura/.data
+fi
+
+#set up users and grant permissions to them
+cp ${INSTALL_DIR}/kura/install/manage_kura_users.sh ${INSTALL_DIR}/kura/.data/manage_kura_users.sh
+chmod 700 ${INSTALL_DIR}/kura/.data/manage_kura_users.sh
+${INSTALL_DIR}/kura/.data/manage_kura_users.sh -i 
+
+#copy snapshot_0.xml
+cp ${INSTALL_DIR}/kura/user/snapshots/snapshot_0.xml ${INSTALL_DIR}/kura/.data/snapshot_0.xml
+
+#set up recover default configuration script
+cp ${INSTALL_DIR}/kura/install/recover_default_config.init ${INSTALL_DIR}/kura/bin/.recoverDefaultConfig.sh
+chmod +x ${INSTALL_DIR}/kura/bin/.recoverDefaultConfig.sh
+
+keytool -genkey -alias localhost -keyalg RSA -keysize 2048 -keystore /opt/eclipse/kura/user/security/httpskeystore.ks -deststoretype pkcs12 -dname "CN=Kura, OU=Kura, O=Eclipse Foundation, L=Ottawa, S=Ontario, C=CA" -ext ku=digitalSignature,nonRepudiation,keyEncipherment,dataEncipherment,keyAgreement,keyCertSign -ext eku=serverAuth,clientAuth,codeSigning,timeStamping -validity 1000 -storepass changeit -keypass changeit  

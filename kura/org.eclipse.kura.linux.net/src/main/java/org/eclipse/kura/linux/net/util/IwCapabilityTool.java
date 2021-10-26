cbb5bbbcc23150a89b08a1bece7e620412ba2550
Salvatore Coppola <salvatore.coppola@eurotech.com>
2021-10-26 15:22:21 +0200

    Fixed possible catastrophic backtracking#5

--- kura/org.eclipse.kura.linux.net/src/main/java/org/eclipse/kura/linux/net/util/IwCapabilityTool.java
+++ kura/org.eclipse.kura.linux.net/src/main/java/org/eclipse/kura/linux/net/util/IwCapabilityTool.java
@@ -49,7 +49,7 @@ public class IwCapabilityTool {
     private static final Pattern COUNTRY_PATTERN = Pattern.compile("country (..): .*");
 
     private static final Pattern FREQUENCY_CHANNEL_PATTERN = Pattern
-            .compile("^\\* ([0-9]+) MHz \\[([0-9]+)\\](?: \\((.+) dBm\\)){0,1}.*$");
+            .compile("^\\* ([0-9]+) MHz \\[([0-9]+)\\](?: \\((\\d{0,3}\\.\\d{0,2}) dBm\\)){0,1}[\\(\\w\\\\\\s,)]*$");
 
     private enum ParseState {
         HAS_RSN,

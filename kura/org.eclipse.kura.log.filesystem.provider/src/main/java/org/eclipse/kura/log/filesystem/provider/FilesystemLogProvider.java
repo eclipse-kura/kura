26991194816771a3adbd58437f1974c9ea9f8bf7
Marcello Martina <martina.marcello.rinaldo@outlook.com>
2021-11-15 18:02:37 +0100

    Separated kura log parser in a different class
    Signed-off-by: Marcello Martina <martina.marcello.rinaldo@outlook.com>

--- kura/org.eclipse.kura.log.filesystem.provider/src/main/java/org/eclipse/kura/log/filesystem/provider/FilesystemLogProvider.java
+++ kura/org.eclipse.kura.log.filesystem.provider/src/main/java/org/eclipse/kura/log/filesystem/provider/FilesystemLogProvider.java
@@ -16,7 +16,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
-import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
@@ -69,49 +68,6 @@ public class FilesystemLogProvider implements ConfigurableComponent, LogProvider
         this.registeredListeners.remove(listener);
     }
 
-    private void notifyListeners(String newLogLine) {
-
-        if (newLogLine != null) {
-            String timestamp = "example_timestamp";
-            String pid = "example_pid";
-            String priority = "example_priority";
-            String message = newLogLine;
-
-            if (this.filePath.contains("kura.log")) {
-                /*
-                 * kura.log message format:
-                 * 
-                 * _SOURCE_REALTIME_TIMESTAMP [PID] PRIORITY MESSAGE_WITH_POSSIBLE_SPACES
-                 */
-                String[] splits = newLogLine.split(" ");
-                if (splits.length > 3) {
-                    timestamp = splits[0];
-                    pid = splits[1];
-                    pid = pid.replace("[", "");
-                    pid = pid.replace("]", "");
-                    priority = splits[2];
-                    StringBuilder sb = new StringBuilder();
-                    for (int i = 3; i < splits.length; i++) {
-                        sb.append(splits[i]);
-                        sb.append(" ");
-                    }
-                    message = sb.toString();
-                }
-            }
-
-            for (LogListener listener : this.registeredListeners) {
-                Map<String, Object> entryProperties = new HashMap<>();
-                entryProperties.put("_SOURCE_REALTIME_TIMESTAMP", timestamp);
-                entryProperties.put("_PID", pid);
-                entryProperties.put("MESSAGE", message.trim());
-                entryProperties.put("PRIORITY", priority);
-                entryProperties.put("SYSLOG_IDENTIFIER", "Kura");
-                entryProperties.put("_TRANSPORT", this.filePath);
-                listener.newLogEntry(new LogEntry(entryProperties));
-            }
-        }
-    }
-
     public class FileLogReader extends Thread {
 
         private static final long SAMPLE_INTERVAL = 100;
@@ -149,13 +105,26 @@ public class FilesystemLogProvider implements ConfigurableComponent, LogProvider
                     sleep(SAMPLE_INTERVAL);
                 }
             } catch (FileNotFoundException fnf) {
-                logger.error("File '{}' not found.", this.logFile);
+                logger.error("File '{}' not found.", this.logFile.getPath());
             } catch (IOException ioe) {
-                logger.error("IOException {}", ioe.getStackTrace());
+                logger.error("IOException {}", ioe.getMessage());
+            } catch (InterruptedException ie) {
+                // nothing to do
             } catch (Exception e) {
-                logger.error("Unexpected exception in FilesystemLogProvider: {}", e.getStackTrace());
+                logger.error("Unexpected exception in FilesystemLogProvider: {}", e.getMessage());
             } finally {
                 Thread.currentThread().interrupt();
+            }
+        }
+
+        private void notifyListeners(String newLogLine) {
+
+            if (newLogLine != null) {
+                for (LogListener listener : FilesystemLogProvider.this.registeredListeners) {
+                    LogEntry entry = KuraLogLineParser.stringToLogEntry(newLogLine,
+                            FilesystemLogProvider.this.filePath);
+                    listener.newLogEntry(entry);
+                }
             }
         }
     }
---
+++ kura/org.eclipse.kura.log.filesystem.provider/src/main/java/org/eclipse/kura/log/filesystem/provider/KuraLogLineParser.java
@@ -0,0 +1,113 @@
+/*******************************************************************************
+ * Copyright (c) 2021 Eurotech and/or its affiliates and others
+ * 
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ * 
+ * SPDX-License-Identifier: EPL-2.0
+ * 
+ * Contributors:
+ *  Eurotech
+ *******************************************************************************/
+package org.eclipse.kura.log.filesystem.provider;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.eclipse.kura.log.LogEntry;
+
+public class KuraLogLineParser {
+
+    private String timestamp;
+    private String pid;
+    private String priority;
+    private String message;
+    private String transport;
+    private String syslogIdentifier;
+    private String stackTrace;
+
+    private static KuraLogLineParser instance;
+
+    private KuraLogLineParser(String logLine, String filepath) {
+        this.timestamp = "example_timestamp";
+        this.pid = "example_pid";
+        this.priority = "example_priority";
+        this.message = logLine;
+        this.transport = filepath;
+        this.syslogIdentifier = "Kura";
+        this.stackTrace = "";
+    }
+
+    public static LogEntry stringToLogEntry(String logLine, String filepath) {
+        instance = new KuraLogLineParser(logLine, filepath);
+
+        if (filepath.contains("kura.log")) {
+            instance.parseKuraLog(logLine);
+        }
+
+        if (filepath.contains("kura-audit.log")) {
+            instance.parseKuraAuditLog(logLine);
+        }
+
+        return instance.generateLogEntry();
+    }
+
+    /*
+     * kura.log message format:
+     * 
+     * _SOURCE_REALTIME_TIMESTAMP [PID] PRIORITY MESSAGE_WITH_POSSIBLE_SPACES
+     */
+    private void parseKuraLog(String logLine) {
+        String[] splits = logLine.split(" ");
+        if (splits.length >= 3) {
+            instance.timestamp = splits[0];
+            instance.pid = splits[1];
+            instance.pid = instance.pid.replace("[", "");
+            instance.pid = instance.pid.replace("]", "");
+            instance.priority = splits[2];
+            StringBuilder sb = new StringBuilder();
+            for (int i = 3; i < splits.length; i++) {
+                sb.append(splits[i]);
+                sb.append(" ");
+            }
+            instance.message = sb.toString().trim();
+        }
+    }
+
+    /*
+     * kura-audit.log message format:
+     * 
+     * ID TIMESTAMP DEVICE SYSLOG_IDENTIFIER - - [RequestContext@28392 category="AuditLogger"
+     * exception="STACKTRACE" priority="PRIORITY" thread="PID"] MESSAGE_WITH_POSSIBLE_SPACES
+     */
+    private void parseKuraAuditLog(String logLine) {
+        String[] splits = logLine.split(" ");
+        if (splits.length >= 11) {
+            instance.timestamp = splits[1];
+            instance.transport = splits[2];
+            instance.syslogIdentifier = splits[3];
+            instance.stackTrace = splits[8].replace("exception=", "").replace("\"", "");
+            instance.priority = splits[9].replace("priority=", "").replace("\"", "");
+            instance.pid = splits[10].replace("thread=", "").replace("\"", "").replace("]", "");
+            StringBuilder sb = new StringBuilder();
+            for (int i = 11; i < splits.length; i++) {
+                sb.append(splits[i]);
+                sb.append(" ");
+            }
+            instance.message = sb.toString().trim();
+        }
+    }
+
+    private LogEntry generateLogEntry() {
+        Map<String, Object> entryProperties = new HashMap<>();
+        entryProperties.put("_SOURCE_REALTIME_TIMESTAMP", instance.timestamp);
+        entryProperties.put("_PID", instance.pid);
+        entryProperties.put("MESSAGE", instance.message);
+        entryProperties.put("PRIORITY", instance.priority);
+        entryProperties.put("SYSLOG_IDENTIFIER", instance.syslogIdentifier);
+        entryProperties.put("_TRANSPORT", instance.transport);
+        entryProperties.put("STACKTRACE", instance.stackTrace);
+        return new LogEntry(entryProperties);
+    }
+}

69aca08693e13c521d42c95c1e3e543003e613b7
Nicola Timeus <nicola.timeus@eurotech.com>
2022-02-05 20:15:55 +0100

    Reverted change to ConfigurationService
    Signed-off-by: Nicola Timeus <nicola.timeus@eurotech.com>

--- kura/org.eclipse.kura.core.configuration/src/main/java/org/eclipse/kura/core/configuration/ConfigurationServiceImpl.java
+++ kura/org.eclipse.kura.core.configuration/src/main/java/org/eclipse/kura/core/configuration/ConfigurationServiceImpl.java
@@ -440,7 +440,6 @@ public class ConfigurationServiceImpl implements ConfigurationService, OCDServic
             mergeWithDefaults(ocd, mergedProperties);
 
             mergedProperties.put(ConfigurationService.KURA_SERVICE_PID, pid);
-            mergedProperties.put(ConfigurationAdmin.SERVICE_FACTORYPID, factoryPid);
 
             Dictionary<String, Object> dict = CollectionsUtil.mapToDictionary(mergedProperties);
             Configuration config = this.configurationAdmin.getConfiguration(servicePid, "?");

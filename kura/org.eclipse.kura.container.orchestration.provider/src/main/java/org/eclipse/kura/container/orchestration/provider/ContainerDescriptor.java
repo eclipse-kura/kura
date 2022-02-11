57117d7169518aa9a5ffaac06521615a47c121df
Gregory Ivo <gregorybivo@gmail.com>
2022-02-11 18:35:08 -0500

    moved dependancies out of bundle and into kura

--- kura/org.eclipse.kura.container.orchestration.provider/src/main/java/org/eclipse/kura/container/orchestration/provider/ContainerDescriptor.java
+++ kura/org.eclipse.kura.container.orchestration.provider/src/main/java/org/eclipse/kura/container/orchestration/provider/ContainerDescriptor.java
@@ -20,7 +20,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
-import org.apache.commons.lang.ArrayUtils;
+import org.apache.commons.lang3.ArrayUtils;
 
 /**
  * Object which represents a container. Used to track running containers.

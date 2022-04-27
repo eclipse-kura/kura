97dc422172e057445434e1dd21a5b6c4f3e80d31
Marcello Martina <martina.marcello.rinaldo@outlook.com>
2022-04-27 17:19:04 +0200

    Added audit annotation to updatePassword RPC
    Signed-off-by: Marcello Martina <martina.marcello.rinaldo@outlook.com>

--- kura/org.eclipse.kura.web2/src/main/java/org/eclipse/kura/web/shared/service/GwtSessionService.java
+++ kura/org.eclipse.kura.web2/src/main/java/org/eclipse/kura/web/shared/service/GwtSessionService.java
@@ -31,6 +31,7 @@ public interface GwtSessionService extends RemoteService {
 
     public GwtUserConfig getUserConfig(GwtXSRFToken token) throws GwtKuraException;
 
+    @Audit(componentName = "UI Session", description = "Password update")
     public void updatePassword(GwtXSRFToken token, final String oldPassword, final String newPassword)
             throws GwtKuraException;
 }

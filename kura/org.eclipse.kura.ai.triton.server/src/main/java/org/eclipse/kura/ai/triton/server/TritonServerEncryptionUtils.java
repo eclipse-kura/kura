860ed5520d8274789473f23a7321f8a8c539f97b
Mattia Dal Ben <matthewdibi@gmail.com>
2022-05-10 11:04:26 +0200

    feat(EncryptionUtils): add class and methods skeleton
    Signed-off-by: Mattia Dal Ben <matthewdibi@gmail.com>

---
+++ kura/org.eclipse.kura.ai.triton.server/src/main/java/org/eclipse/kura/ai/triton/server/TritonServerEncryptionUtils.java
@@ -0,0 +1,29 @@
+package org.eclipse.kura.ai.triton.server;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TritonServerEncryptionUtils {
+
+    private static final Logger logger = LoggerFactory.getLogger(TritonServerEncryptionUtils.class);
+
+    private TritonServerEncryptionUtils() {
+        // TODO
+    }
+
+    protected static void createDecryptionFolder(String folderPath) {
+        // TODO
+    }
+
+    protected static void decryptModel(String password, String inputFilePath, String outputFolder) {
+        // TODO
+    }
+
+    protected static void unzipModel(String inputFilePath, String outputFolder) {
+        // TODO
+    }
+
+    protected static void deleteModel(String modelName) {
+        // TODO
+    }
+}

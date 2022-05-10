dce2ea55678ed2d6a786d6c579aff61711206264
Mattia Dal Ben <matthewdibi@gmail.com>
2022-05-10 11:05:48 +0200

    test(EncryptionUtils): add test skeleton
    Signed-off-by: Mattia Dal Ben <matthewdibi@gmail.com>

---
+++ kura/test/org.eclipse.kura.ai.triton.server.test/src/test/java/org/eclipse/kura/ai/triton/server/TritonServerEncryptionUtilsTest.java
@@ -0,0 +1,14 @@
+package org.eclipse.kura.ai.triton.server;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class TritonServerEncryptionUtilsTest {
+
+    @Test
+    public void sanityCheckTest() {
+        assertTrue(true);
+    }
+
+}

064aa01c91ca3047c9524dff5a8defe86c7acbc3
Mattia Dal Ben <matthewdibi@gmail.com>
2022-05-11 15:59:35 +0200

    feat(EncryptionUtils): add first implementation of decryptModel method
    Signed-off-by: Mattia Dal Ben <matthewdibi@gmail.com>

--- kura/org.eclipse.kura.ai.triton.server/src/main/java/org/eclipse/kura/ai/triton/server/TritonServerEncryptionUtils.java
+++ kura/org.eclipse.kura.ai.triton.server/src/main/java/org/eclipse/kura/ai/triton/server/TritonServerEncryptionUtils.java
@@ -13,15 +13,32 @@
 
 package org.eclipse.kura.ai.triton.server;
 
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.attribute.PosixFilePermission;
+import java.security.Security;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.openpgp.PGPCompressedData;
+import org.bouncycastle.openpgp.PGPEncryptedDataList;
+import org.bouncycastle.openpgp.PGPException;
+import org.bouncycastle.openpgp.PGPLiteralData;
+import org.bouncycastle.openpgp.PGPPBEEncryptedData;
+import org.bouncycastle.openpgp.PGPUtil;
+import org.bouncycastle.openpgp.jcajce.JcaPGPObjectFactory;
+import org.bouncycastle.openpgp.operator.jcajce.JcaPGPDigestCalculatorProviderBuilder;
+import org.bouncycastle.openpgp.operator.jcajce.JcePBEDataDecryptorFactoryBuilder;
+import org.bouncycastle.util.io.Streams;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -49,8 +66,22 @@ public class TritonServerEncryptionUtils {
         Files.setPosixFilePermissions(targetFolderPath, permissions);
     }
 
-    protected static void decryptModel(String password, String inputFilePath, String outputFolder) throws IOException {
-        // TODO
+    protected static void decryptModel(String password, String inputFilePath, String outputFilePath)
+            throws IOException {
+        Security.addProvider(new BouncyCastleProvider());
+
+        InputStream in = new BufferedInputStream(new FileInputStream(inputFilePath));
+        OutputStream out = new FileOutputStream(outputFilePath);
+        try {
+            decryptFile(in, out, password.toCharArray());
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (PGPException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        in.close();
     }
 
     protected static void unzipModel(String inputFilePath, String outputFolder) {
@@ -59,5 +90,53 @@ public class TritonServerEncryptionUtils {
 
     protected static void deleteModel(String modelName) {
         // TODO
+    }
+
+    private static void decryptFile(InputStream inStream, OutputStream outStream, char[] passPhrase)
+            throws IOException, PGPException {
+        inStream = PGPUtil.getDecoderStream(inStream);
+
+        JcaPGPObjectFactory pgpF = new JcaPGPObjectFactory(inStream);
+        PGPEncryptedDataList enc;
+        Object currentObj = pgpF.nextObject();
+
+        if (currentObj instanceof PGPEncryptedDataList) {
+            enc = (PGPEncryptedDataList) currentObj;
+        } else {
+            enc = (PGPEncryptedDataList) pgpF.nextObject();
+        }
+
+        PGPPBEEncryptedData pbe = (PGPPBEEncryptedData) enc.get(0);
+
+        InputStream clear = pbe.getDataStream(new JcePBEDataDecryptorFactoryBuilder(
+                new JcaPGPDigestCalculatorProviderBuilder().setProvider("BC").build()).setProvider("BC")
+                        .build(passPhrase));
+
+        JcaPGPObjectFactory pgpFact = new JcaPGPObjectFactory(clear);
+
+        currentObj = pgpFact.nextObject();
+        if (currentObj instanceof PGPCompressedData) {
+            PGPCompressedData cData = (PGPCompressedData) currentObj;
+
+            pgpFact = new JcaPGPObjectFactory(cData.getDataStream());
+
+            currentObj = pgpFact.nextObject();
+        }
+
+        PGPLiteralData ld = (PGPLiteralData) currentObj;
+        InputStream unc = ld.getInputStream();
+
+        Streams.pipeAll(unc, outStream);
+        outStream.close();
+
+        if (pbe.isIntegrityProtected()) {
+            if (!pbe.verify()) {
+                logger.error("File failed integrity check");
+            } else {
+                logger.info("File integrity check passed");
+            }
+        } else {
+            logger.info("No file integrity check");
+        }
     }
 }

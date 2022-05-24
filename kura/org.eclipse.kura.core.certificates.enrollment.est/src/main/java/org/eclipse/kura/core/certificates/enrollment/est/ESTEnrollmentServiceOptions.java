4dbc5d91fea296fc91c1af65e31ad196e932cc91
Salvatore Coppola <salvatore.coppola@eurotech.com>
2022-05-24 16:02:21 +0200

    Fixed style.

--- kura/org.eclipse.kura.core.certificates.enrollment.est/src/main/java/org/eclipse/kura/core/certificates/enrollment/est/ESTEnrollmentServiceOptions.java
+++ kura/org.eclipse.kura.core.certificates.enrollment.est/src/main/java/org/eclipse/kura/core/certificates/enrollment/est/ESTEnrollmentServiceOptions.java
@@ -99,12 +99,15 @@ public class ESTEnrollmentServiceOptions {
 
     @Override
     public boolean equals(Object obj) {
-        if (this == obj)
+        if (this == obj) {
             return true;
-        if (obj == null)
+        }
+        if (obj == null) {
             return false;
-        if (getClass() != obj.getClass())
+        }
+        if (getClass() != obj.getClass()) {
             return false;
+        }
         ESTEnrollmentServiceOptions other = (ESTEnrollmentServiceOptions) obj;
         return Objects.equals(client, other.client) && Objects.equals(enabled, other.enabled)
                 && Objects.equals(server, other.server);

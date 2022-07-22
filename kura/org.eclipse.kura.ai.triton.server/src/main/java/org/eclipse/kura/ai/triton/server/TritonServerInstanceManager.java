f09f7cc3b654dcefbf7e65f9762067b9f3e295ac
Mattia Dal Ben <matthewdibi@gmail.com>
2022-07-22 11:15:39 +0200

    docs: add TritonServerInstanceManager comments

--- kura/org.eclipse.kura.ai.triton.server/src/main/java/org/eclipse/kura/ai/triton/server/TritonServerInstanceManager.java
+++ kura/org.eclipse.kura.ai.triton.server/src/main/java/org/eclipse/kura/ai/triton/server/TritonServerInstanceManager.java
@@ -2,14 +2,34 @@ package org.eclipse.kura.ai.triton.server;
 
 public interface TritonServerInstanceManager {
 
+    /**
+     * Start the managed Triton Server instance
+     */
     public void start();
 
+    /**
+     * Stop the managed Triton Server instance
+     */
     public void stop();
 
+    /**
+     * Stop forcefully the managed Triton Server instance
+     */
     public void kill();
 
+    /**
+     * Check the managed Triton Server instance status
+     *
+     * @return whether the server instance is running
+     */
     public boolean isServerRunning();
 
+    /**
+     * Check whether the manager can handle the Triton Server instance lifecycle
+     * should be always true for local instances (Native, Container)
+     *
+     * @return whether the server instance lifecycle is managed
+     */
     public boolean isLifecycleManaged();
 
 }

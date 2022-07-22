d5dbb64d7bf281a8eae06c9f0c5fb088d909512d
MMaiero <matteo.maiero@eurotech.com>
2022-07-22 14:04:03 +0200

    Removed unneeded comment. Applied formatting
    Signed-off-by: MMaiero <matteo.maiero@eurotech.com>

--- kura/org.eclipse.kura.net.admin/src/main/java/org/eclipse/kura/net/admin/visitor/linux/AbstractLinuxConfigWriter.java
+++ kura/org.eclipse.kura.net.admin/src/main/java/org/eclipse/kura/net/admin/visitor/linux/AbstractLinuxConfigWriter.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
  * Copyright (c) 2022 Eurotech and/or its affiliates and others
- * 
+ *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
  * which is available at https://www.eclipse.org/legal/epl-2.0/
- * 
+ *
  * SPDX-License-Identifier: EPL-2.0
- * 
+ *
  * Contributors:
  *  Eurotech
  *******************************************************************************/
@@ -59,8 +59,8 @@ public abstract class AbstractLinuxConfigWriter implements NetworkConfigurationV
 
         for (NetInterfaceConfig<? extends NetInterfaceAddressConfig> netInterfaceConfig : netInterfaceConfigs) {
 
-            // skip monitor interface
-            if (netInterfaceConfig.getType() != NetInterfaceType.WIFI || netInterfaceConfig.getName().startsWith("mon.")) {
+            if (netInterfaceConfig.getType() != NetInterfaceType.WIFI
+                    || netInterfaceConfig.getName().startsWith("mon.")) {
                 continue;
             }
 

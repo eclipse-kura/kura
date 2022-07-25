d95fdbf56e273d2421ab2e3187d91536cd0b2d2a
Mattia Dal Ben <matthewdibi@gmail.com>
2022-07-25 15:46:20 +0200

    test(ai.triton.server.test): add createServerCommand tests

--- kura/test/org.eclipse.kura.ai.triton.server.test/src/test/java/org/eclipse/kura/ai/triton/server/TritonServerNativeManagerTest.java
+++ kura/test/org.eclipse.kura.ai.triton.server.test/src/test/java/org/eclipse/kura/ai/triton/server/TritonServerNativeManagerTest.java
@@ -14,6 +14,9 @@
 package org.eclipse.kura.ai.triton.server;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -21,21 +24,27 @@ import static org.mockito.Mockito.when;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Consumer;
 
 import org.eclipse.kura.core.linux.executor.LinuxSignal;
+import org.eclipse.kura.executor.Command;
 import org.eclipse.kura.executor.CommandExecutorService;
+import org.eclipse.kura.executor.CommandStatus;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 
 public class TritonServerNativeManagerTest {
 
     private static final String[] TRITONSERVERCMD = new String[] { "tritonserver" };
-    private static final String MOCK_DECRYPT_FOLDER = "test";
+    private static final String MOCK_DECRYPT_FOLDER = "testDecryptionFolder";
 
     private Map<String, Object> properties = new HashMap<>();
     private TritonServerServiceOptions options = new TritonServerServiceOptions(properties);
 
     private CommandExecutorService ces;
     private TritonServerNativeManager manager;
+
+    private boolean exceptionOccurred = false;
 
     @Test
     public void killMethodShouldWork() {
@@ -116,6 +125,46 @@ public class TritonServerNativeManagerTest {
         thenServerIsRunningReturns(false);
     }
 
+    @Test
+    public void decryptionFolderIsUsedOnlyForEncryptedModels() {
+        givenPropertyWith("server.address", "localhost");
+        givenPropertyWith("server.ports", new Integer[] { 4000, 4001, 4002 });
+        givenPropertyWith("enable.local", Boolean.TRUE);
+        givenPropertyWith("local.model.repository.password", "password123");
+        givenPropertyWith("local.model.repository.path", "modelRepositoryPath");
+        givenServiceOptionsBuiltWith(properties);
+
+        givenMockCommandExecutionService();
+        givenMockCommandExecutionServiceReturnsTritonIsRunning(false);
+
+        givenLocalManagerBuiltWith(this.options, this.ces, MOCK_DECRYPT_FOLDER);
+
+        whenStartIsCalled();
+
+        thenCommandServiceExecuteWasCalledWithCommandContaining(MOCK_DECRYPT_FOLDER);
+        thenNoExceptionOccurred();
+    }
+
+    @Test
+    public void backendConfigurationIsCorrectlyUsedOnStart() {
+        givenPropertyWith("server.address", "localhost");
+        givenPropertyWith("server.ports", new Integer[] { 4000, 4001, 4002 });
+        givenPropertyWith("enable.local", Boolean.TRUE);
+        givenPropertyWith("local.backends.config", "testConfiguration");
+        givenPropertyWith("local.model.repository.path", "modelRepositoryPath");
+        givenServiceOptionsBuiltWith(properties);
+
+        givenMockCommandExecutionService();
+        givenMockCommandExecutionServiceReturnsTritonIsRunning(false);
+
+        givenLocalManagerBuiltWith(this.options, this.ces, MOCK_DECRYPT_FOLDER);
+
+        whenStartIsCalled();
+
+        thenCommandServiceExecuteWasCalledWithCommandContaining("--backend-config=");
+        thenNoExceptionOccurred();
+    }
+
     /*
      * Given
      */
@@ -151,11 +200,31 @@ public class TritonServerNativeManagerTest {
         this.manager.stop();
     }
 
+    private void whenStartIsCalled() {
+        this.manager.start();
+
+        try {
+            // Wait for monitor thread to do its job
+            Thread.sleep(10);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            this.exceptionOccurred = true;
+        }
+    }
+
     /*
      * Then
      */
     private void thenCommandServiceKillWasCalledWith(String[] expectedCmd, LinuxSignal expectedSignal) {
         verify(this.ces, times(1)).kill(expectedCmd, expectedSignal);
+    }
+
+    @SuppressWarnings("unchecked")
+    private void thenCommandServiceExecuteWasCalledWithCommandContaining(String expectedString) {
+        ArgumentCaptor<Command> argument = ArgumentCaptor.forClass(Command.class);
+        verify(this.ces, times(1)).execute(argument.capture(), (Consumer<CommandStatus>) any(Object.class));
+
+        assertTrue(argument.getValue().toString().contains(expectedString));
     }
 
     private void thenServerIsRunningReturns(boolean expectedState) {
@@ -164,6 +233,10 @@ public class TritonServerNativeManagerTest {
 
     private void thenServerIsManagedReturns(boolean expectedState) {
         assertEquals(expectedState, this.manager.isLifecycleManaged());
+    }
+
+    private void thenNoExceptionOccurred() {
+        assertFalse(this.exceptionOccurred);
     }
 
 }

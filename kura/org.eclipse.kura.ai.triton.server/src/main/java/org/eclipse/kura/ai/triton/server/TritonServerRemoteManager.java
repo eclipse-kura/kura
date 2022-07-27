e898a40b389bef8daf54e0349010749c38555448
Mattia Dal Ben <matthewdibi@gmail.com>
2022-07-27 11:22:13 +0200

    refactor: add TritonServerRemoteManager

---
+++ kura/org.eclipse.kura.ai.triton.server/src/main/java/org/eclipse/kura/ai/triton/server/TritonServerRemoteManager.java
@@ -0,0 +1,38 @@
+/*******************************************************************************
+ * Copyright (c) 2022 Eurotech and/or its affiliates and others
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *  Eurotech
+ ******************************************************************************/
+package org.eclipse.kura.ai.triton.server;
+
+public class TritonServerRemoteManager implements TritonServerInstanceManager {
+
+    private boolean isRunning = false;
+
+    @Override
+    public void start() {
+        this.isRunning = true;
+    }
+
+    @Override
+    public void stop() {
+        this.isRunning = false;
+    }
+
+    @Override
+    public void kill() {
+        this.isRunning = false;
+    }
+
+    @Override
+    public boolean isServerRunning() {
+        return this.isRunning;
+    }
+}

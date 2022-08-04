306bf5de578033b8da5bb0da753468f585eb36d8
Marcello Martina <martina.marcello.rinaldo@outlook.com>
2022-08-04 14:50:40 +0200

    feat: added topic.prefix property and changed defaults in metatype
    Signed-off-by: Marcello Martina <martina.marcello.rinaldo@outlook.com>

--- kura/org.eclipse.kura.configuration.change.publisher/src/main/java/org/eclipse/kura/configuration/change/publisher/ConfigurationChangePublisherOptions.java
+++ kura/org.eclipse.kura.configuration.change.publisher/src/main/java/org/eclipse/kura/configuration/change/publisher/ConfigurationChangePublisherOptions.java
@@ -19,13 +19,14 @@ import org.eclipse.kura.core.cloud.CloudServiceOptions;
 
 public class ConfigurationChangePublisherOptions {
     
-    protected static final String TOPIC_PREFIX = "$EVT";
-
+    protected static final String TOPIC_PREFIX_PROP_NAME = "topic.prefix";
     protected static final String TOPIC_PROP_NAME = "topic";
     protected static final String QOS_PROP_NAME = "qos";
     protected static final String RETAIN_PROP_NAME = "retain";
     protected static final String PRIORITY_PROP_NAME = "priority";
-    protected static final String DEFAULT_TOPIC = "$EVT/$ACCOUNT_NAME/$CLIENT_ID/CONF/V1";
+
+    protected static final String DEFAULT_TOPIC_PREFIX = "$EVT";
+    protected static final String DEFAULT_TOPIC = "CONF/V1/CHANGE";
     protected static final int DEFAULT_QOS = 0;
     protected static final boolean DEFAULT_RETAIN = false;
     protected static final int DEFAULT_PRIORITY = 7;
@@ -33,12 +34,15 @@ public class ConfigurationChangePublisherOptions {
     private static final Property<String> PROPERTY_CLOUD_SERVICE_PID = new Property<>(
             CloudConnectionConstants.CLOUD_ENDPOINT_SERVICE_PID_PROP_NAME.value(),
             "org.eclipse.kura.cloud.CloudService");
+    private static final Property<String> PROPERTY_TOPIC_PREFIX = new Property<>(TOPIC_PREFIX_PROP_NAME,
+            DEFAULT_TOPIC_PREFIX);
     private static final Property<String> PROPERTY_TOPIC = new Property<>(TOPIC_PROP_NAME, DEFAULT_TOPIC);
     private static final Property<Integer> PROPERTY_QOS = new Property<>(QOS_PROP_NAME, DEFAULT_QOS);
     private static final Property<Boolean> PROPERTY_RETAIN = new Property<>(RETAIN_PROP_NAME, DEFAULT_RETAIN);
     private static final Property<Integer> PROPERTY_PRIORITY = new Property<>(PRIORITY_PROP_NAME, DEFAULT_PRIORITY);
 
     private String cloudEndpointPid;
+    private String topicPrefix;
     private String topic;
     private int qos;
     private boolean retain;
@@ -46,6 +50,7 @@ public class ConfigurationChangePublisherOptions {
 
     public ConfigurationChangePublisherOptions(final Map<String, Object> properties) {
         this.cloudEndpointPid = PROPERTY_CLOUD_SERVICE_PID.get(properties);
+        this.topicPrefix = PROPERTY_TOPIC_PREFIX.get(properties);
         this.topic = PROPERTY_TOPIC.get(properties);
         this.qos = PROPERTY_QOS.get(properties);
         this.retain = PROPERTY_RETAIN.get(properties);
@@ -56,17 +61,12 @@ public class ConfigurationChangePublisherOptions {
         return this.cloudEndpointPid;
     }
 
+    public String getTopicPrefix() {
+        return removeTopicSeparatorsFromExtremes(this.topicPrefix);
+    }
+
     public String getTopic() {
-        String noSeparatorOnExtremes = this.topic;
-        if (noSeparatorOnExtremes.startsWith(CloudServiceOptions.getTopicSeparator())) {
-            noSeparatorOnExtremes = noSeparatorOnExtremes.substring(1);
-        }
-
-        if (noSeparatorOnExtremes.endsWith(CloudServiceOptions.getTopicSeparator())) {
-            noSeparatorOnExtremes = noSeparatorOnExtremes.substring(0, noSeparatorOnExtremes.length() - 1);
-        }
-
-        return noSeparatorOnExtremes;
+        return removeTopicSeparatorsFromExtremes(this.topic);
     }
 
     public int getQos() {
@@ -81,8 +81,17 @@ public class ConfigurationChangePublisherOptions {
         return this.priority;
     }
 
-    public String getTopicPrefix() {
-        return TOPIC_PREFIX;
+    private String removeTopicSeparatorsFromExtremes(String input) {
+        String result = new String(input);
+        if (result.startsWith(CloudServiceOptions.getTopicSeparator())) {
+            result = result.substring(1);
+        }
+
+        if (result.endsWith(CloudServiceOptions.getTopicSeparator())) {
+            result = result.substring(0, result.length() - 1);
+        }
+
+        return result;
     }
 
     private static final class Property<T> {

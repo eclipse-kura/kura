be4ce159e29c1e2c76b52550b72faa7795290e05
Mattia Dal Ben <matthewdibi@gmail.com>
2022-08-09 15:51:11 +0200

    fix: rename Orchestrator->Orchestration

--- kura/org.eclipse.kura.ai.triton.server/src/main/java/org/eclipse/kura/ai/triton/server/TritonServerServiceAbs.java
+++ kura/org.eclipse.kura.ai.triton.server/src/main/java/org/eclipse/kura/ai/triton/server/TritonServerServiceAbs.java
@@ -93,7 +93,7 @@ public abstract class TritonServerServiceAbs implements InferenceEngineService, 
         this.commandExecutorService = executorService;
     }
 
-    public void setContainerOrchestratorService(ContainerOrchestrationService containerOrchestrationService) {
+    public void setContainerOrchestrationService(ContainerOrchestrationService containerOrchestrationService) {
         this.containerOrchestrationService = containerOrchestrationService;
     }
 

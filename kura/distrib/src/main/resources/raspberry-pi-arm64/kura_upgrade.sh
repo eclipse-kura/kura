808752a23cbf9bc9592ca8c0b71ea21ef0b1762f
Gregory Ivo <gregorybivo@gmail.com>
2022-10-19 15:37:45 -0400

    build: renamed raspberry-pi-ubuntu-20 to raspberry-pi-arm64

---
+++ kura/distrib/src/main/resources/raspberry-pi-arm64/kura_upgrade.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+#
+#  Copyright (c) 2011, 2020 Eurotech and/or its affiliates and others
+#
+#  This program and the accompanying materials are made
+#  available under the terms of the Eclipse Public License 2.0
+#  which is available at https://www.eclipse.org/legal/epl-2.0/
+#
+#  SPDX-License-Identifier: EPL-2.0
+#
+#  Contributors:
+#   Eurotech
+#
+
+INSTALL_DIR=/opt/eclipse
+
+#create known kura install location
+ln -sf ${INSTALL_DIR}/kura_* ${INSTALL_DIR}/kura
+
+#set up network helper scripts
+cp ${INSTALL_DIR}/kura/install/ifup-local.raspbian /etc/network/if-up.d/ifup-local
+cp ${INSTALL_DIR}/kura/install/ifdown-local /etc/network/if-down.d/ifdown-local
+chmod +x /etc/network/if-up.d/ifup-local
+chmod +x /etc/network/if-down.d/ifdown-local
+
+#set up logrotate - no need to restart as it is a cronjob
+cp ${INSTALL_DIR}/kura/install/logrotate.conf /etc/logrotate.conf
+cp ${INSTALL_DIR}/kura/install/kura.logrotate /etc/logrotate.d/kura

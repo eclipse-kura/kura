0cf89d16dcc34812ab671659cfc7bdf93153c3c4
Marcello Rinaldo Martina <39562568+marcellorinaldo@users.noreply.github.com>
2022-11-03 17:55:52 +0100

    fix:(core.ssl): prevent SSLManager to updated keystore password if password did not change (#4192)
    * fix(core.ssl): prevent SSLManager to update keystore password update when password did not change
    
    Signed-off-by: Marcello Martina <martina.marcello.rinaldo@outlook.com>
    
    * chore(SSLManagerServiceImpl): removed unused imports
    
    Signed-off-by: Marcello Martina <martina.marcello.rinaldo@outlook.com>
    
    * chore(SSLManagerServiceImpl): updated copyright headers
    
    Signed-off-by: Marcello Martina <martina.marcello.rinaldo@outlook.com>
    
    Signed-off-by: Marcello Martina <martina.marcello.rinaldo@outlook.com>

--- kura/org.eclipse.kura.core/src/main/java/org/eclipse/kura/core/ssl/SslManagerServiceImpl.java
+++ kura/org.eclipse.kura.core/src/main/java/org/eclipse/kura/core/ssl/SslManagerServiceImpl.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011, 2018 Eurotech and/or its affiliates
+ * Copyright (c) 2011, 2022 Eurotech and/or its affiliates
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -307,7 +307,10 @@ public class SslManagerServiceImpl implements SslManagerService, ConfigurableCom
             } catch (KuraException e) {
                 logger.warn("Failed to decrypt keystore password");
             }
-            updateKeystorePassword(oldPassword, newPassword);
+
+            if (!Arrays.equals(oldPassword, newPassword)) {
+                updateKeystorePassword(oldPassword, newPassword);
+            }
         }
     }
 

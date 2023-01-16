6de738b5693a7d72dc31d3544bbb2a3f6b4f80ad
Mattia Dal Ben <matthewdibi@gmail.com>
2023-01-16 11:56:50 +0100

    fix: byte -> Byte in resources

--- kura/org.eclipse.kura.net2.admin/src/main/resources/org/freedesktop/networkmanager/PropertyAddressesStruct.java
+++ kura/org.eclipse.kura.net2.admin/src/main/resources/org/freedesktop/networkmanager/PropertyAddressesStruct.java
@@ -1,6 +1,7 @@
 package org.freedesktop.networkmanager;
 
 import java.util.List;
+
 import org.freedesktop.dbus.Struct;
 import org.freedesktop.dbus.annotations.Position;
 import org.freedesktop.dbus.types.UInt32;
@@ -9,21 +10,21 @@ import org.freedesktop.dbus.types.UInt32;
  * Auto-generated class.
  */
 public class PropertyAddressesStruct extends Struct {
+
     @Position(0)
-    private final List<byte> member0;
+    private final List<Byte> member0;
     @Position(1)
     private final UInt32 member1;
     @Position(2)
-    private final List<byte> member2;
+    private final List<Byte> member2;
 
-    public PropertyAddressesStruct(List<byte> member0, UInt32 member1, List<byte> member2) {
+    public PropertyAddressesStruct(List<Byte> member0, UInt32 member1, List<Byte> member2) {
         this.member0 = member0;
         this.member1 = member1;
         this.member2 = member2;
     }
 
-
-    public List<byte> getMember0() {
+    public List<Byte> getMember0() {
         return member0;
     }
 
@@ -31,9 +32,8 @@ public class PropertyAddressesStruct extends Struct {
         return member1;
     }
 
-    public List<byte> getMember2() {
+    public List<Byte> getMember2() {
         return member2;
     }
-
 
 }
--- kura/org.eclipse.kura.net2.admin/src/main/resources/org/freedesktop/networkmanager/PropertyRoutesStruct.java
+++ kura/org.eclipse.kura.net2.admin/src/main/resources/org/freedesktop/networkmanager/PropertyRoutesStruct.java
@@ -1,6 +1,7 @@
 package org.freedesktop.networkmanager;
 
 import java.util.List;
+
 import org.freedesktop.dbus.Struct;
 import org.freedesktop.dbus.annotations.Position;
 import org.freedesktop.dbus.types.UInt32;
@@ -9,24 +10,24 @@ import org.freedesktop.dbus.types.UInt32;
  * Auto-generated class.
  */
 public class PropertyRoutesStruct extends Struct {
+
     @Position(0)
-    private final List<byte> member0;
+    private final List<Byte> member0;
     @Position(1)
     private final UInt32 member1;
     @Position(2)
-    private final List<byte> member2;
+    private final List<Byte> member2;
     @Position(3)
     private final UInt32 member3;
 
-    public PropertyRoutesStruct(List<byte> member0, UInt32 member1, List<byte> member2, UInt32 member3) {
+    public PropertyRoutesStruct(List<Byte> member0, UInt32 member1, List<Byte> member2, UInt32 member3) {
         this.member0 = member0;
         this.member1 = member1;
         this.member2 = member2;
         this.member3 = member3;
     }
 
-
-    public List<byte> getMember0() {
+    public List<Byte> getMember0() {
         return member0;
     }
 
@@ -34,13 +35,12 @@ public class PropertyRoutesStruct extends Struct {
         return member1;
     }
 
-    public List<byte> getMember2() {
+    public List<Byte> getMember2() {
         return member2;
     }
 
     public UInt32 getMember3() {
         return member3;
     }
-
 
 }

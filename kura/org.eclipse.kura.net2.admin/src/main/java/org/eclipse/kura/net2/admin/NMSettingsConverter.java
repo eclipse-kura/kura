28ad24127e66e1f20f093d90d030ad6eb06d06ce
Mattia Dal Ben <matthewdibi@gmail.com>
2023-01-18 18:51:13 +0100

    refactor: simplify property retrival (WIP)

--- kura/org.eclipse.kura.net2.admin/src/main/java/org/eclipse/kura/net2/admin/NMSettingsConverter.java
+++ kura/org.eclipse.kura.net2.admin/src/main/java/org/eclipse/kura/net2/admin/NMSettingsConverter.java
@@ -19,6 +19,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.regex.Pattern;
 
 import org.freedesktop.dbus.types.UInt32;
@@ -29,19 +30,15 @@ public class NMSettingsConverter {
     public static Map<String, Variant<?>> buildIpv4Settings(Map<String, Object> networkConfiguration, String iface) {
         Map<String, Variant<?>> ipv4Map = new HashMap<>();
 
-        String dhcpClient4EnabledProperty = String.format("net.interface.%s.config.dhcpClient4.enabled", iface);
-        Boolean dhcpClient4Enabled = (Boolean) networkConfiguration.get(dhcpClient4EnabledProperty);
+        Boolean dhcpClient4Enabled = get(networkConfiguration, "net.interface.%s.config.dhcpClient4.enabled", iface);
 
         // Should handle net.interface.eth0.config.ip4.status here
 
         if (Boolean.FALSE.equals(dhcpClient4Enabled)) {
             ipv4Map.put("method", new Variant<>("manual"));
 
-            String dhcpClient4AddressProperty = String.format("net.interface.%s.config.ip4.address", iface);
-            String dhcpClient4Address = (String) networkConfiguration.get(dhcpClient4AddressProperty);
-
-            String dhcpClient4PrefixProperty = String.format("net.interface.%s.config.ip4.prefix", iface);
-            Short dhcpClient4Prefix = (Short) networkConfiguration.get(dhcpClient4PrefixProperty);
+            String dhcpClient4Address = get(networkConfiguration, "net.interface.%s.config.ip4.address", iface);
+            Short dhcpClient4Prefix = get(networkConfiguration, "net.interface.%s.config.ip4.prefix", iface);
 
             Map<String, Variant<?>> address = new HashMap<>();
             address.put("address", new Variant<>(dhcpClient4Address));
@@ -50,30 +47,44 @@ public class NMSettingsConverter {
             List<Map<String, Variant<?>>> addressData = Arrays.asList(address);
             ipv4Map.put("address-data", new Variant<>(addressData, "aa{sv}"));
 
-            String dhcpClient4DNSProperty = String.format("net.interface.%s.config.ip4.dnsServers", iface);
-            if (networkConfiguration.containsKey(dhcpClient4DNSProperty)) {
-                String dhcpClient4DNS = (String) networkConfiguration.get(dhcpClient4DNSProperty);
-                ipv4Map.put("dns-search", new Variant<>(splitCommaSeparatedStrings(dhcpClient4DNS)));
+            Optional<String> dhcpClient4DNS = getOpt(networkConfiguration, "net.interface.%s.config.ip4.dnsServers",
+                    iface);
+            if (dhcpClient4DNS.isPresent()) {
+                ipv4Map.put("dns-search", new Variant<>(splitCommaSeparatedStrings(dhcpClient4DNS.get())));
             }
             ipv4Map.put("ignore-auto-dns", new Variant<>(true));
 
-            String dhcpClient4GatewayProperty = String.format("net.interface.%s.config.ip4.gateway", iface);
-            if (networkConfiguration.containsKey(dhcpClient4GatewayProperty)) {
-                String dhcpClient4Gateway = (String) networkConfiguration.get(dhcpClient4GatewayProperty);
+            Optional<String> dhcpClient4Gateway = getOpt(networkConfiguration, "net.interface.%s.config.ip4.gateway",
+                    iface);
+            if (dhcpClient4Gateway.isPresent()) {
                 ipv4Map.put("gateway", new Variant<>(dhcpClient4Gateway));
             }
         } else {
             ipv4Map.put("method", new Variant<>("auto"));
 
-            String dhcpClient4DNSProperty = String.format("net.interface.%s.config.ip4.dnsServers", iface);
-            if (networkConfiguration.containsKey(dhcpClient4DNSProperty)) {
-                String dhcpClient4DNS = (String) networkConfiguration.get(dhcpClient4DNSProperty);
+            Optional<String> dhcpClient4DNS = getOpt(networkConfiguration, "net.interface.%s.config.ip4.dnsServers",
+                    iface);
+            if (dhcpClient4DNS.isPresent()) {
                 ipv4Map.put("ignore-auto-dns", new Variant<>(true));
-                ipv4Map.put("dns-search", new Variant<>(splitCommaSeparatedStrings(dhcpClient4DNS)));
+                ipv4Map.put("dns-search", new Variant<>(splitCommaSeparatedStrings(dhcpClient4DNS.get())));
             }
         }
 
         return ipv4Map;
+    }
+
+    public static <T> T get(Map<String, Object> properties, String key, Object... args) {
+        String formattedKey = String.format(key, args);
+        return (T) properties.get(formattedKey);
+    }
+
+    public static <T> Optional<T> getOpt(Map<String, Object> properties, String key, Object... args) {
+        String formattedKey = String.format(key, args);
+        if (properties.containsKey(formattedKey)) {
+            return Optional.of((T) properties.get(formattedKey));
+        } else {
+            return Optional.empty();
+        }
     }
 
     public static List<String> splitCommaSeparatedStrings(String commaSeparatedString) {
@@ -85,5 +96,4 @@ public class NMSettingsConverter {
 
         return stringList;
     }
-
 }

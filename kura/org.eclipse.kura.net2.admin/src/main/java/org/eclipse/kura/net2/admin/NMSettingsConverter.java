9ec8e0fece87c7313323100db0ca51f28699a662
Mattia Dal Ben <matthewdibi@gmail.com>
2023-01-20 10:10:18 +0100

    style: fix copyright headers

--- kura/org.eclipse.kura.net2.admin/src/main/java/org/eclipse/kura/net2/admin/NMSettingsConverter.java
+++ kura/org.eclipse.kura.net2.admin/src/main/java/org/eclipse/kura/net2/admin/NMSettingsConverter.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011, 2022 Eurotech and/or its affiliates and others
+ * Copyright (c) 2023 Eurotech and/or its affiliates and others
  *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
@@ -9,7 +9,6 @@
  *
  * Contributors:
  *  Eurotech
- *  Red Hat Inc
  *******************************************************************************/
 package org.eclipse.kura.net2.admin;
 

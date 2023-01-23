77bf2f09f73af8748c7b4e05d8f76308f0e26128
MMaiero <matteo.maiero@eurotech.com>
2023-01-23 11:04:17 +0100

    chore: copyright date update
    Signed-off-by: MMaiero <matteo.maiero@eurotech.com>

--- kura/org.eclipse.kura.net.admin/src/main/java/org/eclipse/kura/net/admin/modem/zte/me3630/ZteMe3630ConfigGenerator.java
+++ kura/org.eclipse.kura.net.admin/src/main/java/org/eclipse/kura/net/admin/modem/zte/me3630/ZteMe3630ConfigGenerator.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2019, 2022 3 PORT d.o.o. and others
+ * Copyright (c) 2019, 2023 3 PORT d.o.o. and others
  *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0

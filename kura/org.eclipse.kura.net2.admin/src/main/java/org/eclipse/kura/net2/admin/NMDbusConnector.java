23ba925b55c4349b6a106029c8387297119eb19e
Mattia Dal Ben <matthewdibi@gmail.com>
2023-01-27 11:43:52 +0100

    feat: disable ipv6 support for now

--- kura/org.eclipse.kura.net2.admin/src/main/java/org/eclipse/kura/net2/admin/NMDbusConnector.java
+++ kura/org.eclipse.kura.net2.admin/src/main/java/org/eclipse/kura/net2/admin/NMDbusConnector.java
@@ -105,8 +105,7 @@ public class NMDbusConnector {
 
             KuraInterfaceStatus ip4Status = KuraInterfaceStatus
                     .fromString(properties.get(String.class, "net.interface.%s.config.ip4.status", iface));
-            KuraInterfaceStatus ip6Status = KuraInterfaceStatus
-                    .fromString(properties.get(String.class, "net.interface.%s.config.ip6.status", iface));
+            KuraInterfaceStatus ip6Status = KuraInterfaceStatus.DISABLED; // TBD
             NMDeviceEnable deviceStatus = NMDeviceEnable.fromKuraInterfaceStatus(ip4Status, ip6Status);
 
             if (!SUPPORTED_DEVICES.contains(deviceType) || !SUPPORTED_STATUSES.contains(ip4Status)

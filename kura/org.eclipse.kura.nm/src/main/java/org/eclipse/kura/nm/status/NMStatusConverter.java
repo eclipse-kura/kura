bb03e4d85dc1f6b755ef420514122c5f065c7695
Mattia Dal Ben <matthewdibi@gmail.com>
2023-02-03 17:29:13 +0100

    feat: add support for Loopback interface status

--- kura/org.eclipse.kura.nm/src/main/java/org/eclipse/kura/nm/status/NMStatusConverter.java
+++ kura/org.eclipse.kura.nm/src/main/java/org/eclipse/kura/nm/status/NMStatusConverter.java
@@ -7,6 +7,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.eclipse.kura.core.net.EthernetInterfaceImpl;
+import org.eclipse.kura.core.net.LoopbackInterfaceImpl;
 import org.eclipse.kura.core.net.NetInterfaceAddressImpl;
 import org.eclipse.kura.net.IPAddress;
 import org.eclipse.kura.net.NetInterface;
@@ -99,6 +100,73 @@ public class NMStatusConverter {
 
         return ethInterface;
 
+    }
+
+    public static NetInterface<NetInterfaceAddress> buildLoopbackStatus(String interfaceName,
+            Properties deviceProperties, Properties ip4ConfigProperties) {
+        LoopbackInterfaceImpl<NetInterfaceAddress> loInterface = new LoopbackInterfaceImpl<>(interfaceName);
+
+        loInterface.setVirtual(false);
+        loInterface.setLoopback(true);
+        loInterface.setPointToPoint(false);
+
+        loInterface.setAutoConnect(deviceProperties.Get(NM_DEVICE_BUS_NAME, "Autoconnect"));
+        loInterface.setFirmwareVersion(deviceProperties.Get(NM_DEVICE_BUS_NAME, "FirmwareVersion"));
+        loInterface.setDriver(deviceProperties.Get(NM_DEVICE_BUS_NAME, "Driver"));
+        loInterface.setDriverVersion(deviceProperties.Get(NM_DEVICE_BUS_NAME, "DriverVersion"));
+
+        NMDeviceState deviceState = NMDeviceState.fromUInt32(deviceProperties.Get(NM_DEVICE_BUS_NAME, "State"));
+        loInterface.setState(DEVICE_STATE_CONVERTER.get(deviceState));
+        loInterface.setUp(NMDeviceState.isConnected(deviceState));
+
+        UInt32 mtu = deviceProperties.Get(NM_DEVICE_BUS_NAME, "Mtu");
+        loInterface.setMTU(mtu.intValue());
+
+        String hwAddress = deviceProperties.Get(NM_DEVICE_BUS_NAME, "HwAddress");
+        loInterface.setHardwareAddress(getMacAddressBytes(hwAddress));
+
+        List<NetInterfaceAddress> addressList = new ArrayList<>();
+
+        String gateway = ip4ConfigProperties.Get(NM_IP4CONFIG_BUS_NAME, "Gateway");
+        List<Map<String, Variant<?>>> addressData = ip4ConfigProperties.Get(NM_IP4CONFIG_BUS_NAME, "AddressData");
+        List<Map<String, Variant<?>>> nameserverData = ip4ConfigProperties.Get(NM_IP4CONFIG_BUS_NAME, "NameserverData");
+        for (Map<String, Variant<?>> data : addressData) {
+            NetInterfaceAddressImpl address = new NetInterfaceAddressImpl();
+
+            try {
+                IPAddress ipGateway = IPAddress.parseHostAddress(gateway);
+                address.setGateway(ipGateway);
+            } catch (UnknownHostException e) {
+                logger.debug("Could not retrieve gateway address \"{}\" due to:", gateway, e);
+            }
+
+            String addressStr = String.class.cast(data.get("address").getValue());
+            UInt32 prefix = UInt32.class.cast(data.get("prefix").getValue());
+            try {
+                address.setAddress(IPAddress.parseHostAddress(addressStr));
+                address.setNetworkPrefixLength(prefix.shortValue());
+                address.setNetmask(IPAddress.parseHostAddress(getNetmaskStringFrom(prefix.intValue())));
+            } catch (UnknownHostException e) {
+                logger.debug("Could not retrieve ip address due to:", e);
+            }
+
+            List<IPAddress> dnsServers = new ArrayList<>();
+            for (Map<String, Variant<?>> dns : nameserverData) {
+                String dnsAddressStr = String.class.cast(dns.get("address").getValue());
+                try {
+                    dnsServers.add(IPAddress.parseHostAddress(dnsAddressStr));
+                } catch (UnknownHostException e) {
+                    logger.debug("Could not retrieve ip address \"{}\" due to:", dnsAddressStr, e);
+                }
+            }
+            address.setDnsServers(dnsServers);
+
+            addressList.add(address);
+        }
+
+        loInterface.setNetInterfaceAddresses(addressList);
+
+        return loInterface;
     }
 
     private static String getNetmaskStringFrom(int prefix) {

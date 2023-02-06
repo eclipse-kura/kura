43c5460cb61429199760c390d1934e3e138e7bfc
pierantoniomerlino <pierantonio.merlino@eurotech.com>
2023-02-06 14:50:35 +0100

    Finally fixed copyright headers
    Signed-off-by: pierantoniomerlino <pierantonio.merlino@eurotech.com>

--- kura/distrib/src/main/resources/common/manage_kura_users.sh
+++ kura/distrib/src/main/resources/common/manage_kura_users.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-#  Copyright (c) 2020, 2021 Eurotech and/or its affiliates and others
+#  Copyright (c) 2020, 2023 Eurotech and/or its affiliates and others
 #
 #  This program and the accompanying materials are made
 #  available under the terms of the Eclipse Public License 2.0

599776c8a385eb434ce9f0187a35f743a632c777
Marco Matteo Buzzulini <marco.buzzulini@eurotech.com>
2023-02-07 11:34:46 +0100

    Fixed unused imports
    Signed-off-by: Marco Matteo Buzzulini <marco.buzzulini@eurotech.com>

--- kura/org.eclipse.kura.wire.component.provider/src/main/java/org/eclipse/kura/internal/wire/publisher/CloudPublisher.java
+++ kura/org.eclipse.kura.wire.component.provider/src/main/java/org/eclipse/kura/internal/wire/publisher/CloudPublisher.java
@@ -21,12 +21,10 @@ import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Optional;
-import java.util.Set;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;

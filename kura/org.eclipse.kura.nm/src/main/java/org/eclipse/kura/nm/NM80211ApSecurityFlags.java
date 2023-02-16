c3bec5f5cecdee46b12e847b595e156b9dab9fe9
Mattia Dal Ben <matthewdibi@gmail.com>
2023-02-16 16:51:03 +0100

    feat: add NM80211ApSecurityFlags skeleton

---
+++ kura/org.eclipse.kura.nm/src/main/java/org/eclipse/kura/nm/NM80211ApSecurityFlags.java
@@ -0,0 +1,27 @@
+package org.eclipse.kura.nm;
+
+import org.freedesktop.dbus.types.UInt32;
+
+public enum NM80211ApSecurityFlags {
+
+    NM_802_11_AP_SEC_NONE,
+    NM_802_11_AP_SEC_PAIR_WEP40,
+    NM_802_11_AP_SEC_PAIR_WEP104,
+    NM_802_11_AP_SEC_PAIR_TKIP,
+    NM_802_11_AP_SEC_PAIR_CCMP,
+    NM_802_11_AP_SEC_GROUP_WEP40,
+    NM_802_11_AP_SEC_GROUP_WEP104,
+    NM_802_11_AP_SEC_GROUP_TKIP,
+    NM_802_11_AP_SEC_GROUP_CCMP,
+    NM_802_11_AP_SEC_KEY_MGMT_PSK,
+    NM_802_11_AP_SEC_KEY_MGMT_802_1X,
+    NM_802_11_AP_SEC_KEY_MGMT_SAE,
+    NM_802_11_AP_SEC_KEY_MGMT_OWE,
+    NM_802_11_AP_SEC_KEY_MGMT_OWE_TM,
+    NM_802_11_AP_SEC_KEY_MGMT_EAP_SUITE_B_192;
+
+    public static NM80211ApSecurityFlags fromUInt32(UInt32 value) {
+        // TODO
+        return NM80211ApSecurityFlags.NM_802_11_AP_SEC_NONE;
+    }
+}

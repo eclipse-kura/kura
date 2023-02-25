82b85d81f8a6d8ed72fd219601149f160b690cbe
Mattia Dal Ben <matthewdibi@gmail.com>
2023-02-25 11:13:59 +0100

    refactor(nm): smarter NM80211ApSecurityFlags conversion

--- kura/org.eclipse.kura.nm/src/main/java/org/eclipse/kura/nm/NM80211ApSecurityFlags.java
+++ kura/org.eclipse.kura.nm/src/main/java/org/eclipse/kura/nm/NM80211ApSecurityFlags.java
@@ -8,83 +8,45 @@ import org.freedesktop.dbus.types.UInt32;
 
 public enum NM80211ApSecurityFlags {
 
-    NM_802_11_AP_SEC_NONE,
-    NM_802_11_AP_SEC_PAIR_WEP40,
-    NM_802_11_AP_SEC_PAIR_WEP104,
-    NM_802_11_AP_SEC_PAIR_TKIP,
-    NM_802_11_AP_SEC_PAIR_CCMP,
-    NM_802_11_AP_SEC_GROUP_WEP40,
-    NM_802_11_AP_SEC_GROUP_WEP104,
-    NM_802_11_AP_SEC_GROUP_TKIP,
-    NM_802_11_AP_SEC_GROUP_CCMP,
-    NM_802_11_AP_SEC_KEY_MGMT_PSK,
-    NM_802_11_AP_SEC_KEY_MGMT_802_1X,
-    NM_802_11_AP_SEC_KEY_MGMT_SAE,
-    NM_802_11_AP_SEC_KEY_MGMT_OWE,
-    NM_802_11_AP_SEC_KEY_MGMT_OWE_TM,
-    NM_802_11_AP_SEC_KEY_MGMT_EAP_SUITE_B_192;
+    NM_802_11_AP_SEC_NONE(0x00000000),
+    NM_802_11_AP_SEC_PAIR_WEP40(0x00000001),
+    NM_802_11_AP_SEC_PAIR_WEP104(0x00000002),
+    NM_802_11_AP_SEC_PAIR_TKIP(0x00000004),
+    NM_802_11_AP_SEC_PAIR_CCMP(0x00000008),
+    NM_802_11_AP_SEC_GROUP_WEP40(0x00000010),
+    NM_802_11_AP_SEC_GROUP_WEP104(0x00000020),
+    NM_802_11_AP_SEC_GROUP_TKIP(0x00000040),
+    NM_802_11_AP_SEC_GROUP_CCMP(0x00000080),
+    NM_802_11_AP_SEC_KEY_MGMT_PSK(0x00000100),
+    NM_802_11_AP_SEC_KEY_MGMT_802_1X(0x00000200),
+    NM_802_11_AP_SEC_KEY_MGMT_SAE(0x00000400),
+    NM_802_11_AP_SEC_KEY_MGMT_OWE(0x00000800),
+    NM_802_11_AP_SEC_KEY_MGMT_OWE_TM(0x00001000),
+    NM_802_11_AP_SEC_KEY_MGMT_EAP_SUITE_B_192(0x00002000);
+
+    private final int value;
+
+    private NM80211ApSecurityFlags(int value) {
+        this.value = value;
+    }
 
     public static List<NM80211ApSecurityFlags> fromUInt32(UInt32 val) {
-        if (val.intValue() == 0x00000000) {
+        int intVal = val.intValue();
+
+        if (intVal == NM80211ApSecurityFlags.NM_802_11_AP_SEC_NONE.value) {
             return Arrays.asList(NM80211ApSecurityFlags.NM_802_11_AP_SEC_NONE);
         }
 
         List<NM80211ApSecurityFlags> flags = new ArrayList<>();
 
-        if ((val.intValue() & 0x00000001) == 0x00000001) {
-            flags.add(NM80211ApSecurityFlags.NM_802_11_AP_SEC_PAIR_WEP40);
-        }
+        for (NM80211ApSecurityFlags flag : NM80211ApSecurityFlags.values()) {
+            if (flag == NM80211ApSecurityFlags.NM_802_11_AP_SEC_NONE) {
+                continue;
+            }
 
-        if ((val.intValue() & 0x00000002) == 0x00000002) {
-            flags.add(NM80211ApSecurityFlags.NM_802_11_AP_SEC_PAIR_WEP104);
-        }
-
-        if ((val.intValue() & 0x00000004) == 0x00000004) {
-            flags.add(NM80211ApSecurityFlags.NM_802_11_AP_SEC_PAIR_TKIP);
-        }
-
-        if ((val.intValue() & 0x00000008) == 0x00000008) {
-            flags.add(NM80211ApSecurityFlags.NM_802_11_AP_SEC_PAIR_CCMP);
-        }
-
-        if ((val.intValue() & 0x00000010) == 0x00000010) {
-            flags.add(NM80211ApSecurityFlags.NM_802_11_AP_SEC_GROUP_WEP40);
-        }
-
-        if ((val.intValue() & 0x00000020) == 0x00000020) {
-            flags.add(NM80211ApSecurityFlags.NM_802_11_AP_SEC_GROUP_WEP104);
-        }
-
-        if ((val.intValue() & 0x00000040) == 0x00000040) {
-            flags.add(NM80211ApSecurityFlags.NM_802_11_AP_SEC_GROUP_TKIP);
-        }
-
-        if ((val.intValue() & 0x00000080) == 0x00000080) {
-            flags.add(NM80211ApSecurityFlags.NM_802_11_AP_SEC_GROUP_CCMP);
-        }
-
-        if ((val.intValue() & 0x00000100) == 0x00000100) {
-            flags.add(NM80211ApSecurityFlags.NM_802_11_AP_SEC_KEY_MGMT_PSK);
-        }
-
-        if ((val.intValue() & 0x00000200) == 0x00000200) {
-            flags.add(NM80211ApSecurityFlags.NM_802_11_AP_SEC_KEY_MGMT_802_1X);
-        }
-
-        if ((val.intValue() & 0x00000400) == 0x00000400) {
-            flags.add(NM80211ApSecurityFlags.NM_802_11_AP_SEC_KEY_MGMT_SAE);
-        }
-
-        if ((val.intValue() & 0x00000800) == 0x00000800) {
-            flags.add(NM80211ApSecurityFlags.NM_802_11_AP_SEC_KEY_MGMT_OWE);
-        }
-
-        if ((val.intValue() & 0x00001000) == 0x00001000) {
-            flags.add(NM80211ApSecurityFlags.NM_802_11_AP_SEC_KEY_MGMT_OWE_TM);
-        }
-
-        if ((val.intValue() & 0x00002000) == 0x00002000) {
-            flags.add(NM80211ApSecurityFlags.NM_802_11_AP_SEC_KEY_MGMT_EAP_SUITE_B_192);
+            if ((intVal & flag.value) == flag.value) {
+                flags.add(flag);
+            }
         }
 
         return flags;

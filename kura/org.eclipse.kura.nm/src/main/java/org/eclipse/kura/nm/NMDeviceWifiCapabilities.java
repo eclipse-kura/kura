b93b5601d03ea79b495324747abae18ce3ee8503
Mattia Dal Ben <matthewdibi@gmail.com>
2023-02-25 11:25:32 +0100

    refactor(nm): smarter NMDeviceWifiCapabilities conversion

--- kura/org.eclipse.kura.nm/src/main/java/org/eclipse/kura/nm/NMDeviceWifiCapabilities.java
+++ kura/org.eclipse.kura.nm/src/main/java/org/eclipse/kura/nm/NMDeviceWifiCapabilities.java
@@ -8,78 +8,44 @@ import org.freedesktop.dbus.types.UInt32;
 
 public enum NMDeviceWifiCapabilities {
 
-    NM_WIFI_DEVICE_CAP_NONE,
-    NM_WIFI_DEVICE_CAP_CIPHER_WEP40,
-    NM_WIFI_DEVICE_CAP_CIPHER_WEP104,
-    NM_WIFI_DEVICE_CAP_CIPHER_TKIP,
-    NM_WIFI_DEVICE_CAP_CIPHER_CCMP,
-    NM_WIFI_DEVICE_CAP_WPA,
-    NM_WIFI_DEVICE_CAP_RSN,
-    NM_WIFI_DEVICE_CAP_AP,
-    NM_WIFI_DEVICE_CAP_ADHOC,
-    NM_WIFI_DEVICE_CAP_FREQ_VALID,
-    NM_WIFI_DEVICE_CAP_FREQ_2GHZ,
-    NM_WIFI_DEVICE_CAP_FREQ_5GHZ,
-    NM_WIFI_DEVICE_CAP_MESH,
-    NM_WIFI_DEVICE_CAP_IBSS_RSN;
+    NM_WIFI_DEVICE_CAP_NONE(0x00000000),
+    NM_WIFI_DEVICE_CAP_CIPHER_WEP40(0x00000001),
+    NM_WIFI_DEVICE_CAP_CIPHER_WEP104(0x00000002),
+    NM_WIFI_DEVICE_CAP_CIPHER_TKIP(0x00000004),
+    NM_WIFI_DEVICE_CAP_CIPHER_CCMP(0x00000008),
+    NM_WIFI_DEVICE_CAP_WPA(0x00000010),
+    NM_WIFI_DEVICE_CAP_RSN(0x00000020),
+    NM_WIFI_DEVICE_CAP_AP(0x00000040),
+    NM_WIFI_DEVICE_CAP_ADHOC(0x00000080),
+    NM_WIFI_DEVICE_CAP_FREQ_VALID(0x00000100),
+    NM_WIFI_DEVICE_CAP_FREQ_2GHZ(0x00000200),
+    NM_WIFI_DEVICE_CAP_FREQ_5GHZ(0x00000400),
+    NM_WIFI_DEVICE_CAP_MESH(0x00001000),
+    NM_WIFI_DEVICE_CAP_IBSS_RSN(0x00002000);
+
+    private final int value;
+
+    NMDeviceWifiCapabilities(int value) {
+        this.value = value;
+    }
 
     public static List<NMDeviceWifiCapabilities> fromUInt32(UInt32 val) {
-        if (val.intValue() == 0x00000000) {
+        int intVal = val.intValue();
+
+        if (intVal == NMDeviceWifiCapabilities.NM_WIFI_DEVICE_CAP_NONE.value) {
             return Arrays.asList(NMDeviceWifiCapabilities.NM_WIFI_DEVICE_CAP_NONE);
         }
 
         List<NMDeviceWifiCapabilities> capabilities = new ArrayList<>();
 
-        if ((val.intValue() & 0x00000001) == 0x00000001) {
-            capabilities.add(NMDeviceWifiCapabilities.NM_WIFI_DEVICE_CAP_CIPHER_WEP40);
-        }
+        for (NMDeviceWifiCapabilities capability : NMDeviceWifiCapabilities.values()) {
+            if (capability == NMDeviceWifiCapabilities.NM_WIFI_DEVICE_CAP_NONE) {
+                continue;
+            }
 
-        if ((val.intValue() & 0x00000002) == 0x00000002) {
-            capabilities.add(NMDeviceWifiCapabilities.NM_WIFI_DEVICE_CAP_CIPHER_WEP104);
-        }
-
-        if ((val.intValue() & 0x00000004) == 0x00000004) {
-            capabilities.add(NMDeviceWifiCapabilities.NM_WIFI_DEVICE_CAP_CIPHER_TKIP);
-        }
-
-        if ((val.intValue() & 0x00000008) == 0x00000008) {
-            capabilities.add(NMDeviceWifiCapabilities.NM_WIFI_DEVICE_CAP_CIPHER_CCMP);
-        }
-
-        if ((val.intValue() & 0x00000010) == 0x00000010) {
-            capabilities.add(NMDeviceWifiCapabilities.NM_WIFI_DEVICE_CAP_WPA);
-        }
-
-        if ((val.intValue() & 0x00000020) == 0x00000020) {
-            capabilities.add(NMDeviceWifiCapabilities.NM_WIFI_DEVICE_CAP_RSN);
-        }
-
-        if ((val.intValue() & 0x00000040) == 0x00000040) {
-            capabilities.add(NMDeviceWifiCapabilities.NM_WIFI_DEVICE_CAP_AP);
-        }
-
-        if ((val.intValue() & 0x00000080) == 0x00000080) {
-            capabilities.add(NMDeviceWifiCapabilities.NM_WIFI_DEVICE_CAP_ADHOC);
-        }
-
-        if ((val.intValue() & 0x00000100) == 0x00000100) {
-            capabilities.add(NMDeviceWifiCapabilities.NM_WIFI_DEVICE_CAP_FREQ_VALID);
-        }
-
-        if ((val.intValue() & 0x00000200) == 0x00000200) {
-            capabilities.add(NMDeviceWifiCapabilities.NM_WIFI_DEVICE_CAP_FREQ_2GHZ);
-        }
-
-        if ((val.intValue() & 0x00000400) == 0x00000400) {
-            capabilities.add(NMDeviceWifiCapabilities.NM_WIFI_DEVICE_CAP_FREQ_5GHZ);
-        }
-
-        if ((val.intValue() & 0x00001000) == 0x00001000) {
-            capabilities.add(NMDeviceWifiCapabilities.NM_WIFI_DEVICE_CAP_MESH);
-        }
-
-        if ((val.intValue() & 0x00002000) == 0x00002000) {
-            capabilities.add(NMDeviceWifiCapabilities.NM_WIFI_DEVICE_CAP_IBSS_RSN);
+            if ((intVal & capability.value) == capability.value) {
+                capabilities.add(capability);
+            }
         }
 
         return capabilities;

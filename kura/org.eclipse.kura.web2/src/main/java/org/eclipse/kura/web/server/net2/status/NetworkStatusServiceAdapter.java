a1afa5d5e502572f4e0754e7939e24b9c337b307
pierantoniomerlino <pierantonio.merlino@eurotech.com>
2023-03-20 16:45:14 +0100

    Code clean up
    Signed-off-by: pierantoniomerlino <pierantonio.merlino@eurotech.com>

--- kura/org.eclipse.kura.web2/src/main/java/org/eclipse/kura/web/server/net2/status/NetworkStatusServiceAdapter.java
+++ kura/org.eclipse.kura.web2/src/main/java/org/eclipse/kura/web/server/net2/status/NetworkStatusServiceAdapter.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
  * Copyright (c) 2023 Eurotech and/or its affiliates and others
- * 
+ *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
  * which is available at https://www.eclipse.org/legal/epl-2.0/
- * 
+ *
  * SPDX-License-Identifier: EPL-2.0
- * 
+ *
  * Contributors:
  *  Eurotech
  *******************************************************************************/
@@ -137,7 +137,7 @@ public class NetworkStatusServiceAdapter {
                     return ((WifiInterfaceStatus) ifaceStatus.get()).getCountryCode();
                 }
             } catch (KuraException e) {
-                logger.error("Cannot retrieve wifi country code", e);
+                this.logger.error("Cannot retrieve wifi country code", e);
             }
         }
 

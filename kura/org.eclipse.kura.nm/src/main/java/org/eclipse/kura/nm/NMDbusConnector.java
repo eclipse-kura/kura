491d3131cedca72a84e6e0da251a147c8b9af4c0
Gregory Ivo <gregorybivo@gmail.com>
2023-03-20 14:38:06 -0400

    refactor: made veriable for connection string

--- kura/org.eclipse.kura.nm/src/main/java/org/eclipse/kura/nm/NMDbusConnector.java
+++ kura/org.eclipse.kura.nm/src/main/java/org/eclipse/kura/nm/NMDbusConnector.java
@@ -66,6 +66,7 @@ public class NMDbusConnector {
     private static final String NM_DEVICE_PROPERTY_DEVICETYPE = "DeviceType";
     private static final String NM_DEVICE_PROPERTY_STATE = "State";
     private static final String NM_DEVICE_PROPERTY_IP4CONFIG = "Ip4Config";
+    private static final String NM_DEVICE_PROPERTY_CONFIGURATION = "connection";
 
     private static final String NM_DEVICE_GENERIC_PROPERTY_TYPEDESCRIPTION = "TypeDescription";
 
@@ -434,7 +435,7 @@ public class NMDbusConnector {
         try {
             Map<String, Map<String, Variant<?>>> connectionSettings = dev.GetAppliedConnection(new UInt32(0))
                     .getConnection();
-            String uuid = String.valueOf(connectionSettings.get("connection").get("uuid").getValue());
+            String uuid = String.valueOf(connectionSettings.get(NM_DEVICE_PROPERTY_CONFIGURATION).get("uuid").getValue());
 
             Settings settings = this.dbusConnection.getRemoteObject(NM_BUS_NAME, NM_SETTINGS_BUS_PATH, Settings.class);
 
@@ -470,8 +471,8 @@ public class NMDbusConnector {
                 
                 Map<String, Map<String, Variant<?>>> workingSettings = availableConnection.GetSettings();
                 
-                String availableConnectionId = (String) workingSettings.get("connection").get("id").getValue();
-                String availableConnectionUuid = (String) workingSettings.get("connection").get("uuid").getValue();
+                String availableConnectionId = (String) workingSettings.get(NM_DEVICE_PROPERTY_CONFIGURATION).get("id").getValue();
+                String availableConnectionUuid = (String) workingSettings.get(NM_DEVICE_PROPERTY_CONFIGURATION).get("uuid").getValue();
                 
                 if (availableConnectionId.equals(expectedConnectionName)) {
                     if (isFirst) {

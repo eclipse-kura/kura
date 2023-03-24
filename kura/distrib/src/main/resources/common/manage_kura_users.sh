3fff0675faffdc2d8a7775876f1b53dfa7b136ea
Salvatore Coppola <salvatore.coppola@eurotech.com>
2023-03-24 14:28:21 +0100

    Replaced regex in polkit rules and added new rules for network profiles.

--- kura/distrib/src/main/resources/common/manage_kura_users.sh
+++ kura/distrib/src/main/resources/common/manage_kura_users.sh
@@ -27,23 +27,23 @@ function create_users {
     # add kurad to dialout group (for managing serial ports)
     gpasswd -a kurad dialout
     
-	# get polkit package version
-	POLKIT=`apt list --installed | grep libpolkit`
-	IFS=" " POLKIT_ARRAY=($POLKIT)
-	POLKIT_VERSION=${POLKIT_ARRAY[1]}
-	
+    # get polkit package version
+    POLKIT=`apt list --installed | grep libpolkit`
+    IFS=" " POLKIT_ARRAY=($POLKIT)
+    POLKIT_VERSION=${POLKIT_ARRAY[1]}
+    
     # add polkit policy
-	if [[ ${POLKIT_VERSION} < 0.106 ]]; then
-		if [ ! -f /etc/polkit-1/localauthority/50-local.d/51-org.freedesktop.systemd1.pkla ]; then
-		    echo "[No password prompt for kurad user]
+    if [[ ${POLKIT_VERSION} < 0.106 ]]; then
+        if [ ! -f /etc/polkit-1/localauthority/50-local.d/51-org.freedesktop.systemd1.pkla ]; then
+            echo "[No password prompt for kurad user]
 Identity=unix-user:kurad
 Action=org.freedesktop.systemd1.*
 ResultInactive=no
 ResultActive=no
 ResultAny=yes" > /etc/polkit-1/localauthority/50-local.d/51-org.freedesktop.systemd1.pkla
-	    fi
-	    if [[ $NN == "NO" ]]; then
-    	    if [ ! -f /etc/polkit-1/localauthority/50-local.d/52-org.freedesktop.networkmanager.pkla ]; then
+        fi
+        if [[ $NN == "NO" ]]; then
+            if [ ! -f /etc/polkit-1/localauthority/50-local.d/52-org.freedesktop.networkmanager.pkla ]; then
                 echo "[No password prompt for kurad user when using NetworkManager]
 Identity=unix-user:kurad
 Action=org.freedesktop.NetworkManager.*
@@ -54,16 +54,16 @@ ResultAny=yes" > /etc/polkit-1/localauthority/50-local.d/52-org.freedesktop.netw
             if [ ! -f /etc/polkit-1/localauthority/50-local.d/53-org.freedesktop.modemmanager.pkla ]; then
                 echo "[No password prompt for kurad user when using ModemManager]
 Identity=unix-user:kurad
-Action=org.freedesktop.ModemManager.*
+Action=org.freedesktop.ModemManager1.*
 ResultInactive=no
 ResultActive=no
 ResultAny=yes" > /etc/polkit-1/localauthority/50-local.d/53-org.freedesktop.modemmanager.pkla
             fi
         fi
-	else  
-	    if [ ! -f /usr/share/polkit-1/rules.d/kura.rules ]; then
-	    	if [[ $NN == "NO" ]]; then
-	            echo "polkit.addRule(function(action, subject) {
+    else  
+        if [ ! -f /usr/share/polkit-1/rules.d/kura.rules ]; then
+            if [[ $NN == "NO" ]]; then
+                echo "polkit.addRule(function(action, subject) {
     if (action.id == \"org.freedesktop.systemd1.manage-units\" &&
         subject.user == \"kurad\" &&
         (action.lookup(\"unit\") == \"named.service\" ||
@@ -76,14 +76,9 @@ ResultAny=yes" > /etc/polkit-1/localauthority/50-local.d/53-org.freedesktop.mode
         subject.user == \"kurad\") {
         return polkit.Result.YES;
     }
-    if (action.id.indexOf(\"org.freedesktop.NetworkManager.\") == 0 ||
-        action.id.indexOf(\"org.freedesktop.ModemManager.\") == 0 &&
-        subject.user == \"kurad\") {
-        return polkit.Result.YES;
-    }
 });" > /usr/share/polkit-1/rules.d/kura.rules
-			else
-			    echo "polkit.addRule(function(action, subject) {
+            else
+                echo "polkit.addRule(function(action, subject) {
     if (action.id == \"org.freedesktop.systemd1.manage-units\" &&
         subject.user == \"kurad\" &&
         (action.lookup(\"unit\") == \"bluetooth.service\" ||
@@ -96,15 +91,24 @@ ResultAny=yes" > /etc/polkit-1/localauthority/50-local.d/53-org.freedesktop.mode
         return polkit.Result.YES;
     }
 });" > /usr/share/polkit-1/rules.d/kura.rules
-			fi
-	    fi
-	fi
-	
-	# modify pam policy
-	if [ -f /etc/pam.d/su ]; then
-		sed -i '/^auth       sufficient pam_rootok.so/a auth       [success=ignore default=1] pam_succeed_if.so user = kura\nauth       sufficient                 pam_succeed_if.so use_uid user = kurad' /etc/pam.d/su 
-	fi
-	
+            fi
+        fi
+        if [ ! -f /usr/share/polkit-1/rules.d/kura-nm.rules ]; then
+            echo "polkit.addRule(function(action, subject) {
+        if ((action.id.indexOf(\"org.freedesktop.NetworkManager.\") == 0 ||
+            action.id.indexOf(\"org.freedesktop.ModemManager1.\") == 0) &&
+            subject.user == \"kurad\") {
+            return polkit.Result.YES;
+        }
+    });" > /usr/share/polkit-1/rules.d/kura-nm.rules
+        fi
+    fi
+    
+    # modify pam policy
+    if [ -f /etc/pam.d/su ]; then
+        sed -i '/^auth       sufficient pam_rootok.so/a auth       [success=ignore default=1] pam_succeed_if.so user = kura\nauth       sufficient                 pam_succeed_if.so use_uid user = kurad' /etc/pam.d/su 
+    fi
+    
         
     # grant kurad user the privileges to manage ble via dbus
     grep -lR kurad /etc/dbus-1/system.d/bluetooth.conf
@@ -134,39 +138,39 @@ ResultAny=yes" > /etc/polkit-1/localauthority/50-local.d/53-org.freedesktop.mode
 }
 
 function delete_users {
-	# delete kura user
-	userdel kura
-	
-	# we cannot delete kurad system user because there should be several process owned by this user,
-	# so only try to remove it from sudoers.
-	if [ -f /etc/sudoers.d/kurad ]; then
-		rm -f /etc/sudoers.d/kurad
-	fi
-	# remove kurad from dialout group
-	gpasswd -d kurad dialout
-	
-	# remove polkit policy
-	if [ -f /usr/share/polkit-1/rules.d/kura.rules ]; then
-		rm -f /usr/share/polkit-1/rules.d/kura.rules
-	fi
-	if [ -f /etc/polkit-1/localauthority/50-local.d/51-org.freedesktop.systemd1.pkla ]; then
-		rm -f /etc/polkit-1/localauthority/50-local.d/51-org.freedesktop.systemd1.pkla
-	fi
-	if [ -f /etc/polkit-1/localauthority/50-local.d/52-org.freedesktop.networkmanager.pkla ]; then
+    # delete kura user
+    userdel kura
+    
+    # we cannot delete kurad system user because there should be several process owned by this user,
+    # so only try to remove it from sudoers.
+    if [ -f /etc/sudoers.d/kurad ]; then
+        rm -f /etc/sudoers.d/kurad
+    fi
+    # remove kurad from dialout group
+    gpasswd -d kurad dialout
+    
+    # remove polkit policy
+    if [ -f /usr/share/polkit-1/rules.d/kura.rules ]; then
+        rm -f /usr/share/polkit-1/rules.d/kura.rules
+    fi
+    if [ -f /etc/polkit-1/localauthority/50-local.d/51-org.freedesktop.systemd1.pkla ]; then
+        rm -f /etc/polkit-1/localauthority/50-local.d/51-org.freedesktop.systemd1.pkla
+    fi
+    if [ -f /etc/polkit-1/localauthority/50-local.d/52-org.freedesktop.networkmanager.pkla ]; then
         rm -f /etc/polkit-1/localauthority/50-local.d/52-org.freedesktop.networkmanager.pkla
     fi
     if [ -f /etc/polkit-1/localauthority/50-local.d/53-org.freedesktop.modemmanager.pkla ]; then
         rm -f /etc/polkit-1/localauthority/50-local.d/53-org.freedesktop.modemmanager.pkla
     fi
-	
-	# recover pam policy
-	if [ -f /etc/pam.d/su ]; then
-		sed -i '/^auth       sufficient pam_rootok.so/ {n;d}' /etc/pam.d/su
-		sed -i '/^auth       sufficient pam_rootok.so/ {n;d}' /etc/pam.d/su
-	fi
-	
-	# recover old dbus config
-	mv /etc/dbus-1/system.d/bluetooth.conf.save /etc/dbus-1/system.d/bluetooth.conf
+    
+    # recover pam policy
+    if [ -f /etc/pam.d/su ]; then
+        sed -i '/^auth       sufficient pam_rootok.so/ {n;d}' /etc/pam.d/su
+        sed -i '/^auth       sufficient pam_rootok.so/ {n;d}' /etc/pam.d/su
+    fi
+    
+    # recover old dbus config
+    mv /etc/dbus-1/system.d/bluetooth.conf.save /etc/dbus-1/system.d/bluetooth.conf
 }
 
 INSTALL=YES
@@ -207,5 +211,5 @@ if [[ $INSTALL == "YES" ]]
 then
     create_users
 else
-	delete_users
+    delete_users
 fi

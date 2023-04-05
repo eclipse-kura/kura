3984ce03112cb70fbd8b2375aa262c3ad480bb37
Marcello Rinaldo Martina <39562568+marcellorinaldo@users.noreply.github.com>
2023-04-05 15:45:35 +0200

    feat(nm): add firewall NAT rules update on configuration change (#4512)
    * build(nm): added dependency to linux.net.iptables
    
    Signed-off-by: Marcello Martina <martina.marcello.rinaldo@outlook.com>
    
    * feat(nm): added NAT firewall rules writing
    
    Signed-off-by: Marcello Martina <martina.marcello.rinaldo@outlook.com>
    
    * refactor: added copyright header, clean code
    
    Signed-off-by: Marcello Martina <martina.marcello.rinaldo@outlook.com>
    
    * test: added unit tests for FirewallNatConfigWriter
    
    Signed-off-by: Marcello Martina <martina.marcello.rinaldo@outlook.com>
    
    * fix: merged loops
    
    Signed-off-by: Marcello Martina <martina.marcello.rinaldo@outlook.com>
    
    ---------
    
    Signed-off-by: Marcello Martina <martina.marcello.rinaldo@outlook.com>

---
+++ kura/org.eclipse.kura.nm/src/main/java/org/eclipse/kura/nm/configuration/writer/FirewallNatConfigWriter.java
@@ -0,0 +1,73 @@
+/*******************************************************************************
+ * Copyright (c) 2023 Eurotech and/or its affiliates and others
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *  Eurotech
+ ******************************************************************************/
+package org.eclipse.kura.nm.configuration.writer;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.eclipse.kura.KuraErrorCode;
+import org.eclipse.kura.KuraException;
+import org.eclipse.kura.executor.CommandExecutorService;
+import org.eclipse.kura.linux.net.iptables.LinuxFirewall;
+import org.eclipse.kura.linux.net.iptables.NATRule;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class FirewallNatConfigWriter {
+
+    private static final Logger logger = LoggerFactory.getLogger(FirewallNatConfigWriter.class);
+
+    private final CommandExecutorService executorService;
+    private LinuxFirewall firewall;
+    private final List<String> wanInterfaceNames;
+    private final List<String> natInterfaceNames;
+
+    public FirewallNatConfigWriter(CommandExecutorService executorService, List<String> wanInterfaceNames, List<String> natInterfaceNames) {
+        this.executorService = executorService;
+        this.wanInterfaceNames = wanInterfaceNames;
+        this.natInterfaceNames = natInterfaceNames;
+        this.firewall = LinuxFirewall.getInstance(this.executorService);
+    }
+
+    public void writeConfiguration() throws KuraException {
+        try {
+            Set<NATRule> natRules = new HashSet<>();
+
+            for (String source : this.natInterfaceNames) {
+                for (String destination : this.wanInterfaceNames) {
+                    if (!source.equalsIgnoreCase(destination)) {
+                        natRules.add(createNATRule(source, destination));
+                    }
+                }
+            }
+
+            this.firewall.replaceAllNatRules(natRules);
+        } catch (Exception e) {
+            throw new KuraException(KuraErrorCode.CONFIGURATION_ERROR,
+                    "Failed to replace all NAT rules with new ones for interfaces: " + this.natInterfaceNames, e);
+        }
+    }
+
+    private NATRule createNATRule(String source, String destination) {
+        NATRule natRule = new NATRule();
+        natRule.setSourceInterface(source);
+        natRule.setDestinationInterface(destination);
+        natRule.setMasquerade(true);
+
+        logger.info("Applying NAT rule {} -> {}: {}", source, destination, natRule);
+
+        return natRule;
+    }
+
+}

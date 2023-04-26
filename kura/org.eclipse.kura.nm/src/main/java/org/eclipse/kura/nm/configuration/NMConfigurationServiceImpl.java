a32ec6cc4674001ede22b99d3fb27f7b028ae5de
Marcello Martina <martina.marcello.rinaldo@outlook.com>
2023-04-26 16:36:10 +0200

    chore: removed unecessary code
    Signed-off-by: Marcello Martina <martina.marcello.rinaldo@outlook.com>

--- kura/org.eclipse.kura.nm/src/main/java/org/eclipse/kura/nm/configuration/NMConfigurationServiceImpl.java
+++ kura/org.eclipse.kura.nm/src/main/java/org/eclipse/kura/nm/configuration/NMConfigurationServiceImpl.java
@@ -374,7 +374,7 @@ public class NMConfigurationServiceImpl implements SelfConfiguringComponent {
     private void writeDhcpServerConfiguration(Set<String> interfaceNames) {
         interfaceNames.forEach(interfaceName -> {
             if (isDhcpServerValid(interfaceName)) {
-                DhcpServerConfigWriter dhcpServerConfigWriter = buildDhcpServerConfigWriter(interfaceName,
+                DhcpServerConfigWriter dhcpServerConfigWriter = new DhcpServerConfigWriter(interfaceName,
                         this.networkProperties);
                 try {
                     dhcpServerConfigWriter.writeConfiguration();
@@ -387,11 +387,6 @@ public class NMConfigurationServiceImpl implements SelfConfiguringComponent {
                 this.dhcpServerMonitor.putDhcpServerInterfaceConfiguration(interfaceName, false);
             }
         });
-    }
-
-    protected DhcpServerConfigWriter buildDhcpServerConfigWriter(final String interfaceName,
-            final NetworkProperties properties) {
-        return new DhcpServerConfigWriter(interfaceName, properties);
     }
 
     private boolean isDhcpServerValid(String interfaceName) {

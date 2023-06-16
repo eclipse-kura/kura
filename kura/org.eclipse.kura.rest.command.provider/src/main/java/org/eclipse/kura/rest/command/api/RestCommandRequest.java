e27d55d110fbccaec21062d6c4b93f53ce2dc8c6
Gregory Ivo <gregorybivo@gmail.com>
2023-06-16 13:39:42 -0400

    feat: added request and response API's

---
+++ kura/org.eclipse.kura.rest.command.provider/src/main/java/org/eclipse/kura/rest/command/api/RestCommandRequest.java
@@ -0,0 +1,81 @@
+/*******************************************************************************
+ * Copyright (c) 2021 Eurotech and/or its affiliates and others
+ * 
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ * 
+ * SPDX-License-Identifier: EPL-2.0
+ * 
+ * Contributors:
+ *  Eurotech
+ *******************************************************************************/
+package org.eclipse.kura.rest.command.api;
+
+public class RestCommandRequest {
+
+    private String command;
+    private String[] arguments;
+    private String[] enviromentPairs;
+    private String workingDirectory;
+    private String password;
+    private boolean isRunAsync;
+    private byte[] zipBytes;
+
+    public void setCommand(String command) {
+        this.command = command;
+    }
+
+    public void setArguments(String[] arguments) {
+        this.arguments = arguments;
+    }
+
+    public void setEnviromentPairs(String[] enviromentPairs) {
+        this.arguments = enviromentPairs;
+    }
+
+    public void setWorkingDirectory(String workingDirectory) {
+        this.workingDirectory = workingDirectory;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setIsRunAsync(boolean isRunAsync) {
+        this.isRunAsync = isRunAsync;
+    }
+
+    public void setZipBytes(byte[] body) {
+        this.zipBytes = body;
+    }
+
+    public String getCommand() {
+        return this.command;
+    }
+
+    public String[] getArguments() {
+        return this.arguments;
+    }
+
+    public String[] getEnviromentPairs() {
+        return this.enviromentPairs;
+    }
+
+    public String getWorkingDirectory() {
+        return this.workingDirectory;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public boolean getIsRunAsync() {
+        return this.isRunAsync;
+    }
+
+    public byte[] getZipBytes() {
+        return this.zipBytes;
+    }
+
+}

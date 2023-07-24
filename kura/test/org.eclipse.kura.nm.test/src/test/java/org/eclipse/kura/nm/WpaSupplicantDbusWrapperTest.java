1c506ddb168bc95a58311300281ed760d06a1ff9
Mattia Dal Ben <matthewdibi@gmail.com>
2023-07-24 09:31:42 +0200

    style: add forgotten copyright header

--- kura/test/org.eclipse.kura.nm.test/src/test/java/org/eclipse/kura/nm/WpaSupplicantDbusWrapperTest.java
+++ kura/test/org.eclipse.kura.nm.test/src/test/java/org/eclipse/kura/nm/WpaSupplicantDbusWrapperTest.java
@@ -1,9 +1,20 @@
+/*******************************************************************************
+ * Copyright (c) 2023 Eurotech and/or its affiliates and others
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *  Eurotech
+ *******************************************************************************/
 package org.eclipse.kura.nm;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;

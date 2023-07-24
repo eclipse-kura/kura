95afbd754ccf2413a5b0240ecf9a871be4faf748
Mattia Dal Ben <matthewdibi@gmail.com>
2023-07-24 09:58:48 +0200

    refactor: use gerkhin style

--- kura/test/org.eclipse.kura.nm.test/src/test/java/org/eclipse/kura/nm/signal/handlers/WpaScanDoneHandlerTest.java
+++ kura/test/org.eclipse.kura.nm.test/src/test/java/org/eclipse/kura/nm/signal/handlers/WpaScanDoneHandlerTest.java
@@ -4,41 +4,63 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 import java.util.concurrent.CountDownLatch;
 
+import org.freedesktop.dbus.exceptions.DBusException;
 import org.junit.Test;
 
 import fi.w1.wpa_supplicant1.Interface;
 
 public class WpaScanDoneHandlerTest {
 
+    private CountDownLatch mockLatch = mock(CountDownLatch.class);
+    private WPAScanDoneHandler handler;
+
     @Test
-    public void WpaScanDoneHandlerShouldTriggerWithExpectedDBusPath() {
-        CountDownLatch mockLatch = mock(CountDownLatch.class);
+    public void WpaScanDoneHandlerShouldTriggerWithExpectedDBusPath() throws DBusException {
 
-        WPAScanDoneHandler handler = new WPAScanDoneHandler(mockLatch, "/fi/w1/wpa_supplicant1/Interfaces/0");
+        givenWpaScanDoneHandlerWithDBusPath("/fi/w1/wpa_supplicant1/Interfaces/0");
 
-        Interface.ScanDone scanDone = mock(Interface.ScanDone.class);
-        when(scanDone.getPath()).thenReturn("/fi/w1/wpa_supplicant1/Interfaces/0");
+        whenHandlerReceivesScanDoneSignalWith("/fi/w1/wpa_supplicant1/Interfaces/0");
 
-        handler.handle(scanDone);
-
-        verify(mockLatch, times(1)).countDown();
+        thenLatchShouldBeCountedDown();
     }
 
     @Test
-    public void WpaScanDoneHandlerShouldNotTriggerWithDifferentDBusPath() {
-        CountDownLatch mockLatch = mock(CountDownLatch.class);
+    public void WpaScanDoneHandlerShouldNotTriggerWithDifferentDBusPath() throws DBusException {
+        givenWpaScanDoneHandlerWithDBusPath("/fi/w1/wpa_supplicant1/Interfaces/0");
 
-        WPAScanDoneHandler handler = new WPAScanDoneHandler(mockLatch, "/fi/w1/wpa_supplicant1/Interfaces/0");
+        whenHandlerReceivesScanDoneSignalWith("/fi/w1/wpa_supplicant1/Interfaces/6");
 
-        Interface.ScanDone scanDone = mock(Interface.ScanDone.class);
-        when(scanDone.getPath()).thenReturn("/fi/w1/wpa_supplicant1/Interfaces/6");
+        thenLatchShouldNotBeUpdated();
+    }
 
-        handler.handle(scanDone);
+    /*
+     * Given
+     */
 
-        verify(mockLatch, never()).countDown();
+    private void givenWpaScanDoneHandlerWithDBusPath(String dbusPath) {
+        this.handler = new WPAScanDoneHandler(this.mockLatch, dbusPath);
+    }
+
+    /*
+     * When
+     */
+
+    private void whenHandlerReceivesScanDoneSignalWith(String dbusPath) throws DBusException {
+        Interface.ScanDone scanDoneSignal = new Interface.ScanDone(dbusPath, true);
+        handler.handle(scanDoneSignal);
+    }
+
+    /*
+     * Then
+     */
+    private void thenLatchShouldBeCountedDown() {
+        verify(this.mockLatch, times(1)).countDown();
+    }
+
+    private void thenLatchShouldNotBeUpdated() {
+        verify(this.mockLatch, never()).countDown();
     }
 }

812e3dc93e089a6f6e664feeb0f550e3be4ee1f4
SimoneFiorani <simone.fiorani@abinsula.com>
2023-07-27 14:51:27 +0200

    refactor(nm): refactor Ip6Privacy

--- kura/org.eclipse.kura.nm/src/main/java/org/eclipse/kura/nm/KuraIp6Privacy.java
+++ kura/org.eclipse.kura.nm/src/main/java/org/eclipse/kura/nm/KuraIp6Privacy.java
@@ -16,15 +16,12 @@ import org.eclipse.kura.nm.enums.NMSettingIP6ConfigPrivacy;
 
 public enum KuraIp6Privacy {
 
-    UNKNOWN,
     DISABLED,
     ENABLED_PUBLIC_ADD,
     ENABLED_TEMP_ADD;
 
     public static KuraIp6Privacy fromString(String status) {
         switch (status) {
-        case "netIPv6PrivacyUnknown":
-            return KuraIp6Privacy.UNKNOWN;
         case "netIPv6PrivacyDisabled":
             return KuraIp6Privacy.DISABLED;
         case "netIPv6PrivacyEnabledPubAdd":
@@ -38,8 +35,6 @@ public enum KuraIp6Privacy {
 
     public static NMSettingIP6ConfigPrivacy toNMSettingIP6ConfigPrivacy(KuraIp6Privacy privacyValue) {
         switch (privacyValue) {
-        case UNKNOWN:
-            return NMSettingIP6ConfigPrivacy.NM_SETTING_IP6_CONFIG_PRIVACY_UNKNOWN;
         case DISABLED:
             return NMSettingIP6ConfigPrivacy.NM_SETTING_IP6_CONFIG_PRIVACY_DISABLED;
         case ENABLED_PUBLIC_ADD:
@@ -47,7 +42,7 @@ public enum KuraIp6Privacy {
         case ENABLED_TEMP_ADD:
             return NMSettingIP6ConfigPrivacy.NM_SETTING_IP6_CONFIG_PRIVACY_PREFER_TEMP_ADDR;
         default:
-            return NMSettingIP6ConfigPrivacy.NM_SETTING_IP6_CONFIG_PRIVACY_UNKNOWN;
+            throw new IllegalArgumentException(String.format("Unsupported IPv6 privacy value: \"%s\"", privacyValue));
         }
     }
 }

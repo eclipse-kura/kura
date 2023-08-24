fb3b9fceb34f851f2216306114d5881eecfcf57a
Marcello Martina <martina.marcello.rinaldo@outlook.com>
2023-08-24 11:26:00 +0200

    Missing commit
    Signed-off-by: Marcello Martina <martina.marcello.rinaldo@outlook.com>

--- kura/org.eclipse.kura.rest.system.provider/src/main/java/org/eclipse/kura/rest/system/Constants.java
+++ kura/org.eclipse.kura.rest.system.provider/src/main/java/org/eclipse/kura/rest/system/Constants.java
@@ -20,7 +20,6 @@ public class Constants {
     public static final String REST_APP_ID = "system/v1";
     public static final String RESOURCE_PROPERTIES = "/properties";
     public static final String RESOURCE_PROPERTIES_FILTER = "/properties/filter";
-    public static final String RESOURCE_BUNDLES = "/bundles";
     public static final String REST_ROLE_NAME = "system";
     public static final String KURA_PERMISSION_REST_ROLE = "kura.permission.rest." + REST_ROLE_NAME;
 

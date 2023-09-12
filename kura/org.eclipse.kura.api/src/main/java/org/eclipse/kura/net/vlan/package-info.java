3208904aeee9f9fe120043a16d2bf4053bb5689a
Mattia Dal Ben <mattdibi@users.noreply.github.com>
2023-09-12 09:28:23 +0200

    style: fix copyright headers
    Fix copyright headers as per https://gitlab.eclipse.org/eclipsefdn/emo-team/iplab/-/issues/10343

--- kura/org.eclipse.kura.api/src/main/java/org/eclipse/kura/net/vlan/package-info.java
+++ kura/org.eclipse.kura.api/src/main/java/org/eclipse/kura/net/vlan/package-info.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2023 Eurotech and/or its affiliates and others
+ * Copyright (c) 2023 Areti and others
  *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
@@ -9,10 +9,8 @@
  *
  * Contributors:
  *  Areti
+ *  Eurotech
  ******************************************************************************/
-/**
- *
- */
 /**
  * Provides interfaces for instances and configurations of  Vlan.
  *

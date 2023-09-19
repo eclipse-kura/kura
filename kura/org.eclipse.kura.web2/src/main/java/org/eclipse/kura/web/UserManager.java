7edaa6d88df851e54e9ffa3181c2ea533ab58378
nicolatimeus <nicola.timeus@eurotech.com>
2023-09-19 09:18:18 +0200

    feat: Added session management support for REST service (#4855)
    * feat: Added session management support for REST service
    
    Signed-off-by: Nicola Timeus <nicola.timeus@eurotech.com>
    
    * fix: Fixed incorrect endpoint paths in metatype
    
    Signed-off-by: Nicola Timeus <nicola.timeus@eurotech.com>
    
    * fix: Added minimum value for session inactivity interval
    
    * fix: Removed unused method
    
    Signed-off-by: Nicola Timeus <nicola.timeus@eurotech.com>
    
    * fix: Fixed spelling error
    
    * fix: Do not return XSRF token for expired session
    
    Signed-off-by: Nicola Timeus <nicola.timeus@eurotech.com>
    
    ---------
    
    Signed-off-by: Nicola Timeus <nicola.timeus@eurotech.com>

--- kura/org.eclipse.kura.web2/src/main/java/org/eclipse/kura/web/UserManager.java
+++ kura/org.eclipse.kura.web2/src/main/java/org/eclipse/kura/web/UserManager.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2020, 2022 Eurotech and/or its affiliates
+ * Copyright (c) 2020, 2023 Eurotech and/or its affiliates
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -12,7 +12,6 @@ package org.eclipse.kura.web;
 
 import java.io.UnsupportedEncodingException;
 import java.security.NoSuchAlgorithmException;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.Dictionary;
 import java.util.HashMap;
@@ -25,15 +24,18 @@ import java.util.Set;
 import org.eclipse.kura.KuraErrorCode;
 import org.eclipse.kura.KuraException;
 import org.eclipse.kura.crypto.CryptoService;
+import org.eclipse.kura.util.useradmin.UserAdminHelper;
+import org.eclipse.kura.util.useradmin.UserAdminHelper.AuthenticationException;
+import org.eclipse.kura.util.useradmin.UserAdminHelper.FallibleConsumer;
 import org.eclipse.kura.web.shared.KuraPermission;
 import org.eclipse.kura.web.shared.model.GwtUserConfig;
 import org.eclipse.kura.web.shared.model.GwtUserData;
 import org.osgi.framework.InvalidSyntaxException;
-import org.osgi.service.useradmin.Group;
 import org.osgi.service.useradmin.Role;
 import org.osgi.service.useradmin.User;
 import org.osgi.service.useradmin.UserAdmin;
 
+@SuppressWarnings("restriction")
 public class UserManager {
 
     private static final String PERMISSION_ROLE_NAME_PREFIX = "kura.permission.";
@@ -41,12 +43,13 @@ public class UserManager {
     private static final String PASSWORD_PROPERTY = "kura.password";
     private static final String KURA_NEED_PASSWORD_CHANGE = "kura.need.password.change";
 
-    private final UserAdmin userAdmin;
     private final CryptoService cryptoService;
 
+    private final UserAdminHelper userAdminHelper;
+
     public UserManager(final UserAdmin userAdmin, final CryptoService cryptoService) {
-        this.userAdmin = userAdmin;
         this.cryptoService = cryptoService;
+        this.userAdminHelper = new UserAdminHelper(userAdmin, cryptoService);
     }
 
     public void update(final ConsoleOptions consoleOptions)
@@ -55,120 +58,55 @@ public class UserManager {
     }
 
     public void authenticateWithPassword(final String username, final String password) throws KuraException {
-        final Role role = userAdmin.getRole(getUserRoleName(username));
-
-        if (!(role instanceof User)) {
-            throw new KuraException(KuraErrorCode.SECURITY_EXCEPTION);
-        }
-
-        final User asUser = (User) role;
-
         try {
-            String sha256Password = cryptoService.sha256Hash(password);
-
-            if (!Objects.equals(sha256Password, asUser.getCredentials().get(PASSWORD_PROPERTY))) {
-                throw new KuraException(KuraErrorCode.SECURITY_EXCEPTION);
-            }
-        } catch (final KuraException e) {
-            throw e;
-        } catch (Exception e) {
-            throw new KuraException(KuraErrorCode.SECURITY_EXCEPTION);
+            this.userAdminHelper.verifyUsernamePassword(username, password);
+        } catch (final AuthenticationException e) {
+            throw new KuraException(KuraErrorCode.SECURITY_EXCEPTION, e);
         }
     }
 
     public void requirePermissions(final String username, final String... permissions) throws KuraException {
-        final String userRoleName = getUserRoleName(username);
-        final Role role = userAdmin.getRole(userRoleName);
-
-        if (!(role instanceof User)) {
-            throw new KuraException(KuraErrorCode.SECURITY_EXCEPTION);
-        }
-
-        final Group admin = getOrCreatePermission(KuraPermission.ADMIN);
-
-        if (admin.getMembers() != null && Arrays.stream(admin.getMembers()).anyMatch(role::equals)) {
-            return;
-        }
-
-        for (final String permission : permissions) {
-            final String permissionRoleName = getPermissionRoleName(permission);
-            final Role permissionRole = userAdmin.getRole(permissionRoleName);
-
-            if (!(permissionRole instanceof Group)) {
-                throw new KuraException(KuraErrorCode.SECURITY_EXCEPTION);
-            }
-
-            final Group asGroup = (Group) permissionRole;
-            final Role[] members = asGroup.getMembers();
-
-            if (members != null && !Arrays.stream(members).anyMatch(r -> r.getName().equals(permissionRoleName))) {
-                throw new KuraException(KuraErrorCode.SECURITY_EXCEPTION);
-            }
+        try {
+            this.userAdminHelper.requirePermissions(username, permissions);
+        } catch (final AuthenticationException e) {
+            throw new KuraException(KuraErrorCode.SECURITY_EXCEPTION, e);
         }
     }
 
-    public boolean isPasswordChangeRequired(final String username) throws KuraException {
-        final String userRoleName = getUserRoleName(username);
-        final Role role = userAdmin.getRole(userRoleName);
-
-        if (!(role instanceof User)) {
-            throw new KuraException(KuraErrorCode.SECURITY_EXCEPTION);
-        }
-
-        final User asUser = (User) role;
-
-        return "true".equals(asUser.getProperties().get(KURA_NEED_PASSWORD_CHANGE));
+    public boolean isPasswordChangeRequired(final String username) {
+        return this.userAdminHelper.isPasswordChangeRequired(username);
     }
 
     public void createUser(final String userName) {
-        getOrCreateUser(getUserRoleName(userName));
+        this.userAdminHelper.createUser(userName);
     }
 
     public void deleteUser(final String userName) {
 
-        final Optional<User> user = getUser(userName);
-
-        if (!user.isPresent()) {
-            return;
-        }
-
-        foreachPermission((name, group) -> group.removeMember(user.get()));
-
-        this.userAdmin.removeRole(user.get().getName());
+        this.userAdminHelper.deleteUser(userName);
     }
 
-    @SuppressWarnings("unchecked")
     public boolean setUserPassword(final String userName, final String userPassword) throws KuraException {
-        final User user = getUser(userName).orElseThrow(() -> new KuraException(KuraErrorCode.NOT_FOUND));
-
         try {
-            final String newHash = cryptoService.sha256Hash(userPassword);
-
-            if (Objects.equals(user.getCredentials().get(PASSWORD_PROPERTY), newHash)) {
+            this.userAdminHelper.changeUserPassword(userName, userPassword);
+            return true;
+        } catch (final AuthenticationException e) {
+            if (e.getReason() == AuthenticationException.Reason.PASSWORD_CHANGE_WITH_SAME_PASSWORD) {
                 return false;
             }
 
-            user.getCredentials().put(PASSWORD_PROPERTY, newHash);
-            user.getProperties().remove(KURA_NEED_PASSWORD_CHANGE);
-
-            return true;
-        } catch (final Exception e) {
-            throw new KuraException(KuraErrorCode.SERVICE_UNAVAILABLE, e);
+            throw new KuraException(KuraErrorCode.SECURITY_EXCEPTION);
         }
     }
 
     public Set<String> getDefinedPermissions() {
-        final Set<String> result = new HashSet<>();
-
-        foreachPermission((permission, group) -> result.add(permission));
-
-        return result;
+        return this.userAdminHelper.getDefinedPermissions();
     }
 
     public Set<GwtUserConfig> getUserConfig() {
         final Map<String, GwtUserConfig> result = new HashMap<>();
 
-        foreachUser((name, user) -> {
+        this.userAdminHelper.foreachUser((name, user) -> {
 
             final GwtUserConfig userData = initUserConfig(user);
 
@@ -182,7 +120,7 @@ public class UserManager {
 
     public Optional<GwtUserConfig> getUserConfig(final String userName) {
 
-        final Optional<User> user = getUser(userName);
+        final Optional<User> user = this.userAdminHelper.getUser(userName);
 
         if (!user.isPresent()) {
             return Optional.empty();
@@ -197,33 +135,21 @@ public class UserManager {
     }
 
     public Optional<Integer> getCredentialsHash(final String userName) {
-        final Optional<User> user = getUser(userName);
-
-        if (!user.isPresent()) {
-            return Optional.empty();
-        }
-
-        final Dictionary<?, ?> credentials = user.get().getCredentials();
-
-        if (credentials == null) {
-            return Optional.empty();
-        }
-
-        return Optional.of(credentials.hashCode());
+        return this.userAdminHelper.getCredentialsHash(userName);
     }
 
     @SuppressWarnings("unchecked")
     public void setUserConfig(final Set<GwtUserConfig> userData) throws KuraException {
-        foreachUser((name, user) -> {
+        this.userAdminHelper.foreachUser((name, user) -> {
             if (userData.stream().noneMatch(data -> data.getUserName().equals(name))) {
                 deleteUser(name);
             }
         });
 
-        foreachPermission((permissionName, permissionGroup) -> {
+        this.userAdminHelper.foreachPermission((permissionName, permissionGroup) -> {
             for (final GwtUserData data : userData) {
 
-                final User user = getOrCreateUser(data.getUserName());
+                final User user = this.userAdminHelper.getOrCreateUser(data.getUserName());
 
                 if (data.getPermissions().contains(permissionName)) {
                     permissionGroup.addMember(user);
@@ -234,7 +160,7 @@ public class UserManager {
         });
 
         for (final GwtUserConfig config : userData) {
-            final User user = getOrCreateUser(config.getUserName());
+            final User user = this.userAdminHelper.getOrCreateUser(config.getUserName());
 
             @SuppressWarnings("rawtypes")
             final Dictionary credentials = user.getCredentials();
@@ -264,22 +190,6 @@ public class UserManager {
         }
     }
 
-    private Optional<User> getUser(final String name) {
-        final String roleName = getUserRoleName(name);
-
-        final Role role = userAdmin.getRole(roleName);
-
-        if (!(role instanceof User)) {
-            return Optional.empty();
-        }
-
-        if (!getBaseName(role).equals(name)) {
-            return Optional.empty();
-        }
-
-        return Optional.of((User) role);
-    }
-
     private GwtUserConfig initUserConfig(final User user) {
 
         final boolean isPasswordEnabled = user.getCredentials().get(PASSWORD_PROPERTY) instanceof String;
@@ -290,7 +200,7 @@ public class UserManager {
     }
 
     private void fillPermissions(final Map<String, ? extends GwtUserData> userData) {
-        foreachPermission((permission, group) -> {
+        this.userAdminHelper.foreachPermission((permission, group) -> {
 
             forEach(group.getMembers(), member -> {
                 final GwtUserData data = userData.get(member.getName());
@@ -300,14 +210,6 @@ public class UserManager {
                 }
             });
         });
-    }
-
-    private static String getUserRoleName(final String name) {
-        return USER_ROLE_NAME_PREFIX + name;
-    }
-
-    private static String getPermissionRoleName(final String name) {
-        return PERMISSION_ROLE_NAME_PREFIX + name;
     }
 
     private static boolean isKuraUser(final Role role) {
@@ -330,50 +232,6 @@ public class UserManager {
         }
     }
 
-    private Group getOrCreatePermission(final String name) {
-        return getOrCreateRole(Group.class, getPermissionRoleName(name));
-    }
-
-    private User getOrCreateUser(final String name) {
-        return getOrCreateRole(User.class, getUserRoleName(name));
-    }
-
-    private void initializeUserAdmin(final ConsoleOptions options)
-            throws NoSuchAlgorithmException, UnsupportedEncodingException, KuraException, InvalidSyntaxException {
-
-        for (final String defaultPermission : KuraPermission.DEFAULT_PERMISSIONS) {
-            getOrCreatePermission(defaultPermission);
-        }
-    }
-
-    @SuppressWarnings("unchecked")
-    private <T extends Role> T getOrCreateRole(final Class<T> classz, final String name) {
-
-        final int type;
-
-        if (classz == Role.class) {
-            type = Role.ROLE;
-        } else if (classz == User.class) {
-            type = Role.USER;
-        } else if (classz == Group.class) {
-            type = Role.GROUP;
-        } else {
-            throw new IllegalArgumentException("unknown role type");
-        }
-
-        final Role result = userAdmin.getRole(name);
-
-        if (result != null && result.getType() == type) {
-            return (T) result;
-        } else if (result == null) {
-
-            return (T) userAdmin.createRole(name, type);
-
-        } else {
-            throw new IllegalArgumentException("role exists but has different type");
-        }
-    }
-
     private static <T, E extends Exception> void forEach(final T[] items, final FallibleConsumer<T, E> consumer)
             throws E {
         if (items != null) {
@@ -383,63 +241,12 @@ public class UserManager {
         }
     }
 
-    @SuppressWarnings("unchecked")
-    private <R extends Role, E extends Exception> void foreachRole(final Class<R> classz,
-            final FallibleConsumer<R, E> consumer) throws E {
-        try {
-            final Role[] existingRoles = userAdmin.getRoles(null);
+    private void initializeUserAdmin(final ConsoleOptions options)
+            throws NoSuchAlgorithmException, UnsupportedEncodingException, KuraException, InvalidSyntaxException {
 
-            if (existingRoles != null) {
-                for (final Role role : existingRoles) {
-                    if (!classz.isInstance(role)) {
-                        continue;
-                    }
-
-                    consumer.accept((R) role);
-                }
-            }
-        } catch (InvalidSyntaxException e) {
-            // no need
+        for (final String defaultPermission : KuraPermission.DEFAULT_PERMISSIONS) {
+            this.userAdminHelper.getOrCreatePermission(defaultPermission);
         }
     }
 
-    private <E extends Exception> void foreachUser(final UserConsumer<E> consumer) throws E {
-        foreachRole(User.class, user -> {
-
-            final String name = user.getName();
-
-            if (!name.startsWith(USER_ROLE_NAME_PREFIX)) {
-                return;
-            }
-
-            consumer.accept(name.substring(USER_ROLE_NAME_PREFIX.length()), user);
-        });
-    }
-
-    private <E extends Exception> void foreachPermission(final PermissionConsumer<E> consumer) throws E {
-        foreachRole(Group.class, group -> {
-            final String name = group.getName();
-
-            if (!name.startsWith(PERMISSION_ROLE_NAME_PREFIX)) {
-                return;
-            }
-
-            consumer.accept(name.substring(PERMISSION_ROLE_NAME_PREFIX.length()), group);
-        });
-    }
-
-    private interface UserConsumer<E extends Exception> {
-
-        public void accept(final String userName, final User user) throws E;
-    }
-
-    private interface PermissionConsumer<E extends Exception> {
-
-        public void accept(final String permissionName, final Group group) throws E;
-    }
-
-    private interface FallibleConsumer<T, E extends Exception> {
-
-        public void accept(final T item) throws E;
-    }
 }

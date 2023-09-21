84f476ea581f8d82b5cd358a39d91add94f04b3a
Mattia Dal Ben <matthewdibi@gmail.com>
2023-09-21 14:02:46 +0200

    style: add copyright headers

--- kura/org.eclipse.kura.rest.packages.provider/src/main/java/org/eclipse/kura/internal/rest/deployment/agent/DeploymentPackageInfo.java
+++ kura/org.eclipse.kura.rest.packages.provider/src/main/java/org/eclipse/kura/internal/rest/deployment/agent/DeploymentPackageInfo.java
@@ -1,5 +1,14 @@
 /*******************************************************************************
- * Copyright (c) 2019, 2020 Eurotech and/or its affiliates. All rights reserved.
+ * Copyright (c) 2023 Eurotech and/or its affiliates and others
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *  Eurotech
  *******************************************************************************/
 package org.eclipse.kura.internal.rest.deployment.agent;
 

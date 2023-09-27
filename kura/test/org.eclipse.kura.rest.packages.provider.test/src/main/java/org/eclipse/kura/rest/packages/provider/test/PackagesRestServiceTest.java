7da9d3665905185e30b10578a75a6b2447f0c93c
Mattia Dal Ben <matthewdibi@gmail.com>
2023-09-27 09:28:19 +0200

    test: remove unused imports

--- kura/test/org.eclipse.kura.rest.packages.provider.test/src/main/java/org/eclipse/kura/rest/packages/provider/test/PackagesRestServiceTest.java
+++ kura/test/org.eclipse.kura.rest.packages.provider.test/src/main/java/org/eclipse/kura/rest/packages/provider/test/PackagesRestServiceTest.java
@@ -12,85 +12,39 @@
  *******************************************************************************/
 package org.eclipse.kura.rest.packages.provider.test;
 
-import static java.util.Collections.singletonMap;
-import static org.eclipse.kura.core.testutil.json.JsonProjection.self;
-import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
-import static org.junit.Assert.fail;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.doThrow;
-import static org.mockito.Mockito.when;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
-import java.net.URL;
-import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.Dictionary;
-import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Hashtable;
-import java.util.List;
-import java.util.Map;
 import java.util.Objects;
-import java.util.Optional;
-import java.util.Set;
-import java.util.TreeSet;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
-import java.util.stream.Collectors;
 
-import org.eclipse.kura.KuraErrorCode;
 import org.eclipse.kura.KuraException;
-import org.eclipse.kura.deployment.agent.DeploymentAgentService;
-import org.eclipse.kura.internal.rest.deployment.agent.DeploymentPackageInfo;
-import org.eclipse.kura.internal.rest.deployment.agent.DeploymentRestService;
-import org.eclipse.kura.configuration.ComponentConfiguration;
-import org.eclipse.kura.configuration.ConfigurationService;
-import org.eclipse.kura.configuration.Password;
-import org.eclipse.kura.configuration.metatype.AD;
-import org.eclipse.kura.configuration.metatype.Icon;
-import org.eclipse.kura.configuration.metatype.OCD;
-import org.eclipse.kura.configuration.metatype.Scalar;
-import org.eclipse.kura.core.testutil.json.JsonProjection;
 import org.eclipse.kura.core.testutil.requesthandler.AbstractRequestHandlerTest;
-import org.eclipse.kura.core.testutil.requesthandler.MqttTransport;
 import org.eclipse.kura.core.testutil.requesthandler.RestTransport;
 import org.eclipse.kura.core.testutil.requesthandler.Transport;
 import org.eclipse.kura.core.testutil.requesthandler.Transport.MethodSpec;
-import org.eclipse.kura.crypto.CryptoService;
-import org.eclipse.kura.util.wire.test.WireTestUtil;
+import org.eclipse.kura.deployment.agent.DeploymentAgentService;
+import org.eclipse.kura.internal.rest.deployment.agent.DeploymentRestService;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
-import org.mockito.ArgumentMatchers;
 import org.mockito.Mockito;
-import org.mockito.stubbing.Answer;
-import org.osgi.framework.FrameworkUtil;
-import org.osgi.framework.InvalidSyntaxException;
-import org.osgi.service.cm.Configuration;
-import org.osgi.service.cm.ConfigurationAdmin;
-import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.FrameworkUtil;
 import org.osgi.framework.ServiceReference;
 import org.osgi.framework.Version;
-import org.osgi.service.deploymentadmin.BundleInfo;
 import org.osgi.service.deploymentadmin.DeploymentAdmin;
-import org.osgi.service.deploymentadmin.DeploymentException;
 import org.osgi.service.deploymentadmin.DeploymentPackage;
-
-import com.eclipsesource.json.Json;
-import com.eclipsesource.json.JsonValue;
 
 @RunWith(Parameterized.class)
 public class PackagesRestServiceTest extends AbstractRequestHandlerTest {

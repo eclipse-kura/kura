f5843cbc61628e290faf4c757343540d493bedc5
SimoneFiorani <simone.fiorani@abinsula.com>
2023-10-27 10:47:03 +0200

    working apis, missing tests
    Signed-off-by: SimoneFiorani <simone.fiorani@abinsula.com>

--- kura/org.eclipse.kura.rest.network.configuration.provider/src/main/java/org/eclipse/kura/internal/rest/network/configuration/NetworkConfigurationRestService.java
+++ kura/org.eclipse.kura.rest.network.configuration.provider/src/main/java/org/eclipse/kura/internal/rest/network/configuration/NetworkConfigurationRestService.java
@@ -12,30 +12,39 @@
  *******************************************************************************/
 package org.eclipse.kura.internal.rest.network.configuration;
 
+import static java.util.Objects.isNull;
+
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 import javax.annotation.security.RolesAllowed;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import org.eclipse.kura.KuraException;
 import org.eclipse.kura.cloudconnection.request.RequestHandler;
 import org.eclipse.kura.cloudconnection.request.RequestHandlerRegistry;
 import org.eclipse.kura.configuration.ComponentConfiguration;
 import org.eclipse.kura.configuration.ConfigurationService;
-import org.eclipse.kura.configuration.metatype.OCDService;
 import org.eclipse.kura.crypto.CryptoService;
 import org.eclipse.kura.request.handler.jaxrs.DefaultExceptionHandler;
 import org.eclipse.kura.request.handler.jaxrs.JaxRsRequestHandlerProxy;
-import org.eclipse.kura.rest.network.configuration.api.ComponentConfigurationDTO;
-import org.eclipse.kura.rest.network.configuration.api.ComponentConfigurationList;
-import org.eclipse.kura.rest.network.configuration.api.DTOUtil;
-import org.eclipse.kura.rest.network.configuration.api.PidSet;
+import org.eclipse.kura.rest.configuration.api.ComponentConfigurationDTO;
+import org.eclipse.kura.rest.configuration.api.ComponentConfigurationList;
+import org.eclipse.kura.rest.configuration.api.DTOUtil;
+import org.eclipse.kura.rest.configuration.api.FailureHandler;
+import org.eclipse.kura.rest.configuration.api.PidSet;
+import org.eclipse.kura.rest.configuration.api.UpdateComponentConfigurationRequest;
 import org.osgi.service.useradmin.Role;
 import org.osgi.service.useradmin.UserAdmin;
 import org.slf4j.Logger;
@@ -54,10 +63,14 @@ public class NetworkConfigurationRestService {
     private static final String IP4_FIREWALL_CONF_SERVICE_PID = "org.eclipse.kura.net.admin.FirewallConfigurationService";
     private static final String IP6_FIREWALL_CONF_SERVICE_PID = "org.eclipse.kura.net.admin.ipv6.FirewallConfigurationServiceIPv6";
 
+    private static final List<String> NETWORK_CONFIGURATION_PIDS = Arrays.asList(NETWORK_CONF_SERVICE_PID,
+            IP4_FIREWALL_CONF_SERVICE_PID, IP6_FIREWALL_CONF_SERVICE_PID);
+
+    private static final String SNAPSHOT_SUBTASK_ID = "snapshot";
+
     private final RequestHandler requestHandler = new JaxRsRequestHandlerProxy(this);
 
     private ConfigurationService configurationService;
-    private OCDService ocdService;
     private CryptoService cryptoService;
 
     public void setUserAdmin(UserAdmin userAdmin) {
@@ -66,10 +79,6 @@ public class NetworkConfigurationRestService {
 
     public void setConfigurationService(ConfigurationService configurationService) {
         this.configurationService = configurationService;
-    }
-
-    public void setOCDService(OCDService ocdService) {
-        this.ocdService = ocdService;
     }
 
     public void setCryptoService(CryptoService cryptoService) {
@@ -102,15 +111,13 @@ public class NetworkConfigurationRestService {
      *         {@link ConfigurationService}
      */
     @GET
-    @RolesAllowed("networkconfiguration")
+    @RolesAllowed("network.configuration")
     @Path("/networkConfigurableComponents")
     @Produces(MediaType.APPLICATION_JSON)
     public PidSet listNetworkConfigurableComponentsPids() {
 
-        Set<String> pids = this.configurationService
-                .getConfigurableComponentPids().stream().filter(c -> c.equals(NETWORK_CONF_SERVICE_PID)
-                        || c.equals(IP4_FIREWALL_CONF_SERVICE_PID) || c.equals(IP6_FIREWALL_CONF_SERVICE_PID))
-                .collect(Collectors.toSet());
+        Set<String> pids = this.configurationService.getConfigurableComponentPids().stream()
+                .filter(this::isNetworkConfigurationPid).collect(Collectors.toSet());
 
         return new PidSet(pids);
     }
@@ -147,113 +154,125 @@ public class NetworkConfigurationRestService {
 
     }
 
-    // /**
-    // * POST method.
-    // *
-    // * Lists the component configurations of all the ConfigurableComponents tracked
-    // * by the
-    // * {@link ConfigurationService} that match the filter specified
-    // *
-    // * @param filter
-    // * A String representing an OSGi filter
-    // * @return a list of {@link ComponentConfigurationDTO}s for the components that
-    // * match the specified filter
-    // */
-    // @POST
-    // @RolesAllowed("networkconfiguration")
-    // @Path("/configurableComponents/configurations/byPid")
-    // @Produces(MediaType.APPLICATION_JSON)
-    // @Consumes(MediaType.APPLICATION_JSON)
-    // public ComponentConfigurationList listComponentConfigurations(final PidSet pids) {
-    // pids.validate();
-    //
-    // final List<ComponentConfiguration> configs = new ArrayList<>();
-    //
-    // pids.getPids().forEach(pid -> {
-    // try {
-    // ComponentConfiguration config = this.configurationService.getComponentConfiguration(pid);
-    // if (!isNull(config)) {
-    // configs.add(config);
-    // }
-    // } catch (Exception e) {
-    // throw DefaultExceptionHandler.toWebApplicationException(e);
-    // }
-    // });
-    //
-    // return DTOUtil.toComponentConfigurationList(configs, this.cryptoService, false)
-    // .replacePasswordsWithPlaceholder();
-    // }
+    /**
+     * POST method.
+     *
+     * Lists the network component configurations of all the network ConfigurableComponents tracked
+     * by the
+     * {@link ConfigurationService} that match the filter specified
+     *
+     * @param filter
+     *            A String representing an OSGi filter
+     * @return a list of {@link ComponentConfigurationDTO}s for the components that
+     *         match the specified filter
+     */
+    @POST
+    @RolesAllowed("network.configuration")
+    @Path("/networkConfigurableComponents/configurations/byPid")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public ComponentConfigurationList listNetworkComponentConfigurations(final PidSet pids) {
+        pids.validate();
 
-    // /**
-    // * POST method.
-    // *
-    // * Provides the default Component Configuration for the component identified by
-    // * the specified PID
-    // *
-    // * @param componentPid
-    // * @return The default {@link ComponentConfiguration} or a null object if the
-    // * component is not tracked
-    // */
-    // @POST
-    // @RolesAllowed("networkconfiguration")
-    // @Path("/configurableComponents/configurations/byPid/_default")
-    // @Consumes(MediaType.APPLICATION_JSON)
-    // @Produces(MediaType.APPLICATION_JSON)
-    // public ComponentConfigurationList listDefaultComponentConfiguration(final PidSet pids) {
-    // pids.validate();
-    //
-    // final List<ComponentConfigurationDTO> result = new ArrayList<>();
-    //
-    // for (final String pid : pids.getPids()) {
-    // try {
-    // final ComponentConfiguration cc = this.configurationService.getDefaultComponentConfiguration(pid);
-    //
-    // if (cc == null || cc.getDefinition() == null) {
-    // logger.warn("cannot find default configuration for {}", pid);
-    // continue;
-    // }
-    //
-    // result.add(DTOUtil.toComponentConfigurationDTO(cc, this.cryptoService, false));
-    // } catch (final Exception e) {
-    // logger.warn("failed to get default configuration for {}", pid, e);
-    // }
-    // }
-    //
-    // return new ComponentConfigurationList(result);
-    // }
+        final List<ComponentConfiguration> configs = new ArrayList<>();
 
-    // /**
-    // * POST method.
-    // *
-    // * Allows to update the configuration of multiple configurable components
-    // *
-    // * @param request
-    // */
-    // @PUT
-    // @RolesAllowed("networkconfiguration")
-    // @Path("/configurableComponents/configurations/_update")
-    // @Produces(MediaType.APPLICATION_JSON)
-    // @Consumes(MediaType.APPLICATION_JSON)
-    // public Response updateComponentConfigurations(UpdateComponentConfigurationRequest request) {
-    // request.validate();
-    //
-    // final FailureHandler handler = new FailureHandler();
-    //
-    // for (ComponentConfigurationDTO ccr : request.getComponentConfigurations()) {
-    //
-    // handler.runFallibleSubtask("update:" + ccr.getPid(), () -> {
-    // final Map<String, Object> configurationProperties = DTOUtil
-    // .dtosToConfigurationProperties(ccr.getProperties());
-    // this.configurationService.updateConfiguration(ccr.getPid(), configurationProperties, false);
-    // });
-    // }
-    //
-    // if (request.isTakeSnapshot()) {
-    // handler.runFallibleSubtask(SNAPSHOT_SUBTASK_ID, () -> this.configurationService.snapshot());
-    // }
-    //
-    // handler.checkStatus();
-    // return Response.ok().build();
-    // }
+        pids.getPids().forEach(pid -> {
+            try {
+                ComponentConfiguration config = this.configurationService.getComponentConfiguration(pid);
+                if (!isNull(config) && isNetworkConfigurationPid(pid)) {
+                    configs.add(config);
+                }
+            } catch (Exception e) {
+                throw DefaultExceptionHandler.toWebApplicationException(e);
+            }
+        });
+
+        return DTOUtil.toComponentConfigurationList(configs, this.cryptoService, false)
+                .replacePasswordsWithPlaceholder();
+    }
+
+    /**
+     * POST method.
+     *
+     * Provides the default network Component Configuration for the component identified by
+     * the specified PID
+     *
+     * @param componentPid
+     * @return The default {@link ComponentConfiguration} or a null object if the
+     *         component is not tracked
+     */
+    @POST
+    @RolesAllowed("network.configuration")
+    @Path("/networkConfigurableComponents/configurations/byPid/_default")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public ComponentConfigurationList listDefaultNetworkComponentConfiguration(final PidSet pids) {
+        pids.validate();
+
+        final List<ComponentConfigurationDTO> result = new ArrayList<>();
+
+        for (final String pid : pids.getPids()) {
+            try {
+                final ComponentConfiguration cc = this.configurationService.getDefaultComponentConfiguration(pid);
+
+                if (!isNetworkConfigurationPid(pid) || cc == null || cc.getDefinition() == null) {
+                    logger.warn("cannot find default configuration for {}", pid);
+                    continue;
+                }
+
+                result.add(DTOUtil.toComponentConfigurationDTO(cc, this.cryptoService, false));
+            } catch (final Exception e) {
+                logger.warn("failed to get default configuration for {}", pid, e);
+            }
+        }
+
+        return new ComponentConfigurationList(result);
+    }
+
+    /**
+     * POST method.
+     *
+     * Allows to update the configuration of multiple network configurable components
+     *
+     * @param request
+     */
+    @PUT
+    @RolesAllowed("network.configuration")
+    @Path("/networkConfigurableComponents/configurations/_update")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response updateComponentConfigurations(UpdateComponentConfigurationRequest request) {
+        request.validate();
+
+        final FailureHandler handler = new FailureHandler();
+
+        for (ComponentConfigurationDTO ccr : request.getComponentConfigurations()) {
+
+            if (isNetworkConfigurationPid(ccr.getPid())) {
+                handler.runFallibleSubtask("update:" + ccr.getPid(), () -> {
+                    final Map<String, Object> configurationProperties = DTOUtil
+                            .dtosToConfigurationProperties(ccr.getProperties());
+                    this.configurationService.updateConfiguration(ccr.getPid(), configurationProperties, false);
+                });
+            }
+        }
+
+        if (request.isTakeSnapshot()) {
+            handler.runFallibleSubtask(SNAPSHOT_SUBTASK_ID, () -> this.configurationService.snapshot());
+        }
+
+        handler.checkStatus();
+        return Response.ok().build();
+    }
+
+    private boolean isNetworkConfigurationPid(String pid) {
+        boolean result = false;
+        for (String filter : NETWORK_CONFIGURATION_PIDS) {
+            if (pid.equals(filter)) {
+                result = true;
+            }
+        }
+        return result;
+    }
 
 }

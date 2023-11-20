cc81e66d82dab9c7320721376f210ad1c46cae3a
G_Ivo <gregorybivo@gmail.com>
2023-11-20 05:14:58 -0500

    fix: wireless tab and cellular tab will show up when only ipv6 is enabled (#4999)
    * fix: added boolean for ipv6
    
    * Update TabIp6Ui.java
    
    * fix: management of ipv6 and modes with wireless
    
    Signed-off-by: MMaiero <matteo.maiero@eurotech.com>
    
    * fix: condition to detect status change
    
    Signed-off-by: Marcello Martina <martina.marcello.rinaldo@outlook.com>
    
    ---------
    
    Signed-off-by: MMaiero <matteo.maiero@eurotech.com>
    Signed-off-by: Marcello Martina <martina.marcello.rinaldo@outlook.com>
    Co-authored-by: MMaiero <matteo.maiero@eurotech.com>
    Co-authored-by: Marcello Martina <martina.marcello.rinaldo@outlook.com>
    (cherry picked from commit 423a7a4938f438c15d540957d17425617105e7e5)

--- kura/org.eclipse.kura.web2/src/main/java/org/eclipse/kura/web/shared/model/GwtNetIfStatus.java
+++ kura/org.eclipse.kura.web2/src/main/java/org/eclipse/kura/web/shared/model/GwtNetIfStatus.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011, 2020 Eurotech and/or its affiliates and others
+ * Copyright (c) 2011, 2023 Eurotech and/or its affiliates and others
  * 
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
@@ -21,7 +21,10 @@ public enum GwtNetIfStatus implements Serializable, IsSerializable {
     netIPv4StatusUnmanaged("Unmanaged"),
     netIPv4StatusL2Only("L2Only"),
     netIPv4StatusEnabledLAN("LAN"),
-    netIPv4StatusEnabledWAN("WAN");
+    netIPv4StatusEnabledWAN("WAN"),
+    netIPv6StatusDisabled("Disabled"),
+    netIPv6StatusEnabledLAN("LAN"),
+    netIPv6StatusEnabledWAN("WAN");
 
     private String status;
 

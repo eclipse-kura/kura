02f537e3313eed68fbb6fb34f02d9f0b6f47ae76
Mattia Dal Ben <matthewdibi@gmail.com>
2024-02-13 15:46:01 +0100

    refactor: merge two methods into one

--- kura/org.eclipse.kura.api/src/main/java/org/eclipse/kura/container/signature/ContainerSignatureValidationService.java
+++ kura/org.eclipse.kura.api/src/main/java/org/eclipse/kura/container/signature/ContainerSignatureValidationService.java
@@ -38,44 +38,12 @@ public interface ContainerSignatureValidationService {
      *
      * If the device running the verification has no internet access, the verification will fail.
      *
-     * @param containerImageDigest
-     *            The digest of the container image to verify
-     * @param trustAnchor
-     *            The trust anchor to use for verification (e.g. a public key or a x509 certificate) typically in PEM
-     *            format. The trust anchor is used to verify the signature of the container image.
-     * @param verifyInTransparencyLog
-     *            Sets the transparency log verification, to be used when an artifact signature has been uploaded to the
-     *            transparency log. Artifacts cannot be publicly verified when not included in a log.
-     * @param registryUsername
-     *            Optional username for registry authentication. If the registry requires authentication,
-     *            both username and password must be provided.
-     * @param registryPassword
-     *            Optional password for registry authentication. If the registry requires authentication,
-     *            both username and password must be provided.
-     * @return
-     */
-    public boolean verify(String containerImageDigest, String trustAnchor, boolean verifyInTransparencyLog,
-            Optional<String> registryUsername, Optional<Password> registryPassword);
-
-    /**
-     * Verifies the signature of a container image using the provided trust anchor. The trust anchor format depends on
-     * the signature format. For example, if the signature was generated with Cosing, the trust anchor is a ECDSA public
-     * key in PEM format. Other signature formats may require different trust anchors.
-     *
-     * If the signature is not included in a transparency log, the verification will fail unless the
-     * verifyInTransparencyLog is set to false.
-     *
-     * If the image is signed with a different protocol the verification will fail.
-     *
-     * If the device running the verification has no internet access, the verification will fail.
-     *
-     * @warning For improved security, it is recommended to use the verify method that takes the image digest as input.
-     *
      * @param imageName
      *            The image name of the container image to verify. The value will need to be expressed in the form of
      *            registryURL/imagename in case of a custom registry.
-     * @param imageTag
-     *            The image tag of the container image to verify
+     * @param imageReference
+     *            The image tag (e.g. "latest") or the image digest (e.g. "sha256:xxxx") of the container image to verify.
+     *            @warning For improved security, it is recommended to use the image digest as input.
      * @param trustAnchor
      *            The trust anchor to use for verification (e.g. a public key or a x509 certificate) typically in PEM
      *            format. The trust anchor is used to verify the signature of the container image.
@@ -90,7 +58,7 @@ public interface ContainerSignatureValidationService {
      *            both username and password must be provided.
      * @return
      */
-    public boolean verify(String imageName, String imageTag, String publicKey, boolean verifyInTransparencyLog,
+    public boolean verify(String imageName, String imageReference, String trustAnchor, boolean verifyInTransparencyLog,
             Optional<String> registryUsername, Optional<Password> registryPassword);
 
     /**
@@ -107,8 +75,6 @@ public interface ContainerSignatureValidationService {
      *
      * @param imageDescriptor
      *            The image descriptor of the container image to verify (see {@link ImageInstanceDescriptor})
-     * @param imageTag
-     *            The image tag of the container image to verify
      * @param trustAnchor
      *            The trust anchor to use for verification (e.g. a public key or a x509 certificate) typically in PEM
      *            format. The trust anchor is used to verify the signature of the container image.
@@ -123,6 +89,6 @@ public interface ContainerSignatureValidationService {
      *            both username and password must be provided.
      * @return
      */
-    public boolean verify(ImageInstanceDescriptor imageDescriptor, String publicKey, boolean verifyInTransparencyLog,
+    public boolean verify(ImageInstanceDescriptor imageDescriptor, String trustAnchor, boolean verifyInTransparencyLog,
             Optional<String> registryUsername, Optional<Password> registryPassword);
 }

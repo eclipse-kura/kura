a544061d8b755c054d553798580242470f5c0ee1
Mattia Dal Ben <matthewdibi@gmail.com>
2024-02-15 10:09:46 +0100

    test(examples): scaffolding for /DummyContainerSignatureValidationService unit tests

---
+++ kura/examples/test/org.eclipse.kura.example.container.signature.validation.test/src/test/java/org/eclipse/kura/example/container/signature/validation/DummyContainerSignatureValidationServiceTest.java
@@ -0,0 +1,11 @@
+package org.eclipse.kura.example.container.signature.validation;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+public class DummyContainerSignatureValidationServiceTest {
+    @Test
+    public void testDummy() {
+        assertTrue(true);
+    }
+}

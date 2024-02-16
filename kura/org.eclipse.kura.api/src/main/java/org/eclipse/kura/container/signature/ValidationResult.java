30848c6976384333dbf041febe67874998208d34
Mattia Dal Ben <matthewdibi@gmail.com>
2024-02-16 17:27:01 +0100

    feat: add image digest to ContainerSignatureValidationService output

---
+++ kura/org.eclipse.kura.api/src/main/java/org/eclipse/kura/container/signature/ValidationResult.java
@@ -0,0 +1,31 @@
+package org.eclipse.kura.container.signature;
+
+import java.util.Optional;
+
+/**
+ * Class representing the result of the signature validation performed by {@link:ContainerSignatureValidationService}
+ * 
+ * The result of the validation is composed of two main parts: whether or not the container image signature was
+ * validated and the container image digest (in the "shaXXX:YYY" format).
+ *
+ * @since 2.7
+ */
+public final class ValidationResult {
+
+    private boolean isSignatureValid = false;
+    private Optional<String> imageDigest = Optional.empty();
+
+    public ValidationResult(boolean signatureValid, Optional<String> digest) {
+        this.imageDigest = digest;
+        this.isSignatureValid = signatureValid;
+    }
+
+    public boolean isSignatureValid() {
+        return this.isSignatureValid;
+    }
+
+    public Optional<String> imageDigest() {
+        return this.imageDigest;
+    }
+
+}

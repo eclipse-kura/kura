e3e8f96820010860d5b84018b381992df0e8a1f0
Mattia Dal Ben <matthewdibi@gmail.com>
2024-02-19 08:51:15 +0100

    docs: add reference to specs

--- kura/org.eclipse.kura.api/src/main/java/org/eclipse/kura/container/signature/ValidationResult.java
+++ kura/org.eclipse.kura.api/src/main/java/org/eclipse/kura/container/signature/ValidationResult.java
@@ -9,7 +9,9 @@ import org.osgi.annotation.versioning.ProviderType;
  * Class representing the result of the signature validation performed by {@link:ContainerSignatureValidationService}
  * 
  * The result of the validation is composed of two main parts: whether or not the container image signature was
- * validated and the container image digest (in the "shaXXX:YYY" format).
+ * validated and the container image digest (in the "algorithm:encoded" format, @see
+ * <a href="https://github.com/opencontainers/image-spec/blob/main/descriptor.md#digests">Opencontainers specs
+ * </a>)
  *
  * If the signature is valid, the image digest MUST be provided.
  *

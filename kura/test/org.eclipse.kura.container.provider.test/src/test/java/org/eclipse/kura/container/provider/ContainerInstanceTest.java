64804b66343b8dd2c85e1eadb71fdb5742b7b6e7
sfiorani <109297780+sfiorani@users.noreply.github.com>
2024-04-11 10:03:50 +0200

    fix(container.instance): refactored code to remove unreachable code (#5212)
    * fix: refactored code to remove unreachable code
    
    * test: added unit test
    
    * test: removed useless line

--- kura/test/org.eclipse.kura.container.provider.test/src/test/java/org/eclipse/kura/container/provider/ContainerInstanceTest.java
+++ kura/test/org.eclipse.kura.container.provider.test/src/test/java/org/eclipse/kura/container/provider/ContainerInstanceTest.java
@@ -290,6 +290,28 @@ public class ContainerInstanceTest {
     }
 
     @Test
+    public void disabledInstanceDeletesAlreadyRunningContainerWithSameName()
+            throws KuraException, InterruptedException {
+
+        givenContainerOrchestratorWithRunningContainer("test-instance", "test-id");
+
+        givenContainerInstanceWith(this.mockContainerOrchestrationService);
+
+        givenPropertiesWith(CONTAINER_ENABLED, false);
+        givenPropertiesWith(CONTAINER_IMAGE, "nginx");
+        givenPropertiesWith(CONTAINER_IMAGE_TAG, "latest");
+        givenPropertiesWith(CONTAINER_NAME, "test-instance");
+        givenPropertiesWith("kura.service.pid", "test-instance");
+        givenContainerInstanceActivatedWith(this.properties);
+
+        thenNoExceptionOccurred();
+        thenWaitForContainerInstanceToBecome(CONTAINER_STATE_DISABLED);
+        thenStopContainerWasCalledFor("test-id");
+        thenDeleteContainerWasCalledFor("test-id");
+
+    }
+
+    @Test
     public void signatureValidationDoesntGetCalledWithMissingTrustAnchor() throws KuraException, InterruptedException {
         givenContainerOrchestratorWithNoRunningContainers();
         givenContainerOrchestratorReturningOnStart("1234");

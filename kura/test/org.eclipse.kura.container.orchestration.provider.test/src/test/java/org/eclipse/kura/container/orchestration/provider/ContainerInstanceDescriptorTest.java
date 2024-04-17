1dd1e541c2a868b373d519fb2d902a8da1e6d6ce
SimoneFiorani <simone.fiorani@abinsula.com>
2024-04-17 12:19:20 +0200

    refactor: removed containerConfiguration based deprecated code

--- kura/test/org.eclipse.kura.container.orchestration.provider.test/src/test/java/org/eclipse/kura/container/orchestration/provider/ContainerInstanceDescriptorTest.java
+++ kura/test/org.eclipse.kura.container.orchestration.provider.test/src/test/java/org/eclipse/kura/container/orchestration/provider/ContainerInstanceDescriptorTest.java
@@ -23,14 +23,15 @@ import java.util.List;
 
 import org.apache.commons.lang.ArrayUtils;
 import org.eclipse.kura.container.orchestration.ContainerInstanceDescriptor;
+import org.eclipse.kura.container.orchestration.ContainerPort;
 import org.junit.Test;
 
 public class ContainerInstanceDescriptorTest {
 
     private static final String H2_DB_NAME = "Kura_H2DB";
     private static final String H2_DB_IMAGE = "joedoe/h2db";
-    private static final List<Integer> H2_DB_PORTS_EXTERNAL = new ArrayList<>(Arrays.asList(1521, 81));
-    private static final List<Integer> H2_DB_PORTS_INTERNAL = new ArrayList<>(Arrays.asList(1521, 81));
+    private static final List<ContainerPort> H2_DB_PORTS = new ArrayList<>(
+            Arrays.asList(new ContainerPort(1521, 1521), new ContainerPort(81, 81)));
     private ContainerInstanceDescriptor firstContainerConfig;
     private ContainerInstanceDescriptor seccondContainerConfig;
 
@@ -57,23 +58,21 @@ public class ContainerInstanceDescriptorTest {
     private void givenContainerOne() {
 
         this.firstContainerConfig = ContainerInstanceDescriptor.builder().setContainerImageTag(H2_DB_NAME)
-                .setContainerName(H2_DB_NAME).setContainerImage(H2_DB_IMAGE).setExternalPorts(H2_DB_PORTS_EXTERNAL)
-                .setInternalPorts(H2_DB_PORTS_INTERNAL).build();
+                .setContainerName(H2_DB_NAME).setContainerImage(H2_DB_IMAGE).setContainerPorts(H2_DB_PORTS).build();
     }
 
     private void givenContainerTwoDiffrent() {
 
         this.seccondContainerConfig = ContainerInstanceDescriptor.builder().setContainerImageTag(H2_DB_NAME)
                 .setContainerName(H2_DB_NAME).setContainerImage(H2_DB_IMAGE).setContainerImageTag("diffrent")
-                .setExternalPorts(H2_DB_PORTS_EXTERNAL).setInternalPorts(H2_DB_PORTS_INTERNAL).build();
+                .setContainerPorts(H2_DB_PORTS).build();
     }
 
     // then
     private void thenCompareContainerOneToExpectedOutput() {
         assertEquals(H2_DB_NAME, this.firstContainerConfig.getContainerName());
         assertEquals(H2_DB_IMAGE, this.firstContainerConfig.getContainerImage());
-        assertTrue(ArrayUtils.isEquals(H2_DB_PORTS_EXTERNAL, this.firstContainerConfig.getContainerPortsExternal()));
-        assertTrue(ArrayUtils.isEquals(H2_DB_PORTS_INTERNAL, this.firstContainerConfig.getContainerPortsInternal()));
+        assertTrue(ArrayUtils.isEquals(H2_DB_PORTS, this.firstContainerConfig.getContainerPorts()));
     }
 
     private void thenFirstContainerDoesntEqualSeccond() {

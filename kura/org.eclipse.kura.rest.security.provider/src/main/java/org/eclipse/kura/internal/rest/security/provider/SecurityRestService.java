6970c7fb36ba1e0d2f63aecab395678e7d835945
pierantoniomerlino <pierantonio.merlino@eurotech.com>
2024-05-06 16:12:40 +0200

    Added rest method to apply default security policy

--- kura/org.eclipse.kura.rest.security.provider/src/main/java/org/eclipse/kura/internal/rest/security/provider/SecurityRestService.java
+++ kura/org.eclipse.kura.rest.security.provider/src/main/java/org/eclipse/kura/internal/rest/security/provider/SecurityRestService.java
@@ -1,30 +1,16 @@
 /*******************************************************************************
  * Copyright (c) 2023 Eurotech and/or its affiliates and others
- * 
+ *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
  * which is available at https://www.eclipse.org/legal/epl-2.0/
- * 
+ *
  * SPDX-License-Identifier: EPL-2.0
- * 
+ *
  * Contributors:
  *  Eurotech
  ******************************************************************************/
 package org.eclipse.kura.internal.rest.security.provider;
-
-import java.util.Optional;
-
-import javax.annotation.security.RolesAllowed;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.WebApplicationException;
-import javax.ws.rs.container.ContainerRequestContext;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-import javax.ws.rs.core.Response.Status;
 
 import org.eclipse.kura.cloudconnection.request.RequestHandler;
 import org.eclipse.kura.cloudconnection.request.RequestHandlerRegistry;
@@ -32,22 +18,37 @@ import org.eclipse.kura.internal.rest.security.provider.dto.DebugEnabledDTO;
 import org.eclipse.kura.request.handler.jaxrs.DefaultExceptionHandler;
 import org.eclipse.kura.request.handler.jaxrs.JaxRsRequestHandlerProxy;
 import org.eclipse.kura.security.SecurityService;
+import org.eclipse.kura.system.SystemService;
 import org.osgi.service.useradmin.Role;
 import org.osgi.service.useradmin.UserAdmin;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.annotation.security.RolesAllowed;
+import javax.ws.rs.*;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.Optional;
+
 @Path("security/v1")
 public class SecurityRestService {
 
     private static final Logger logger = LoggerFactory.getLogger(SecurityRestService.class);
-    private static final String DEBUG_MESSSAGE = "Processing request for method '{}'";
+    private static final String DEBUG_MESSAGE = "Processing request for method '{}'";
 
     private static final String MQTT_APP_ID = "SEC-V1";
     private static final String REST_ROLE_NAME = "security";
     private static final String KURA_PERMISSION_REST_ROLE = "kura.permission.rest." + REST_ROLE_NAME;
 
     private SecurityService security;
+    private SystemService system;
     private final RequestHandler requestHandler = new JaxRsRequestHandlerProxy(this);
 
     public void bindSecurityService(SecurityService securityService) {
@@ -74,21 +75,20 @@ public class SecurityRestService {
         }
     }
 
-    /**
-     * POST method
-     * <br />
-     * This method allows the reload of the security policy's fingerprint
-     * 
-     */
+    public void bindSystemService(SystemService systemService) {
+        this.system = systemService;
+    }
 
+    /**
+     * POST method <br /> This method allows the reload of the security policy's fingerprint
+     */
     @POST
     @RolesAllowed(REST_ROLE_NAME)
     @Path("/security-policy-fingerprint/reload")
     @Produces(MediaType.APPLICATION_JSON)
     public Response reloadSecurityPolicyFingerprint() {
-
         try {
-            logger.debug(DEBUG_MESSSAGE, "reloadSecurityPolicyFingerprint");
+            logger.debug(DEBUG_MESSAGE, "reloadSecurityPolicyFingerprint");
             this.security.reloadSecurityPolicyFingerprint();
         } catch (Exception e) {
             throw DefaultExceptionHandler.toWebApplicationException(e);
@@ -98,19 +98,15 @@ public class SecurityRestService {
     }
 
     /**
-     * POST method
-     * <br />
-     * This method allows the reload of the command line fingerprint
-     * 
+     * POST method <br /> This method allows the reload of the command line fingerprint
      */
-
     @POST
     @RolesAllowed(REST_ROLE_NAME)
     @Path("/command-line-fingerprint/reload")
     @Produces(MediaType.APPLICATION_JSON)
     public Response reloadCommandLineFingerprint() {
         try {
-            logger.debug(DEBUG_MESSSAGE, "reloadCommandLineFingerprint");
+            logger.debug(DEBUG_MESSAGE, "reloadCommandLineFingerprint");
             this.security.reloadCommandLineFingerprint();
         } catch (Exception e) {
             throw DefaultExceptionHandler.toWebApplicationException(e);
@@ -120,8 +116,40 @@ public class SecurityRestService {
     }
 
     /**
+     * POST method <br /> This method replaces the security policy with the default production one. Then a fingerprint
+     * reload is performed.
+     */
+    @POST
+    @RolesAllowed(REST_ROLE_NAME)
+    @Path("/security-policy/default-load")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response loadDefaultSecurityPolicy() {
+        try {
+            logger.debug(DEBUG_MESSAGE, "loadDefaultSecurityPolicy");
+            copyDefaultSecurityPolicy();
+            this.security.reloadSecurityPolicyFingerprint();
+        } catch (Exception e) {
+            throw DefaultExceptionHandler.toWebApplicationException(e);
+        }
+
+        return Response.ok().build();
+    }
+
+    /**
+     * POST method <br /> This method replaces the security policy with the provided one. Then a fingerprint reload is
+     * performed.
+     */
+    @POST
+    @RolesAllowed(REST_ROLE_NAME)
+    @Path("/security-policy/upload")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response uploadSecurityPolicy() {
+        return null;
+    }
+
+    /**
      * GET method
-     * 
+     *
      * @return true if the debug is permitted. False otherwise.
      */
     @GET
@@ -135,7 +163,7 @@ public class SecurityRestService {
                 throw new WebApplicationException(Status.UNAUTHORIZED);
             }
 
-            logger.debug(DEBUG_MESSSAGE, "isDebugEnabled");
+            logger.debug(DEBUG_MESSAGE, "isDebugEnabled");
             return new DebugEnabledDTO(this.security.isDebugEnabled());
         } catch (Exception e) {
             throw DefaultExceptionHandler.toWebApplicationException(e);
@@ -143,4 +171,13 @@ public class SecurityRestService {
 
     }
 
+    private void copyDefaultSecurityPolicy() throws IOException {
+        String kuraHomeFolder = this.system.getKuraHome();
+        java.nio.file.Path defaultSecurityPolicyPath = Paths.get(
+                kuraHomeFolder + "/.data/security_policy_backup/security-production.policy");
+        String kuraUserFolder = this.system.getKuraUserConfigDirectory();
+        java.nio.file.Path securityPolicyPath = Paths.get(kuraUserFolder + "/security/security.policy");
+        Files.copy(defaultSecurityPolicyPath, securityPolicyPath, StandardCopyOption.REPLACE_EXISTING,
+                StandardCopyOption.COPY_ATTRIBUTES);
+    }
 }

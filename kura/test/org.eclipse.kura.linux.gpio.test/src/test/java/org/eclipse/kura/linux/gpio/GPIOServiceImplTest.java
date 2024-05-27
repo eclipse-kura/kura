658d95adb26cb396f842b1d2b6d97d070ba55bf5
pierantoniomerlino <pierantonio.merlino@eurotech.com>
2024-05-27 15:46:50 +0200

    Removed useless import

--- kura/test/org.eclipse.kura.linux.gpio.test/src/test/java/org/eclipse/kura/linux/gpio/GPIOServiceImplTest.java
+++ kura/test/org.eclipse.kura.linux.gpio.test/src/test/java/org/eclipse/kura/linux/gpio/GPIOServiceImplTest.java
@@ -20,7 +20,6 @@ import org.eclipse.kura.gpio.KuraGPIOTrigger;
 import org.junit.Test;
 
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;

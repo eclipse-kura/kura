e629c2fa6df8be2cf19bddacb57a09614da7237e
pierantoniomerlino <pierantonio.merlino@eurotech.com>
2024-07-17 15:42:29 +0200

    Small refactor again
    Signed-off-by: pierantoniomerlino <pierantonio.merlino@eurotech.com>

--- kura/org.eclipse.kura.api/src/main/java/org/eclipse/kura/net/wifi/WifiAccessPointRemovedEvent.java
+++ kura/org.eclipse.kura.api/src/main/java/org/eclipse/kura/net/wifi/WifiAccessPointRemovedEvent.java
@@ -21,7 +21,7 @@ import org.osgi.service.event.Event;
  * Emitted when an access point disappears from view of the device.
  *
  * @noextend This class is not intended to be subclassed by clients.
- * @deprecated since 3.0.
+ * @deprecated since 3.0
  */
 @ProviderType
 @Deprecated

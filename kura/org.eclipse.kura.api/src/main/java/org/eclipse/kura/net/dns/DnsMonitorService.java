680f8919e4867ae818efe1042fcbaf83ce5ab287
pierantoniomerlino <pierantonio.merlino@eurotech.com>
2024-07-17 12:56:32 +0200

    Deprecated DnsMonitorService
    Signed-off-by: pierantoniomerlino <pierantonio.merlino@eurotech.com>

--- kura/org.eclipse.kura.api/src/main/java/org/eclipse/kura/net/dns/DnsMonitorService.java
+++ kura/org.eclipse.kura.api/src/main/java/org/eclipse/kura/net/dns/DnsMonitorService.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011, 2020 Eurotech and/or its affiliates and others
+ * Copyright (c) 2011, 2024 Eurotech and/or its affiliates and others
  * 
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
@@ -18,8 +18,10 @@ import org.osgi.annotation.versioning.ProviderType;
  * Marker interface for dns server monitoring service
  *
  * @noimplement This interface is not intended to be implemented by clients.
+ * @deprecated since version 3.0.
  */
 @ProviderType
+@Deprecated
 public interface DnsMonitorService {
 
 }

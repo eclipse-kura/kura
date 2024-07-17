a43f394be444fe5a50072a5eadd34ab406918ac1
pierantoniomerlino <pierantonio.merlino@eurotech.com>
2024-07-17 15:14:12 +0200

    Deprecated several modem related classes and interfaces
    Signed-off-by: pierantoniomerlino <pierantonio.merlino@eurotech.com>

--- kura/org.eclipse.kura.api/src/main/java/org/eclipse/kura/net/modem/ModemManagerService.java
+++ kura/org.eclipse.kura.api/src/main/java/org/eclipse/kura/net/modem/ModemManagerService.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011, 2021 Eurotech and/or its affiliates and others
+ * Copyright (c) 2011, 2024 Eurotech and/or its affiliates and others
  * 
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
@@ -21,19 +21,27 @@ import org.osgi.annotation.versioning.ProviderType;
 
 /**
  * @noimplement This interface is not intended to be implemented by clients.
+ * @deprecated since version 3.0.
  */
 @ProviderType
+@Deprecated
 public interface ModemManagerService {
 
     /**
      * Returns the modem tracked with the given id.
      * 
-     * @deprecated since 2.2 use {@link ModemManagerService#withModemService(String, Function)} instead
+     * @deprecated since 2.2 use
+     *             {@link ModemManagerService#withModemService(String, Function)}
+     *             instead
      * @param id
-     *            The id of the modem, in case of an USB modem, the id is the USB port as returned by
-     *            {@link org.eclipse.kura.usb.UsbModemDevice#getUsbPort()}, in case of a serial modem the id is the
-     *            product name as returned by {@link org.eclipse.kura.usb.UsbModemDevice#getProductName()}
-     * @return The cellular modem instance, or {@code null} if a modem with the given id is not currently tracked
+     *           The id of the modem, in case of an USB modem, the id is the USB
+     *           port as returned by
+     *           {@link org.eclipse.kura.usb.UsbModemDevice#getUsbPort()}, in case
+     *           of a serial modem the id is the
+     *           product name as returned by
+     *           {@link org.eclipse.kura.usb.UsbModemDevice#getProductName()}
+     * @return The cellular modem instance, or {@code null} if a modem with the
+     *         given id is not currently tracked
      */
     @Deprecated
     public CellularModem getModemService(String id);
@@ -41,7 +49,9 @@ public interface ModemManagerService {
     /**
      * Returns the list of currently tracked modems
      * 
-     * @deprecated since 2.2 use {@link ModemManagerService#withAllModemServices(Function)} instead
+     * @deprecated since 2.2 use
+     *             {@link ModemManagerService#withAllModemServices(Function)}
+     *             instead
      * @return the list of currently tracked modems
      */
     @Deprecated
@@ -54,11 +64,14 @@ public interface ModemManagerService {
      * @since 2.2
      * 
      * @param <T>
-     *            The return type of the function
+     *             The return type of the function
      * @param id
-     *            The id of the modem, in case of an USB modem, the id is the USB port as returned by
-     *            {@link org.eclipse.kura.usb.UsbModemDevice#getUsbPort()}, in case of a serial modem the id is the
-     *            product name as returned by {@link org.eclipse.kura.usb.UsbModemDevice#getProductName()}
+     *             The id of the modem, in case of an USB modem, the id is the USB
+     *             port as returned by
+     *             {@link org.eclipse.kura.usb.UsbModemDevice#getUsbPort()}, in case
+     *             of a serial modem the id is the
+     *             product name as returned by
+     *             {@link org.eclipse.kura.usb.UsbModemDevice#getProductName()}
      * @param func
      * @return The result of the provided function applied to the modem
      */
@@ -72,9 +85,9 @@ public interface ModemManagerService {
      * @since 2.2
      * 
      * @param <T>
-     *            The return type of the function
+     *             The return type of the function
      * @param func
-     *            The function to be called
+     *             The function to be called
      * @return The result of the provided function applied to the modem
      */
     public <T> T withAllModemServices(ModemFunction<Collection<CellularModem>, T> func) throws KuraException;

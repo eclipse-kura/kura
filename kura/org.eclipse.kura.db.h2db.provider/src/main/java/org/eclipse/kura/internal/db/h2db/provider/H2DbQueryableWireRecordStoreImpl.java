f8732a207525f0fcbb1eab4c586271dc8c377951
pierantoniomerlino <pierantonio.merlino@eurotech.com>
2024-09-02 10:51:42 +0200

    Added db.h2db.provider bundle
    Signed-off-by: pierantoniomerlino <pierantonio.merlino@eurotech.com>

---
+++ kura/org.eclipse.kura.db.h2db.provider/src/main/java/org/eclipse/kura/internal/db/h2db/provider/H2DbQueryableWireRecordStoreImpl.java
@@ -0,0 +1,50 @@
+/*******************************************************************************
+ * Copyright (c) 2023, 2024 Eurotech and/or its affiliates and others
+ * 
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ * 
+ * SPDX-License-Identifier: EPL-2.0
+ * 
+ * Contributors:
+ *  Eurotech
+ ******************************************************************************/
+package org.eclipse.kura.internal.db.h2db.provider;
+
+import java.sql.Blob;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.util.Optional;
+
+import org.eclipse.kura.util.jdbc.ConnectionProvider;
+import org.eclipse.kura.util.wire.store.AbstractJdbcQueryableWireRecordStoreImpl;
+
+import static java.util.Objects.isNull;
+
+@SuppressWarnings("restriction")
+public class H2DbQueryableWireRecordStoreImpl extends AbstractJdbcQueryableWireRecordStoreImpl {
+
+    protected H2DbQueryableWireRecordStoreImpl(ConnectionProvider provider) {
+        super(provider);
+    }
+
+    @Override
+    protected Optional<Object> extractColumnValue(final ResultSet rset, final ResultSetMetaData rmet, final int i)
+            throws SQLException {
+        Object dbExtractedData = rset.getObject(i);
+
+        if (isNull(dbExtractedData)) {
+            return Optional.empty();
+        }
+
+        if (dbExtractedData instanceof Blob) {
+            final Blob dbExtractedBlob = (Blob) dbExtractedData;
+            final int dbExtractedBlobLength = (int) dbExtractedBlob.length();
+            dbExtractedData = dbExtractedBlob.getBytes(1, dbExtractedBlobLength);
+        }
+
+        return Optional.of(dbExtractedData);
+    }
+}

2dbe9395bf838d5fbe866b8038fd94b0bfce38d7
Mattia Dal Ben <mattdibi@users.noreply.github.com>
2024-10-04 18:18:03 +0200

    feat(api): add getSupportedGpsModes() to modem status API (#5414)
    * feat(api): add getSupportedGpsModes() to modem status API
    
    * feat(nm): populate newly introduced property
    
    * build: fix rest.network.status.provider build
    
    * test: add test for newly introduced property
    
    * docs: add "since" annotation
    
    (cherry picked from commit 893fb1058bd832b04ba4106049b6dbda923d128c)

---
+++ kura/org.eclipse.kura.api/src/main/java/org/eclipse/kura/net/status/modem/ModemGpsMode.java
@@ -0,0 +1,32 @@
+/*******************************************************************************
+ * Copyright (c) 2024 Eurotech and/or its affiliates and others
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *  Eurotech
+ ******************************************************************************/
+
+package org.eclipse.kura.net.status.modem;
+
+/*
+ * The GPS mode supported by the modem.
+ *
+ * @since 2.8
+ */
+public enum ModemGpsMode {
+    /*
+     * Unmanaged GPS mode. In this mode the GPS device of the modem will be setup but not directly managed, therefore
+     * freeing the serial port for other services to use.
+     */
+    UNMANAGED,
+    /*
+     * Managed GPS mode. In this mode the GPS device of the modem will be setup and directly managed (typically by
+     * ModemManager) therefore the serial port won't be available for other services to use.
+     */
+    MANAGED_GPS
+}

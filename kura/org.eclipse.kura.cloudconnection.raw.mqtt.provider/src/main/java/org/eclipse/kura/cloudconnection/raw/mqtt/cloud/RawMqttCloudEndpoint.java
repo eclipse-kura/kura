a93cfc679c1fbfa3319d103897c2234d77cafb1a
MMaiero <matteo.maiero@eurotech.com>
2024-10-29 14:42:06 +0100

    fix: Improvement on Raw cloud connection
    Fixed factory hiding
    Fixed Error message if missing body in payload
    
    Signed-off-by: MMaiero <matteo.maiero@eurotech.com>

--- kura/org.eclipse.kura.cloudconnection.raw.mqtt.provider/src/main/java/org/eclipse/kura/cloudconnection/raw/mqtt/cloud/RawMqttCloudEndpoint.java
+++ kura/org.eclipse.kura.cloudconnection.raw.mqtt.provider/src/main/java/org/eclipse/kura/cloudconnection/raw/mqtt/cloud/RawMqttCloudEndpoint.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2019, 2020 Eurotech and/or its affiliates and others
+ * Copyright (c) 2019, 2024 Eurotech and/or its affiliates and others
  * 
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
@@ -144,7 +144,7 @@ public class RawMqttCloudEndpoint
         final byte[] body = kuraPayload.getBody();
 
         if (body == null) {
-            throw new KuraException(KuraErrorCode.INVALID_PARAMETER, null, null, "missing message body");
+            throw new KuraException(KuraErrorCode.INVALID_PARAMETER, "Missing message body in received payload.");
         }
 
         final int qos = options.getQos().getValue();

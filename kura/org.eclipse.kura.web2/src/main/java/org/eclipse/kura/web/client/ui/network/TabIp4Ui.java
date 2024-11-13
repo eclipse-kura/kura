24df05e287386369a4350bdb3fb64d34e39eb18b
MMaiero <matteo.maiero@eurotech.com>
2024-11-13 15:06:49 +0100

    fix: Updated check on regex and empty string
    Signed-off-by: MMaiero <matteo.maiero@eurotech.com>

--- kura/org.eclipse.kura.web2/src/main/java/org/eclipse/kura/web/client/ui/network/TabIp4Ui.java
+++ kura/org.eclipse.kura.web2/src/main/java/org/eclipse/kura/web/client/ui/network/TabIp4Ui.java
@@ -531,7 +531,7 @@ public class TabIp4Ui extends Composite implements NetworkTab {
         this.gateway.addValueChangeHandler(event -> {
             setDirty(true);
             if (!TabIp4Ui.this.gateway.getText().trim().matches(FieldType.IPV4_ADDRESS.getRegex())
-                    && TabIp4Ui.this.gateway.getText().trim().length() <= 0) {
+                    || TabIp4Ui.this.gateway.getText().trim().length() <= 0) {
                 TabIp4Ui.this.groupGateway.setValidationState(ValidationState.ERROR);
                 TabIp4Ui.this.helpGateway.setText(MSGS.netIPv4InvalidAddress());
             } else {
@@ -552,7 +552,7 @@ public class TabIp4Ui extends Composite implements NetworkTab {
         this.subnet.addValueChangeHandler(event -> {
             setDirty(true);
             if (!TabIp4Ui.this.subnet.getText().trim().matches(FieldType.IPV4_ADDRESS.getRegex())
-                    && TabIp4Ui.this.subnet.getText().trim().length() <= 0) {
+                    || TabIp4Ui.this.subnet.getText().trim().length() <= 0) {
                 TabIp4Ui.this.groupSubnet.setValidationState(ValidationState.ERROR);
                 TabIp4Ui.this.helpSubnet.setText(MSGS.netIPv4InvalidAddress());
             } else {

47d515238b58da1d6da5d3d69b3512cd288a504a
Mattia Dal Ben <matthewdibi@gmail.com>
2024-12-09 16:20:41 +0100

    test: re-enable ConfigurationRestServiceTest

--- kura/test/org.eclipse.kura.rest.configuration.provider.test/src/main/java/org/eclipse/kura/rest/configuration/provider/test/ConfigurationRestServiceTest.java
+++ kura/test/org.eclipse.kura.rest.configuration.provider.test/src/main/java/org/eclipse/kura/rest/configuration/provider/test/ConfigurationRestServiceTest.java
@@ -63,7 +63,6 @@ import org.eclipse.kura.core.testutil.requesthandler.Transport.MethodSpec;
 import org.eclipse.kura.crypto.CryptoService;
 import org.eclipse.kura.util.wire.test.WireTestUtil;
 import org.junit.BeforeClass;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
@@ -78,7 +77,6 @@ import org.osgi.service.cm.ConfigurationAdmin;
 import com.eclipsesource.json.Json;
 import com.eclipsesource.json.JsonValue;
 
-@Ignore
 @RunWith(Parameterized.class)
 public class ConfigurationRestServiceTest extends AbstractRequestHandlerTest {
 
@@ -110,7 +108,7 @@ public class ConfigurationRestServiceTest extends AbstractRequestHandlerTest {
 
         whenRequestIsPerformed(new MethodSpec("GET"), "/snapshots");
 
-        thenResponseCodeIs(500);
+        thenResponseCodeIs(400);
     }
 
     @Test

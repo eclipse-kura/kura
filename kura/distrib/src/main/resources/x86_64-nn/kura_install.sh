2ffc28830d5ba8fc02a6586c33b32641833c96fb
pierantoniomerlino <pierantonio.merlino@eurotech.com>
2024-12-18 10:51:34 +0100

    Added kura.service for NN; updated unit deps
    Signed-off-by: pierantoniomerlino <pierantonio.merlino@eurotech.com>

--- kura/distrib/src/main/resources/x86_64-nn/kura_install.sh
+++ kura/distrib/src/main/resources/x86_64-nn/kura_install.sh
@@ -33,7 +33,7 @@ INSTALL_DIR=/opt/eclipse
 ln -sf ${INSTALL_DIR}/kura_* ${INSTALL_DIR}/kura
 
 # set up kura init
-sed "s|INSTALL_DIR|${INSTALL_DIR}|" ${INSTALL_DIR}/kura/install/kura.service > /lib/systemd/system/kura.service
+sed "s|INSTALL_DIR|${INSTALL_DIR}|" ${INSTALL_DIR}/kura/install/kura.service.nn > /lib/systemd/system/kura.service
 systemctl daemon-reload
 systemctl enable kura
 chmod +x ${INSTALL_DIR}/kura/bin/*.sh

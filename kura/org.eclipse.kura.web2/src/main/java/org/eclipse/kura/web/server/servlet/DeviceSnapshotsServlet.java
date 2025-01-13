083e2e7f80075af793a0e50a8e0ea788b5903905
Nicola Timeus <nicola.timeus@eurotech.com>
2025-01-13 14:56:45 +0100

    Updated copyright headers
    Signed-off-by: Nicola Timeus <nicola.timeus@eurotech.com>

--- kura/org.eclipse.kura.web2/src/main/java/org/eclipse/kura/web/server/servlet/DeviceSnapshotsServlet.java
+++ kura/org.eclipse.kura.web2/src/main/java/org/eclipse/kura/web/server/servlet/DeviceSnapshotsServlet.java
@@ -1,6 +1,6 @@
 
 /*******************************************************************************
- * Copyright (c) 2011, 2024 Eurotech and/or its affiliates and others
+ * Copyright (c) 2011, 2025 Eurotech and/or its affiliates and others
  * 
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0

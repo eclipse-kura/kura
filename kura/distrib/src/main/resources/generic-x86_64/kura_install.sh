ee8df2d120b9c45933b95f88c793fdd0be3b6515
Réda Housni Alaoui <reda-alaoui@hey.com>
2025-01-14 09:31:37 +0100

    fix(distrib): kura_install.sh is broken (#5645)
    * kura_install.sh is broken
    
    * Update kura_install.sh
    
    (cherry picked from commit 0ea4057da05f2f80af9059462a1d47a72704c5ed)

--- kura/distrib/src/main/resources/generic-x86_64/kura_install.sh
+++ kura/distrib/src/main/resources/generic-x86_64/kura_install.sh
@@ -90,7 +90,7 @@ cp ${INSTALL_DIR}/kura/install/manage_kura_users.sh ${INSTALL_DIR}/kura/.data/ma
 chmod 700 ${INSTALL_DIR}/kura/.data/manage_kura_users.sh
 ${INSTALL_DIR}/kura/.data/manage_kura_users.sh -i
 
-bash "${INSTALL_DIR}/kura/install/customize-installation.sh"${IS_NETWORKING_PROFILE}
+bash "${INSTALL_DIR}/kura/install/customize-installation.sh" ${IS_NETWORKING_PROFILE}
 
 # copy snapshot_0.xml
 cp ${INSTALL_DIR}/kura/user/snapshots/snapshot_0.xml ${INSTALL_DIR}/kura/.data/snapshot_0.xml

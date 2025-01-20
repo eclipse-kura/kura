c5770dd7305cea05125da4eae6daa112fc44dae3
Mattia Dal Ben <matthewdibi@gmail.com>
2025-01-20 11:58:19 +0100

    fix: cover case in which ipv4 is l2only and ipv6 is disabled

--- kura/org.eclipse.kura.nm/src/main/java/org/eclipse/kura/nm/KuraInterfaceStatus.java
+++ kura/org.eclipse.kura.nm/src/main/java/org/eclipse/kura/nm/KuraInterfaceStatus.java
@@ -22,18 +22,21 @@ public enum KuraInterfaceStatus {
         Boolean ip4Enabled = KuraIpStatus.isEnabled(ip4Status);
         boolean ip4Disabled = ip4Status == KuraIpStatus.DISABLED;
         boolean ip4Unmanaged = ip4Status == KuraIpStatus.UNMANAGED;
+        boolean ip4L2Only = ip4Status == KuraIpStatus.L2ONLY;
         boolean ip4Unknown = ip4Status == KuraIpStatus.UNKNOWN;
 
         Boolean ip6Enabled = KuraIpStatus.isEnabled(ip6Status);
         boolean ip6Disabled = ip6Status == KuraIpStatus.DISABLED;
         boolean ip6Unmanaged = ip6Status == KuraIpStatus.UNMANAGED;
+        boolean ip6L2Only = ip6Status == KuraIpStatus.L2ONLY;
         boolean ip6Unknown = ip6Status == KuraIpStatus.UNKNOWN;
 
         if (ip4Enabled && ip6Enabled || ip4Enabled && ip6Disabled || ip4Disabled && ip6Enabled) {
             return ENABLED;
         }
 
-        if (ip4Unmanaged && ip6Unmanaged) {
+        if (ip4Unmanaged && ip6Unmanaged || ip4L2Only && ip6L2Only || ip4L2Only && ip6Unmanaged
+                || ip4Unmanaged && ip6L2Only || ip4L2Only && ip6Disabled) {
             return UNMANAGED;
         }
 

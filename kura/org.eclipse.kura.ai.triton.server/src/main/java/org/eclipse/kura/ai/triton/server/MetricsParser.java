03fb2dab445a0470c9c7c84feb116c21a5d57e9d
pierantoniomerlino <pierantonio.merlino@eurotech.com>
2025-02-07 10:41:53 +0100

    Updated copyright and added comments
    Signed-off-by: pierantoniomerlino <pierantonio.merlino@eurotech.com>

--- kura/org.eclipse.kura.ai.triton.server/src/main/java/org/eclipse/kura/ai/triton/server/MetricsParser.java
+++ kura/org.eclipse.kura.ai.triton.server/src/main/java/org/eclipse/kura/ai/triton/server/MetricsParser.java
@@ -26,34 +26,33 @@ public class MetricsParser {
     }
 
     /*
+     * Parse the metrics provided by a Triton Server. An example of metrics are the following:
+     * 
      * # HELP nv_inference_compute_output_duration_us Cumulative inference compute output duration in microseconds (does
      * not include cached requests)
      * # TYPE nv_inference_compute_output_duration_us counter
      * nv_inference_compute_output_duration_us{model="preprocessor",version="1"} 851348
      * nv_inference_compute_output_duration_us{model="identity_long",version="1"} 0
-     * <metric-name>{model=<model_name>,version=<version>} <value>
+     * nv_inference_request_failure{model="preprocessor",reason="OTHER",version="1"} 0
+     * nv_pinned_memory_pool_used_bytes 0
+     * 
+     * The lines beginning with a # are filtered. The metric names have the following format:
+     * 
+     * <metric_name>-<model-name>-<parameter-1>-<parameter-2>...
+     * 
+     * For example the metric
      * 
      * nv_inference_request_failure{model="preprocessor",reason="OTHER",version="1"} 0
-     * nv_inference_request_failure{model="preprocessor",reason="BACKEND",version="1"} 0
-     * nv_inference_request_failure{model="preprocessor",reason="CANCELED",version="1"} 0
-     * nv_inference_request_failure{model="preprocessor",reason="REJECTED",version="1"} 0
-     * nv_inference_request_failure{model="identity_long",reason="OTHER",version="1"} 0
-     * nv_inference_request_failure{model="identity_long",reason="CANCELED",version="1"} 0
-     * nv_inference_request_failure{model="identity_long",reason="BACKEND",version="1"} 0
-     * nv_inference_request_failure{model="identity_long",reason="REJECTED",version="1"} 0
-     * <metric-name>{model=<model_name>,reason=<reason>,version=<version>} <value>
      * 
-     * nv_pinned_memory_pool_used_bytes 0
-     * <metric-name> <value>
+     * is converted to
+     * 
+     * nv_inference_request_failure-preprocessor-OTHER-1 0
+     * 
      */
     public Map<String, String> parse() {
         Map<String, String> metrics = new HashMap<>();
         rawMetrics.stream().filter(line -> !line.startsWith("#")).forEach(line -> {
             String[] elements = line.split("\\s+");
-            // key: <metric_name>-<model-name>-<version>
-            // key: <metric_name>-<model-name>-<reason>-<version>
-            // key: <metric_name>
-            // value: <value>
             Optional<String> key = parseKey(elements);
             String value = parseValue(elements);
             if (key.isPresent()) {
@@ -89,76 +88,4 @@ public class MetricsParser {
         return value;
     }
 
-    // private static final Pattern NEWLINE_PATTERN = Pattern.compile("\\R");
-    // private final String rawMetrics;
-    //
-    // public MetricsParser(String metrics) {
-    // this.rawMetrics = metrics;
-    // }
-    //
-    // /*
-    // * # HELP nv_inference_compute_output_duration_us Cumulative inference compute output duration in microseconds
-    // (does
-    // * not include cached requests)
-    // * # TYPE nv_inference_compute_output_duration_us counter
-    // * nv_inference_compute_output_duration_us{model="preprocessor",version="1"} 851348
-    // * nv_inference_compute_output_duration_us{model="identity_long",version="1"} 0
-    // * <metric-name>{model=<model_name>,version=<version>} <value>
-    // *
-    // * nv_inference_request_failure{model="preprocessor",reason="OTHER",version="1"} 0
-    // * nv_inference_request_failure{model="preprocessor",reason="BACKEND",version="1"} 0
-    // * nv_inference_request_failure{model="preprocessor",reason="CANCELED",version="1"} 0
-    // * nv_inference_request_failure{model="preprocessor",reason="REJECTED",version="1"} 0
-    // * nv_inference_request_failure{model="identity_long",reason="OTHER",version="1"} 0
-    // * nv_inference_request_failure{model="identity_long",reason="CANCELED",version="1"} 0
-    // * nv_inference_request_failure{model="identity_long",reason="BACKEND",version="1"} 0
-    // * nv_inference_request_failure{model="identity_long",reason="REJECTED",version="1"} 0
-    // * <metric-name>{model=<model_name>,reason=<reason>,version=<version>} <value>
-    // *
-    // * nv_pinned_memory_pool_used_bytes 0
-    // * <metric-name> <value>
-    // */
-    // public Map<String, String> parse() {
-    // Map<String, String> metrics = new HashMap<>();
-    // Stream<String> lines = NEWLINE_PATTERN.splitAsStream(rawMetrics);
-    // lines.filter(line -> !line.startsWith("#")).forEach(line -> {
-    // String[] elements = line.split("\\s+");
-    // // key: <metric_name>-<model-name>-<version>
-    // // key: <metric_name>-<model-name>-<reason>-<version>
-    // // key: <metric_name>
-    // // value: <value>
-    // Optional<String> key = parseKey(elements);
-    // String value = parseValue(elements);
-    // if (key.isPresent()) {
-    // metrics.put(key.get(), value);
-    // }
-    // });
-    // return metrics;
-    // }
-    //
-    // private Optional<String> parseKey(String[] elements) {
-    // Optional<String> key = Optional.empty();
-    // if (elements.length < 1) {
-    // return key;
-    // }
-    //
-    // StringBuilder keyBuilder = new StringBuilder();
-    // String[] metricElements = elements[0].split("\\{");
-    // keyBuilder.append(metricElements[0]);
-    // if (metricElements.length > 1) {
-    // String[] configs = metricElements[1].replace("\"", "").replace("}", "").split(",");
-    // for (String config : configs) {
-    // keyBuilder.append("-").append(config.split("=")[1]);
-    // }
-    // }
-    // return Optional.of(keyBuilder.toString());
-    // }
-    //
-    // private String parseValue(String[] elements) {
-    // String value = "";
-    // if (elements.length >= 2) {
-    // value = elements[1];
-    // }
-    // return value;
-    // }
 }

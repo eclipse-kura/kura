d79c0b9959eb197560eb69fc6dc17337e731b77c
SimoneFiorani <simone.fiorani@abinsula.com>
2025-02-21 14:43:22 +0100

    fix: missing copyright header
    Signed-off-by: SimoneFiorani <simone.fiorani@abinsula.com>

--- kura/org.eclipse.kura.core.crypto/src/main/java/org/eclipse/kura/core/crypto/CryptoServiceImpl.java
+++ kura/org.eclipse.kura.core.crypto/src/main/java/org/eclipse/kura/core/crypto/CryptoServiceImpl.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011, 2020 Eurotech and/or its affiliates and others
+ * Copyright (c) 2011, 2025 Eurotech and/or its affiliates and others
  *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
@@ -9,8 +9,7 @@
  *
  * Contributors:
  *  Eurotech
- *  Red Hat Inc
- *******************************************************************************/
+ ******************************************************************************/
 package org.eclipse.kura.core.crypto;
 
 import java.io.BufferedInputStream;

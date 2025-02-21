c6935b19078492f182c4f8331c942e26c686f251
pierantoniomerlino <pierantonio.merlino@eurotech.com>
2025-02-21 15:58:27 +0100

    Fixed typo in NM converter
    Signed-off-by: pierantoniomerlino <pierantonio.merlino@eurotech.com>

--- kura/org.eclipse.kura.nm/src/main/java/org/eclipse/kura/nm/configuration/NMSettingsConverter.java
+++ kura/org.eclipse.kura.nm/src/main/java/org/eclipse/kura/nm/configuration/NMSettingsConverter.java
@@ -619,10 +619,12 @@ public class NMSettingsConverter {
     public static Map<String, Variant<?>> buildPPPSettings(NetworkProperties props, String deviceId) {
         Map<String, Variant<?>> settings = new HashMap<>();
 
-        Optional<Integer> lcpEchoInterval = props.getOpt(Integer.class, "net.interface.%s.config.lcpEchoInterval",
+        // The property is wrongly named in Kura, it should be "net.interface.%s.config.lcpEchoInterval"
+        Optional<Integer> lcpEchoInterval = props.getOpt(Integer.class, "net.interface.%s.config.lpcEchoInterval",
                 deviceId);
         lcpEchoInterval.ifPresent(interval -> settings.put("lcp-echo-interval", new Variant<>(interval)));
-        Optional<Integer> lcpEchoFailure = props.getOpt(Integer.class, "net.interface.%s.config.lcpEchoFailure",
+        // The property is wrongly named in Kura, it should be "net.interface.%s.config.lcpEchoFailure"
+        Optional<Integer> lcpEchoFailure = props.getOpt(Integer.class, "net.interface.%s.config.lpcEchoFailure",
                 deviceId);
         lcpEchoFailure.ifPresent(failure -> settings.put("lcp-echo-failure", new Variant<>(failure)));
 

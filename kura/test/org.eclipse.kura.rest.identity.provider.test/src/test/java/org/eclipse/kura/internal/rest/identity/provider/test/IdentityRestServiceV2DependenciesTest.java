48c36f8709218d88e497180e1c6b17984a40f619
Salvatore Coppola <salvatore.coppola@eurotech.com>
2025-02-25 09:49:17 +0100

    Removed unused unbind methods. unrequired optional cardinality in identityrest services and fixed some tests

--- kura/test/org.eclipse.kura.rest.identity.provider.test/src/test/java/org/eclipse/kura/internal/rest/identity/provider/test/IdentityRestServiceV2DependenciesTest.java
+++ kura/test/org.eclipse.kura.rest.identity.provider.test/src/test/java/org/eclipse/kura/internal/rest/identity/provider/test/IdentityRestServiceV2DependenciesTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2024 Eurotech and/or its affiliates and others
+ * Copyright (c) 2024, 2025 Eurotech and/or its affiliates and others
  *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
@@ -99,9 +99,10 @@ public class IdentityRestServiceV2DependenciesTest {
     }
 
     @Test
-    public void shouldBindRestIdentityServiceDependencies() throws KuraException {
+    public void shouldBindRestIdentityServiceDependencies() {
         givenUserAdminMock();
         givenIdentityServiceMock();
+        givenRequestHandlerRegistryMock();
 
         whenBindDependencies();
 

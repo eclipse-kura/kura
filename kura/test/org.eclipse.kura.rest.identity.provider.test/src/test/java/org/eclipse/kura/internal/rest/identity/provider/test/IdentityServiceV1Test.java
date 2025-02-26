01ad29617f51acdff7f073263f60262e2e6ec125
Salvatore Coppola <83589980+salvatore-coppola@users.noreply.github.com>
2025-02-26 09:54:24 +0100

    build: Updated tycho to version 4 (#5744)
    * updated tycho to version 4
    
    * Removed unused unbind methods. unrequired optional cardinality in identityrest services and fixed some tests
    
    * fixed copyright plate

--- kura/test/org.eclipse.kura.rest.identity.provider.test/src/test/java/org/eclipse/kura/internal/rest/identity/provider/test/IdentityServiceV1Test.java
+++ kura/test/org.eclipse.kura.rest.identity.provider.test/src/test/java/org/eclipse/kura/internal/rest/identity/provider/test/IdentityServiceV1Test.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2023, 2024 Eurotech and/or its affiliates and others
+ * Copyright (c) 2023, 2025 Eurotech and/or its affiliates and others
  *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
@@ -287,7 +287,7 @@ public class IdentityServiceV1Test {
         }
     }
 
-    private void thenUserIsCreated() throws KuraException {
+    private void thenUserIsCreated() {
         verify(this.userAdmin, times(1)).createRole(USER_ROLE_NAME_PREFIX + this.newUser.getUserName(), Role.USER);
     }
 

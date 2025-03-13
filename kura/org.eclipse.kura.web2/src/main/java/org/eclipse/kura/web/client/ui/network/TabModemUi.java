63ddecf71b1184b6f42c23e00651d88841fae685
pierantoniomerlino <pierantonio.merlino@eurotech.com>
2025-03-13 10:39:40 +0100

    Removed idle and activeFilter from web2
    Signed-off-by: pierantoniomerlino <pierantonio.merlino@eurotech.com>

--- kura/org.eclipse.kura.web2/src/main/java/org/eclipse/kura/web/client/ui/network/TabModemUi.java
+++ kura/org.eclipse.kura.web2/src/main/java/org/eclipse/kura/web/client/ui/network/TabModemUi.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011, 2024 Eurotech and/or its affiliates and others
+ * Copyright (c) 2011, 2025 Eurotech and/or its affiliates and others
  * 
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
@@ -12,25 +12,19 @@
  *******************************************************************************/
 package org.eclipse.kura.web.client.ui.network;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.eclipse.kura.web.client.messages.Messages;
-import org.eclipse.kura.web.client.util.FailureHandler;
 import org.eclipse.kura.web.client.util.HelpButton;
 import org.eclipse.kura.web.client.util.MessageUtils;
 import org.eclipse.kura.web.shared.model.GwtModemAuthType;
 import org.eclipse.kura.web.shared.model.GwtModemInterfaceConfig;
 import org.eclipse.kura.web.shared.model.GwtNetInterfaceConfig;
 import org.eclipse.kura.web.shared.model.GwtSession;
-import org.eclipse.kura.web.shared.model.GwtXSRFToken;
 import org.eclipse.kura.web.shared.service.GwtNetworkService;
 import org.eclipse.kura.web.shared.service.GwtNetworkServiceAsync;
 import org.eclipse.kura.web.shared.service.GwtSecurityTokenService;
 import org.eclipse.kura.web.shared.service.GwtSecurityTokenServiceAsync;
-import org.gwtbootstrap3.client.ui.Alert;
-import org.gwtbootstrap3.client.ui.Button;
 import org.gwtbootstrap3.client.ui.FieldSet;
 import org.gwtbootstrap3.client.ui.FormControlStatic;
 import org.gwtbootstrap3.client.ui.FormGroup;
@@ -39,24 +33,17 @@ import org.gwtbootstrap3.client.ui.HelpBlock;
 import org.gwtbootstrap3.client.ui.InlineRadio;
 import org.gwtbootstrap3.client.ui.Input;
 import org.gwtbootstrap3.client.ui.ListBox;
-import org.gwtbootstrap3.client.ui.Modal;
 import org.gwtbootstrap3.client.ui.PanelHeader;
 import org.gwtbootstrap3.client.ui.TextBox;
 import org.gwtbootstrap3.client.ui.constants.ValidationState;
-import org.gwtbootstrap3.client.ui.gwt.CellTable;
 import org.gwtbootstrap3.client.ui.html.Span;
-import org.gwtbootstrap3.client.ui.html.Text;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
-import com.google.gwt.user.cellview.client.TextColumn;
-import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.ScrollPanel;
 import com.google.gwt.user.client.ui.Widget;
-import com.google.gwt.view.client.ListDataProvider;
-import com.google.gwt.view.client.SingleSelectionModel;
 
 public class TabModemUi extends Composite implements NetworkTab {
 
@@ -88,8 +75,6 @@ public class TabModemUi extends Composite implements NetworkTab {
     FormGroup groupHoldoff;
     @UiField
     FormGroup groupMaxfail;
-    @UiField
-    FormGroup groupIdle;
     @UiField
     FormGroup groupInterval;
     @UiField
@@ -126,10 +111,6 @@ public class TabModemUi extends Composite implements NetworkTab {
     @UiField
     FormLabel labelMaxfail;
     @UiField
-    FormLabel labelIdle;
-    @UiField
-    FormLabel labelActive;
-    @UiField
     FormLabel labelInterval;
     @UiField
     FormLabel labelFailure;
@@ -140,8 +121,6 @@ public class TabModemUi extends Composite implements NetworkTab {
     HelpBlock helpHoldoff;
     @UiField
     HelpBlock helpMaxfail;
-    @UiField
-    HelpBlock helpIdle;
     @UiField
     HelpBlock helpInterval;
     @UiField
@@ -168,10 +147,6 @@ public class TabModemUi extends Composite implements NetworkTab {
     TextBox holdoff;
     @UiField
     TextBox maxfail;
-    @UiField
-    TextBox idle;
-    @UiField
-    TextBox active;
     @UiField
     TextBox interval;
     @UiField
@@ -222,10 +197,6 @@ public class TabModemUi extends Composite implements NetworkTab {
     @UiField
     HelpButton maxfailHelp;
     @UiField
-    HelpButton idleHelp;
-    @UiField
-    HelpButton activeHelp;
-    @UiField
     HelpButton intervalHelp;
     @UiField
     HelpButton failureHelp;
@@ -264,9 +235,6 @@ public class TabModemUi extends Composite implements NetworkTab {
         if (this.holdoff.getText() == null || "".equals(this.holdoff.getText().trim())) {
             this.groupHoldoff.setValidationState(ValidationState.ERROR);
         }
-        if (this.idle.getText() == null || "".equals(this.idle.getText().trim())) {
-            this.groupIdle.setValidationState(ValidationState.ERROR);
-        }
         if (this.interval.getText() == null || "".equals(this.interval.getText().trim())) {
             this.groupInterval.setValidationState(ValidationState.ERROR);
         }
@@ -278,7 +246,6 @@ public class TabModemUi extends Composite implements NetworkTab {
                 || this.groupApn.getValidationState().equals(ValidationState.ERROR)
                 || this.groupMaxfail.getValidationState().equals(ValidationState.ERROR)
                 || this.groupHoldoff.getValidationState().equals(ValidationState.ERROR)
-                || this.groupIdle.getValidationState().equals(ValidationState.ERROR)
                 || this.groupInterval.getValidationState().equals(ValidationState.ERROR)
                 || this.groupFailure.getValidationState().equals(ValidationState.ERROR)) {
             return false;
@@ -341,8 +308,6 @@ public class TabModemUi extends Composite implements NetworkTab {
             updatedModemNetIf.setPersist(this.radio1.getValue());
             updatedModemNetIf.setMaxFail(Integer.parseInt(this.maxfail.getText().trim()));
             updatedModemNetIf.setHoldoff(Integer.parseInt(this.holdoff.getText().trim()));
-            updatedModemNetIf.setIdle(Integer.parseInt(this.idle.getText().trim()));
-            updatedModemNetIf.setActiveFilter(this.active.getText() != "" ? this.active.getText().trim() : "");
             updatedModemNetIf.setLcpEchoInterval(Integer.parseInt(this.interval.getText().trim()));
             updatedModemNetIf.setLcpEchoFailure(Integer.parseInt(this.failure.getText().trim()));
             // ---
@@ -359,8 +324,6 @@ public class TabModemUi extends Composite implements NetworkTab {
             updatedModemNetIf.setPersist(this.selectedNetIfConfig.isPersist());
             updatedModemNetIf.setMaxFail(this.selectedNetIfConfig.getMaxFail());
             updatedModemNetIf.setHoldoff(this.selectedNetIfConfig.getHoldoff());
-            updatedModemNetIf.setIdle(this.selectedNetIfConfig.getIdle());
-            updatedModemNetIf.setActiveFilter(this.selectedNetIfConfig.getActiveFilter());
             updatedModemNetIf.setLcpEchoInterval(this.selectedNetIfConfig.getLcpEchoInterval());
             updatedModemNetIf.setLcpEchoFailure(this.selectedNetIfConfig.getLcpEchoFailure());
         }
@@ -380,8 +343,6 @@ public class TabModemUi extends Composite implements NetworkTab {
         this.persistHelp.setHelpText(MSGS.netModemToolTipPersist());
         this.maxfailHelp.setHelpText(MSGS.netModemToolTipMaxFail());
         this.holdoffHelp.setHelpText(MSGS.netModemToolTipHoldoff());
-        this.activeHelp.setHelpText(MSGS.netModemToolTipActiveFilter());
-        this.idleHelp.setHelpText(MSGS.netModemToolTipIdle());
         this.intervalHelp.setHelpText(MSGS.netModemToolTipLcpEchoInterval());
         this.failureHelp.setHelpText(MSGS.netModemToolTipLcpEchoFailure());
     }
@@ -567,39 +528,6 @@ public class TabModemUi extends Composite implements NetworkTab {
             }
         });
 
-        // DISCONNET IF IDLE
-        this.labelIdle.setText(MSGS.netModemIdle() + "*");
-        this.idle.addMouseOverHandler(event -> {
-            if (TabModemUi.this.idle.isEnabled()) {
-                TabModemUi.this.helpText.clear();
-                TabModemUi.this.helpText.add(new Span(MSGS.netModemToolTipIdle()));
-            }
-        });
-        this.idle.addMouseOutHandler(event -> resetHelp());
-        this.idle.addValueChangeHandler(event -> {
-            setDirty(true);
-            if (TabModemUi.this.idle.getText().trim() != null
-                    && (!TabModemUi.this.idle.getText().trim().matches(REGEX_NUM)
-                            || Integer.parseInt(TabModemUi.this.idle.getText()) < 0)) {
-                TabModemUi.this.helpIdle.setText(MSGS.netModemInvalidIdle());
-                TabModemUi.this.groupIdle.setValidationState(ValidationState.ERROR);
-            } else {
-                TabModemUi.this.helpIdle.setText("");
-                TabModemUi.this.groupIdle.setValidationState(ValidationState.NONE);
-            }
-        });
-
-        // ACTIVE FILTER
-        this.labelActive.setText(MSGS.netModemActiveFilter());
-        this.active.addMouseOverHandler(event -> {
-            if (TabModemUi.this.active.isEnabled()) {
-                TabModemUi.this.helpText.clear();
-                TabModemUi.this.helpText.add(new Span(MSGS.netModemToolTipActiveFilter()));
-            }
-        });
-        this.active.addMouseOutHandler(event -> resetHelp());
-        this.active.addValueChangeHandler(event -> setDirty(true));
-
         // LCP ECHO INTERVAL
         this.labelInterval.setText(MSGS.netModemLcpEchoInterval() + "*");
         this.interval.addMouseOverHandler(event -> {
@@ -654,7 +582,6 @@ public class TabModemUi extends Composite implements NetworkTab {
     private void resetValidations() {
         this.groupApn.setValidationState(ValidationState.NONE);
         this.groupFailure.setValidationState(ValidationState.NONE);
-        this.groupIdle.setValidationState(ValidationState.NONE);
         this.groupInterval.setValidationState(ValidationState.NONE);
         this.groupMaxfail.setValidationState(ValidationState.NONE);
         this.groupHoldoff.setValidationState(ValidationState.NONE);
@@ -664,7 +591,6 @@ public class TabModemUi extends Composite implements NetworkTab {
         this.helpReset.setText("");
         this.helpMaxfail.setText("");
         this.helpHoldoff.setText("");
-        this.helpIdle.setText("");
         this.helpInterval.setText("");
         this.helpFailure.setText("");
         this.helpNumber.setText("");
@@ -716,8 +642,6 @@ public class TabModemUi extends Composite implements NetworkTab {
 
             this.maxfail.setText(String.valueOf(this.selectedNetIfConfig.getMaxFail()));
             this.holdoff.setText(String.valueOf(this.selectedNetIfConfig.getHoldoff()));
-            this.idle.setText(String.valueOf(this.selectedNetIfConfig.getIdle()));
-            this.active.setText(this.selectedNetIfConfig.getActiveFilter());
             this.interval.setText(String.valueOf(this.selectedNetIfConfig.getLcpEchoInterval()));
             this.failure.setText(String.valueOf(this.selectedNetIfConfig.getLcpEchoFailure()));
         }
@@ -737,8 +661,6 @@ public class TabModemUi extends Composite implements NetworkTab {
         this.radio2.setEnabled(true);
         this.maxfail.setEnabled(true);
         this.holdoff.setEnabled(true);
-        this.idle.setEnabled(true);
-        this.active.setEnabled(true);
         this.interval.setEnabled(true);
         this.failure.setEnabled(true);
 
@@ -779,8 +701,6 @@ public class TabModemUi extends Composite implements NetworkTab {
         this.radio2.setValue(false);
         this.maxfail.setText(null);
         this.holdoff.setText(null);
-        this.idle.setText(null);
-        this.active.setText(null);
         this.interval.setText(null);
         this.failure.setText(null);
         update();

42c30eacdf4b05394ce7cff3b9c84b2a9f797f82
pierantoniomerlino <pierantonio.merlino@eurotech.com>
2025-03-13 14:20:09 +0100

    Fixed tests again
    Signed-off-by: pierantoniomerlino <pierantonio.merlino@eurotech.com>

--- kura/test/org.eclipse.kura.net.configuration.test/src/test/java/org/eclipse/kura/net/configuration/NetworkConfigurationServiceCommonTest.java
+++ kura/test/org.eclipse.kura.net.configuration.test/src/test/java/org/eclipse/kura/net/configuration/NetworkConfigurationServiceCommonTest.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
- * Copyright (c) 2023, 2024 Eurotech and/or its affiliates and others
- *
+ * Copyright (c) 2023, 2025 Eurotech and/or its affiliates and others
+ * 
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
  * which is available at https://www.eclipse.org/legal/epl-2.0/
- *
+ *     
  * SPDX-License-Identifier: EPL-2.0
- *
+ * 
  * Contributors:
  *  Eurotech
  ******************************************************************************/
@@ -324,7 +324,7 @@ public class NetworkConfigurationServiceCommonTest {
 
     private void thenComponentDefinitionHasCorrectNumberOfResources() {
         assertNotNull(this.ads);
-        assertEquals(191, this.ads.size());
+        assertEquals(189, this.ads.size());
     }
 
     private void thenReturnedPropertyEquals(final String key, final Object value) {
@@ -628,7 +628,7 @@ public class NetworkConfigurationServiceCommonTest {
     }
 
     private void thenComponentDefinitionHasModemProperties() {
-        assertEquals(39, this.ads.stream().filter(ad -> ad.getName().contains("1-4")).count());
+        assertEquals(37, this.ads.stream().filter(ad -> ad.getName().contains("1-4")).count());
     }
 
     private void thenComponentDefinitionHasVlanProperties() {
@@ -937,7 +937,7 @@ public class NetworkConfigurationServiceCommonTest {
                 adsConfigured++;
             }
         }
-        assertEquals(12, adsConfigured);
+        assertEquals(10, adsConfigured);
     }
 
     private void thenComponentDefinitionHasVlanProperties(String interfaceName) {

d237fda53a4dcb3fcee9059dd362cac2bbccbb51
Salvatore Coppola <salvatore.coppola@eurotech.com>
2025-06-18 15:51:36 +0200

    removed web2 and com.gwt.user

--- kura/org.eclipse.kura.web2/src/main/java/org/eclipse/kura/web/shared/validator/Validator.java
+++
@@ -1,8 +0,0 @@
-package org.eclipse.kura.web.shared.validator;
-
-import java.util.function.Consumer;
-
-public interface Validator<T> {
-
-    void validate(T value, Consumer<String> errorMessageConsumer);
-}

abb8efb8c4e18d916ab75140f55034fa9ad4da7a
MMaiero <matteo.maiero@eurotech.com>
2025-08-25 17:48:05 +0200

    fix(net.configuration): add missing 802.1X password field definition
    - Add CONFIG_802_1X_PASSWORD to NetworkConfigurationPropertyNames enum
    - Add password field definition to network configuration metadata
    - Ensures 802.1X authentication password field is properly exposed in configuration

--- kura/org.eclipse.kura.net.configuration/src/main/java/org/eclipse/kura/net/configuration/NetworkConfigurationServiceCommon.java
+++ kura/org.eclipse.kura.net.configuration/src/main/java/org/eclipse/kura/net/configuration/NetworkConfigurationServiceCommon.java
@@ -301,6 +301,9 @@ public class NetworkConfigurationServiceCommon {
                 NetworkConfigurationPropertyNames.CONFIG_WIFI_INFRA_IGNORE_SSID, Tscalar.BOOLEAN)
                         .withDefault(String.valueOf(NetworkConfigurationConstants.DEFAULT_WIFI_IGNORE_SSID_VALUE))
                         .build());
+
+        tocd.addAD(builder(String.format(PREFIX + "%s.config.802-1x.password", ifaceName),
+                NetworkConfigurationPropertyNames.CONFIG_802_1X_PASSWORD, Tscalar.PASSWORD).build());
     }
 
     private static void getWifiCommonDefinition(Tocd tocd, String ifaceName) {

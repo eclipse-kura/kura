1a8890b9e076241cf959a53de879589769e7de91
Salvatore Coppola <salvatore.coppola@eurotech.com>
2025-08-27 13:05:26 +0200

    checkstyle fix

--- kura/org.eclipse.kura.api/src/main/java/org/eclipse/kura/identity/PasswordStrengthVerificationService.java
+++ kura/org.eclipse.kura.api/src/main/java/org/eclipse/kura/identity/PasswordStrengthVerificationService.java
@@ -38,7 +38,9 @@ public interface PasswordStrengthVerificationService {
     public void checkPasswordStrength(final char[] password) throws KuraException;
 
     /**
-     * Similar to {@link #checkPasswordStrength(char[])} checks whether the provided password satisfies the password strength requirements currently configured on the system and in addition verifies that the password
+     * Similar to {@link #checkPasswordStrength(char[])} checks whether 
+     * the provided password satisfies the password strength requirements currently configured 
+     * on the system and in addition verifies that the password
      * does not match the identityName
      * 
      * @param identityName

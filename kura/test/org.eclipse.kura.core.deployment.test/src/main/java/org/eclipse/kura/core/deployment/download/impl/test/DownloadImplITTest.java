d154b9689f50e29f112efd023f4cba5fe1e79d85
Salvatore Coppola <salvatore.coppola@eurotech.com>
2025-09-03 15:12:58 +0200

    Added integration test

--- kura/test/org.eclipse.kura.core.deployment.test/src/main/java/org/eclipse/kura/core/deployment/download/impl/test/DownloadImplITTest.java
+++ kura/test/org.eclipse.kura.core.deployment.test/src/main/java/org/eclipse/kura/core/deployment/download/impl/test/DownloadImplITTest.java
@@ -12,11 +12,16 @@
  *******************************************************************************/
 package org.eclipse.kura.core.deployment.download.impl.test;
 
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
+import static org.mockito.Mockito.mock;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.security.KeyPair;
 import java.security.KeyStore;
 import java.security.cert.Certificate;
@@ -37,6 +42,7 @@ import org.bouncycastle.asn1.x500.X500Name;
 import org.eclipse.kura.KuraException;
 import org.eclipse.kura.configuration.ConfigurableComponent;
 import org.eclipse.kura.configuration.ConfigurationService;
+import org.eclipse.kura.core.deployment.CloudDeploymentHandlerV2;
 import org.eclipse.kura.core.deployment.download.DeploymentPackageDownloadOptions;
 import org.eclipse.kura.core.deployment.download.impl.DownloadImpl;
 import org.eclipse.kura.core.testutil.pki.TestCA;
@@ -45,6 +51,7 @@ import org.eclipse.kura.core.testutil.pki.TestCA.TestCAException;
 import org.eclipse.kura.core.testutil.service.ServiceUtil;
 import org.eclipse.kura.crypto.CryptoService;
 import org.eclipse.kura.ssl.SslManagerService;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.osgi.framework.BundleContext;
@@ -55,34 +62,46 @@ import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 import org.osgi.util.tracker.ServiceTracker;
 import org.osgi.util.tracker.ServiceTrackerCustomizer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 @Component(immediate = true)
 public class DownloadImplITTest {
 
-    private static final Logger logger = LoggerFactory.getLogger(DownloadImplITTest.class);
+    private static final String TEST_FILENAME = "test";
+    private static final String DP_EXTENSION = "dp";
+    private static final String DP_VERSION = "0.0.0";
 
-    private static final CountDownLatch dependencies = new CountDownLatch(4);
+    private static final String DEFAULT_KEYSTORE_PASSWORD = "changeit";
 
-    private static final String TEST_KEYSTORE_PID = "testKeystore";
+    private static final CountDownLatch dependencies = new CountDownLatch(5);
+
+    private static final String SERVER_KEYSTORE_PID = "serverKeystore";
+    private static final String CLIENT_KEYSTORE_PID = "clientKeystore";
     private static final String HTTP_SERVICE_PID = "org.eclipse.kura.http.server.manager.HttpService";
+    private static final String REST_SERVICE_PID = "org.eclipse.kura.internal.rest.provider.RestService";
+    private static final String SSL_MANAGER_SERVICE_PID = "org.eclipse.kura.ssl.SslManagerService";
 
+    private static final int HTTP_PORT = 8080;
     private static final int HTTPS_PORT = 9993;
 
     private static SslManagerService sslManagerService;
     private static ConfigurationService configurationService;
+    @SuppressWarnings("unused")
     private static ConfigurableComponent httpService;
     private static CryptoService cryptoService;
+    @SuppressWarnings("unused")
+    private static ConfigurableComponent restService;
 
-    private static File serverKeystore;
-    private static File clientKeystore;
-    private static TestCA clientCA;
-    private static X509Certificate clientCertificate;
+    private static TestKeystore serverKeystore;
+
+    private static TestKeystore clientKeystore;
 
     private DownloadImpl downloadImpl;
 
     private DeploymentPackageDownloadOptions deploymentPackageDownloadOptions;
+
+    private Exception occurredException;
+
+    private String tempDir = System.getProperty("java.io.tmpdir");
 
     @Reference
     public void setSslManagerService(SslManagerService sslManagerService) {
@@ -108,73 +127,175 @@ public class DownloadImplITTest {
         dependencies.countDown();
     }
 
+    @Reference(target = "(kura.service.pid=" + REST_SERVICE_PID + ")")
+    public static void setRestService(ConfigurableComponent restService) {
+        DownloadImplITTest.restService = restService;
+        dependencies.countDown();
+    }
+
     @BeforeClass
     public static void setupEnvironment() throws InterruptedException, TestCAException, IOException {
         awaitDependencies();
-        setUpCA();
+        setupCAAndClientKeystore();
+        setupSslManagerService();
         setupHttpService();
+        setupRestService();
+    }
+
+    @Before
+    public void cleanup() throws IOException {
+        Files.deleteIfExists(Path.of(this.tempDir, TEST_FILENAME));
     }
 
     @Test
-    public void ciao() {
-        assertNotNull(sslManagerService);
+    public void shouldDownloadFileFromHttpsUrlWithoutHostnameVerificationEnabled() {
+        givenDeploymentPackageDownloadOptions("https://localhost:9993/services/test/download", TEST_FILENAME,
+                DP_VERSION);
+        givenDownloadImpl();
+        givenHostnameVerificationInSslManagerService(false);
+
+        whenDownload();
+
+        thenNoExceptionOccured();
+        thenDownloadedFileIs(TEST_FILENAME + "_" + DP_VERSION + "." + DP_EXTENSION);
     }
 
     @Test
-    public void ciao2() {
-        assertNotNull(configurationService);
+    public void shouldNotDownloadFileFromHttpsUrlWithHostnameVerificationEnabled() {
+        givenDeploymentPackageDownloadOptions("https://localhost:9993/services/test/download", TEST_FILENAME,
+                DP_VERSION);
+        givenDownloadImpl();
+        givenHostnameVerificationInSslManagerService(true);
+
+        whenDownload();
+
+        thenNoExceptionOccured();
+        thenFileIsEmpty(TEST_FILENAME + "_" + DP_VERSION + "." + DP_EXTENSION);
+    }
+
+    private void givenDeploymentPackageDownloadOptions(String deployUri, String dpName, String dpVersion) {
+        this.deploymentPackageDownloadOptions = new DeploymentPackageDownloadOptions(deployUri, dpName, dpVersion);
+        this.deploymentPackageDownloadOptions.setJobId(123l);
+        this.deploymentPackageDownloadOptions.setDownloadProtocol("HTTP");
+        this.deploymentPackageDownloadOptions.setDownloadDirectory(tempDir);
     }
 
     private void givenDownloadImpl() {
         this.downloadImpl = new DownloadImpl(this.deploymentPackageDownloadOptions,
-                null /* callback not needed, we are only testing download" */);
+                mock(CloudDeploymentHandlerV2.class));
+        this.downloadImpl.setSslManager(sslManagerService);
     }
 
-    private static void setUpCA() throws TestCAException, IOException {
+    private void givenHostnameVerificationInSslManagerService(Boolean hostnameVerification) {
+        Map<String, Object> props = new HashMap<>();
+        props.put("ssl.hostname.verification", hostnameVerification);
+        try {
+            updateComponentConfiguration(configurationService, SSL_MANAGER_SERVICE_PID, props).get(30,
+                    TimeUnit.SECONDS);
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
+    }
+
+    private void whenDownload() {
+        try {
+            this.downloadImpl.downloadDeploymentPackageInternal();
+        } catch (KuraException e) {
+            occurredException = e;
+        }
+    }
+
+    private void thenNoExceptionOccured() {
+        assertNull(this.occurredException);
+    }
+
+    private void thenFileIsEmpty(String filename) {
+        Path tempFile = Path.of(this.tempDir, filename);
+        try {
+            assertTrue(Files.readAllBytes(tempFile).length == 0);
+        } catch (IOException e) {
+            fail(e.getMessage());
+        }
+    }
+
+    private void thenDownloadedFileIs(String filename) {
+        byte[] expectedContent;
+        try {
+            expectedContent = getClass().getClassLoader().getResourceAsStream(TEST_FILENAME).readAllBytes();
+            byte[] downloadedContent = Files.readAllBytes(Path.of(this.tempDir, filename));
+            assertArrayEquals(expectedContent, downloadedContent);
+        } catch (IOException e) {
+            fail(e.getMessage());
+        }
+    }
+
+    private static void setupCAAndClientKeystore() throws TestCAException, IOException {
 
         final TestCA serverCA = new TestCA(
                 CertificateCreationOptions.builder(new X500Name("cn=Server CA, dn=foo.org")).build());
 
         final KeyPair serverKeyPair = TestCA.generateKeyPair();
 
-        final X509Certificate serverCertificate = serverCA.createAndSignCertificate(
+        X509Certificate serverCertificate = serverCA.createAndSignCertificate(
                 CertificateCreationOptions.builder(new X500Name("cn=Server Cert, dn=foo.org")).build(), serverKeyPair);
 
-        clientCA = new TestCA(CertificateCreationOptions.builder(new X500Name("cn=Client CA, dn=bar.org")).build());
+        File serverKeystoreFile = TestCA.writeKeystore(new KeyStore.PrivateKeyEntry(serverKeyPair.getPrivate(),
+                new Certificate[] { serverCertificate, serverCA.getCertificate() }));
 
-        serverKeystore = TestCA.writeKeystore(
-                new KeyStore.PrivateKeyEntry(serverKeyPair.getPrivate(),
-                        new Certificate[] { serverCertificate, serverCA.getCertificate() }),
-                new KeyStore.TrustedCertificateEntry(clientCA.getCertificate()));
+        File clientKeystoreFile = TestCA.writeKeystore(new KeyStore.TrustedCertificateEntry(serverCertificate));
 
-        final KeyPair clientKeyPair = TestCA.generateKeyPair();
-
-        clientCertificate = clientCA.createAndSignCertificate(
-                CertificateCreationOptions.builder(new X500Name("cn=admin, dn=bar.org")).build(), clientKeyPair);
-
-        clientKeystore = TestCA.writeKeystore(new KeyStore.PrivateKeyEntry(clientKeyPair.getPrivate(),
-                new Certificate[] { clientCertificate, clientCA.getCertificate() }));
-    }
-
-    private static void setupHttpService() {
         try {
-            try (final TestKeystore testKeystore = new TestKeystore(DownloadImplITTest.configurationService,
-                    DownloadImplITTest.cryptoService, TEST_KEYSTORE_PID,
-                    HttpsKeystoreServiceOptions.defaultConfiguration())) {
-
-                Map<String, Object> props = new HashMap<>();
-                props.put("https.ports", new Integer[] { HTTPS_PORT });
-                props.put("KeystoreService.target", testKeystore.getTargetFilter());
-                updateComponentConfiguration(DownloadImplITTest.configurationService, HTTP_SERVICE_PID, props) //
-                        .get(30, TimeUnit.SECONDS);
-            }
-
+            serverKeystore = new TestKeystore(DownloadImplITTest.configurationService, DownloadImplITTest.cryptoService,
+                    SERVER_KEYSTORE_PID, serverKeystoreFile.getAbsolutePath(), DEFAULT_KEYSTORE_PASSWORD);
+            clientKeystore = new TestKeystore(DownloadImplITTest.configurationService, DownloadImplITTest.cryptoService,
+                    CLIENT_KEYSTORE_PID, clientKeystoreFile.getAbsolutePath(), DEFAULT_KEYSTORE_PASSWORD);
         } catch (Exception e) {
             fail(e.getMessage());
         }
+
     }
 
-    private static void awaitDependencies() throws InterruptedException, TestCAException, IOException {
+    private static void setupSslManagerService() {
+        Map<String, Object> props = new HashMap<>();
+        props.put("KeystoreService.target", clientKeystore.getTargetFilter());
+        props.put("TruststoreKeystoreService.target", clientKeystore.getTargetFilter());
+        try {
+            updateComponentConfiguration(configurationService, SSL_MANAGER_SERVICE_PID, props).get(30,
+                    TimeUnit.SECONDS);
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
+
+    }
+
+    private static void setupHttpService() {
+
+        Map<String, Object> props = new HashMap<>();
+        props.put("https.ports", new Integer[] { HTTPS_PORT });
+        props.put("KeystoreService.target", serverKeystore.getTargetFilter());
+
+        try {
+            updateComponentConfiguration(DownloadImplITTest.configurationService, HTTP_SERVICE_PID, props) //
+                    .get(30, TimeUnit.SECONDS);
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
+
+    }
+
+    private static void setupRestService() {
+        Map<String, Object> props = new HashMap<>();
+        props.put("allowed.ports", new Integer[] { HTTP_PORT, HTTPS_PORT });
+        try {
+            updateComponentConfiguration(DownloadImplITTest.configurationService, REST_SERVICE_PID, props) //
+                    .get(30, TimeUnit.SECONDS);
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
+
+    }
+
+    private static void awaitDependencies() throws InterruptedException {
         if (!dependencies.await(30, TimeUnit.SECONDS)) {
             throw new IllegalStateException("dependencies not resolved in 30 seconds");
         }
@@ -182,21 +303,24 @@ public class DownloadImplITTest {
 
     private static class TestKeystore implements AutoCloseable {
 
+        private static final String FILESYSTEM_KEYSTORE_SERVICE_PID = "org.eclipse.kura.core.keystore.FilesystemKeystoreServiceImpl";
+
         private final ConfigurationService configSvc;
         private final String pid;
 
-        public TestKeystore(final ConfigurationService configSvc, final CryptoService cryptoSvc, final String pid,
-                final HttpsKeystoreServiceOptions options)
+        public TestKeystore(final ConfigurationService configurationService, final CryptoService cryptoService,
+                final String pid, final String keystorePath, String keystorePassword)
                 throws InterruptedException, ExecutionException, TimeoutException, KuraException {
-            this.configSvc = configSvc;
+            this.configSvc = configurationService;
             this.pid = pid;
 
-            ServiceUtil
-                    .createFactoryConfiguration(configSvc, ConfigurableComponent.class, pid,
-                            "org.eclipse.kura.core.keystore.FilesystemKeystoreServiceImpl",
-                            options.withKeystorePath(serverKeystore.getAbsolutePath())
-                                    .withKeystorePassword("changeit", cryptoSvc).toProperties())
-                    .get(30, TimeUnit.SECONDS);
+            Map<String, Object> props = new HashMap<>();
+
+            props.put("keystore.path", keystorePath);
+            props.put("keystore.password", new String(cryptoService.encryptAes(keystorePassword.toCharArray())));
+
+            ServiceUtil.createFactoryConfiguration(configurationService, ConfigurableComponent.class, pid,
+                    FILESYSTEM_KEYSTORE_SERVICE_PID, props).get(30, TimeUnit.SECONDS);
         }
 
         String getTargetFilter() {
@@ -208,47 +332,6 @@ public class DownloadImplITTest {
             ServiceUtil.deleteFactoryConfiguration(configSvc, pid).get(30, TimeUnit.SECONDS);
         }
 
-    }
-
-    private static class HttpsKeystoreServiceOptions {
-
-        private String keystorePath = "";
-        private String keystorePassword = "";
-        private boolean crlManagerEnabled = false;
-        private Optional<String[]> crlUrls = Optional.empty();
-
-        private HttpsKeystoreServiceOptions() {
-        }
-
-        static HttpsKeystoreServiceOptions defaultConfiguration() {
-            return new HttpsKeystoreServiceOptions();
-        }
-
-        HttpsKeystoreServiceOptions withKeystorePath(final String keystorePath) {
-            this.keystorePath = keystorePath;
-            return this;
-        }
-
-        HttpsKeystoreServiceOptions withKeystorePassword(final String keystorePassword,
-                final CryptoService cryptoService) throws KuraException {
-            this.keystorePassword = new String(cryptoService.encryptAes(keystorePassword.toCharArray()));
-            return this;
-        }
-
-        Map<String, Object> toProperties() {
-            final Map<String, Object> result = new HashMap<>();
-
-            result.put("keystore.path", this.keystorePath);
-            result.put("keystore.password", this.keystorePassword);
-            result.put("crl.check.interval", 1L);
-            result.put("crl.check.interval.time.unit", TimeUnit.SECONDS.name());
-            result.put("crl.update.interval", 1L);
-            result.put("crl.update.interval.time.unit", TimeUnit.SECONDS.name());
-            result.put("crl.management.enabled", crlManagerEnabled);
-            this.crlUrls.ifPresent(u -> result.put("crl.urls", u));
-
-            return result;
-        }
     }
 
     static CompletableFuture<Void> updateComponentConfiguration(final ConfigurationService configurationService,

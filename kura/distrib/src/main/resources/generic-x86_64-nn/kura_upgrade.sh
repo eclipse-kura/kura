9d45197c42963c5c209e68df8ca8d912e2102edc
Mattia Dal Ben <mattdibi@users.noreply.github.com>
2025-09-18 08:50:18 +0200

    fix: manage log rotation via log4j [backport #5974] (#5975)
    * refactor: remove logrotate and cron dependencies
    
    * fix: remove logrotate cronjob installation from installer scripts
    
    * fix: disable debug logs
    
    * revert: max number of log files not correctly enforced in high-volume logging circumstances [backport #5961] (#5962)
    
    This reverts commit 11e7ebc54a6b7b0f4148bfc60ec41f5280bfe1f7.
    
    * fix: log4j configuration
    
    * style: remove whitespaces
    
    * style: remove whitespaces again
    
    * style: remove whitespaces again again

--- kura/distrib/src/main/resources/generic-x86_64-nn/kura_upgrade.sh
+++ kura/distrib/src/main/resources/generic-x86_64-nn/kura_upgrade.sh
@@ -16,7 +16,3 @@ INSTALL_DIR=/opt/eclipse
 
 #create known kura install location
 ln -sf ${INSTALL_DIR}/kura_* ${INSTALL_DIR}/kura
-
-#set up logrotate - no need to restart as it is a cronjob
-cp ${INSTALL_DIR}/kura/install/logrotate.conf /etc/logrotate.conf
-cp ${INSTALL_DIR}/kura/install/kura.logrotate /etc/logrotate.d/kura

0dbce7a8c2de4bc1f2f599cd05e06de9d08a2acb
pierantoniomerlino <pierantonio.merlino@eurotech.com>
2025-09-25 17:39:20 +0200

    Added missing files
    Signed-off-by: pierantoniomerlino <pierantonio.merlino@eurotech.com>

---
+++ kura/org.eclipse.kura.linux.gpio.libgpiod/src/main/java/org/eclipse/kura/linux/gpio/libgpiod1/LibGpiodV1NativeWrapper.java
@@ -0,0 +1,25 @@
+/*******************************************************************************
+ * Copyright (c) 2025 Eurotech and/or its affiliates and others
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *  Eurotech
+ *******************************************************************************/
+package org.eclipse.kura.linux.gpio.libgpiod1;
+
+public class LibGpiodV1NativeWrapper {
+
+    private LibGpiodV1NativeWrapper() {
+        // Empty private constructor for static factory class
+    }
+
+    public static LibGpiodV1Native getInstance() {
+        return LibGpiodV1Native.INSTANCE;
+    }
+
+}

735ed28b8a9b017e763babb057fadc6989b85839
Salvatore Coppola <salvatore.coppola@eurotech.com>
2026-09-15 16:39:31 +0200

    refactor(core.configuration): unmarshal the snapshot in a helper to avoid a nested try

--- kura/org.eclipse.kura.core.configuration/src/main/java/org/eclipse/kura/core/configuration/ConfigurationServiceImpl.java
+++ kura/org.eclipse.kura.core.configuration/src/main/java/org/eclipse/kura/core/configuration/ConfigurationServiceImpl.java
@@ -1392,18 +1392,21 @@ public class ConfigurationServiceImpl implements ConfigurationService, OCDServic
         }
 
         try (InputStream fileStream = new FileInputStream(fSnapshot)) {
-            InputStream decryptedStream = this.cryptoService.aesDecryptingStream(fileStream);
-            try {
-                return unmarshal(decryptedStream, XmlComponentConfigurations.class);
-            } catch (KuraException e) {
-                logger.warn("Error parsing xml", e);
-                return null;
-            }
+            return unmarshalSnapshot(this.cryptoService.aesDecryptingStream(fileStream));
         } catch (FileNotFoundException e) {
             logger.error("Error loading file from disk", e);
             return null;
         } catch (IOException e) {
             throw new KuraIOException(e);
+        }
+    }
+
+    private XmlComponentConfigurations unmarshalSnapshot(final InputStream decryptedStream) {
+        try {
+            return unmarshal(decryptedStream, XmlComponentConfigurations.class);
+        } catch (KuraException e) {
+            logger.warn("Error parsing xml", e);
+            return null;
         }
     }
 

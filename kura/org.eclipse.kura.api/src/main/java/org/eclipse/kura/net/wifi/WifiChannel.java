77e8e2ddf07f8d4fec043844ed59698b109338c3
Mattia Dal Ben <matthewdibi@gmail.com>
2026-09-23 17:52:55 +0200

    style: update copyright headers

--- kura/org.eclipse.kura.api/src/main/java/org/eclipse/kura/net/wifi/WifiChannel.java
+++ kura/org.eclipse.kura.api/src/main/java/org/eclipse/kura/net/wifi/WifiChannel.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2021 Sterwen-Technology and others
+ * Copyright (c) 2021, 2026 Sterwen Technology and/or its affiliates and others
  *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0

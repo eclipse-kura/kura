bc34bad10b1f3c6167ea64d3a0aee97f3f65d452
Mattia Dal Ben <matthewdibi@gmail.com>
2026-09-23 17:52:55 +0200

    style: update copyright headers

--- kura/org.eclipse.kura.api/src/main/java/org/eclipse/kura/net/dhcp/DhcpLease.java
+++ kura/org.eclipse.kura.api/src/main/java/org/eclipse/kura/net/dhcp/DhcpLease.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2022 Sterwen Technology and others
+ * Copyright (c) 2022, 2026 Sterwen Technology and/or its affiliates and others
  *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
@@ -9,6 +9,7 @@
  *
  * Contributors:
  *  Sterwen-Technology
+ *  Eurotech S.p.A.
  *******************************************************************************/
 package org.eclipse.kura.net.dhcp;
 
